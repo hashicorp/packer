1748ca31f66e8ba17f967f69fec51afafa05a095
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-03-23 00:41:25 -0700

    Use a GOPATH workspace format

--- src/packer/bin-packer/main.go
+++ src/packer/bin-packer/main.go
@@ -1,8 +1,12 @@
 // This is the main package for the `packer` application.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	fmt.Printf("%#v\n", os.Args)
 	fmt.Println("Hello, world.")
 }
