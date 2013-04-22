f49c0cb313669183bc65c36f605203e80643c694
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-04-21 18:45:44 -0700

    Pull out Command interface out into separate file

--- packer/environment.go
+++ packer/environment.go
@@ -9,24 +9,6 @@ import (
 	"strings"
 )
 
-// A command is a runnable sub-command of the `packer` application.
-// When `packer` is called with the proper subcommand, this will be
-// called.
-//
-// The mapping of command names to command interfaces is in the
-// Environment struct.
-//
-// Run should run the actual command with the given environmet and
-// command-line arguments. It should return the exit status when it is
-// finished.
-//
-// Synopsis should return a one-line, short synopsis of the command.
-// This should be less than 50 characters ideally.
-type Command interface {
-	Run(env *Environment, args []string) int
-	Synopsis() string
-}
-
 // The environment struct contains all the state necessary for a single
 // instance of Packer.
 //
