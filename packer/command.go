575489fa782dd6c8166baec2b44b705bf56ac488
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-05-02 15:20:12 -0700

    RPC commands and some basic tests for this [GH-1]

--- packer/command.go
+++ packer/command.go
@@ -22,6 +22,41 @@ type Command interface {
 
 // An RPCCommand is an implementation of the Command interface where the
 // command is actually executed over an RPC connection.
-type RPCCommand struct {
+type RPCClientCommand struct {
 	client *rpc.Client
+}
+
+// An RPCServerCommand wraps a Command and makes it exportable as part
+// of a Golang RPC server.
+type RPCServerCommand struct {
+	command Command
+}
+
+type RPCCommandRunArgs struct {
+	Env Environment
+	Args []string
+}
+
+type RPCCommandSynopsisArgs byte
+
+func (c *RPCClientCommand) Run(env Environment, args []string) (result int) {
+	// TODO: Environment
+	rpcArgs := &RPCCommandRunArgs{nil, args}
+	c.client.Call("Command.Run", rpcArgs, &result)
+	return
+}
+
+func (c *RPCClientCommand) Synopsis() (result string) {
+	c.client.Call("Command.Synopsis", RPCCommandSynopsisArgs(0), &result)
+	return
+}
+
+func (c *RPCServerCommand) Run(args *RPCCommandRunArgs, reply *int) error {
+	*reply = c.command.Run(args.Env, args.Args)
+	return nil
+}
+
+func (c *RPCServerCommand) Synopsis(args *RPCCommandSynopsisArgs, reply *string) error {
+	*reply = c.command.Synopsis()
+	return nil
 }
--- packer/environment_test.go
+++ packer/environment_test.go
@@ -8,23 +8,6 @@ import (
 	"testing"
 )
 
-type TestCommand struct {
-	runArgs []string
-	runCalled bool
-	runEnv Environment
-}
-
-func (tc *TestCommand) Run(env Environment, args []string) int {
-	tc.runCalled = true
-	tc.runArgs = args
-	tc.runEnv = env
-	return 0
-}
-
-func (tc *TestCommand) Synopsis() string {
-	return ""
-}
-
 func testEnvironment() Environment {
 	config := &EnvironmentConfig{}
 	config.Ui = &ReaderWriterUi{
