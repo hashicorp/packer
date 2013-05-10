368ccab8e87c05bf87f03a9120c9c2cbf0f6e3a5
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-05-10 16:59:28 -0700

    packer: Put Builder definition into its own file

---
+++ packer/builder.go
@@ -0,0 +1,16 @@
+package packer
+
+// Implementers of Builder are responsible for actually building images
+// on some platform given some configuration.
+//
+// Prepare is responsible for reading in some configuration, in the raw form
+// of map[string]interface{}, and storing that state for use later. Any setup
+// should be done in this method. Note that NO side effects should really take
+// place in prepare. It is meant as a state setup step only.
+//
+// Run is where the actual build should take place. It takes a Build and a Ui.
+type Builder interface {
+	Prepare(config interface{}) error
+	Run(build Build, ui Ui)
+}
+
