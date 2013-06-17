dd89716ed5add9eaa909d3e87cb771a75ff23523
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-06-17 11:56:26 -0700

    packer: An initial PostProcessor interface

---
+++ packer/post_processor.go
@@ -0,0 +1,17 @@
+package packer
+
+// A PostProcessor is responsible for taking an artifact of a build
+// and doing some sort of post-processing to turn this into another
+// artifact. An example of a post-processor would be something that takes
+// the result of a build, compresses it, and returns a new artifact containing
+// a single file of the prior artifact compressed.
+type PostProcessor interface {
+	// Configure is responsible for setting up configuration, storing
+	// the state for later, and returning and errors, such as validation
+	// errors.
+	Configure(interface{}) error
+
+	// PostProcess takes a previously created Artifact and produces another
+	// Artifact. If an error occurs, it should return that error.
+	PostProcess(Artifact) (Artifact, error)
+}
