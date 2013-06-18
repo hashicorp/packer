04a8bfb455dfd54b64fac60a6a5122f4274a7f10
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-06-18 10:31:52 -0700

    packer: Post-processors are configured

--- packer/post_processor_test.go
+++ packer/post_processor_test.go
@@ -1,11 +1,20 @@
 package packer
 
-type TestPostProcessor struct{}
+type TestPostProcessor struct {
+	configCalled bool
+	configVal    interface{}
+	ppCalled     bool
+	ppArtifact   Artifact
+}
 
-func (*TestPostProcessor) Configure(interface{}) error {
+func (pp *TestPostProcessor) Configure(v interface{}) error {
+	pp.configCalled = true
+	pp.configVal = v
 	return nil
 }
 
-func (*TestPostProcessor) PostProcess(Artifact) (Artifact, error) {
+func (pp *TestPostProcessor) PostProcess(a Artifact) (Artifact, error) {
+	pp.ppCalled = true
+	pp.ppArtifact = a
 	return nil, nil
 }
