338298b8aff1e70ceafd0f9e9131a928e559f268
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-06-18 16:29:29 -0700

    command/build, command/validate: Setup proper components to avoid nil

--- command/validate/commang.go
+++ command/validate/commang.go
@@ -56,9 +56,10 @@ func (c Command) Run(env packer.Environment, args []string) int {
 
 	// The component finder for our builds
 	components := &packer.ComponentFinder{
-		Builder:     env.Builder,
-		Hook:        env.Hook,
-		Provisioner: env.Provisioner,
+		Builder:       env.Builder,
+		Hook:          env.Hook,
+		PostProcessor: env.PostProcessor,
+		Provisioner:   env.Provisioner,
 	}
 
 	// Otherwise, get all the builds
