0a90d3e7917de7eb8cc33257f2a19bd9d04853ec
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-06-18 21:18:41 -0700

    post-processor/compress: Boilerplate for the compress PP

---
+++ post-processor/compress/post-processor.go
@@ -0,0 +1,38 @@
+// compress implements the packer.PostProcessor interface and adds a
+// post-processor for compressing output.
+package compress
+
+import (
+	"errors"
+	"github.com/mitchellh/packer/packer"
+	"github.com/mitchellh/mapstructure"
+)
+
+type Config struct {
+	Format string
+}
+
+type PostProcessor struct {
+	config Config
+}
+
+func (p *PostProcessor) Configure(raw interface{}) error {
+	if err := mapstructure.Decode(raw, &p.config); err != nil {
+		return err
+	}
+
+	if p.config.Format == "" {
+		p.config.Format = "tar.gz"
+	}
+
+	if p.config.Format != "tar.gz" {
+		return errors.New("only 'tar.gz' is a supported format right now")
+	}
+
+	return nil
+}
+
+func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (packer.Artifact, error) {
+	ui.Say("We made it to here.")
+	return nil, nil
+}
