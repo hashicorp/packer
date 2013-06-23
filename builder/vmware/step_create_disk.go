456aec339018810d6faa4e91fc80edb2174feeeb
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-06-23 15:07:19 -0700

    builder/vmware: configurable disk size (default 40 GB)

--- builder/vmware/step_create_disk.go
+++ builder/vmware/step_create_disk.go
@@ -19,16 +19,13 @@ import (
 type stepCreateDisk struct{}
 
 func (stepCreateDisk) Run(state map[string]interface{}) multistep.StepAction {
-	// TODO(mitchellh): Configurable disk size
-	// TODO(mitchellh): Capture error output in case things go wrong to report it
-
 	config := state["config"].(*config)
 	driver := state["driver"].(Driver)
 	ui := state["ui"].(packer.Ui)
 
 	ui.Say("Creating virtual machine disk")
 	output := filepath.Join(config.OutputDir, config.DiskName+".vmdk")
-	if err := driver.CreateDisk(output, "40000M"); err != nil {
+	if err := driver.CreateDisk(output, fmt.Sprintf("%dM", config.DiskSize)); err != nil {
 		err := fmt.Errorf("Error creating disk: %s", err)
 		state["error"] = err
 		ui.Error(err.Error())
