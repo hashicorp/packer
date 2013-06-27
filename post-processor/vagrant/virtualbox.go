b229e02d33d7f8c940b203352cf539d7ab6e4211
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-06-27 07:33:32 -0700

    post-processor/vagrant: VirtualBox post-processor

---
+++ post-processor/vagrant/virtualbox.go
@@ -0,0 +1,98 @@
+package vagrant
+
+import (
+	"fmt"
+	"github.com/mitchellh/mapstructure"
+	"github.com/mitchellh/packer/packer"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"text/template"
+)
+
+type VBoxBoxConfig struct {
+	OutputPath string `mapstructure:"output"`
+}
+
+type VBoxVagrantfileTemplate struct {
+	BaseMacAddress string
+}
+
+type VBoxBoxPostProcessor struct {
+	config VBoxBoxConfig
+}
+
+func (p *VBoxBoxPostProcessor) Configure(raw interface{}) error {
+	err := mapstructure.Decode(raw, &p.config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *VBoxBoxPostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (packer.Artifact, error) {
+	// TODO(mitchellh): Actually parse the base mac address
+	tplData := &VBoxVagrantfileTemplate{}
+
+	// Create a temporary directory for us to build the contents of the box in
+	dir, err := ioutil.TempDir("", "packer")
+	if err != nil {
+		return nil, err
+	}
+	defer os.RemoveAll(dir)
+
+	// Copy all of the original contents into the temporary directory
+	for _, path := range artifact.Files() {
+		ui.Message(fmt.Sprintf("Copying: %s", path))
+		src, err := os.Open(path)
+		if err != nil {
+			return nil, err
+		}
+		defer src.Close()
+
+		dst, err := os.Create(filepath.Join(dir, filepath.Base(path)))
+		if err != nil {
+			return nil, err
+		}
+		defer dst.Close()
+
+		if _, err := io.Copy(dst, src); err != nil {
+			return nil, err
+		}
+	}
+
+	// Create the Vagrantfile from the template
+	vf, err := os.Create(filepath.Join(dir, "Vagrantfile"))
+	if err != nil {
+		return nil, err
+	}
+	defer vf.Close()
+
+	t := template.Must(template.New("vagrantfile").Parse(defaultVBoxVagrantfile))
+	t.Execute(vf, tplData)
+	vf.Close()
+
+	// Create the metadata
+	metadata := map[string]string{"provider": "virtualbox"}
+	if err := WriteMetadata(dir, metadata); err != nil {
+		return nil, err
+	}
+
+	// Compress the directory to the given output path
+	ui.Message(fmt.Sprintf("Compressing box..."))
+	if err := DirToBox(p.config.OutputPath, dir); err != nil {
+		return nil, err
+	}
+
+	return NewArtifact("virtualbox", p.config.OutputPath), nil
+}
+
+var defaultVBoxVagrantfile = `
+Vagrant.configure("2") do |config|
+  config.vm.provider "virtualbox" do |vb|
+    # TODO
+  end
+end
+`
