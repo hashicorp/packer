d4cd9352d6a629db2876f7fc62c4b89e41bb55a4
Steven Merrill <steven.merrill@gmail.com>
2013-07-01 22:25:33 -0400

    First try at a compaction step.

---
+++ builder/vmware/step_compact_disk.go
@@ -0,0 +1,37 @@
+package vmware
+
+import (
+	"fmt"
+	"github.com/mitchellh/multistep"
+	"github.com/mitchellh/packer/packer"
+	"path/filepath"
+)
+
+// This step compacts the virtual disks for the VM.
+//
+// Uses:
+//   config *config
+//   driver Driver
+//   ui     packer.Ui
+//
+// Produces:
+//   <nothing>
+type stepCompactDisk struct{}
+
+func (stepCompactDisk) Run(state map[string]interface{}) multistep.StepAction {
+	config := state["config"].(*config)
+	driver := state["driver"].(Driver)
+	ui := state["ui"].(packer.Ui)
+
+	ui.Say("Compacting the disk image")
+	if err := driver.CompactDisk(config.FullDiskPath, fmt.Sprintf("%dM", config.DiskSize)); err != nil {
+		err := fmt.Errorf("Error compacting disk: %s", err)
+		state["error"] = err
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+
+	return multistep.ActionContinue
+}
+
+func (stepCompactDisk) Cleanup(map[string]interface{}) {}
