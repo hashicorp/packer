f31f5ed75940bb2fd39426339e48933ebf0ca863
Brandon Liu <bdon@bdon.org>
2013-07-02 13:06:34 -0700

    Command helpText indicates that options must come before template.

--- command/build/help.go
+++ command/build/help.go
@@ -1,7 +1,7 @@
 package build
 
 const helpText = `
-Usage: packer build TEMPLATE
+Usage: packer build [options] TEMPLATE
 
   Will execute multiple builds in parallel as defined in the template.
   The various artifacts created by the template will be outputted.
