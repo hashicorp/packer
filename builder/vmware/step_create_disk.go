ba14170f8f6b08198ee38a666870b1a6d26a9e9a
Steven Merrill <steven.merrill@gmail.com>
2013-07-02 01:32:50 -0400

    Small fix to be sure "full_disk_path" is put into state.

--- builder/vmware/step_create_disk.go
+++ builder/vmware/step_create_disk.go
@@ -25,13 +25,13 @@ func (stepCreateDisk) Run(state map[string]interface{}) multistep.StepAction {
 
 	ui.Say("Creating virtual machine disk")
 	full_disk_path := filepath.Join(config.OutputDir, config.DiskName+".vmdk")
+	state["full_disk_path"] = full_disk_path
 	if err := driver.CreateDisk(full_disk_path, fmt.Sprintf("%dM", config.DiskSize)); err != nil {
 		err := fmt.Errorf("Error creating disk: %s", err)
 		state["error"] = err
 		ui.Error(err.Error())
 		return multistep.ActionHalt
 	}
-	state["full_disk_path"] = full_disk_path
 
 	return multistep.ActionContinue
 }
