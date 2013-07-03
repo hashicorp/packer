58daa1d84e2cc24b707dfb48406f8ed3c24f055a
Steven Merrill <steven.merrill@gmail.com>
2013-07-02 20:22:11 -0400

    Add a 'skip_compaction' step.

--- builder/vmware/step_compact_disk.go
+++ builder/vmware/step_compact_disk.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"github.com/mitchellh/multistep"
 	"github.com/mitchellh/packer/packer"
+	"log"
 )
 
-// This step compacts the virtual disk for the VM.
+// This step compacts the virtual disk for the VM unless the "skip_compaction"
+// boolean is true.
 //
 // Uses:
+//   config *config
 //   driver Driver
 //   full_disk_path string
 //   ui     packer.Ui
@@ -18,9 +21,15 @@ import (
 type stepCompactDisk struct{}
 
 func (stepCompactDisk) Run(state map[string]interface{}) multistep.StepAction {
+	config := state["config"].(*config)
 	driver := state["driver"].(Driver)
 	ui := state["ui"].(packer.Ui)
 	full_disk_path := state["full_disk_path"].(string)
+
+	if config.SkipCompaction == true {
+		log.Println("Skipping disk compaction step...")
+		return multistep.ActionContinue
+	}
 
 	ui.Say("Compacting the disk image")
 	if err := driver.CompactDisk(full_disk_path); err != nil {
