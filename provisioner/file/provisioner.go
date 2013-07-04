4f2344f48bdaf8ffb2e5401cb3721b495effe52b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-04 12:50:00 -0700

    provisioner/file: style nitpicks
    /cc @ericlathrop

--- provisioner/file/provisioner.go
+++ provisioner/file/provisioner.go
@@ -30,16 +30,18 @@ func (p *Provisioner) Prepare(raws ...interface{}) error {
 	errs := []error{}
 
 	if _, err := os.Stat(p.config.Source); err != nil {
-		errs = append(errs, fmt.Errorf("Bad source file '%s': %s", p.config.Source, err))
+		errs = append(errs,
+			fmt.Errorf("Bad source '%s': %s", p.config.Source, err))
 	}
 
-	if len(p.config.Destination) == 0 {
+	if p.config.Destination == "" {
 		errs = append(errs, errors.New("Destination must be specified."))
 	}
 
 	if len(errs) > 0 {
 		return &packer.MultiError{errs}
 	}
+
 	return nil
 }
 
@@ -49,5 +51,7 @@ func (p *Provisioner) Provision(ui packer.Ui, comm packer.Communicator) error {
 	if err != nil {
 		return err
 	}
+	defer f.Close()
+
 	return comm.Upload(p.config.Destination, f)
 }
