5b4fc718aa32fbd4ff1830448d3e889e303848dd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-14 15:58:56 +0900

    builder/vmware: generic checksum types [GH-175]

--- builder/vmware/step_download_iso.go
+++ builder/vmware/step_download_iso.go
@@ -1,7 +1,6 @@
 package vmware
 
 import (
-	"crypto/md5"
 	"encoding/hex"
 	"fmt"
 	"github.com/mitchellh/multistep"
@@ -27,7 +26,7 @@ func (s stepDownloadISO) Run(state map[string]interface{}) multistep.StepAction 
 	config := state["config"].(*config)
 	ui := state["ui"].(packer.Ui)
 
-	checksum, err := hex.DecodeString(config.ISOMD5)
+	checksum, err := hex.DecodeString(config.ISOChecksum)
 	if err != nil {
 		err := fmt.Errorf("Error parsing checksum: %s", err)
 		state["error"] = err
@@ -43,7 +42,7 @@ func (s stepDownloadISO) Run(state map[string]interface{}) multistep.StepAction 
 		Url:        config.ISOUrl,
 		TargetPath: cachePath,
 		CopyFile:   false,
-		Hash:       md5.New(),
+		Hash:       common.HashForType(config.ISOChecksumType),
 		Checksum:   checksum,
 	}
 
