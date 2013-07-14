f91d4f22447d7572061f7d0517b2e9c637cbaee7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-14 16:51:20 +0900

    command/fix: fixer for iso_md5 change

---
+++ command/fix/fixer_iso_md5_test.go
@@ -0,0 +1,46 @@
+package fix
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFixerISOMD5_Impl(t *testing.T) {
+	var raw interface{}
+	raw = new(FixerISOMD5)
+	if _, ok := raw.(Fixer); !ok {
+		t.Fatalf("must be a Fixer")
+	}
+}
+
+func TestFixerISOMD5_Fix(t *testing.T) {
+	var f FixerISOMD5
+
+	input := map[string]interface{}{
+		"builders": []interface{}{
+			map[string]string{
+				"type":    "foo",
+				"iso_md5": "bar",
+			},
+		},
+	}
+
+	expected := map[string]interface{}{
+		"builders": []map[string]interface{}{
+			map[string]interface{}{
+				"type":              "foo",
+				"iso_checksum":      "bar",
+				"iso_checksum_type": "md5",
+			},
+		},
+	}
+
+	output, err := f.Fix(input)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if !reflect.DeepEqual(output, expected) {
+		t.Fatalf("unexpected: %#v\nexpected: %#v\n", output, expected)
+	}
+}
