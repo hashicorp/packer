2cea79c54a7ae66bf8730159240a6f107fa5062e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-15 15:02:18 +0900

    builder/amazon/ebs: rename to amazon/ebs in order to support others

--- plugin/builder-amazon-ebs/main.go
+++ plugin/builder-amazon-ebs/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/mitchellh/packer/builder/amazonebs"
+	"github.com/mitchellh/packer/builder/amazon/ebs"
 	"github.com/mitchellh/packer/packer/plugin"
 )
 
 func main() {
-	plugin.ServeBuilder(new(amazonebs.Builder))
+	plugin.ServeBuilder(new(ebs.Builder))
 }
