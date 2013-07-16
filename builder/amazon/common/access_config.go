8f649af16575c776d091a6bd43664c95825783eb
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-16 13:08:19 +0900

    builder/amazon/common: AccessConfig for standard access config

---
+++ builder/amazon/common/access_config.go
@@ -0,0 +1,21 @@
+package common
+
+import (
+	"github.com/mitchellh/goamz/aws"
+)
+
+// AccessConfig is for common configuration related to AWS access
+type AccessConfig struct {
+	AccessKey string `mapstructure:"access_key"`
+	SecretKey string `mapstructure:"secret_key"`
+}
+
+// Auth returns a valid aws.Auth object for access to AWS services, or
+// an error if the authentication couldn't be resolved.
+func (c *AccessConfig) Auth() (aws.Auth, error) {
+	return aws.GetAuth(c.AccessKey, c.SecretKey)
+}
+
+func (c *AccessConfig) Validate() []error {
+	return nil
+}
