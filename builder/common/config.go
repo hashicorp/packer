5dbae2efde76b73d84252f6b9ab824583e67f391
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-19 15:09:13 -0400

    builder/common: CheckUnusedConfig

--- builder/common/config.go
+++ builder/common/config.go
@@ -1,8 +1,34 @@
 package common
 
 import (
+	"fmt"
 	"github.com/mitchellh/mapstructure"
+	"github.com/mitchellh/packer/packer"
+	"sort"
+	"strings"
 )
+
+// CheckUnusedConfig is a helper that makes sure that the there are no
+// unused configuration keys, properly ignoring keys that don't matter.
+func CheckUnusedConfig(md *mapstructure.Metadata) error {
+	errs := make([]error, 0)
+
+	if md.Unused != nil && len(md.Unused) > 0 {
+		sort.Strings(md.Unused)
+		for _, unused := range md.Unused {
+			if unused != "type" && !strings.HasPrefix(unused, "packer_") {
+				errs = append(
+					errs, fmt.Errorf("Unknown configuration key: %s", unused))
+			}
+		}
+	}
+
+	if len(errs) > 0 {
+		return &packer.MultiError{errs}
+	}
+
+	return nil
+}
 
 // DecodeConfig is a helper that handles decoding raw configuration using
 // mapstructure. It returns the metadata and any errors that may happen.
