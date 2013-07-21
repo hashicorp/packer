d11cabf893b239ed6eaf2b39ee377eef5274cc4c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-20 21:00:12 -0700

    builder/amazon/instance: upload x509 cert

--- builder/amazon/instance/builder_test.go
+++ builder/amazon/instance/builder_test.go
@@ -2,11 +2,26 @@ package instance
 
 import (
 	"github.com/mitchellh/packer/packer"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
 func testConfig() map[string]interface{} {
-	return map[string]interface{}{}
+	tf, err := ioutil.TempFile("", "packer")
+	if err != nil {
+		panic(err)
+	}
+
+	return map[string]interface{}{
+		"instance_type":    "m1.small",
+		"region":           "us-east-1",
+		"source_ami":       "foo",
+		"ssh_username":     "bob",
+		"x509_cert_path":   tf.Name(),
+		"x509_key_path":    tf.Name(),
+		"x509_upload_path": "/foo",
+	}
 }
 
 func TestBuilder_ImplementsBuilder(t *testing.T) {
@@ -23,6 +38,75 @@ func TestBuilderPrepare_InvalidKey(t *testing.T) {
 
 	// Add a random key
 	config["i_should_not_be_valid"] = true
+	err := b.Prepare(config)
+	if err == nil {
+		t.Fatal("should have error")
+	}
+}
+
+func TestBuilderPrepare_X509CertPath(t *testing.T) {
+	b := &Builder{}
+	config := testConfig()
+
+	config["x509_cert_path"] = ""
+	err := b.Prepare(config)
+	if err == nil {
+		t.Fatal("should have error")
+	}
+
+	config["x509_cert_path"] = "i/am/a/file/that/doesnt/exist"
+	err = b.Prepare(config)
+	if err == nil {
+		t.Error("should have error")
+	}
+
+	tf, err := ioutil.TempFile("", "packer")
+	if err != nil {
+		t.Fatalf("error tempfile: %s", err)
+	}
+	defer os.Remove(tf.Name())
+
+	config["x509_cert_path"] = tf.Name()
+	err = b.Prepare(config)
+	if err != nil {
+		t.Fatalf("should not have error: %s", err)
+	}
+}
+
+func TestBuilderPrepare_X509KeyPath(t *testing.T) {
+	b := &Builder{}
+	config := testConfig()
+
+	config["x509_key_path"] = ""
+	err := b.Prepare(config)
+	if err == nil {
+		t.Fatal("should have error")
+	}
+
+	config["x509_key_path"] = "i/am/a/file/that/doesnt/exist"
+	err = b.Prepare(config)
+	if err == nil {
+		t.Error("should have error")
+	}
+
+	tf, err := ioutil.TempFile("", "packer")
+	if err != nil {
+		t.Fatalf("error tempfile: %s", err)
+	}
+	defer os.Remove(tf.Name())
+
+	config["x509_key_path"] = tf.Name()
+	err = b.Prepare(config)
+	if err != nil {
+		t.Fatalf("should not have error: %s", err)
+	}
+}
+
+func TestBuilderPrepare_X509UploadPath(t *testing.T) {
+	b := &Builder{}
+	config := testConfig()
+
+	config["x509_upload_path"] = ""
 	err := b.Prepare(config)
 	if err == nil {
 		t.Fatal("should have error")
