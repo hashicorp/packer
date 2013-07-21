1ee7a3a1a6a5701e34f0419f7a698cd5f1a9d37d
Matt <matt.ho@gmail.com>
2013-07-20 20:30:11 -0700

    fixes #207
    - adds a separate check to see if the return code from amazon was ami
    instance not found
    - added test cases

--- builder/amazon/ebs/step_create_ami.go
+++ builder/amazon/ebs/step_create_ami.go
@@ -2,6 +2,7 @@ package ebs
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"github.com/mitchellh/goamz/ec2"
 	"github.com/mitchellh/multistep"
@@ -50,33 +51,66 @@ func (s *stepCreateAMI) Run(state map[string]interface{}) multistep.StepAction {
 	}
 
 	// Set the AMI ID in the state
-	ui.Say(fmt.Sprintf("AMI: %s", createResp.ImageId))
+	imageId := createResp.ImageId
+	ui.Say(fmt.Sprintf("AMI: %s", imageId))
 	amis := make(map[string]string)
-	amis[config.Region] = createResp.ImageId
+	amis[config.Region] = imageId
 	state["amis"] = amis
 
+	// To simplify testing, we abstract the call to ec2.Images into a function, imageFetch
+	imageFetcher := func() (*ec2.ImagesResp, error) {
+		return ec2conn.Images([]string{imageId}, ec2.NewFilter())
+	}
+
 	// Wait for the image to become ready
-	ui.Say("Waiting for AMI to become ready...")
-	for {
-		imageResp, err := ec2conn.Images([]string{createResp.ImageId}, ec2.NewFilter())
-		if err != nil {
-			err := fmt.Errorf("Error querying images: %s", err)
-			state["error"] = err
-			ui.Error(err.Error())
-			return multistep.ActionHalt
-		}
-
-		if imageResp.Images[0].State == "available" {
-			break
-		}
-
-		log.Printf("Image in state %s, sleeping 2s before checking again",
-			imageResp.Images[0].State)
-
-		time.Sleep(2 * time.Second)
+	timeout := 1 * time.Minute                        // maximum time to wait for
+	wait := 2 * time.Second                           // time to wait between checks
+	err = waitForAMI(ui, imageFetcher, timeout, wait) // Wait for the AMI to be ready
+	if err != nil {
+		message := fmt.Errorf(":1s: %s", err)
+		state["error"] = message
+		ui.Error(err.Error())
+		return multistep.ActionHalt
 	}
 
 	return multistep.ActionContinue
+}
+
+// Wait for the image to become ready
+//
+// imageFetcher - function to encapsulate the call to ec2.Images.  simplifies testing
+// timeout - if the image is not available after this duration, blow up
+// wait - the amount of time to wait between checks to ec2
+func waitForAMI(ui packer.Ui, imageFetcher func() (*ec2.ImagesResp, error), timeout, wait time.Duration) error {
+	// encapsulates the for loop from #Run into a separate, easily testable function
+	ui.Say("Waiting for AMI to become ready...")
+	timeoutTimer := time.After(timeout)
+
+	for {
+		imageResp, err := imageFetcher()
+		if err != nil {
+			if ec2err := err.(*ec2.Error); ec2err.Code != "InvalidAMIID.NotFound" {
+				return err
+			}
+		}
+
+		if imageResp != nil { // imageResp check is required since it's possible we could be here in err (e.g. InvalidAMIID.NotFound)
+			if imageResp.Images[0].State == "available" {
+				break
+			}
+
+			log.Printf("Image in state %s, sleeping 2s before checking again",
+				imageResp.Images[0].State)
+		}
+
+		select {
+		case <-time.After(wait):
+		case <-timeoutTimer:
+			return errors.New("AMI not available after 1 minute")
+		}
+	}
+
+	return nil
 }
 
 func (s *stepCreateAMI) Cleanup(map[string]interface{}) {
