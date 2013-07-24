c6358b0475ce051fd185bacbab8ed97c2b58b0f6
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-24 15:41:49 -0500

    builder/amazon/instance: ec2-bundle-vol is called

--- builder/amazon/instance/step_bundle_volume.go
+++ builder/amazon/instance/step_bundle_volume.go
@@ -1,16 +1,31 @@
 package instance
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/mitchellh/goamz/ec2"
 	"github.com/mitchellh/multistep"
 	"github.com/mitchellh/packer/packer"
+	"text/template"
 )
+
+type bundleCmdData struct {
+	AccountId    string
+	Architecture string
+	CertPath     string
+	KeyPath      string
+	PrivatePath  string
+}
 
 type StepBundleVolume struct{}
 
 func (s *StepBundleVolume) Run(state map[string]interface{}) multistep.StepAction {
 	comm := state["communicator"].(packer.Communicator)
+	config := state["config"].(*Config)
+	instance := state["instance"].(*ec2.Instance)
 	ui := state["ui"].(packer.Ui)
+	x509RemoteCertPath := state["x509RemoteCertPath"].(string)
+	x509RemoteKeyPath := state["x509RemoteKeyPath"].(string)
 
 	// Verify the AMI tools are available
 	ui.Say("Checking for EC2 AMI tools...")
@@ -27,6 +42,27 @@ func (s *StepBundleVolume) Run(state map[string]interface{}) multistep.StepActio
 			"The EC2 AMI tools could not be detected. These must be manually\n" +
 				"via a provisioner or some other means and are required for Packer\n" +
 				"to create an instance-store AMI.")
+		ui.Error(state["error"].(error).Error())
+		return multistep.ActionHalt
+	}
+
+	// Bundle the volume
+	var bundleCmd bytes.Buffer
+	tData := bundleCmdData{
+		AccountId:    config.AccountId,
+		Architecture: instance.Architecture,
+		CertPath:     x509RemoteCertPath,
+		KeyPath:      x509RemoteKeyPath,
+		PrivatePath:  config.X509UploadPath,
+	}
+	t := template.Must(template.New("bundleCmd").Parse(config.BundleVolCommand))
+	t.Execute(&bundleCmd, tData)
+
+	ui.Say("Bundling the volume...")
+	cmd = new(packer.RemoteCmd)
+	cmd.Command = bundleCmd.String()
+	if err := cmd.StartWithUi(comm, ui); err != nil {
+		state["error"] = fmt.Errorf("Error bundling volume: %s", err)
 		ui.Error(state["error"].(error).Error())
 		return multistep.ActionHalt
 	}
