e5b14ac450642462ab01516a61eda0cc0d4f5844
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-26 09:38:16 -0700

    builder/amazon/common: wait for AMI to be available [GH-207]

--- builder/amazon/common/ami.go
+++ builder/amazon/common/ami.go
@@ -11,6 +11,11 @@ func WaitForAMI(c *ec2.EC2, imageId string) error {
 	for {
 		imageResp, err := c.Images([]string{imageId}, ec2.NewFilter())
 		if err != nil {
+			if ec2err, ok := err.(*ec2.Error); ok && ec2err.Code == "InvalidAMIID.NotFound" {
+				log.Println("AMI not found, probably state issues on AWS side. Trying again.")
+				continue
+			}
+
 			return err
 		}
 
