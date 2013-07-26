74ef543d5c4152ee3639b523b528c3131a11272b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-25 21:24:49 -0500

    packer/plugin: connect stdin to parent stdin

--- packer/plugin/plugin_test.go
+++ packer/plugin/plugin_test.go
@@ -2,6 +2,7 @@ package plugin
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"testing"
@@ -65,6 +66,19 @@ func TestHelperProcess(*testing.T) {
 		ServeProvisioner(new(helperProvisioner))
 	case "start-timeout":
 		time.Sleep(1 * time.Minute)
+		os.Exit(1)
+	case "stdin":
+		fmt.Println(":1234")
+		data := make([]byte, 5)
+		if _, err := os.Stdin.Read(data); err != nil {
+			log.Printf("stdin read error: %s", err)
+			os.Exit(100)
+		}
+
+		if string(data) == "hello" {
+			os.Exit(0)
+		}
+
 		os.Exit(1)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command: %q\n", cmd)
