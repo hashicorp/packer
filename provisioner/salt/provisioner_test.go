1b0d316456cbf11a6686ce0605f16dec7fc5e9a1
Rafael Garcia <rafael.garcia@getclever.com>
2013-07-27 00:11:04 -0700

    provisioner/salt: simple tests

---
+++ provisioner/salt/provisioner_test.go
@@ -0,0 +1,46 @@
+package salt
+
+import (
+	"github.com/mitchellh/packer/packer"
+	"testing"
+)
+
+func testConfig() map[string]interface{} {
+	return map[string]interface{}{
+		"local_state_tree": "/Users/me/salt",
+	}
+}
+
+func TestProvisioner_Impl(t *testing.T) {
+	var raw interface{}
+	raw = &Provisioner{}
+	if _, ok := raw.(packer.Provisioner); !ok {
+		t.Fatalf("must be a Provisioner")
+	}
+}
+
+func TestProvisionerPrepare_Defaults(t *testing.T) {
+	var p Provisioner
+	config := testConfig()
+
+	err := p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if p.config.TempConfigDir != DefaultTempConfigDir {
+		t.Errorf("unexpected temp config dir: %s", p.config.TempConfigDir)
+	}
+}
+
+func TestProvisionerPrepare_InvalidKey(t *testing.T) {
+	var p Provisioner
+	config := testConfig()
+
+	// Add a random key
+	config["i_should_not_be_valid"] = true
+	err := p.Prepare(config)
+	if err == nil {
+		t.Fatal("should have error")
+	}
+}
