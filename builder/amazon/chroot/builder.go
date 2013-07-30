702f299343764479a1226b89716159d09d967b12
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-29 19:07:51 -0700

    builder/amazon/chroot: Attach volume

--- builder/amazon/chroot/builder.go
+++ builder/amazon/chroot/builder.go
@@ -75,7 +75,7 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		&StepInstanceInfo{},
 		&StepSourceAMIInfo{},
 		&StepCreateVolume{},
-		//&StepAttachVolume{},
+		&StepAttachVolume{},
 	}
 
 	// Run!
---
+++ builder/amazon/chroot/step_attach_volume.go
@@ -0,0 +1,115 @@
+package chroot
+
+import (
+	"errors"
+	"fmt"
+	"github.com/mitchellh/goamz/ec2"
+	"github.com/mitchellh/multistep"
+	awscommon "github.com/mitchellh/packer/builder/amazon/common"
+	"github.com/mitchellh/packer/packer"
+)
+
+// StepAttachVolume attaches the previously created volume to an
+// available device location.
+//
+// Produces:
+//   volume_id string - The ID of the created volume
+type StepAttachVolume struct {
+	attached bool
+	volumeId string
+}
+
+func (s *StepAttachVolume) Run(state map[string]interface{}) multistep.StepAction {
+	ec2conn := state["ec2"].(*ec2.EC2)
+	instance := state["instance"].(*ec2.Instance)
+	ui := state["ui"].(packer.Ui)
+	volumeId := state["volume_id"].(string)
+
+	device := "/dev/sdh"
+
+	ui.Say("Attaching the root volume...")
+	_, err := ec2conn.AttachVolume(volumeId, instance.InstanceId, device)
+	if err != nil {
+		err := fmt.Errorf("Error attaching volume: %s", err)
+		state["error"] = err
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+
+	// Mark that we attached it so we can detach it later
+	s.attached = true
+	s.volumeId = volumeId
+
+	// Wait for the volume to become attached
+	stateChange := awscommon.StateChangeConf{
+		Conn:      ec2conn,
+		Pending:   []string{"attaching"},
+		StepState: state,
+		Target:    "attached",
+		Refresh: func() (interface{}, string, error) {
+			resp, err := ec2conn.Volumes([]string{volumeId}, ec2.NewFilter())
+			if err != nil {
+				return nil, "", err
+			}
+
+			if len(resp.Volumes[0].Attachments) == 0 {
+				return nil, "", errors.New("No attachments on volume.")
+			}
+
+			return nil, resp.Volumes[0].Attachments[0].Status, nil
+		},
+	}
+
+	_, err = awscommon.WaitForState(&stateChange)
+	if err != nil {
+		err := fmt.Errorf("Error waiting for volume: %s", err)
+		state["error"] = err
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+
+	return multistep.ActionContinue
+}
+
+func (s *StepAttachVolume) Cleanup(state map[string]interface{}) {
+	if !s.attached {
+		return
+	}
+
+	ec2conn := state["ec2"].(*ec2.EC2)
+	ui := state["ui"].(packer.Ui)
+
+	ui.Say("Detaching EBS volume...")
+	_, err := ec2conn.DetachVolume(s.volumeId)
+	if err != nil {
+		ui.Error(fmt.Sprintf("Error detaching EBS volume: %s", err))
+		return
+	}
+
+	// Wait for the volume to detach
+	stateChange := awscommon.StateChangeConf{
+		Conn:      ec2conn,
+		Pending:   []string{"detaching"},
+		StepState: state,
+		Target:    "detached",
+		Refresh: func() (interface{}, string, error) {
+			resp, err := ec2conn.Volumes([]string{s.volumeId}, ec2.NewFilter())
+			if err != nil {
+				return nil, "", err
+			}
+
+			state := "detached"
+			if len(resp.Volumes[0].Attachments) > 0 {
+				state = resp.Volumes[0].Attachments[0].Status
+			}
+
+			return nil, state, nil
+		},
+	}
+
+	_, err = awscommon.WaitForState(&stateChange)
+	if err != nil {
+		ui.Error(fmt.Sprintf("Error waiting for volume: %s", err))
+		return
+	}
+}
