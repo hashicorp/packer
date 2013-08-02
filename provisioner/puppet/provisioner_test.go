d37f287f8c7f2693b8aa03fa392a4a94459aa52f
James Turnbull <james@lovedthanlost.net>
2013-08-01 20:05:23 -0400

    Added basic Puppet provisioner

---
+++ provisioner/puppet/provisioner_test.go
@@ -0,0 +1,20 @@
+package puppet
+
+import (
+	"github.com/mitchellh/packer/packer"
+	"testing"
+)
+
+func testConfig() map[string]interface{} {
+	return map[string]interface{}{
+	// "inline": []interface{}{"foo", "bar"},
+	}
+}
+
+func TestProvisioner_Impl(t *testing.T) {
+	var raw interface{}
+	raw = &Provisioner{}
+	if _, ok := raw.(packer.Provisioner); !ok {
+		t.Fatalf("must be a Provisioner")
+	}
+}
