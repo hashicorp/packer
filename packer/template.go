777f595c8bd9fb2752eb7cfe9b123ec5cec9169b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-08-09 16:50:24 -0700

    common/json: add Unmarshal with method with syntax errors

--- packer/template.go
+++ packer/template.go
@@ -1,10 +1,9 @@
 package packer
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"github.com/mitchellh/mapstructure"
+	jsonutil "github.com/mitchellh/packer/common/json"
 	"io/ioutil"
 	"sort"
 )
@@ -68,31 +67,8 @@ type rawProvisionerConfig struct {
 // way.
 func ParseTemplate(data []byte) (t *Template, err error) {
 	var rawTplInterface interface{}
-	err = json.Unmarshal(data, &rawTplInterface)
+	err = jsonutil.Unmarshal(data, &rawTplInterface)
 	if err != nil {
-		syntaxErr, ok := err.(*json.SyntaxError)
-		if !ok {
-			return
-		}
-
-		// We have a syntax error. Extract out the line number and friends.
-		// https://groups.google.com/forum/#!topic/golang-nuts/fizimmXtVfc
-		newline := []byte{'\x0a'}
-
-		// Calculate the start/end position of the line where the error is
-		start := bytes.LastIndex(data[:syntaxErr.Offset], newline) + 1
-		end := len(data)
-		if idx := bytes.Index(data[start:], newline); idx >= 0 {
-			end = start + idx
-		}
-
-		// Count the line number we're on plus the offset in the line
-		line := bytes.Count(data[:start], newline) + 1
-		pos := int(syntaxErr.Offset) - start - 1
-
-		err = fmt.Errorf("Error in line %d, char %d: %s\n%s",
-			line, pos, syntaxErr, data[start:end])
-
 		return
 	}
 
