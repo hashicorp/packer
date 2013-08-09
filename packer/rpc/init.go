2349706a68b5e9d86039a9a5cd48c094a19b8dfc
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-08-09 16:52:59 -0700

    packer/rpc: register map[string]string since we transfer that

--- packer/rpc/init.go
+++ packer/rpc/init.go
@@ -4,6 +4,7 @@ import "encoding/gob"
 
 func init() {
 	gob.Register(new(map[string]interface{}))
+	gob.Register(new(map[string]string))
 	gob.Register(make([]interface{}, 0))
 	gob.Register(new(BasicError))
 }
