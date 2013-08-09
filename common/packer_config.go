21fb039fce8bd0ce308369b9ff16fd1f970f8342
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-08-09 14:11:13 -0700

    common: PackerConfig understands user vars

--- common/packer_config.go
+++ common/packer_config.go
@@ -4,7 +4,9 @@ package common
 // are sent by packer, properly tagged already so mapstructure can load
 // them. Embed this structure into your configuration class to get it.
 type PackerConfig struct {
-	PackerBuildName string `mapstructure:"packer_build_name"`
-	PackerDebug     bool   `mapstructure:"packer_debug"`
-	PackerForce     bool   `mapstructure:"packer_force"`
+	PackerBuildName   string            `mapstructure:"packer_build_name"`
+	PackerBuilderType string            `mapstructure:"packer_builder_type"`
+	PackerDebug       bool              `mapstructure:"packer_debug"`
+	PackerForce       bool              `mapstructure:"packer_force"`
+	PackerUserVars    map[string]string `mapstructure:"packer_user_variables"`
 }
