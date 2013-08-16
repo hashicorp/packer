b49fe4971acbf3f5df8f3a3ce2196e6e5366e6a8
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-08-15 20:28:20 -0700

    builder/amazon: modifying more than one AMI attribute type works

--- builder/amazon/common/step_modify_ami_attributes.go
+++ builder/amazon/common/step_modify_ami_attributes.go
@@ -31,20 +31,44 @@ func (s *StepModifyAMIAttributes) Run(state map[string]interface{}) multistep.St
 		return multistep.ActionContinue
 	}
 
-	options := &ec2.ModifyImageAttribute{
-		Description:  s.Description,
-		AddUsers:     s.Users,
-		AddGroups:    s.Groups,
-		ProductCodes: s.ProductCodes,
+	// Construct the modify image attribute requests we're going to make.
+	// We need to make each separately since the EC2 API only allows changing
+	// one type at a kind currently.
+	options := make(map[string]*ec2.ModifyImageAttribute)
+	if s.Description != "" {
+		options["description"] = &ec2.ModifyImageAttribute{
+			Description: s.Description,
+		}
+	}
+
+	if len(s.Groups) > 0 {
+		options["groups"] = &ec2.ModifyImageAttribute{
+			AddGroups: s.Groups,
+		}
+	}
+
+	if len(s.Users) > 0 {
+		options["users"] = &ec2.ModifyImageAttribute{
+			AddUsers: s.Users,
+		}
+	}
+
+	if len(s.ProductCodes) > 0 {
+		options["product codes"] = &ec2.ModifyImageAttribute{
+			ProductCodes: s.ProductCodes,
+		}
 	}
 
 	ui.Say("Modifying AMI attributes...")
-	_, err := ec2conn.ModifyImageAttribute(ami, options)
-	if err != nil {
-		err := fmt.Errorf("Error modify AMI attributes: %s", err)
-		state["error"] = err
-		ui.Error(err.Error())
-		return multistep.ActionHalt
+	for name, opts := range options {
+		ui.Message(fmt.Sprintf("Modifying: %s", name))
+		_, err := ec2conn.ModifyImageAttribute(ami, opts)
+		if err != nil {
+			err := fmt.Errorf("Error modify AMI attributes: %s", err)
+			state["error"] = err
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
 	}
 
 	return multistep.ActionContinue
