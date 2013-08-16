1e520b161b0fe5688df7d774ac5f59a910a7d1ae
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-08-15 19:11:27 -0700

    packer: ConfigTemplate, move from common

---
+++ packer/config_template_test.go
@@ -0,0 +1,67 @@
+package packer
+
+import (
+	"math"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestConfigTemplateProcess_timestamp(t *testing.T) {
+	tpl, err := NewConfigTemplate()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	result, err := tpl.Process(`{{timestamp}}`, nil)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	val, err := strconv.ParseInt(result, 10, 64)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	currentTime := time.Now().UTC().Unix()
+	if math.Abs(float64(currentTime-val)) > 10 {
+		t.Fatalf("val: %d (current: %d)", val, currentTime)
+	}
+}
+
+func TestConfigTemplateProcess_user(t *testing.T) {
+	tpl, err := NewConfigTemplate()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	tpl.UserVars["foo"] = "bar"
+
+	result, err := tpl.Process(`{{user "foo"}}`, nil)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if result != "bar" {
+		t.Fatalf("bad: %s", result)
+	}
+}
+
+func TestConfigTemplateValidate(t *testing.T) {
+	tpl, err := NewConfigTemplate()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Valid
+	err = tpl.Validate(`{{user "foo"}}`)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Invalid
+	err = tpl.Validate(`{{idontexist}}`)
+	if err == nil {
+		t.Fatal("should have error")
+	}
+}
