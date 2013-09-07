2a04f49bcae7ddaa2950bfe658e13e6531e22087
Sean Mackrory <sean@cloudera.com>
2013-09-06 19:08:06 -0700

    Using the vm_name parameter to name OVA/OVF files and their attachments

--- builder/virtualbox/step_export.go
+++ builder/virtualbox/step_export.go
@@ -50,7 +50,7 @@ func (s *stepExport) Run(state multistep.StateBag) multistep.StepAction {
 	}
 
 	// Export the VM to an OVF
-	outputPath := filepath.Join(config.OutputDir, "packer."+config.Format)
+	outputPath := filepath.Join(config.OutputDir, vmName+"."+config.Format)
 
 	command = []string{
 		"export",
