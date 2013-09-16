f3edfb9a26e9e9e5133edb9ededb74141509cd57
Jason A. Beranek <jason.beranek@gmail.com>
2013-09-16 00:17:53 -0500

    post-processor/vagrant: Fix builder specific configuration [GH-413]

--- post-processor/vagrant/virtualbox.go
+++ post-processor/vagrant/virtualbox.go
@@ -32,7 +32,7 @@ type VBoxBoxPostProcessor struct {
 }
 
 func (p *VBoxBoxPostProcessor) Configure(raws ...interface{}) error {
-	md, err := common.DecodeConfig(&p.config, raws...)
+	_, err := common.DecodeConfig(&p.config, raws...)
 	if err != nil {
 		return err
 	}
@@ -43,8 +43,12 @@ func (p *VBoxBoxPostProcessor) Configure(raws ...interface{}) error {
 	}
 	p.config.tpl.UserVars = p.config.PackerUserVars
 
+	log.Printf("Output Path Template is: %s", p.config.OutputPath)
+	log.Printf("VagrantfileTemplate is: %s", p.config.VagrantfileTemplate)
+
 	// Accumulate any errors
-	errs := common.CheckUnusedConfig(md)
+	errs := new(packer.MultiError)
+	//errs := common.CheckUnusedConfig(md)
 
 	validates := map[string]*string{
 		"output":               &p.config.OutputPath,
