5315b19822b4865d86f3892d3f6d9f11b8acc47e
Matthew Hooker <mwhooker@gmail.com>
2013-10-11 03:50:08 -0700

    common/config: config filter function [GH-521]
    Fixes #521

--- builder/amazon/ebs/builder.go
+++ builder/amazon/ebs/builder.go
@@ -56,7 +56,7 @@ func (b *Builder) Prepare(raws ...interface{}) error {
 		return errs
 	}
 
-	log.Printf("Config: %+v", b.config)
+	log.Println(common.ScrubConfig(b.config), b.config.AccessKey, b.config.SecretKey)
 	return nil
 }
 
