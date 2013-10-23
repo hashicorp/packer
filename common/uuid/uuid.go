d094cb7c5c2ba7259c290611d0672837268ea46e
Jonas Pfenniger <zimbatm@zimbatm.com>
2013-10-23 10:58:48 +0100

    Fixes missing entropy in the uuid package.
    math/crypto is seeded with 1 and thus will create predictable UUIDs. Because
    amazon-instance and amazon-ebs in the same second when building both targets
    the timestamp in front doesn't help either. See #552

--- common/uuid/uuid.go
+++ common/uuid/uuid.go
@@ -2,17 +2,26 @@ package uuid
 
 import (
 	"fmt"
-	"math/rand"
+	"crypto/rand"
+	"encoding/binary"
 	"time"
 )
+
+func uint32rand() (value uint32) {
+	err := binary.Read(rand.Reader, binary.LittleEndian, &value)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
 
 // Generates a time ordered UUID. Top 32 bits are a timestamp,
 // bottom 96 are random.
 func TimeOrderedUUID() string {
 	unix := uint32(time.Now().UTC().Unix())
-	rand1 := rand.Uint32()
-	rand2 := rand.Uint32()
-	rand3 := rand.Uint32()
+	rand1 := uint32rand()
+	rand2 := uint32rand()
+	rand3 := uint32rand()
 	return fmt.Sprintf("%08x-%04x-%04x-%04x-%04x%08x",
 		unix,
 		uint16(rand1>>16),
