7e30cf3479daf1c665c827eb4aaecd00d676ea5b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-11-09 13:03:01 -0800

    builder/docker: StepRun tests

--- builder/docker/driver_docker.go
+++ builder/docker/driver_docker.go
@@ -1,8 +1,12 @@
 package docker
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/mitchellh/packer/packer"
+	"log"
 	"os/exec"
+	"strings"
 )
 
 type DockerDriver struct {
@@ -12,4 +16,43 @@ type DockerDriver struct {
 func (d *DockerDriver) Pull(image string) error {
 	cmd := exec.Command("docker", "pull", image)
 	return runAndStream(cmd, d.Ui)
+}
+
+func (d *DockerDriver) StartContainer(config *ContainerConfig) (string, error) {
+	// Args that we're going to pass to Docker
+	args := []string{"run", "-d", "-i", "-t"}
+
+	if len(config.Volumes) > 0 {
+		volumes := make([]string, 0, len(config.Volumes))
+		for host, guest := range config.Volumes {
+			volumes = append(volumes, fmt.Sprintf("%s:%s", host, guest))
+		}
+
+		args = append(args, "-v", strings.Join(volumes, ","))
+	}
+
+	args = append(args, config.Image, "/bin/bash")
+
+	// Start the container
+	var stdout, stderr bytes.Buffer
+	cmd := exec.Command("docker", args...)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	log.Printf("Starting container with args: %v", args)
+	if err := cmd.Start(); err != nil {
+		return "", err
+	}
+
+	log.Println("Waiting for container to finish starting")
+	if err := cmd.Wait(); err != nil {
+		return "", err
+	}
+
+	// Capture the container ID, which is alone on stdout
+	return strings.TrimSpace(stdout.String()), nil
+}
+
+func (d *DockerDriver) StopContainer(id string) error {
+	return exec.Command("docker", "kill", id).Run()
 }
--- builder/docker/step_run.go
+++ builder/docker/step_run.go
@@ -1,13 +1,9 @@
 package docker
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/mitchellh/multistep"
 	"github.com/mitchellh/packer/packer"
-	"log"
-	"os/exec"
-	"strings"
 )
 
 type StepRun struct {
@@ -16,47 +12,30 @@ type StepRun struct {
 
 func (s *StepRun) Run(state multistep.StateBag) multistep.StepAction {
 	config := state.Get("config").(*Config)
+	driver := state.Get("driver").(Driver)
 	tempDir := state.Get("temp_dir").(string)
 	ui := state.Get("ui").(packer.Ui)
 
-	ui.Say("Starting docker container with /bin/bash")
-
-	// Args that we're going to pass to Docker
-	args := []string{
-		"run",
-		"-d", "-i", "-t",
-		"-v", fmt.Sprintf("%s:/packer-files", tempDir),
-		config.Image,
-		"/bin/bash",
+	runConfig := ContainerConfig{
+		Image: config.Image,
+		Volumes: map[string]string{
+			tempDir: "/packer-files",
+		},
 	}
 
-	// Start the container
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("docker", args...)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	log.Printf("Starting container with args: %v", args)
-	if err := cmd.Start(); err != nil {
+	ui.Say("Starting docker container with /bin/bash")
+	containerId, err := driver.StartContainer(&runConfig)
+	if err != nil {
 		err := fmt.Errorf("Error running container: %s", err)
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return multistep.ActionHalt
 	}
 
-	if err := cmd.Wait(); err != nil {
-		err := fmt.Errorf("Error running container: %s\nStderr: %s",
-			err, stderr.String())
-		state.Put("error", err)
-		ui.Error(err.Error())
-		return multistep.ActionHalt
-	}
-
-	// Capture the container ID, which is alone on stdout
-	s.containerId = strings.TrimSpace(stdout.String())
-	ui.Message(fmt.Sprintf("Container ID: %s", s.containerId))
-
+	// Save the container ID
+	s.containerId = containerId
 	state.Put("container_id", s.containerId)
+	ui.Message(fmt.Sprintf("Container ID: %s", s.containerId))
 	return multistep.ActionContinue
 }
 
@@ -68,5 +47,9 @@ func (s *StepRun) Cleanup(state multistep.StateBag) {
 	// Kill the container. We don't handle errors because errors usually
 	// just mean that the container doesn't exist anymore, which isn't a
 	// big deal.
-	exec.Command("docker", "kill", s.containerId).Run()
+	driver := state.Get("driver").(Driver)
+	driver.StopContainer(s.containerId)
+
+	// Reset the container ID so that we're idempotent
+	s.containerId = ""
 }
