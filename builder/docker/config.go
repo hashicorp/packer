da683afde0224d72bc5aa7170ca0a4c381f441a8
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-11-09 11:47:32 -0800

    builder/docker: config validation test

--- builder/docker/config.go
+++ builder/docker/config.go
@@ -1,6 +1,7 @@
 package docker
 
 import (
+	"fmt"
 	"github.com/mitchellh/packer/common"
 	"github.com/mitchellh/packer/packer"
 )
@@ -12,4 +13,32 @@ type Config struct {
 	Image      string
 
 	tpl *packer.ConfigTemplate
+}
+
+func (c *Config) Prepare() ([]string, []error) {
+	errs := make([]error, 0)
+
+	templates := map[string]*string{
+		"export_path": &c.ExportPath,
+		"image":       &c.Image,
+	}
+
+	for n, ptr := range templates {
+		var err error
+		*ptr, err = c.tpl.Process(*ptr, nil)
+		if err != nil {
+			errs = append(
+				errs, fmt.Errorf("Error processing %s: %s", n, err))
+		}
+	}
+
+	if c.ExportPath == "" {
+		errs = append(errs, fmt.Errorf("export_path must be specified"))
+	}
+
+	if c.Image == "" {
+		errs = append(errs, fmt.Errorf("image must be specified"))
+	}
+
+	return nil, errs
 }
