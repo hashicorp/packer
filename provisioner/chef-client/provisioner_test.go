4695735e20a5a81e0a4bfb6b8492b8c7e3594515
Bailey Johnson <bjohnson@red5studios.com>
2013-12-03 00:56:21 +0000

    Adding tests

---
+++ provisioner/chef-client/provisioner_test.go
@@ -0,0 +1,183 @@
+package chefclient
+
+import (
+	"github.com/mitchellh/packer/packer"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func testConfig() map[string]interface{} {
+	return map[string]interface{}{}
+}
+
+func TestProvisioner_Impl(t *testing.T) {
+	var raw interface{}
+	raw = &Provisioner{}
+	if _, ok := raw.(packer.Provisioner); !ok {
+		t.Fatalf("must be a Provisioner")
+	}
+}
+
+func TestProvisionerPrepare_configTemplate(t *testing.T) {
+	var err error
+	var p Provisioner
+
+	// Test no config template
+	config := testConfig()
+	delete(config, "config_template")
+	err = p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Test with a file
+	tf, err := ioutil.TempFile("", "packer")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer os.Remove(tf.Name())
+
+	config = testConfig()
+	config["config_template"] = tf.Name()
+	p = Provisioner{}
+	err = p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Test with a directory
+	td, err := ioutil.TempDir("", "packer")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer os.RemoveAll(td)
+
+	config = testConfig()
+	config["config_template"] = td
+	p = Provisioner{}
+	err = p.Prepare(config)
+	if err == nil {
+		t.Fatal("should have err")
+	}
+}
+
+func TestProvisionerPrepare_cookbookPaths(t *testing.T) {
+        var p Provisioner
+
+        path1, err := ioutil.TempDir("", "cookbooks_one")
+        if err != nil {
+                t.Fatalf("err: %s", err)
+        }
+
+        path2, err := ioutil.TempDir("", "cookbooks_two")
+        if err != nil {
+                t.Fatalf("err: %s", err)
+        }
+
+        rolesPath, err := ioutil.TempDir("", "roles")
+        if err != nil {
+                t.Fatalf("err: %s", err)
+        }
+
+        dataBagsPath, err := ioutil.TempDir("", "data_bags")
+        if err != nil {
+                t.Fatalf("err: %s", err)
+        }
+
+        defer os.Remove(path1)
+        defer os.Remove(path2)
+        defer os.Remove(rolesPath)
+        defer os.Remove(dataBagsPath)
+
+        config := testConfig()
+        config["cookbook_paths"] = []string{path1, path2}
+        config["roles_path"] = rolesPath
+        config["data_bags_path"] = dataBagsPath
+
+        err = p.Prepare(config)
+        if err != nil {
+                t.Fatalf("err: %s", err)
+        }
+
+        if len(p.config.CookbookPaths) != 2 {
+                t.Fatalf("unexpected: %#v", p.config.CookbookPaths)
+        }
+
+        if p.config.CookbookPaths[0] != path1 || p.config.CookbookPaths[1] != path2 {
+                t.Fatalf("unexpected: %#v", p.config.CookbookPaths)
+        }
+
+        if p.config.RolesPath != rolesPath {
+                t.Fatalf("unexpected: %#v", p.config.RolesPath)
+        }
+
+        if p.config.DataBagsPath != dataBagsPath {
+                t.Fatalf("unexpected: %#v", p.config.DataBagsPath)
+        }
+}
+
+func TestProvisionerPrepare_dataBagsPath(t *testing.T) {
+	var p Provisioner
+
+	dataBagsPath, err := ioutil.TempDir("", "data_bags")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer os.Remove(dataBagsPath)
+
+	config := testConfig()
+	config["data_bags_path"] = dataBagsPath
+
+	err = p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if p.config.DataBagsPath != dataBagsPath {
+		t.Fatalf("unexpected: %#v", p.config.DataBagsPath)
+	}
+}
+
+func TestProvisionerPrepare_rolesPath(t *testing.T) {
+	var p Provisioner
+
+	rolesPath, err := ioutil.TempDir("", "roles")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer os.Remove(rolesPath)
+
+	config := testConfig()
+	config["roles_path"] = rolesPath
+
+	err = p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if p.config.RolesPath != rolesPath {
+		t.Fatalf("unexpected: %#v", p.config.RolesPath)
+	}
+}
+
+func TestProvisionerPrepare_json(t *testing.T) {
+	config := testConfig()
+	config["json"] = map[string]interface{}{
+		"foo": "{{ user `foo` }}",
+	}
+
+	config[packer.UserVariablesConfigKey] = map[string]string{
+		"foo": `"bar\baz"`,
+	}
+
+	var p Provisioner
+	err := p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if p.config.Json["foo"] != `"bar\baz"` {
+		t.Fatalf("bad: %#v", p.config.Json)
+	}
+}
