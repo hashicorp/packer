70c0742b6a71d713efd6ad10675f5a031c60ffa8
Bailey Johnson <bjohnson@red5studios.com>
2013-12-03 01:03:22 +0000

    Ran go fmt on files

--- provisioner/chef-client/provisioner_test.go
+++ provisioner/chef-client/provisioner_test.go
@@ -63,58 +63,58 @@ func TestProvisionerPrepare_configTemplate(t *testing.T) {
 }
 
 func TestProvisionerPrepare_cookbookPaths(t *testing.T) {
-        var p Provisioner
+	var p Provisioner
 
-        path1, err := ioutil.TempDir("", "cookbooks_one")
-        if err != nil {
-                t.Fatalf("err: %s", err)
-        }
+	path1, err := ioutil.TempDir("", "cookbooks_one")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
 
-        path2, err := ioutil.TempDir("", "cookbooks_two")
-        if err != nil {
-                t.Fatalf("err: %s", err)
-        }
+	path2, err := ioutil.TempDir("", "cookbooks_two")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
 
-        rolesPath, err := ioutil.TempDir("", "roles")
-        if err != nil {
-                t.Fatalf("err: %s", err)
-        }
+	rolesPath, err := ioutil.TempDir("", "roles")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
 
-        dataBagsPath, err := ioutil.TempDir("", "data_bags")
-        if err != nil {
-                t.Fatalf("err: %s", err)
-        }
+	dataBagsPath, err := ioutil.TempDir("", "data_bags")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
 
-        defer os.Remove(path1)
-        defer os.Remove(path2)
-        defer os.Remove(rolesPath)
-        defer os.Remove(dataBagsPath)
+	defer os.Remove(path1)
+	defer os.Remove(path2)
+	defer os.Remove(rolesPath)
+	defer os.Remove(dataBagsPath)
 
-        config := testConfig()
-        config["cookbook_paths"] = []string{path1, path2}
-        config["roles_path"] = rolesPath
-        config["data_bags_path"] = dataBagsPath
+	config := testConfig()
+	config["cookbook_paths"] = []string{path1, path2}
+	config["roles_path"] = rolesPath
+	config["data_bags_path"] = dataBagsPath
 
-        err = p.Prepare(config)
-        if err != nil {
-                t.Fatalf("err: %s", err)
-        }
+	err = p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
 
-        if len(p.config.CookbookPaths) != 2 {
-                t.Fatalf("unexpected: %#v", p.config.CookbookPaths)
-        }
+	if len(p.config.CookbookPaths) != 2 {
+		t.Fatalf("unexpected: %#v", p.config.CookbookPaths)
+	}
 
-        if p.config.CookbookPaths[0] != path1 || p.config.CookbookPaths[1] != path2 {
-                t.Fatalf("unexpected: %#v", p.config.CookbookPaths)
-        }
+	if p.config.CookbookPaths[0] != path1 || p.config.CookbookPaths[1] != path2 {
+		t.Fatalf("unexpected: %#v", p.config.CookbookPaths)
+	}
 
-        if p.config.RolesPath != rolesPath {
-                t.Fatalf("unexpected: %#v", p.config.RolesPath)
-        }
+	if p.config.RolesPath != rolesPath {
+		t.Fatalf("unexpected: %#v", p.config.RolesPath)
+	}
 
-        if p.config.DataBagsPath != dataBagsPath {
-                t.Fatalf("unexpected: %#v", p.config.DataBagsPath)
-        }
+	if p.config.DataBagsPath != dataBagsPath {
+		t.Fatalf("unexpected: %#v", p.config.DataBagsPath)
+	}
 }
 
 func TestProvisionerPrepare_dataBagsPath(t *testing.T) {
