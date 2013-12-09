cb6e146921e6bf40ecf207a8ad9d908f9b503462
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-09 14:44:26 -0800

    packer/rpc: tests passing

--- packer/rpc/client_test.go
+++ packer/rpc/client_test.go
@@ -1,9 +1,33 @@
 package rpc
 
 import (
+	"net"
 	"testing"
 )
 
 func testClient(t *testing.T, server *Server) *Client {
-	return nil
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	go func() {
+		conn, err := l.Accept()
+		if err != nil {
+			t.Fatalf("err: %s", err)
+		}
+		server.ServeConn(conn)
+	}()
+
+	clientConn, err := net.Dial("tcp", l.Addr().String())
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	client, err := NewClient(clientConn)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	return client
 }
