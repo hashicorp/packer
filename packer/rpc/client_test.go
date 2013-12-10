4f75d778e9ab1afb914e6907b62a6e3757de49c1
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-09 16:22:11 -0800

    packer/rpc: work-in-progress commit

--- packer/rpc/client_test.go
+++ packer/rpc/client_test.go
@@ -5,29 +5,44 @@ import (
 	"testing"
 )
 
-func testClient(t *testing.T, server *Server) *Client {
+func testConn(t *testing.T) (net.Conn, net.Conn) {
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
 
+	var serverConn net.Conn
+	doneCh := make(chan struct{})
 	go func() {
-		conn, err := l.Accept()
+		defer close(doneCh)
+		defer l.Close()
+		var err error
+		serverConn, err = l.Accept()
 		if err != nil {
 			t.Fatalf("err: %s", err)
 		}
-		server.ServeConn(conn)
 	}()
 
 	clientConn, err := net.Dial("tcp", l.Addr().String())
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
+	<-doneCh
+
+	return clientConn, serverConn
+}
+
+func testClientServer(t *testing.T) (*Client, *Server) {
+	clientConn, serverConn := testConn(t)
+
+	server := NewServer(serverConn)
+	go server.Serve()
 
 	client, err := NewClient(clientConn)
 	if err != nil {
+		server.Close()
 		t.Fatalf("err: %s", err)
 	}
 
-	return client
+	return client, server
 }
