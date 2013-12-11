cc1130962b13a1e76e446af763ee8778dd1f2619
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-12-10 19:10:23 -0800

    Use the same license as Packer core

--- builder/googlecompute/api.go
+++ builder/googlecompute/api.go
@@ -1,7 +1,3 @@
-// Copyright (c) 2013 Kelsey Hightower. All rights reserved.
-// Use of this source code is governed by the Apache License, Version 2.0
-// that can be found in the LICENSE file.
-
 package googlecompute
 
 import (
