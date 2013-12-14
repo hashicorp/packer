20a074b4ee3cf348ab5303e8496547826eb59e04
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-13 18:26:00 -0800

    builder/googlecompute: StepCreateImage

--- builder/googlecompute/step_create_image.go
+++ builder/googlecompute/step_create_image.go
@@ -8,39 +8,45 @@ import (
 	"github.com/mitchellh/packer/packer"
 )
 
-// stepCreateImage represents a Packer build step that creates GCE machine
+// StepCreateImage represents a Packer build step that creates GCE machine
 // images.
-type stepCreateImage int
+type StepCreateImage int
 
 // Run executes the Packer build step that creates a GCE machine image.
 //
 // Currently the only way to create a GCE image is to run the gcimagebundle
 // command on the running GCE instance.
-func (s *stepCreateImage) Run(state multistep.StateBag) multistep.StepAction {
-	var (
-		config     = state.Get("config").(*Config)
-		comm       = state.Get("communicator").(packer.Communicator)
-		sudoPrefix = ""
-		ui         = state.Get("ui").(packer.Ui)
-	)
-	ui.Say("Creating image...")
+func (s *StepCreateImage) Run(state multistep.StateBag) multistep.StepAction {
+	config := state.Get("config").(*Config)
+	comm := state.Get("communicator").(packer.Communicator)
+	ui := state.Get("ui").(packer.Ui)
+
+	sudoPrefix := ""
 	if config.SSHUsername != "root" {
 		sudoPrefix = "sudo "
 	}
+
 	imageFilename := fmt.Sprintf("%s.tar.gz", config.ImageName)
 	imageBundleCmd := "/usr/bin/gcimagebundle -d /dev/sda -o /tmp/"
+
+	ui.Say("Creating image...")
 	cmd := new(packer.RemoteCmd)
 	cmd.Command = fmt.Sprintf("%s%s --output_file_name %s",
 		sudoPrefix, imageBundleCmd, imageFilename)
 	err := cmd.StartWithUi(comm, ui)
+	if err == nil && cmd.ExitStatus != 0 {
+		err = fmt.Errorf(
+			"gcimagebundle exited with non-zero exit status: %d", cmd.ExitStatus)
+	}
 	if err != nil {
 		err := fmt.Errorf("Error creating image: %s", err)
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return multistep.ActionHalt
 	}
+
 	state.Put("image_file_name", filepath.Join("/tmp", imageFilename))
 	return multistep.ActionContinue
 }
 
-func (s *stepCreateImage) Cleanup(state multistep.StateBag) {}
+func (s *StepCreateImage) Cleanup(state multistep.StateBag) {}
