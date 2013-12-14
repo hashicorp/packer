9e625979b397a9f58c46b0ad5cbbb983e00b38bb
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-13 18:30:57 -0800

    builder/googlecompute: StepUploadImage

--- builder/googlecompute/builder.go
+++ builder/googlecompute/builder.go
@@ -60,9 +60,9 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		new(common.StepProvision),
 		new(StepUpdateGsutil),
 		new(StepCreateImage),
+		new(StepUploadImage),
 	}
 	/*
-		new(stepUploadImage),
 		new(stepRegisterImage),
 	}*/
 
--- builder/googlecompute/step_upload_image.go
+++ builder/googlecompute/step_upload_image.go
@@ -7,34 +7,39 @@ import (
 	"github.com/mitchellh/packer/packer"
 )
 
-// stepUploadImage represents a Packer build step that uploads GCE machine images.
-type stepUploadImage int
+// StepUploadImage represents a Packer build step that uploads GCE machine images.
+type StepUploadImage int
 
 // Run executes the Packer build step that uploads a GCE machine image.
-func (s *stepUploadImage) Run(state multistep.StateBag) multistep.StepAction {
-	var (
-		config        = state.Get("config").(*Config)
-		comm          = state.Get("communicator").(packer.Communicator)
-		sudoPrefix    = ""
-		ui            = state.Get("ui").(packer.Ui)
-		imageFilename = state.Get("image_file_name").(string)
-	)
-	ui.Say("Uploading image...")
+func (s *StepUploadImage) Run(state multistep.StateBag) multistep.StepAction {
+	comm := state.Get("communicator").(packer.Communicator)
+	config := state.Get("config").(*Config)
+	imageFilename := state.Get("image_file_name").(string)
+	ui := state.Get("ui").(packer.Ui)
+
+	sudoPrefix := ""
 	if config.SSHUsername != "root" {
 		sudoPrefix = "sudo "
 	}
+
+	ui.Say("Uploading image...")
 	cmd := new(packer.RemoteCmd)
 	cmd.Command = fmt.Sprintf("%s/usr/local/bin/gsutil cp %s gs://%s",
 		sudoPrefix, imageFilename, config.BucketName)
 	err := cmd.StartWithUi(comm, ui)
+	if err == nil && cmd.ExitStatus != 0 {
+		err = fmt.Errorf(
+			"gsutil exited with non-zero exit status: %d", cmd.ExitStatus)
+	}
 	if err != nil {
 		err := fmt.Errorf("Error uploading image: %s", err)
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return multistep.ActionHalt
 	}
+
 	return multistep.ActionContinue
 }
 
 // Cleanup.
-func (s *stepUploadImage) Cleanup(state multistep.StateBag) {}
+func (s *StepUploadImage) Cleanup(state multistep.StateBag) {}
