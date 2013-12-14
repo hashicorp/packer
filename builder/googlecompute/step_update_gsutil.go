a4d74392c4ac66e55b2fda84b3529851bd2d2e66
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-13 18:19:21 -0800

    builder/googlecompute: update gsutil

--- builder/googlecompute/step_update_gsutil.go
+++ builder/googlecompute/step_update_gsutil.go
@@ -7,9 +7,9 @@ import (
 	"github.com/mitchellh/packer/packer"
 )
 
-// stepUpdateGsutil represents a Packer build step that updates the gsutil
+// StepUpdateGsutil represents a Packer build step that updates the gsutil
 // utility to the latest version available.
-type stepUpdateGsutil int
+type StepUpdateGsutil int
 
 // Run executes the Packer build step that updates the gsutil utility to the
 // latest version available.
@@ -17,29 +17,36 @@ type stepUpdateGsutil int
 // This step is required to prevent the image creation process from hanging;
 // the image creation process utilizes the gcimagebundle cli tool which will
 // prompt to update gsutil if a newer version is available.
-func (s *stepUpdateGsutil) Run(state multistep.StateBag) multistep.StepAction {
-	var (
-		config     = state.Get("config").(*Config)
-		comm       = state.Get("communicator").(packer.Communicator)
-		sudoPrefix = ""
-		ui         = state.Get("ui").(packer.Ui)
-	)
-	ui.Say("Updating gsutil...")
+func (s *StepUpdateGsutil) Run(state multistep.StateBag) multistep.StepAction {
+	comm := state.Get("communicator").(packer.Communicator)
+	config := state.Get("config").(*Config)
+	ui := state.Get("ui").(packer.Ui)
+
+	sudoPrefix := ""
+
 	if config.SSHUsername != "root" {
 		sudoPrefix = "sudo "
 	}
+
 	gsutilUpdateCmd := "/usr/local/bin/gsutil update -n -f"
 	cmd := new(packer.RemoteCmd)
 	cmd.Command = fmt.Sprintf("%s%s", sudoPrefix, gsutilUpdateCmd)
+
+	ui.Say("Updating gsutil...")
 	err := cmd.StartWithUi(comm, ui)
+	if err == nil && cmd.ExitStatus != 0 {
+		err = fmt.Errorf(
+			"gsutil update exited with non-zero exit status: %d", cmd.ExitStatus)
+	}
 	if err != nil {
 		err := fmt.Errorf("Error updating gsutil: %s", err)
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return multistep.ActionHalt
 	}
+
 	return multistep.ActionContinue
 }
 
 // Cleanup.
-func (s *stepUpdateGsutil) Cleanup(state multistep.StateBag) {}
+func (s *StepUpdateGsutil) Cleanup(state multistep.StateBag) {}
