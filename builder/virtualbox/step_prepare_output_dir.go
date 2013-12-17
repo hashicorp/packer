2920239e6e9068ef9b0ee714e4846a66307c47eb
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-16 20:45:05 -0800

    builder/virtualbox: verify output dir is writable

--- builder/virtualbox/step_prepare_output_dir.go
+++ builder/virtualbox/step_prepare_output_dir.go
@@ -1,11 +1,14 @@
 package virtualbox
 
 import (
-	"github.com/mitchellh/multistep"
-	"github.com/mitchellh/packer/packer"
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"time"
+
+	"github.com/mitchellh/multistep"
+	"github.com/mitchellh/packer/packer"
 )
 
 type stepPrepareOutputDir struct{}
@@ -19,10 +22,21 @@ func (stepPrepareOutputDir) Run(state multistep.StateBag) multistep.StepAction {
 		os.RemoveAll(config.OutputDir)
 	}
 
+	// Create the directory
 	if err := os.MkdirAll(config.OutputDir, 0755); err != nil {
 		state.Put("error", err)
 		return multistep.ActionHalt
 	}
+
+	// Make sure we can write in the directory
+	f, err := os.Create(filepath.Join(config.OutputDir, "_packer_perm_check"))
+	if err != nil {
+		err = fmt.Errorf("Couldn't write to output directory: %s", err)
+		state.Put("error", err)
+		return multistep.ActionHalt
+	}
+	f.Close()
+	os.Remove(f.Name())
 
 	return multistep.ActionContinue
 }
