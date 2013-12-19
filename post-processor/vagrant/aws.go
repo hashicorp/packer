15bf787c6f23f96e9827bd2ccc56599cbc8a0cfd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-19 13:41:48 -0800

    post-processor/vagrant: DigitalOcean

--- post-processor/vagrant/aws.go
+++ post-processor/vagrant/aws.go
@@ -3,9 +3,10 @@ package vagrant
 import (
 	"bytes"
 	"fmt"
-	"github.com/mitchellh/packer/packer"
 	"strings"
 	"text/template"
+
+	"github.com/mitchellh/packer/packer"
 )
 
 type AWSProvider struct{}
