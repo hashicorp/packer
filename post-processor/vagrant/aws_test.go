5158ea7c5bf30ecfe83c8ff122ac9f27931ead57
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-19 13:37:09 -0800

    post-processor/vagrant: transition aws over

--- post-processor/vagrant/aws_test.go
+++ post-processor/vagrant/aws_test.go
@@ -1,14 +1,9 @@
 package vagrant
 
 import (
-	"github.com/mitchellh/packer/packer"
 	"testing"
 )
 
-func TestAWSBoxPostProcessor_ImplementsPostProcessor(t *testing.T) {
-	var raw interface{}
-	raw = &AWSBoxPostProcessor{}
-	if _, ok := raw.(packer.PostProcessor); !ok {
-		t.Fatalf("AWS PostProcessor should be a PostProcessor")
-	}
+func TestAWSProvider_impl(t *testing.T) {
+	var _ Provider = new(AWSProvider)
 }
