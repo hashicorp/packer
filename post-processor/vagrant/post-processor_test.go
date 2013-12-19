8d42c2800218aa1e72f5b6ecec00ce8d3379f408
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-19 14:04:45 -0800

    post-processor/vagrant: more tests

--- post-processor/vagrant/post-processor_test.go
+++ post-processor/vagrant/post-processor_test.go
@@ -1,7 +1,10 @@
 package vagrant
 
 import (
+	"bytes"
+	"compress/flate"
 	"github.com/mitchellh/packer/packer"
+	"strings"
 	"testing"
 )
 
@@ -9,11 +12,53 @@ func testConfig() map[string]interface{} {
 	return map[string]interface{}{}
 }
 
+func testPP(t *testing.T) *PostProcessor {
+	var p PostProcessor
+	if err := p.Configure(testConfig()); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	return &p
+}
+
+func testUi() *packer.BasicUi {
+	return &packer.BasicUi{
+		Reader: new(bytes.Buffer),
+		Writer: new(bytes.Buffer),
+	}
+}
+
 func TestPostProcessor_ImplementsPostProcessor(t *testing.T) {
 	var _ packer.PostProcessor = new(PostProcessor)
 }
 
-func TestBuilderPrepare_OutputPath(t *testing.T) {
+func TestPostProcessorPrepare_compressionLevel(t *testing.T) {
+	var p PostProcessor
+
+	// Default
+	c := testConfig()
+	delete(c, "compression_level")
+	if err := p.Configure(c); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if p.config.CompressionLevel != flate.DefaultCompression {
+		t.Fatalf("bad: %#v", p.config.CompressionLevel)
+	}
+
+	// Set
+	c = testConfig()
+	c["compression_level"] = 7
+	if err := p.Configure(c); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if p.config.CompressionLevel != 7 {
+		t.Fatalf("bad: %#v", p.config.CompressionLevel)
+	}
+}
+
+func TestPostProcessorPrepare_outputPath(t *testing.T) {
 	var p PostProcessor
 
 	// Default
@@ -29,6 +74,17 @@ func TestBuilderPrepare_OutputPath(t *testing.T) {
 	err = p.Configure(c)
 	if err == nil {
 		t.Fatal("should have error")
+	}
+}
+
+func TestPostProcessorPostProcess_badId(t *testing.T) {
+	artifact := &packer.MockArtifact{
+		BuilderIdValue: "invalid.packer",
+	}
+
+	_, _, err := testPP(t).PostProcess(testUi(), artifact)
+	if !strings.Contains(err.Error(), "artifact type") {
+		t.Fatalf("err: %s", err)
 	}
 }
 
