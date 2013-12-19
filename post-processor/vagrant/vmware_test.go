a7366e950a3f64a6485970474ade020a8b758d89
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-19 13:44:18 -0800

    post-processor/vagrant: vmware

--- post-processor/vagrant/vmware_test.go
+++ post-processor/vagrant/vmware_test.go
@@ -1,14 +1,9 @@
 package vagrant
 
 import (
-	"github.com/mitchellh/packer/packer"
 	"testing"
 )
 
-func TestVMwareBoxPostProcessor_ImplementsPostProcessor(t *testing.T) {
-	var raw interface{}
-	raw = &VMwareBoxPostProcessor{}
-	if _, ok := raw.(packer.PostProcessor); !ok {
-		t.Fatalf("VMware PostProcessor should be a PostProcessor")
-	}
+func TestVMwareProvider_impl(t *testing.T) {
+	var _ Provider = new(VMwareProvider)
 }
