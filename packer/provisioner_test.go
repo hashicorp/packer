2264f7d8a2bdf88672d62b1ffb12c65eff1b2cf7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-20 21:36:41 -0800

    packer: PausedProvisioner

--- packer/provisioner_test.go
+++ packer/provisioner_test.go
@@ -80,3 +80,94 @@ func TestProvisionHook_cancel(t *testing.T) {
 }
 
 // TODO(mitchellh): Test that they're run in the proper order
+
+func TestPausedProvisioner_impl(t *testing.T) {
+	var _ Provisioner = new(PausedProvisioner)
+}
+
+func TestPausedProvisionerPrepare(t *testing.T) {
+	mock := new(MockProvisioner)
+	prov := &PausedProvisioner{
+		Provisioner: mock,
+	}
+
+	prov.Prepare(42)
+	if !mock.PrepCalled {
+		t.Fatal("prepare should be called")
+	}
+	if mock.PrepConfigs[0] != 42 {
+		t.Fatal("should have proper configs")
+	}
+}
+
+func TestPausedProvisionerProvision(t *testing.T) {
+	mock := new(MockProvisioner)
+	prov := &PausedProvisioner{
+		Provisioner: mock,
+	}
+
+	ui := testUi()
+	comm := new(MockCommunicator)
+	prov.Provision(ui, comm)
+	if !mock.ProvCalled {
+		t.Fatal("prov should be called")
+	}
+	if mock.ProvUi != ui {
+		t.Fatal("should have proper ui")
+	}
+	if mock.ProvCommunicator != comm {
+		t.Fatal("should have proper comm")
+	}
+}
+
+func TestPausedProvisionerProvision_waits(t *testing.T) {
+	mock := new(MockProvisioner)
+	prov := &PausedProvisioner{
+		PauseBefore: 50 * time.Millisecond,
+		Provisioner: mock,
+	}
+
+	dataCh := make(chan struct{})
+	mock.ProvFunc = func() error {
+		close(dataCh)
+		return nil
+	}
+
+	go prov.Provision(testUi(), new(MockCommunicator))
+
+	select {
+	case <-time.After(10 * time.Millisecond):
+	case <-dataCh:
+		t.Fatal("should not be called")
+	}
+
+	select {
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("never called")
+	case <-dataCh:
+	}
+}
+
+func TestPausedProvisionerCancel(t *testing.T) {
+	mock := new(MockProvisioner)
+	prov := &PausedProvisioner{
+		Provisioner: mock,
+	}
+
+	provCh := make(chan struct{})
+	mock.ProvFunc = func() error {
+		close(provCh)
+		time.Sleep(10 * time.Millisecond)
+		return nil
+	}
+
+	// Start provisioning and wait for it to start
+	go prov.Provision(testUi(), new(MockCommunicator))
+	<-provCh
+
+	// Cancel it
+	prov.Cancel()
+	if !mock.CancelCalled {
+		t.Fatal("cancel should be called")
+	}
+}
