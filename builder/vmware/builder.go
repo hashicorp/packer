5ab83238bf55033aa4df3c5328052b371d10026f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-23 14:38:54 -0800

    builder/vmware: separate step to configure vmx

--- builder/vmware/builder.go
+++ builder/vmware/builder.go
@@ -405,6 +405,9 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		},
 		&stepCreateDisk{},
 		&stepCreateVMX{},
+		&StepConfigureVMX{
+			CustomData: b.config.VMXData,
+		},
 		&stepSuppressMessages{},
 		&stepHTTPServer{},
 		&stepConfigureVNC{},
