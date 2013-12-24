6cf8d9b3194b3c2a80dcf2585f772f83d430da64
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-24 14:26:44 -0700

    builder/vmware/common: StepSuppressMessages

--- builder/vmware/common/step_test.go
+++ builder/vmware/common/step_test.go
@@ -9,6 +9,7 @@ import (
 
 func testState(t *testing.T) multistep.StateBag {
 	state := new(multistep.BasicStateBag)
+	state.Put("driver", new(DriverMock))
 	state.Put("ui", &packer.BasicUi{
 		Reader: new(bytes.Buffer),
 		Writer: new(bytes.Buffer),
