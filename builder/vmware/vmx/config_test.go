141cfeb4bbadfcca4d926be363db8d7ff591837e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-25 16:01:57 -0700

    builder/vmware/vmx: outputdir

---
+++ builder/vmware/vmx/config_test.go
@@ -0,0 +1,27 @@
+package vmx
+
+import (
+	"testing"
+)
+
+func testConfig(t *testing.T) map[string]interface{} {
+	return map[string]interface{}{}
+}
+
+func testConfigErr(t *testing.T, warns []string, err error) {
+	if len(warns) > 0 {
+		t.Fatalf("bad: %#v", warns)
+	}
+	if err == nil {
+		t.Fatal("should error")
+	}
+}
+
+func testConfigOk(t *testing.T, warns []string, err error) {
+	if len(warns) > 0 {
+		t.Fatalf("bad: %#v", warns)
+	}
+	if err != nil {
+		t.Fatalf("bad: %s", err)
+	}
+}
