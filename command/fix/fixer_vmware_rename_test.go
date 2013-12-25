91a088d8d6a257bb31f3af5c142ea18b44c6d190
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-12-25 11:13:32 -0700

    command/fix: rename vmware to vmware-iso

---
+++ command/fix/fixer_vmware_rename_test.go
@@ -0,0 +1,49 @@
+package fix
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFixerVMwareRename_impl(t *testing.T) {
+	var _ Fixer = new(FixerVMwareRename)
+}
+
+func TestFixerVMwareRename_Fix(t *testing.T) {
+	cases := []struct {
+		Input    map[string]interface{}
+		Expected map[string]interface{}
+	}{
+		// No attach field
+		{
+			Input: map[string]interface{}{
+				"type": "vmware",
+			},
+
+			Expected: map[string]interface{}{
+				"type": "vmware-iso",
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		var f FixerVMwareRename
+
+		input := map[string]interface{}{
+			"builders": []map[string]interface{}{tc.Input},
+		}
+
+		expected := map[string]interface{}{
+			"builders": []map[string]interface{}{tc.Expected},
+		}
+
+		output, err := f.Fix(input)
+		if err != nil {
+			t.Fatalf("err: %s", err)
+		}
+
+		if !reflect.DeepEqual(output, expected) {
+			t.Fatalf("unexpected: %#v\nexpected: %#v\n", output, expected)
+		}
+	}
+}
