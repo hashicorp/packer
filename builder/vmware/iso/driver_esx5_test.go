8369fa956e9eb7f06eaaed39c43e1538541e2efe
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-01-01 20:55:08 -0800

    builder/vmware-iso: ESX5Driver impl OutputDir [GH-773]

--- builder/vmware/iso/driver_esx5_test.go
+++ builder/vmware/iso/driver_esx5_test.go
@@ -9,6 +9,10 @@ func TestESX5Driver_implDriver(t *testing.T) {
 	var _ vmwcommon.Driver = new(ESX5Driver)
 }
 
+func TestESX5Driver_implOutputDir(t *testing.T) {
+	var _ vmwcommon.OutputDir = new(ESX5Driver)
+}
+
 func TestESX5Driver_implRemoteDriver(t *testing.T) {
 	var _ RemoteDriver = new(ESX5Driver)
 }
