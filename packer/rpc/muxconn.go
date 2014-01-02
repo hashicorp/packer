ac053eb516f1b86cdb7cbc33df37e51f453b6efd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-01-01 22:20:03 -0800

    packer/rpc: write only the remaining data on muxconn retry

--- packer/rpc/muxconn.go
+++ packer/rpc/muxconn.go
@@ -445,7 +445,7 @@ func (m *MuxConn) write(from muxPacketFrom, id uint32, dataType muxPacketType, p
 	n := 0
 	for n < len(p) {
 		var n2 int
-		n2, err = m.rwc.Write(p)
+		n2, err = m.rwc.Write(p[n:])
 		n += n2
 		if err != nil {
 			log.Printf("[ERR] %p: Stream %d (%s) write error: %s", m, id, from, err)
