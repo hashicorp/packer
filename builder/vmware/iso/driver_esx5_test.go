e372a6d3b64b78df824da41ebaf07e5f9a2155a2
Paul Myjavec <pauly@buzbox.net>
2014-02-13 21:53:47 +1100

    esx5, Get host IP by creating & inspecting connection, allows hypervisor to reside in other networks

--- builder/vmware/iso/driver_esx5_test.go
+++ builder/vmware/iso/driver_esx5_test.go
@@ -1,7 +1,9 @@
 package iso
 
 import (
+	"fmt"
 	vmwcommon "github.com/mitchellh/packer/builder/vmware/common"
+	"net"
 	"testing"
 )
 
@@ -15,4 +17,19 @@ func TestESX5Driver_implOutputDir(t *testing.T) {
 
 func TestESX5Driver_implRemoteDriver(t *testing.T) {
 	var _ RemoteDriver = new(ESX5Driver)
+}
+
+func TestESX5Driver_HostIP(t *testing.T) {
+        expected_host := "127.0.0.1"
+
+        //create mock SSH server
+	listen, _ := net.Listen("tcp", fmt.Sprintf("%s:0", expected_host))
+	port := listen.Addr().(*net.TCPAddr).Port
+	defer listen.Close()
+
+	driver := ESX5Driver{Host: "localhost", Port: uint(port)}
+
+	if host, _ := driver.HostIP(); host != expected_host {
+		t.Error(fmt.Sprintf("Expected string, %s but got %s", expected_host, host))
+	}
 }
