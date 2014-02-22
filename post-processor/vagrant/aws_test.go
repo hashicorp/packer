42b6f3e1ead2d29c65b79ec4e9d3977a212e0d87
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-02-21 20:04:03 -0800

    post-processor/vagrant: tests on input artifact

--- post-processor/vagrant/aws_test.go
+++ post-processor/vagrant/aws_test.go
@@ -7,3 +7,11 @@ import (
 func TestAWSProvider_impl(t *testing.T) {
 	var _ Provider = new(AWSProvider)
 }
+
+func TestAWSProvider_KeepInputArtifact(t *testing.T) {
+	p := new(AWSProvider)
+
+	if !p.KeepInputArtifact() {
+		t.Fatal("should keep input artifact")
+	}
+}
