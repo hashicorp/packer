bc1f2a6f0e082c2a17ddc4e2f2a9fe217b2b4104
Eric Richardson <erichardson@emcien.com>
2014-03-04 13:23:07 -0500

    Initial work to implement additional disk support in the vmware-iso builder
    * Matches the syntax from mitchellh/packer#703
    * Creates disk(s), adds them to the vmx template, and runs compact at the end

--- builder/vmware/iso/step_create_vmx.go
+++ builder/vmware/iso/step_create_vmx.go
@@ -20,6 +20,11 @@ type vmxTemplateData struct {
 	Version  string
 }
 
+type additionalDiskTemplateData struct {
+	DiskNumber	int
+	DiskName	string
+}
+
 // This step creates the VMX file for the VM.
 //
 // Uses:
@@ -69,6 +74,25 @@ func (s *stepCreateVMX) Run(state multistep.StateBag) multistep.StepAction {
 		}
 
 		vmxTemplate = string(rawBytes)
+	}
+	
+	if len(config.AdditionalDiskSize) > 0 {
+		for i, _ := range config.AdditionalDiskSize {
+			data := &additionalDiskTemplateData{
+				DiskNumber: 	i+1,
+				DiskName: 		config.DiskName,
+			}
+			
+			diskTemplate, err := config.tpl.Process(DefaultAdditionalDiskTemplate,data)
+			if err != nil {
+				err := fmt.Errorf("Error preparing VMX template for additional disk: %s", err)
+				state.Put("error", err)
+				ui.Error(err.Error())
+				return multistep.ActionHalt
+			}
+			
+			vmxTemplate += diskTemplate
+		}
 	}
 
 	vmxContents, err := interpolate.Render(vmxTemplate, &ctx)
@@ -190,4 +214,10 @@ vmci0.id = "1861462627"
 vmci0.pciSlotNumber = "35"
 vmci0.present = "TRUE"
 vmotion.checkpointFBSize = "65536000"
+`
+
+const DefaultAdditionalDiskTemplate = `
+scsi0:{{ .DiskNumber }}.fileName = "{{ .DiskName}}-{{ .DiskNumber }}.vmdk"
+scsi0:{{ .DiskNumber }}.present = "TRUE"
+scsi0:{{ .DiskNumber }}.redo = ""
 `
