0417ae3059ca3d106a0c0d319d2f11b539dd2c3a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-04-30 15:43:11 -0700

    builder/amazon: alphabetize some things

--- builder/amazon/common/block_device.go
+++ builder/amazon/common/block_device.go
@@ -6,14 +6,14 @@ import (
 
 // BlockDevice
 type BlockDevice struct {
-	DeviceName          string `mapstructure:"device_name"`
-	VirtualName         string `mapstructure:"virtual_name"`
-	SnapshotId          string `mapstructure:"snapshot_id"`
-	VolumeType          string `mapstructure:"volume_type"`
-	VolumeSize          int64  `mapstructure:"volume_size"`
 	DeleteOnTermination bool   `mapstructure:"delete_on_termination"`
+	DeviceName          string `mapstructure:"device_name"`
 	IOPS                int64  `mapstructure:"iops"`
 	NoDevice            bool   `mapstructure:"no_device"`
+	SnapshotId          string `mapstructure:"snapshot_id"`
+	VirtualName         string `mapstructure:"virtual_name"`
+	VolumeType          string `mapstructure:"volume_type"`
+	VolumeSize          int64  `mapstructure:"volume_size"`
 }
 
 type BlockDevices struct {
