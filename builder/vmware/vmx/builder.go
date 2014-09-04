c991ffddc613b5e8ae77ab332d5f51b4c6babe0d
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-09-03 21:27:54 -0700

    fmt

--- builder/vmware/vmx/builder.go
+++ builder/vmware/vmx/builder.go
@@ -96,7 +96,7 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		&vmwcommon.StepCleanFiles{},
 		&vmwcommon.StepConfigureVMX{
 			CustomData: b.config.VMXDataPost,
-			SkipFloppy:  true,
+			SkipFloppy: true,
 		},
 		&vmwcommon.StepCleanVMX{},
 		&vmwcommon.StepCompactDisk{
