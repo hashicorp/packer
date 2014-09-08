3c6d31952d8aefaea8f6be411ac53e0827ca2cdb
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-09-08 15:25:50 -0700

    packer: check for latest version

--- checkpoint.go
+++ checkpoint.go
@@ -9,18 +9,27 @@ import (
 	"github.com/mitchellh/packer/packer"
 )
 
+func init() {
+	packer.VersionChecker = packerVersionCheck
+	checkpointResult = make(chan *checkpoint.CheckResponse, 1)
+}
+
+var checkpointResult chan *checkpoint.CheckResponse
+
 // runCheckpoint runs a HashiCorp Checkpoint request. You can read about
 // Checkpoint here: https://github.com/hashicorp/go-checkpoint.
 func runCheckpoint(c *config) {
 	// If the user doesn't want checkpoint at all, then return.
 	if c.DisableCheckpoint {
 		log.Printf("[INFO] Checkpoint disabled. Not running.")
+		checkpointResult <- nil
 		return
 	}
 
 	configDir, err := ConfigDir()
 	if err != nil {
 		log.Printf("[ERR] Checkpoint setup error: %s", err)
+		checkpointResult <- nil
 		return
 	}
 
@@ -35,7 +44,7 @@ func runCheckpoint(c *config) {
 		signaturePath = ""
 	}
 
-	_, err = checkpoint.Check(&checkpoint.CheckParams{
+	resp, err := checkpoint.Check(&checkpoint.CheckParams{
 		Product:       "packer",
 		Version:       version,
 		SignatureFile: signaturePath,
@@ -43,5 +52,29 @@ func runCheckpoint(c *config) {
 	})
 	if err != nil {
 		log.Printf("[ERR] Checkpoint error: %s", err)
+		resp = nil
 	}
+
+	checkpointResult <- resp
+}
+
+// packerVersionCheck implements packer.VersionCheckFunc and is used
+// as the version checker.
+func packerVersionCheck(current string) (packer.VersionCheckInfo, error) {
+	info := <-checkpointResult
+	if info == nil {
+		var zero packer.VersionCheckInfo
+		return zero, nil
+	}
+
+	alerts := make([]string, len(info.Alerts))
+	for i, a := range info.Alerts {
+		alerts[i] = a.Message
+	}
+
+	return packer.VersionCheckInfo{
+		Outdated: info.Outdated,
+		Latest:   info.CurrentVersion,
+		Alerts:   alerts,
+	}, nil
 }
