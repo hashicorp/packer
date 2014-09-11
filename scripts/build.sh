5499d46256538d88b1990524b147783acd8ccf9f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-09-11 11:16:59 -0700

    scripts: build should do everything now

--- scripts/build.sh
+++ scripts/build.sh
@@ -47,10 +47,8 @@ gox \
 # Make sure "packer-packer" is renamed properly
 for PLATFORM in $(find ./pkg -mindepth 1 -maxdepth 1 -type d); do
     set +e
-    # The asterisk allows for the packer directory to be a symlink
-    # (to a directory that must begin with the word 'packer')
-    mv ${PLATFORM}/packer-packer*.exe ${PLATFORM}/packer.exe 2>/dev/null
-    mv ${PLATFORM}/packer-packer* ${PLATFORM}/packer 2>/dev/null
+    mv ${PLATFORM}/packer-packer.exe ${PLATFORM}/packer.exe 2>/dev/null
+    mv ${PLATFORM}/packer-packer ${PLATFORM}/packer 2>/dev/null
     set -e
 done
 
@@ -76,14 +74,22 @@ done
 if [ "${TF_DEV}x" = "x" ]; then
     # Zip and copy to the dist dir
     echo "==> Packaging..."
+    rm -rf ./pkg/dist
+    mkdir -p ./pkg/dist
     for PLATFORM in $(find ./pkg -mindepth 1 -maxdepth 1 -type d); do
         OSARCH=$(basename ${PLATFORM})
         echo "--> ${OSARCH}"
 
         pushd $PLATFORM >/dev/null 2>&1
-        zip ../${OSARCH}.zip ./*
+        zip ../dist/packer_${OSARCH}.zip ./*
         popd >/dev/null 2>&1
     done
+
+    # Make the checksums
+    echo "==> Checksumming..."
+    pushd ./pkg/dist >/dev/null 2>&1
+    shasum -a256 * > ./packer_${VERSION}_SHA256SUMS
+    popd >/dev/null 2>&1
 fi
 
 # Done!
