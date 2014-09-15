74bd11b69ef62f06bb778b656efb37e56096d2cd
JessThrysoee <jess@thrysoee.dk>
2014-09-15 23:20:41 +0200

    builder/parallels: Rely on Cleanup functions to detach devices
    Remove the redundant StepRemoveDevices and rely on the cleanup to be handled by:
    * StepAttachParallelsTools.Cleanup
    * StepAttachFloppy.Cleanup
    * stepAttachISO.Cleanup

--- builder/parallels/iso/builder.go
+++ builder/parallels/iso/builder.go
@@ -294,7 +294,6 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 			Command: b.config.ShutdownCommand,
 			Timeout: b.config.ShutdownTimeout,
 		},
-		new(parallelscommon.StepRemoveDevices),
 	}
 
 	// Setup the state bag
