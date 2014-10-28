891e7898a42af20210a86e96d8e5fd7315f1c7cf
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-10-28 10:32:58 -0700

    website:update version

--- website/config.rb
+++ website/config.rb
@@ -5,7 +5,7 @@
 set :base_url, "https://www.packer.io/"
 
 activate :hashicorp do |h|
-  h.version      = '0.7.1'
+  h.version      = ENV["PACKER_VERSION"]
   h.bintray_repo = 'mitchellh/packer'
   h.bintray_user = 'mitchellh'
   h.bintray_key  = ENV['BINTRAY_API_KEY']
