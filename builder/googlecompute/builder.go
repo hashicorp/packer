b9ba9ec158b828060c8479e38e307e8141d26556
Evan Brown <evanbrown@google.com>
2014-11-17 10:06:22 -0800

    Use golang/oauth2, no longer require client_secrets.json, and use Service Account when run from a GCE Instance.

--- builder/googlecompute/builder.go
+++ builder/googlecompute/builder.go
@@ -35,7 +35,7 @@ func (b *Builder) Prepare(raws ...interface{}) ([]string, error) {
 // representing a GCE machine image.
 func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packer.Artifact, error) {
 	driver, err := NewDriverGCE(
-		ui, b.config.ProjectId, &b.config.account, &b.config.clientSecrets)
+		ui, b.config.ProjectId, &b.config.account)
 	if err != nil {
 		return nil, err
 	}
