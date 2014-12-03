5c8e77e0fe7ad1f3a8d2c66e8a1f3c2c70f20822
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2014-12-02 18:22:26 -0800

    builder/amazon: inject special metadata for atlas artifacts

--- builder/amazon/common/artifact_test.go
+++ builder/amazon/common/artifact_test.go
@@ -1,8 +1,10 @@
 package common
 
 import (
-	"github.com/mitchellh/packer/packer"
+	"reflect"
 	"testing"
+
+	"github.com/mitchellh/packer/packer"
 )
 
 func TestArtifact_Impl(t *testing.T) {
@@ -23,6 +25,24 @@ func TestArtifactId(t *testing.T) {
 	result := a.Id()
 	if result != expected {
 		t.Fatalf("bad: %s", result)
+	}
+}
+
+func TestArtifactState_atlasMetadata(t *testing.T) {
+	a := &Artifact{
+		Amis: map[string]string{
+			"east": "foo",
+			"west": "bar",
+		},
+	}
+
+	actual := a.State("atlas.artifact.metadata")
+	expected := map[string]string{
+		"region.east": "foo",
+		"region.west": "bar",
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("bad: %#v", actual)
 	}
 }
 
