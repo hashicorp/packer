bde4d9dbe468249b483e72d3783ca345efa612d8
Colin Hebert <chebert@atlassian.com>
2014-12-10 12:20:53 +1100

    Add dry-run to VirtualBox

--- builder/virtualbox/ovf/builder.go
+++ builder/virtualbox/ovf/builder.go
@@ -122,11 +122,16 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 			Commands: b.config.VBoxManagePost,
 			Tpl:      b.config.tpl,
 		},
-		&vboxcommon.StepExport{
-			Format:     b.config.Format,
-			OutputDir:  b.config.OutputDir,
-			ExportOpts: b.config.ExportOpts.ExportOpts,
-		},
+	}
+
+	if !b.config.PackerDryRun {
+		steps = append(steps,
+			&vboxcommon.StepExport{
+				Format:     b.config.Format,
+				OutputDir:  b.config.OutputDir,
+				ExportOpts: b.config.ExportOpts.ExportOpts,
+			}
+		)
 	}
 
 	// Run the steps.
