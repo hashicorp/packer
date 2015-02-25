e5cc8b150e5997e110912397047263310b2437d3
Aaron Walker <aaronpwalker@gmail.com>
2015-02-25 14:48:03 +1100

    go fmt :)

--- plugin/provisioner-shell-local/main.go
+++ plugin/provisioner-shell-local/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-  "github.com/mitchellh/packer/packer/plugin"
-  "github.com/mitchellh/packer/provisioner/shell-local"
+	"github.com/mitchellh/packer/packer/plugin"
+	"github.com/mitchellh/packer/provisioner/shell-local"
 )
 
 func main() {
-  server, err := plugin.Server()
-  if err != nil {
-    panic(err)
-  }
-  server.RegisterProvisioner(new(shelllocal.Provisioner))
-  server.Serve()
+	server, err := plugin.Server()
+	if err != nil {
+		panic(err)
+	}
+	server.RegisterProvisioner(new(shelllocal.Provisioner))
+	server.Serve()
 }
--- provisioner/shell-local/provisioner_test.go
+++ provisioner/shell-local/provisioner_test.go
@@ -1,104 +1,103 @@
-
 package shelllocal
 
 import (
-  "github.com/mitchellh/packer/packer"
-  "runtime"
-  "strings"
-  "testing"
+	"github.com/mitchellh/packer/packer"
+	"runtime"
+	"strings"
+	"testing"
 )
 
 func testConfig() map[string]interface{} {
-  return map[string]interface{}{
-    "inline": []interface{}{"foo", "bar"},
-  }
+	return map[string]interface{}{
+		"inline": []interface{}{"foo", "bar"},
+	}
 }
 
 func TestProvisioner_Impl(t *testing.T) {
-  var raw interface{}
-  raw = &Provisioner{}
-  if _, ok := raw.(packer.Provisioner); !ok {
-    t.Fatalf("must be a Provisioner")
-  }
+	var raw interface{}
+	raw = &Provisioner{}
+	if _, ok := raw.(packer.Provisioner); !ok {
+		t.Fatalf("must be a Provisioner")
+	}
 }
 
 func TestProvisionerPrepare_InvalidKey(t *testing.T) {
-  var p Provisioner
-  config := testConfig()
+	var p Provisioner
+	config := testConfig()
 
-  // Add a random key
-  config["i_should_not_be_valid"] = true
-  err := p.Prepare(config)
-  if err == nil {
-    t.Fatal("should have error")
-  }
+	// Add a random key
+	config["i_should_not_be_valid"] = true
+	err := p.Prepare(config)
+	if err == nil {
+		t.Fatal("should have error")
+	}
 }
 
 func TestProvisionerPrepare_Defaults(t *testing.T) {
-  var p Provisioner
-  config := testConfig()
+	var p Provisioner
+	config := testConfig()
 
-  err := p.Prepare(config)
-  if err != nil {
-    t.Fatalf("err: %s", err)
-  }
+	err := p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
 
-  if p.config.InlineShebang != DefaultSheBang {
-    t.Errorf("unexpected inline shebang: %s", p.config.InlineShebang)
-  }
+	if p.config.InlineShebang != DefaultSheBang {
+		t.Errorf("unexpected inline shebang: %s", p.config.InlineShebang)
+	}
 }
 
 func TestProvisionerPrepare_InlineShebang(t *testing.T) {
-  config := testConfig()
+	config := testConfig()
 
-  delete(config, "inline_shebang")
-  p := new(Provisioner)
-  err := p.Prepare(config)
-  if err != nil {
-    t.Fatalf("should not have error: %s", err)
-  }
+	delete(config, "inline_shebang")
+	p := new(Provisioner)
+	err := p.Prepare(config)
+	if err != nil {
+		t.Fatalf("should not have error: %s", err)
+	}
 
-  if p.config.InlineShebang != "/bin/sh" {
-    t.Fatalf("bad value: %s", p.config.InlineShebang)
-  }
+	if p.config.InlineShebang != "/bin/sh" {
+		t.Fatalf("bad value: %s", p.config.InlineShebang)
+	}
 
-  // Test with a good one
-  config["inline_shebang"] = "foo"
-  p = new(Provisioner)
-  err = p.Prepare(config)
-  if err != nil {
-    t.Fatalf("should not have error: %s", err)
-  }
+	// Test with a good one
+	config["inline_shebang"] = "foo"
+	p = new(Provisioner)
+	err = p.Prepare(config)
+	if err != nil {
+		t.Fatalf("should not have error: %s", err)
+	}
 
-  if p.config.InlineShebang != "foo" {
-    t.Fatalf("bad value: %s", p.config.InlineShebang)
-  }
+	if p.config.InlineShebang != "foo" {
+		t.Fatalf("bad value: %s", p.config.InlineShebang)
+	}
 }
 
 func TestProvisionerPrepare_Inline(t *testing.T) {
-  var p Provisioner
-  config := testConfig()
+	var p Provisioner
+	config := testConfig()
 
-  delete(config, "inline")
-  err := p.Prepare(config)
-  if err == nil {
-    t.Fatal("should have error")
-  }
+	delete(config, "inline")
+	err := p.Prepare(config)
+	if err == nil {
+		t.Fatal("should have error")
+	}
 
-  // Test with a good one
-  config["inline"] = []interface{}{"foo"}
-  err = p.Prepare(config)
-  if err != nil {
-    t.Fatal("should not have error %s", err)
-  }
+	// Test with a good one
+	config["inline"] = []interface{}{"foo"}
+	err = p.Prepare(config)
+	if err != nil {
+		t.Fatal("should not have error %s", err)
+	}
 }
 
 type stubUi struct {
-  sayMessages string
+	sayMessages string
 }
 
 func (su *stubUi) Ask(string) (string, error) {
-  return "", nil
+	return "", nil
 }
 
 func (su *stubUi) Error(string) {
@@ -111,37 +110,37 @@ func (su *stubUi) Message(string) {
 }
 
 func (su *stubUi) Say(msg string) {
-  su.sayMessages += msg
+	su.sayMessages += msg
 }
 
 func TestProvisionerProvision_Inline(t *testing.T) {
-  var p Provisioner
-  config := testConfig()
+	var p Provisioner
+	config := testConfig()
 
-  config["inline"] = []interface{}{"echo \"Hello World\""}
-  err := p.Prepare(config)
-  if err != nil {
-    t.Fatal("should not have error %s", err)
-  }
+	config["inline"] = []interface{}{"echo \"Hello World\""}
+	err := p.Prepare(config)
+	if err != nil {
+		t.Fatal("should not have error %s", err)
+	}
 
-  if runtime.GOOS == "windows" {
-    //the rest of this test only runs on non-windows systems
-    return
-  }
+	if runtime.GOOS == "windows" {
+		//the rest of this test only runs on non-windows systems
+		return
+	}
 
-  ui := &stubUi{}
-  comm := &packer.MockCommunicator{}
-  err = p.Provision(ui, comm)
-  if err != nil {
-    t.Fatalf("should successfully provision: %s", err)
-  }
+	ui := &stubUi{}
+	comm := &packer.MockCommunicator{}
+	err = p.Provision(ui, comm)
+	if err != nil {
+		t.Fatalf("should successfully provision: %s", err)
+	}
 
-  if !strings.Contains(ui.sayMessages, "Provisioning with local shell script:") {
-    t.Fatalf("should print Provisioning with local shell script")
-  }
+	if !strings.Contains(ui.sayMessages, "Provisioning with local shell script:") {
+		t.Fatalf("should print Provisioning with local shell script")
+	}
 
-  if !strings.Contains(ui.sayMessages, "Hello World") {
-    t.Fatalf("should print Hello")
-  }
+	if !strings.Contains(ui.sayMessages, "Hello World") {
+		t.Fatalf("should print Hello")
+	}
 
 }
