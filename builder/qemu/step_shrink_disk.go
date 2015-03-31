45ad7c64f3fa81c7058b53f8c9afae52ba779f5f
Ville Korhonen <ville@xd.fi>
2015-03-31 22:14:30 +0300

    Fix path to source image.

--- builder/qemu/step_shrink_disk.go
+++ builder/qemu/step_shrink_disk.go
@@ -18,9 +18,10 @@ type stepShrinkDisk struct{}
 func (s *stepShrinkDisk) Run(state multistep.StateBag) multistep.StepAction {
 	config := state.Get("config").(*config)
 	driver := state.Get("driver").(Driver)
-	sourcePath := state.Get("disk_filename").(string)
+	diskName := state.Get("disk_filename").(string)
 	ui := state.Get("ui").(packer.Ui)
 	name := config.VMName + ".shrink." + strings.ToLower(config.Format)
+	sourcePath := filepath.Join(config.OutputDir, diskName)
 	targetPath := filepath.Join(config.OutputDir, name)
 
 	command := []string{
