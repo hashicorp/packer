5e34a8407187b0461e00c442d87ffdb56d4bf23f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-05-15 21:05:47 -0700

    template/interpolate: basic + some funcs

---
+++ template/interpolate/funcs_test.go
@@ -0,0 +1,66 @@
+package interpolate
+
+import (
+	"os"
+	"testing"
+)
+
+func TestFuncEnv(t *testing.T) {
+	cases := []struct {
+		Input  string
+		Output string
+	}{
+		{
+			`{{env "PACKER_TEST_ENV"}}`,
+			`foo`,
+		},
+
+		{
+			`{{env "PACKER_TEST_ENV_NOPE"}}`,
+			``,
+		},
+	}
+
+	os.Setenv("PACKER_TEST_ENV", "foo")
+	defer os.Setenv("PACKER_TEST_ENV", "")
+
+	ctx := &Context{}
+	for _, tc := range cases {
+		i := &I{Value: tc.Input}
+		result, err := i.Render(ctx)
+		if err != nil {
+			t.Fatalf("Input: %s\n\nerr: %s", tc.Input, err)
+		}
+
+		if result != tc.Output {
+			t.Fatalf("Input: %s\n\nGot: %s", tc.Input, result)
+		}
+	}
+}
+
+func TestFuncEnv_disable(t *testing.T) {
+	cases := []struct {
+		Input  string
+		Output string
+		Error  bool
+	}{
+		{
+			`{{env "PACKER_TEST_ENV"}}`,
+			"",
+			true,
+		},
+	}
+
+	ctx := &Context{DisableEnv: true}
+	for _, tc := range cases {
+		i := &I{Value: tc.Input}
+		result, err := i.Render(ctx)
+		if (err != nil) != tc.Error {
+			t.Fatalf("Input: %s\n\nerr: %s", tc.Input, err)
+		}
+
+		if result != tc.Output {
+			t.Fatalf("Input: %s\n\nGot: %s", tc.Input, result)
+		}
+	}
+}
