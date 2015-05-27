cbe6e83b60f56841b5286357ce9bb144e9a2c073
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-05-27 14:27:11 -0700

    post-processor/docker-*: fix compilation errors

--- post-processor/docker-tag/post-processor.go
+++ post-processor/docker-tag/post-processor.go
@@ -2,10 +2,13 @@ package dockertag
 
 import (
 	"fmt"
+
 	"github.com/mitchellh/packer/builder/docker"
 	"github.com/mitchellh/packer/common"
+	"github.com/mitchellh/packer/helper/config"
 	"github.com/mitchellh/packer/packer"
 	"github.com/mitchellh/packer/post-processor/docker-import"
+	"github.com/mitchellh/packer/template/interpolate"
 )
 
 const BuilderId = "packer.post-processor.docker-tag"
@@ -16,7 +19,7 @@ type Config struct {
 	Repository string `mapstructure:"repository"`
 	Tag        string `mapstructure:"tag"`
 
-	tpl *packer.ConfigTemplate
+	ctx interpolate.Context
 }
 
 type PostProcessor struct {
@@ -26,40 +29,14 @@ type PostProcessor struct {
 }
 
 func (p *PostProcessor) Configure(raws ...interface{}) error {
-	_, err := common.DecodeConfig(&p.config, raws...)
+	err := config.Decode(&p.config, &config.DecodeOpts{
+		Interpolate: true,
+		InterpolateFilter: &interpolate.RenderFilter{
+			Exclude: []string{},
+		},
+	}, raws...)
 	if err != nil {
 		return err
-	}
-
-	p.config.tpl, err = packer.NewConfigTemplate()
-	if err != nil {
-		return err
-	}
-	p.config.tpl.UserVars = p.config.PackerUserVars
-
-	// Accumulate any errors
-	errs := new(packer.MultiError)
-
-	templates := map[string]*string{
-		"repository": &p.config.Repository,
-		"tag":        &p.config.Tag,
-	}
-
-	for key, ptr := range templates {
-		if *ptr == "" {
-			errs = packer.MultiErrorAppend(
-				errs, fmt.Errorf("%s must be set", key))
-		}
-
-		*ptr, err = p.config.tpl.Process(*ptr, nil)
-		if err != nil {
-			errs = packer.MultiErrorAppend(
-				errs, fmt.Errorf("Error processing %s: %s", key, err))
-		}
-	}
-
-	if len(errs.Errors) > 0 {
-		return errs
 	}
 
 	return nil
@@ -77,7 +54,7 @@ func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (pac
 	driver := p.Driver
 	if driver == nil {
 		// If no driver is set, then we use the real driver
-		driver = &docker.DockerDriver{Tpl: p.config.tpl, Ui: ui}
+		driver = &docker.DockerDriver{Ctx: &p.config.ctx, Ui: ui}
 	}
 
 	importRepo := p.config.Repository
