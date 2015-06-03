33ca8b7fb54be30cf7c6c8610aad34ed79fdb322
Seth Vargo <sethvargo@gmail.com>
2015-06-03 17:13:52 -0400

    Migrate to new AWS repo

--- builder/amazon/chroot/step_instance_info.go
+++ builder/amazon/chroot/step_instance_info.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/awslabs/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/mitchellh/multistep"
 	"github.com/mitchellh/packer/builder/amazon/common"
 	"github.com/mitchellh/packer/packer"
