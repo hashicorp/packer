fe0c548619b2288c3a196369ed0b27ff21eb27e0
Chris Bednarski <banzaimonkey@gmail.com>
2015-06-16 11:30:49 -0700

    Added acceptance test for file builder

--- post-processor/compress/post-processor_test.go
+++ post-processor/compress/post-processor_test.go
@@ -1,3 +1,22 @@
 package compress
 
-import ()
+// import (
+// 	"testing"
+//
+// 	builderT "github.com/mitchellh/packer/helper/builder/testing"
+// )
+//
+// func TestBuilderTagsAcc_basic(t *testing.T) {
+// 	builderT.Test(t, builderT.TestCase{
+// 		Builder:  &Builder{},
+// 		Template: simpleTestCase,
+// 		Check:    checkTags(),
+// 	})
+// }
+
+const simpleTestCase = `
+{
+  "type": "compress",
+  "output": "foo.tar.gz"
+}
+`
