8bb91bcce705c26f9b69601da12d050894ce6b21
Michele Catalano <michele.catalano@mayflower.de>
2015-06-16 19:53:38 +0200

    Add version field with value 1.0.0 to metadata

--- post-processor/vagrant/lxc.go
+++ post-processor/vagrant/lxc.go
@@ -14,7 +14,7 @@ func (p *LxcProvider) KeepInputArtifact() bool {
 
 func (p *LxcProvider) Process(ui packer.Ui, artifact packer.Artifact, dir string) (vagrantfile string, metadata map[string]interface{}, err error) {
 	// Create the metadata
-	metadata = map[string]interface{}{"provider": "lxc"}
+	metadata = map[string]interface{}{"provider": "lxc", "version": "1.0.0"}
 
 	// Copy all of the original contents into the temporary directory
 	for _, path := range artifact.Files() {
