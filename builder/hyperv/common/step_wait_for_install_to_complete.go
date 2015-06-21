5cc741a5484d8e10d897cfa5ffd0afdbf63efe18
Taliesin Sisson <taliesins@yahoo.com>
2015-06-21 19:35:32 +0100

    Wait until WinRM is available, before continuing with install. This is how other system like VeeWee do it. Add support for number of cpus to use for vm Add support for vm generation

--- builder/hyperv/common/step_wait_for_install_to_complete.go
+++ builder/hyperv/common/step_wait_for_install_to_complete.go
@@ -111,20 +111,3 @@ func (s *StepWaitForInstallToComplete) Run(state multistep.StateBag) multistep.S
 func (s *StepWaitForInstallToComplete) Cleanup(state multistep.StateBag) {
 
 }
-
-
-type StepWaitForWinRm struct {
-}
-
-func (s *StepWaitForWinRm) Run(state multistep.StateBag) multistep.StepAction {
-	ui := state.Get("ui").(packer.Ui)
-	//vmName := state.Get("vmName").(string)
-
-	ui.Say("Waiting for WinRM to be ready...")
-
-	return multistep.ActionContinue
-}
-
-func (s *StepWaitForWinRm) Cleanup(state multistep.StateBag) {
-
-}
