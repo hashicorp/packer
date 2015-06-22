1992c1225db485e1084d19059f08f3a33dbee1f8
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-06-22 12:24:27 -0700

    post-processor/*: fix interpolation context

--- post-processor/docker-push/post-processor.go
+++ post-processor/docker-push/post-processor.go
@@ -33,7 +33,8 @@ type PostProcessor struct {
 
 func (p *PostProcessor) Configure(raws ...interface{}) error {
 	err := config.Decode(&p.config, &config.DecodeOpts{
-		Interpolate: true,
+		Interpolate:        true,
+		InterpolateContext: &p.config.ctx,
 		InterpolateFilter: &interpolate.RenderFilter{
 			Exclude: []string{},
 		},
