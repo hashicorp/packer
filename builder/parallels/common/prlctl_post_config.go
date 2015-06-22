c0197cb2eccc2ea2189e1ca88889e97536d990cf
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-06-22 12:46:13 -0700

    builder/parallels: fix compilation issuse

--- builder/parallels/common/prlctl_post_config.go
+++ builder/parallels/common/prlctl_post_config.go
@@ -1,28 +1,17 @@
 package common
 
 import (
-	"fmt"
-	"github.com/mitchellh/packer/packer"
+	"github.com/mitchellh/packer/template/interpolate"
 )
 
 type PrlctlPostConfig struct {
 	PrlctlPost [][]string `mapstructure:"prlctl_post"`
 }
 
-func (c *PrlctlPostConfig) Prepare(t *packer.ConfigTemplate) []error {
+func (c *PrlctlPostConfig) Prepare(ctx *interpolate.Context) []error {
 	if c.PrlctlPost == nil {
 		c.PrlctlPost = make([][]string, 0)
 	}
 
-	errs := make([]error, 0)
-	for i, args := range c.PrlctlPost {
-		for j, arg := range args {
-			if err := t.Validate(arg); err != nil {
-				errs = append(errs,
-					fmt.Errorf("Error processing prlctl_post[%d][%d]: %s", i, j, err))
-			}
-		}
-	}
-
-	return errs
+	return nil
 }
--- builder/parallels/pvm/builder.go
+++ builder/parallels/pvm/builder.go
@@ -103,7 +103,7 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		},
 		&parallelscommon.StepPrlctl{
 			Commands: b.config.PrlctlPost,
-			Tpl:      b.config.tpl,
+			Ctx:      b.config.ctx,
 		},
 	}
 
