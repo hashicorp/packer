c310fd8b29b5cd618f4772829816cc6312601918
Taliesin Sisson <taliesins@interxion.com>
2015-07-01 13:46:29 +0100

    Add a little safety around variables that are passed into provisioner.

--- provisioner/powershell/provisioner.go
+++ provisioner/powershell/provisioner.go
@@ -347,8 +347,9 @@ func (p *Provisioner) createFlattenedEnvVars(elevated bool) (flattened string, e
 	// Split vars into key/value components
 	for _, envVar := range p.config.Vars {
 		keyValue := strings.Split(envVar, "=")
-		if len(keyValue) != 2 {
-			err = errors.New("Shell provisioner environment variables must be in key=value format")
+		
+		if len(keyValue) != 2 || keyValue[0] == "" {
+			err = errors.New(fmt.Sprintf("Shell provisioner environment variables must be in key=value format. Currently it is '%s'", envVar))
 			return
 		}
 		envVars[keyValue[0]] = keyValue[1]
