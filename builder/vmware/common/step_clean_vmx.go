df1be999dcc6a7861ce19ec7c06e99022c1db6e6
Marc Siegfriedt <marc@techaccelerator.com>
2015-07-01 11:30:39 -0700

    Fixed case sensitive issue with VMX entries not being overwritten

--- builder/vmware/common/step_clean_vmx.go
+++ builder/vmware/common/step_clean_vmx.go
@@ -51,8 +51,8 @@ func (s StepCleanVMX) Run(state multistep.StateBag) multistep.StepAction {
 
 		ui.Message("Detaching ISO from CD-ROM device...")
 
-		vmxData[ide+"devicetype"] = "cdrom-raw"
-		vmxData[ide+"filename"] = "auto detect"
+		vmxData[ide+"deviceType"] = "cdrom-raw"
+		vmxData[ide+"fileName"] = "auto detect"
 	}
 
 	ui.Message("Disabling VNC server...")
