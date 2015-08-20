aa8d953de095411e1136c04ce93dd9d7e31f2b7f
Olivier Tremblay <olivier.tremblay@lightspeedpos.com>
2015-08-20 07:41:02 -0400

    undo portions of general stupidity I have done.

--- common/packer_config.go
+++ common/packer_config.go
@@ -4,10 +4,9 @@ package common
 // are sent by packer, properly tagged already so mapstructure can load
 // them. Embed this structure into your configuration class to get it.
 type PackerConfig struct {
-	PackerBuildName     string            `mapstructure:"packer_build_name"`
-	PackerBuilderType   string            `mapstructure:"packer_builder_type"`
-	PackerDebug         bool              `mapstructure:"packer_debug"`
-	PackerForce         bool              `mapstructure:"packer_force"`
-	PackerUserVars      map[string]string `mapstructure:"packer_user_variables"`
-	PackerIsoTargetPath string            `mapstructure:"packer_iso_target_path"`
+	PackerBuildName   string            `mapstructure:"packer_build_name"`
+	PackerBuilderType string            `mapstructure:"packer_builder_type"`
+	PackerDebug       bool              `mapstructure:"packer_debug"`
+	PackerForce       bool              `mapstructure:"packer_force"`
+	PackerUserVars    map[string]string `mapstructure:"packer_user_variables"`
 }
