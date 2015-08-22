268476c0eceecaa6139694d6885179f45521bd66
Ian Duffy <ian@ianduffy.ie>
2015-08-22 23:41:08 +0100

    Closes #2637 - VRDP support for the virtualbox plugin
    Signed-off-by: Ian Duffy <ian@ianduffy.ie>

--- builder/virtualbox/common/run_config.go
+++ builder/virtualbox/common/run_config.go
@@ -11,6 +11,9 @@ type RunConfig struct {
 	Headless    bool   `mapstructure:"headless"`
 	RawBootWait string `mapstructure:"boot_wait"`
 
+	VRDPPortMin uint `mapstructure:"vrdp_port_min"`
+	VRDPPortMax uint `mapstructure:"vrdp_port_max"`
+
 	BootWait time.Duration ``
 }
 
@@ -19,11 +22,24 @@ func (c *RunConfig) Prepare(ctx *interpolate.Context) []error {
 		c.RawBootWait = "10s"
 	}
 
+	if c.VRDPPortMin == 0 {
+		c.VRDPPortMin = 5900
+	}
+
+	if c.VRDPPortMax == 0 {
+		c.VRDPPortMax = 6000
+	}
+
 	var errs []error
 	var err error
 	c.BootWait, err = time.ParseDuration(c.RawBootWait)
 	if err != nil {
 		errs = append(errs, fmt.Errorf("Failed parsing boot_wait: %s", err))
+	}
+
+	if c.VRDPPortMin > c.VRDPPortMax {
+		errs = append(
+			errs, fmt.Errorf("vrdp_port_min must be less than vrdp_port_max"))
 	}
 
 	return errs
