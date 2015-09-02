68097b02a708029fce8f24f290ca734a3203784f
Marc Siegfriedt <marc@techaccelerator.com>
2015-09-02 10:48:16 -0700

    add case sensitive vmx key functionality

--- builder/vmware/common/vmx_test.go
+++ builder/vmware/common/vmx_test.go
@@ -6,10 +6,11 @@ func TestParseVMX(t *testing.T) {
 	contents := `
 .encoding = "UTF-8"
 config.version = "8"
+scsi0:0.virtualSSD = 1
 `
 
 	results := ParseVMX(contents)
-	if len(results) != 2 {
+	if len(results) != 3 {
 		t.Fatalf("not correct number of results: %d", len(results))
 	}
 
@@ -20,16 +21,22 @@ config.version = "8"
 	if results["config.version"] != "8" {
 		t.Errorf("invalid config.version: %s", results["config.version"])
 	}
+
+	if results["scsi0:0.virtualssd"] != "1" {
+		t.Errorf("invalid scsi0:0.virtualssd: %s", results["scsi0:0.virtualssd"])
+	}
 }
 
 func TestEncodeVMX(t *testing.T) {
 	contents := map[string]string{
 		".encoding":      "UTF-8",
 		"config.version": "8",
+		"scsi0:0.virtualssd": "1",
 	}
 
 	expected := `.encoding = "UTF-8"
 config.version = "8"
+scsi0:0.virtualSSD = 1
 `
 
 	result := EncodeVMX(contents)
