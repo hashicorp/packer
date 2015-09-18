fd6282529c43510670cdc29cfeb9e0b1a7705e55
Rickard von Essen <rickard.von.essen@gmail.com>
2015-09-18 09:27:50 +0200

    Remove deprecated parallels_tools_host_path and guest_os_distribution

--- fix/fixer.go
+++ fix/fixer.go
@@ -20,13 +20,14 @@ var FixerOrder []string
 
 func init() {
 	Fixers = map[string]Fixer{
-		"iso-md5":             new(FixerISOMD5),
-		"createtime":          new(FixerCreateTime),
-		"pp-vagrant-override": new(FixerVagrantPPOverride),
-		"virtualbox-gaattach": new(FixerVirtualBoxGAAttach),
-		"virtualbox-rename":   new(FixerVirtualBoxRename),
-		"vmware-rename":       new(FixerVMwareRename),
-		"parallels-headless":  new(FixerParallelsHeadless),
+		"iso-md5":                new(FixerISOMD5),
+		"createtime":             new(FixerCreateTime),
+		"pp-vagrant-override":    new(FixerVagrantPPOverride),
+		"virtualbox-gaattach":    new(FixerVirtualBoxGAAttach),
+		"virtualbox-rename":      new(FixerVirtualBoxRename),
+		"vmware-rename":          new(FixerVMwareRename),
+		"parallels-headless":     new(FixerParallelsHeadless),
+		"parallels-deprecations": new(FixerParallelsDeprecations),
 	}
 
 	FixerOrder = []string{
@@ -37,5 +38,6 @@ func init() {
 		"virtualbox-rename",
 		"vmware-rename",
 		"parallels-headless",
+		"parallels-deprecations",
 	}
 }
