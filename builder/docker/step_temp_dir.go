c2d297b59813ec00cb65c5930baf2b0ad38223c1
Seth Vargo <sethvargo@gmail.com>
2015-10-15 14:31:13 -0400

    Revert "Merge pull request #2807 from markpeek/markpeek-docker-tmpdir"
    This reverts commit 31d367881408587d662527fd72016ce70de35e28, reversing
    changes made to a3a7c974d0976d5a25d3f8966ddf313e3f7ecfbf.

--- builder/docker/step_temp_dir.go
+++ builder/docker/step_temp_dir.go
@@ -6,7 +6,6 @@ import (
 	"github.com/mitchellh/packer/packer"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 )
 
 // StepTempDir creates a temporary directory that we use in order to
@@ -19,19 +18,7 @@ func (s *StepTempDir) Run(state multistep.StateBag) multistep.StepAction {
 	ui := state.Get("ui").(packer.Ui)
 
 	ui.Say("Creating a temporary directory for sharing data...")
-	// Create the docker temp files in the current working directory
-	// to work around an issue when running with docker-machine
-	// using vm's needing access to shared folder content. This assumes
-	// the current working directory is mapped as a share folder.
-	// Allow TMPDIR to override this location.
-	path := ""
-	if tmpdir := os.Getenv("TMPDIR"); tmpdir == "" {
-		abspath, err := filepath.Abs(".")
-		if err == nil {
-			path = abspath
-		}
-	}
-	td, err := ioutil.TempDir(path, "packer-docker")
+	td, err := ioutil.TempDir("", "packer-docker")
 	if err != nil {
 		err := fmt.Errorf("Error making temp dir: %s", err)
 		state.Put("error", err)
