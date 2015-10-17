20c7ffba238b08c09c6b267ee63180b60b03502a
Mark Peek <mark@peek.org>
2015-10-16 17:32:36 -0700

    Move ConfigFile() and ConfigDir() from package main to packer

---
+++ packer/config_file.go
@@ -0,0 +1,14 @@
+package packer
+
+// ConfigFile returns the default path to the configuration file. On
+// Unix-like systems this is the ".packerconfig" file in the home directory.
+// On Windows, this is the "packer.config" file in the application data
+// directory.
+func ConfigFile() (string, error) {
+	return configFile()
+}
+
+// ConfigDir returns the configuration directory for Packer.
+func ConfigDir() (string, error) {
+	return configDir()
+}
