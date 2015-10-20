56b102cd1d6259a6d77c9c97c4be55f8578b430b
Evan Powell <evan.powell@droidcloud.mobi>
2015-10-20 17:00:48 -0500

    Implement a null-object communicator for 'none'
    Fixes #2736

---
+++ communicator/none/communicator.go
@@ -0,0 +1,40 @@
+package none
+
+import (
+	"errors"
+	"io"
+	"os"
+	"github.com/mitchellh/packer/packer"
+)
+
+type comm struct {
+	config  string
+}
+
+// Creates a null packer.Communicator implementation. This takes
+// an already existing configuration.
+func New(config string) (result *comm, err error) {
+	// Establish an initial connection and connect
+	result = &comm{
+		config:  config,
+	}
+
+	return
+}
+
+func (c *comm) Start(cmd *packer.RemoteCmd) (err error) {
+	cmd.SetExited(0)
+	return
+}
+
+func (c *comm) Upload(path string, input io.Reader, fi *os.FileInfo) error {
+	return errors.New("Upload is not implemented when communicator = 'none'")
+}
+
+func (c *comm) UploadDir(dst string, src string, excl []string) error {
+	return errors.New("UploadDir is not implemented when communicator = 'none'")
+}
+
+func (c *comm) Download(path string, output io.Writer) error {
+	return errors.New("Download is not implemented when communicator = 'none'")
+}
