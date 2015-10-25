71ed8e4a38417f3b17f28e911085940b3d7659f4
Mark Peek <mark@peek.org>
2015-10-25 12:28:06 -0700

    Fix #2742: Include template line numbers on error

--- command/build.go
+++ command/build.go
@@ -39,11 +39,7 @@ func (c BuildCommand) Run(args []string) int {
 	// Parse the template
 	var tpl *template.Template
 	var err error
-	if args[0] == "-" {
-		tpl, err = template.Parse(os.Stdin)
-	} else {
-		tpl, err = template.ParseFile(args[0])
-	}
+	tpl, err = template.ParseFile(args[0])
 	if err != nil {
 		c.Ui.Error(fmt.Sprintf("Failed to parse template: %s", err))
 		return 1
