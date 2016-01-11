7018517374a3be807452e73f52197ff47adf7e10
Chulki Lee <chulki.lee@gmail.com>
2016-01-11 13:04:35 -0800

    one region per line, sorted

--- builder/amazon/common/regions.go
+++ builder/amazon/common/regions.go
@@ -3,9 +3,19 @@ package common
 // IsValidRegion returns true if the supplied region is a valid AWS
 // region and false if it's not.
 func ValidateRegion(region string) bool {
-	var regions = [11]string{"ap-northeast-1", "ap-southeast-1", "ap-southeast-2",
-		"cn-north-1", "eu-central-1", "eu-west-1", "sa-east-1",
-		"us-east-1", "us-gov-west-1", "us-west-1", "us-west-2"}
+	var regions = [11]string{
+		"ap-northeast-1",
+		"ap-southeast-1",
+		"ap-southeast-2",
+		"cn-north-1",
+		"eu-central-1",
+		"eu-west-1",
+		"sa-east-1",
+		"us-east-1",
+		"us-gov-west-1",
+		"us-west-1",
+		"us-west-2",
+	}
 
 	for _, valid := range regions {
 		if region == valid {
