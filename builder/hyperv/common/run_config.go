fb098fcbf09451007b4a7a10e80d2931e69f9999
Taliesin Sisson <taliesins@yahoo.com>
2016-01-25 22:43:40 +0000

    http server has moved to common step

--- builder/hyperv/common/run_config.go
+++ builder/hyperv/common/run_config.go
@@ -1,7 +1,6 @@
 package common
 
 import (
-	"errors"
 	"fmt"
 	"github.com/mitchellh/packer/template/interpolate"
 	"time"
@@ -11,24 +10,12 @@ type RunConfig struct {
 	Headless    bool   `mapstructure:"headless"`
 	RawBootWait string `mapstructure:"boot_wait"`
 
-	HTTPDir     string `mapstructure:"http_directory"`
-	HTTPPortMin uint   `mapstructure:"http_port_min"`
-	HTTPPortMax uint   `mapstructure:"http_port_max"`
-
 	BootWait time.Duration ``
 }
 
 func (c *RunConfig) Prepare(ctx *interpolate.Context) []error {
 	if c.RawBootWait == "" {
 		c.RawBootWait = "10s"
-	}
-
-	if c.HTTPPortMin == 0 {
-		c.HTTPPortMin = 8000
-	}
-
-	if c.HTTPPortMax == 0 {
-		c.HTTPPortMax = 9000
 	}
 
 	var errs []error
@@ -40,11 +27,6 @@ func (c *RunConfig) Prepare(ctx *interpolate.Context) []error {
 			errs = append(
 				errs, fmt.Errorf("Failed parsing boot_wait: %s", err))
 		}
-	}
-
-	if c.HTTPPortMin > c.HTTPPortMax {
-		errs = append(errs,
-			errors.New("http_port_min must be less than http_port_max"))
 	}
 
 	return errs
