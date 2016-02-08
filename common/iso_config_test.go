80def93955ad8b1c4d1aa30c36f98ceb0732959a
Rickard von Essen <rickard.von.essen@gmail.com>
2016-02-08 15:51:43 +0100

    Added "iso_checksum_url" and keep the functionality of "iso_checksum"
    Added support for file shema in "iso_checksum_url".
    Added some unit tests and updated the docs accordingly.

--- common/iso_config_test.go
+++ common/iso_config_test.go
@@ -1,6 +1,8 @@
 package common
 
 import (
+	"fmt"
+	"io/ioutil"
 	"reflect"
 	"testing"
 )
@@ -8,10 +10,21 @@ import (
 func testISOConfig() ISOConfig {
 	return ISOConfig{
 		ISOChecksum:     "foo",
+		ISOChecksumURL:  "",
 		ISOChecksumType: "md5",
-		RawSingleISOUrl: "http://www.packer.io",
+		RawSingleISOUrl: "http://www.packer.io/the-OS.iso",
 	}
 }
+
+var cs_bsd_style = `
+MD5 (other.iso) = bAr
+MD5 (the-OS.iso) = baZ
+`
+
+var cs_gnu_style = `
+bAr0 *the-OS.iso
+baZ0  other.iso
+`
 
 func TestISOConfigPrepare_ISOChecksum(t *testing.T) {
 	i := testISOConfig()
@@ -39,6 +52,53 @@ func TestISOConfigPrepare_ISOChecksum(t *testing.T) {
 
 	if i.ISOChecksum != "foo" {
 		t.Fatalf("should've lowercased: %s", i.ISOChecksum)
+	}
+}
+
+func TestISOConfigPrepare_ISOChecksumURL(t *testing.T) {
+	i := testISOConfig()
+	i.ISOChecksumURL = "file:///not_read"
+
+	// Test ISOChecksum overrides url
+	warns, err := i.Prepare(nil)
+	if len(warns) > 0 && len(err) > 0 {
+		t.Fatalf("bad: %#v, %#v", warns, err)
+	}
+
+	// Test good - ISOChecksumURL BSD style
+	i = testISOConfig()
+	i.ISOChecksum = ""
+	cs_file, _ := ioutil.TempFile("", "packer-test-")
+	ioutil.WriteFile(cs_file.Name(), []byte(cs_bsd_style), 0666)
+	i.ISOChecksumURL = fmt.Sprintf("file://%s", cs_file.Name())
+	warns, err = i.Prepare(nil)
+	if len(warns) > 0 {
+		t.Fatalf("bad: %#v", warns)
+	}
+	if err != nil {
+		t.Fatalf("should not have error: %s", err)
+	}
+
+	if i.ISOChecksum != "baz" {
+		t.Fatalf("should've found \"baz\" got: %s", i.ISOChecksum)
+	}
+
+	// Test good - ISOChecksumURL GNU style
+	i = testISOConfig()
+	i.ISOChecksum = ""
+	cs_file, _ = ioutil.TempFile("", "packer-test-")
+	ioutil.WriteFile(cs_file.Name(), []byte(cs_gnu_style), 0666)
+	i.ISOChecksumURL = fmt.Sprintf("file://%s", cs_file.Name())
+	warns, err = i.Prepare(nil)
+	if len(warns) > 0 {
+		t.Fatalf("bad: %#v", warns)
+	}
+	if err != nil {
+		t.Fatalf("should not have error: %s", err)
+	}
+
+	if i.ISOChecksum != "bar0" {
+		t.Fatalf("should've found \"bar0\" got: %s", i.ISOChecksum)
 	}
 }
 
@@ -113,7 +173,7 @@ func TestISOConfigPrepare_ISOUrl(t *testing.T) {
 
 	// Test iso_url set
 	i = testISOConfig()
-	i.RawSingleISOUrl = "http://www.packer.io"
+	i.RawSingleISOUrl = "http://www.packer.io/the-OS.iso"
 	warns, err = i.Prepare(nil)
 	if len(warns) > 0 {
 		t.Fatalf("bad: %#v", warns)
@@ -122,15 +182,15 @@ func TestISOConfigPrepare_ISOUrl(t *testing.T) {
 		t.Errorf("should not have error: %s", err)
 	}
 
-	expected := []string{"http://www.packer.io"}
+	expected := []string{"http://www.packer.io/the-OS.iso"}
 	if !reflect.DeepEqual(i.ISOUrls, expected) {
 		t.Fatalf("bad: %#v", i.ISOUrls)
 	}
 
 	// Test both set
 	i = testISOConfig()
-	i.RawSingleISOUrl = "http://www.packer.io"
-	i.ISOUrls = []string{"http://www.packer.io"}
+	i.RawSingleISOUrl = "http://www.packer.io/the-OS.iso"
+	i.ISOUrls = []string{"http://www.packer.io/the-OS.iso"}
 	warns, err = i.Prepare(nil)
 	if len(warns) > 0 {
 		t.Fatalf("bad: %#v", warns)
@@ -143,8 +203,8 @@ func TestISOConfigPrepare_ISOUrl(t *testing.T) {
 	i = testISOConfig()
 	i.RawSingleISOUrl = ""
 	i.ISOUrls = []string{
-		"http://www.packer.io",
-		"http://www.hashicorp.com",
+		"http://www.packer.io/the-OS.iso",
+		"http://www.hashicorp.com/the-OS.iso",
 	}
 
 	warns, err = i.Prepare(nil)
@@ -156,8 +216,8 @@ func TestISOConfigPrepare_ISOUrl(t *testing.T) {
 	}
 
 	expected = []string{
-		"http://www.packer.io",
-		"http://www.hashicorp.com",
+		"http://www.packer.io/the-OS.iso",
+		"http://www.hashicorp.com/the-OS.iso",
 	}
 	if !reflect.DeepEqual(i.ISOUrls, expected) {
 		t.Fatalf("bad: %#v", i.ISOUrls)
