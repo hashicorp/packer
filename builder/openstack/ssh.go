c3bc87cb716859b7c597317b937dc5203a280dc3
Rickard von Essen <rickard.von.essen@gmail.com>
2016-02-16 10:55:21 +0100

    OpenStack: Change some log messages about SSH to connect.

--- builder/openstack/ssh.go
+++ builder/openstack/ssh.go
@@ -23,7 +23,7 @@ func CommHost(
 		// If we have a specific interface, try that
 		if sshinterface != "" {
 			if addr := sshAddrFromPool(s, sshinterface); addr != "" {
-				log.Printf("[DEBUG] Using IP address %s from specified interface %s for SSH", addr, sshinterface)
+				log.Printf("[DEBUG] Using IP address %s from specified interface %s to connect", addr, sshinterface)
 				return addr, nil
 			}
 		}
@@ -31,18 +31,18 @@ func CommHost(
 		// If we have a floating IP, use that
 		ip := state.Get("access_ip").(*floatingip.FloatingIP)
 		if ip != nil && ip.IP != "" {
-			log.Printf("[DEBUG] Using floating IP %s for SSH", ip.IP)
+			log.Printf("[DEBUG] Using floating IP %s to connect", ip.IP)
 			return ip.IP, nil
 		}
 
 		if s.AccessIPv4 != "" {
-			log.Printf("[DEBUG] Using AccessIPv4 %s for SSH", s.AccessIPv4)
+			log.Printf("[DEBUG] Using AccessIPv4 %s to connect", s.AccessIPv4)
 			return s.AccessIPv4, nil
 		}
 
 		// Try to get it from the requested interface
 		if addr := sshAddrFromPool(s, sshinterface); addr != "" {
-			log.Printf("[DEBUG] Using IP address %s for SSH", addr)
+			log.Printf("[DEBUG] Using IP address %s to connect", addr)
 			return addr, nil
 		}
 
