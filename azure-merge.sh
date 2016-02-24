29248b97032af85a5b03a01533e8f670b0dcaa7e
Chris Bednarski <banzaimonkey@gmail.com>
2016-02-23 18:36:04 -0800

    Added merge script to automatically pull in and fix the upstream repo

---
+++ azure-merge.sh
@@ -0,0 +1,12 @@
+PACKER=$GOPATH/src/github.com/mitchellh/packer
+AZURE=$HOME/hashi/packer-azure
+
+# copy things
+cp -r $AZURE/packer/builder/azure $PACKER/builder/azure
+# remove legacy API client
+rm -rf $PACKER/builder/azure/smapi
+cp -r $AZURE/packer/communicator/* $PACKER/communicator/
+cp -r $AZURE/packer/provisioner/azureVmCustomScriptExtension $PACKER/provisioner/azureVmCustomScriptExtension
+
+# fix imports
+find $PACKER/builder/azure/ | grep ".go" | xargs sed -i -e 's/Azure\/azure-sdk-for-go/mitchellh\/packer\/builder/g'
