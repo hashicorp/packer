bb879c7cce8d35e344056e90da3c35affa3c2e0f
Ilias Bertsimas <ilias.bertsimas@itv.com>
2016-03-02 11:48:20 +0000

    Add 4 testing scenarios for WinRM helper communicator config.

--- helper/communicator/config_test.go
+++ helper/communicator/config_test.go
@@ -37,6 +37,70 @@ func TestConfig_badtype(t *testing.T) {
 	}
 }
 
+func TestConfig_winrm_noport(t *testing.T) {
+	c := &Config{
+		Type:      "winrm",
+		WinRMUser: "admin",
+	}
+	if err := c.Prepare(testContext(t)); len(err) > 0 {
+		t.Fatalf("bad: %#v", err)
+	}
+
+	if c.WinRMPort != 5985 {
+		t.Fatalf("WinRMPort doesn't match default port 5985 when SSL is not enabled and no port is specified.")
+	}
+
+}
+
+func TestConfig_winrm_noport_ssl(t *testing.T) {
+	c := &Config{
+		Type:        "winrm",
+		WinRMUser:   "admin",
+		WinRMUseSSL: true,
+	}
+	if err := c.Prepare(testContext(t)); len(err) > 0 {
+		t.Fatalf("bad: %#v", err)
+	}
+
+	if c.WinRMPort != 5986 {
+		t.Fatalf("WinRMPort doesn't match default port 5986 when SSL is enabled and no port is specified.")
+	}
+
+}
+
+func TestConfig_winrm_port(t *testing.T) {
+	c := &Config{
+		Type:      "winrm",
+		WinRMUser: "admin",
+		WinRMPort: 5509,
+	}
+	if err := c.Prepare(testContext(t)); len(err) > 0 {
+		t.Fatalf("bad: %#v", err)
+	}
+
+	if c.WinRMPort != 5509 {
+		t.Fatalf("WinRMPort doesn't match custom port 5509 when SSL is not enabled.")
+	}
+
+}
+
+func TestConfig_winrm_port_ssl(t *testing.T) {
+	c := &Config{
+		Type:        "winrm",
+		WinRMUser:   "admin",
+		WinRMPort:   5510,
+		WinRMUseSSL: true,
+	}
+	if err := c.Prepare(testContext(t)); len(err) > 0 {
+		t.Fatalf("bad: %#v", err)
+	}
+
+	if c.WinRMPort != 5510 {
+		t.Fatalf("WinRMPort doesn't match custom port 5510 when SSL is enabled.")
+	}
+
+}
+
 func TestConfig_winrm(t *testing.T) {
 	c := &Config{
 		Type:      "winrm",
