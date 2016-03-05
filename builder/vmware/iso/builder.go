c8467343a0b3105ad4521e831a79ce6fac3808d7
Ali Rizvi-Santiago <arizvisa@gmail.com>
2016-03-05 01:40:16 -0600

    Re-implemented the support for the floppy_files keyword in order to remain backwards-compatible with templates using the old syntax. Moved the support for recursive paths from the floppy_files keyword to the new floppy_contents keyword. Shifted some of the code around to add better logging of what's actually being copied. Added a couple of unit-tests for the new floppy_contents implementation. Ensured that all files that were being added were also being included in state.FilesAdded so that the older unit-tests will work.

--- builder/vmware/iso/builder.go
+++ builder/vmware/iso/builder.go
@@ -231,7 +231,8 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 			Force: b.config.PackerForce,
 		},
 		&common.StepCreateFloppy{
-			Files: b.config.FloppyFiles,
+			Files:    b.config.FloppyConfig.FloppyFiles,
+			Contents: b.config.FloppyConfig.FloppyContents,
 		},
 		&stepRemoteUpload{
 			Key:     "floppy_path",
