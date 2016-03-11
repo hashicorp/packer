2920f7c038d943f8a0615f6c45ec4bacf1969a59
Chris Bednarski <banzaimonkey@gmail.com>
2016-03-10 17:46:22 -0800

    Update LICENSE notice

--- builder/azure/arm/builder_test.go
+++ builder/azure/arm/builder_test.go
@@ -1,11 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See the LICENSE file in the project root for license information.
+// Licensed under the MIT License. See the LICENSE file in builder/azure for license information.
 
 package arm
 
 import (
-	"github.com/mitchellh/packer/builder/azure/common/constants"
 	"testing"
+
+	"github.com/mitchellh/packer/builder/azure/common/constants"
 )
 
 func TestStateBagShouldBePopulatedExpectedValues(t *testing.T) {
--- builder/azure/arm/openssh_key_pair.go
+++ builder/azure/arm/openssh_key_pair.go
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See the LICENSE file in the project root for license information.
+// Licensed under the MIT License. See the LICENSE file in builder/azure for license information.
 
 package arm
 
@@ -10,8 +10,9 @@ import (
 	"encoding/base64"
 	"encoding/pem"
 	"fmt"
-	"golang.org/x/crypto/ssh"
 	"time"
+
+	"golang.org/x/crypto/ssh"
 )
 
 const (
