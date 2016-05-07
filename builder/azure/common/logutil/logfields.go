afc4aff5e11654f8b98fb44f9d6007957dd872e8
Chris Bednarski <banzaimonkey@gmail.com>
2016-05-06 20:32:18 -0700

    Updated license information to reference builder/azure

--- builder/azure/common/logutil/logfields.go
+++ builder/azure/common/logutil/logfields.go
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See the LICENSE file in the project root for license information.
+// Licensed under the MIT License. See the LICENSE file in builder/azure for license information.
 
 package logutil
 
