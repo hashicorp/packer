ea157f862ee7b7fbe0219f6d518c9fa0979b1682
Chris Lundquist <rampantdurandal@gmail.com>
2016-05-25 19:34:51 +0000

    [lxc] go fmt our files

--- builder/lxc/communicator_test.go
+++ builder/lxc/communicator_test.go
@@ -1,14 +1,14 @@
 package lxc
 
 import (
-        "github.com/mitchellh/packer/packer"
-        "testing"
+	"github.com/mitchellh/packer/packer"
+	"testing"
 )
 
 func TestCommunicator_ImplementsCommunicator(t *testing.T) {
-        var raw interface{}
-        raw = &LxcAttachCommunicator{}
-        if _, ok := raw.(packer.Communicator); !ok {
-                t.Fatalf("Communicator should be a communicator")
-        }
+	var raw interface{}
+	raw = &LxcAttachCommunicator{}
+	if _, ok := raw.(packer.Communicator); !ok {
+		t.Fatalf("Communicator should be a communicator")
+	}
 }
