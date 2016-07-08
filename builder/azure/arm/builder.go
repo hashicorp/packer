ee29ecdeae8ea64baed2c3810ec773fa4e4fc2d8
Christopher Boumenot <chrboum@microsoft.com>
2016-07-07 17:28:47 -0700

    Update Azure dependencies.
     * azure-sdk-for-go to 3.1.0-beta
     * go-autorest to 7.0.7
     * dgrijalva/jwt-go to 3.0.0
    
    Add the German cloud configuration.
    Ensure the different cloud URLs are _actually_ used.

--- builder/azure/arm/builder.go
+++ builder/azure/arm/builder.go
@@ -74,6 +74,7 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		b.config.SubscriptionID,
 		b.config.ResourceGroupName,
 		b.config.StorageAccount,
+		b.config.cloudEnvironment,
 		spnCloud,
 		spnKeyVault)
 
@@ -240,7 +241,9 @@ func (b *Builder) getServicePrincipalTokens(say func(string)) (*azure.ServicePri
 			return nil, nil, err
 		}
 
-		servicePrincipalTokenVault, err = auth.getServicePrincipalTokenWithResource(packerAzureCommon.AzureVaultScope)
+		servicePrincipalTokenVault, err = auth.getServicePrincipalTokenWithResource(
+			strings.TrimRight(b.config.cloudEnvironment.KeyVaultEndpoint, "/"))
+
 		if err != nil {
 			return nil, nil, err
 		}
