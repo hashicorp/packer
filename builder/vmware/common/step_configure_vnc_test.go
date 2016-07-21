bbc3a98351c8c91cf9a3399254ab8c1a11396010
Charlie Vieth <cviethjr@pivotal.io>
2016-07-21 12:14:02 -0400

    Do not add remotedisplay.vnc.ip to vmx data on ESXi
    * The remotedisplay.vnc.ip vmx data key breaks ESXi, this commit prevents it
      from being automatically added during VNC configuration when using the ESX5
      driver.
    
    * It can still be configured via the vmx_data section of the builder
      template
    
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

---
+++ builder/vmware/common/step_configure_vnc_test.go
@@ -0,0 +1,25 @@
+package common
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStepConfigureVNC_implVNCAddressFinder(t *testing.T) {
+	var _ VNCAddressFinder = new(StepConfigureVNC)
+}
+
+func TestStepConfigureVNC_UpdateVMX(t *testing.T) {
+	var s StepConfigureVNC
+	data := make(map[string]string)
+	s.UpdateVMX("0.0.0.0", 5900, data)
+	if ip := data["remotedisplay.vnc.ip"]; ip != "0.0.0.0" {
+		t.Errorf("bad VMX data for key remotedisplay.vnc.ip: %v", ip)
+	}
+	if enabled := data["remotedisplay.vnc.enabled"]; enabled != "TRUE" {
+		t.Errorf("bad VMX data for key remotedisplay.vnc.enabled: %v", enabled)
+	}
+	if port := data["remotedisplay.vnc.port"]; port != fmt.Sprint(port) {
+		t.Errorf("bad VMX data for key remotedisplay.vnc.port: %v", port)
+	}
+}
