e8662835180f4a33d4036c171992e9761383ad79
Pieter Lazzaro <pieter.lazzaro@gmail.com>
2016-07-29 17:30:38 -0400

    Code cleanup

--- builder/googlecompute/winrm.go
+++ builder/googlecompute/winrm.go
@@ -5,7 +5,7 @@ import (
 	"github.com/mitchellh/packer/helper/communicator"
 )
 
-// sshConfig returns the ssh configuration.
+// winrmConfig returns the WinRM configuration.
 func winrmConfig(state multistep.StateBag) (*communicator.WinRMConfig, error) {
 	config := state.Get("config").(*Config)
 	password := state.Get("winrm_password").(string)
