30d278825675e1b2a60b74b7c0ae2e06aa250b38
Rickard von Essen <rickard.von.essen@gmail.com>
2016-09-12 07:24:32 +0200

    WIP: Docker API

--- builder/docker/builder.go
+++ builder/docker/builder.go
@@ -2,6 +2,7 @@ package docker
 
 import (
 	"log"
+	"os"
 
 	"github.com/mitchellh/multistep"
 	"github.com/mitchellh/packer/common"
@@ -30,7 +31,15 @@ func (b *Builder) Prepare(raws ...interface{}) ([]string, error) {
 }
 
 func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packer.Artifact, error) {
-	driver := &DockerDriver{Ctx: &b.config.ctx, Ui: ui}
+
+	var driver Driver
+
+	if os.Getenv("PACKER_DOCKER_API") != "" {
+		driver = dockerApiDriverInit(&b.config.ctx, ui)
+	} else {
+		driver = &DockerDriver{Ctx: &b.config.ctx, Ui: ui}
+	}
+
 	if err := driver.Verify(); err != nil {
 		return nil, err
 	}
