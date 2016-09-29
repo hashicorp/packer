8eb14e52241671880d298a4b331ed8ac52c54fcc
Matthew Hooker <mwhooker@gmail.com>
2016-09-29 14:13:04 -0700

    gofmt project.
    noticed many unrelated changes being added to patches because of gofmt.
    
    ran `find . -not -path "./vendor/*" -name "*.go" -exec gofmt -w {} \;`

--- post-processor/manifest/post-processor.go
+++ post-processor/manifest/post-processor.go
@@ -18,9 +18,9 @@ import (
 type Config struct {
 	common.PackerConfig `mapstructure:",squash"`
 
-	Filename     string `mapstructure:"filename"`
-	StripPath    bool   `mapstructure:"strip_path"`
-	ctx          interpolate.Context
+	Filename  string `mapstructure:"filename"`
+	StripPath bool   `mapstructure:"strip_path"`
+	ctx       interpolate.Context
 }
 
 type PostProcessor struct {
