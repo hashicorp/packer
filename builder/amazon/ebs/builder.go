2f66903c61098b6c2156738d4ae81fa53e552d78
Rickard von Essen <rickard.von.essen@gmail.com>
2016-10-02 22:20:36 +0200

    builder/amazon: Allow using ssh_private_key_file and ssh_password
    This adds support for using amazon-ebs/amazon-instance builder without
    a keypair. If a ssh_private_key_file is supplied without a
    ssh_keypair_name no temporary ssh keypair is created. If ssh_password is
    used no temporary ssh keypair is created and the password is used when
    trying to connect.
    
    Closes #2301
    Closes #3156

--- builder/amazon/ebs/builder.go
+++ builder/amazon/ebs/builder.go
@@ -148,7 +148,8 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 				ec2conn,
 				b.config.SSHPrivateIp),
 			SSHConfig: awscommon.SSHConfig(
-				b.config.RunConfig.Comm.SSHUsername),
+				b.config.RunConfig.Comm.SSHUsername,
+				b.config.RunConfig.Comm.SSHPassword),
 		},
 		&common.StepProvision{},
 		&stepStopInstance{
