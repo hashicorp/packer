82a705491f4191d884f4a8342b4782ca0637e969
James Nugent <james@jen20.com>
2016-10-31 16:44:41 -0500

    builder/amazon: Add `ebs-volume` builder
    This commit adds a builder that works like EBS builders, except does not
    create an AMI, and instead is intended to create EBS volumes in an
    initialized state. For example, the following template can be used to
    create and export a set of 3 EBS Volumes in a ZFS zpool named `data` for
    importing by instances running production systems:
    
    ```
    {
    	"variables": {
    		"aws_access_key_id": "{{ env `AWS_ACCESS_KEY_ID` }}",
    		"aws_secret_access_key": "{{ env `AWS_SECRET_ACCESS_KEY` }}",
    		"region": "{{ env `AWS_REGION` }}",
    		"source_ami": "{{ env `PACKER_SOURCE_AMI` }}",
    		"vpc_id": "{{ env `PACKER_VPC_ID` }}",
    		"subnet_id": "{{ env `PACKER_SUBNET_ID` }}"
    	},
    	"builders": [{
    		"type": "amazon-ebs-volume",
    		"access_key": "{{ user `aws_access_key_id` }}",
    		"secret_key": "{{ user `aws_secret_access_key` }}",
    		"region": "{{user `region`}}",
    		"spot_price_auto_product": "Linux/UNIX (Amazon VPC)",
    
    		"ssh_pty": true,
    		"instance_type": "t2.medium",
    		"vpc_id": "{{user `vpc_id` }}",
    		"subnet_id": "{{user `subnet_id` }}",
    		"associate_public_ip_address": true,
    		"source_ami": "{{user `source_ami` }}",
    		"ssh_username": "ubuntu",
    		"ssh_timeout": "5m",
    
    		"ebs_volumes": [
    			{
    				"device_name": "/dev/xvdf",
    				"delete_on_termination": false,
    				"volume_size": 10,
    				"volume_type": "gp2",
    				"tags": {
    					"Name": "TeamCity-Data1",
    					"zpool": "data",
    					"Component": "TeamCity"
    				}
    			},
    			{
    				"device_name": "/dev/xvdg",
    				"delete_on_termination": false,
    				"volume_size": 10,
    				"volume_type": "gp2",
    				"tags": {
    					"Name": "TeamCity-Data2",
    					"zpool": "data",
    					"Component": "TeamCity"
    				}
    			},
    			{
    				"device_name": "/dev/xvdh",
    				"delete_on_termination": false,
    				"volume_size": 10,
    				"volume_type": "gp2",
    				"tags": {
    					"Name": "TeamCity-Data3",
    					"zpool": "data",
    					"Component": "TeamCity"
    				}
    			}
    		]
    	}],
    	"provisioners": [
    	{
    		"type": "shell",
    		"start_retry_timeout": "10m",
    		"inline": [
    			"DEBIAN_FRONTEND=noninteractive sudo apt-get update",
    			"DEBIAN_FRONTEND=noninteractive sudo apt-get install -y zfs",
    			"lsblk",
    			"sudo parted /dev/xvdf --script mklabel GPT",
    			"sudo parted /dev/xvdg --script mklabel GPT",
    			"sudo parted /dev/xvdh --script mklabel GPT",
    			"sudo zpool create -m none data raidz xvdf xvdg xvdh",
    			"sudo zpool status",
    			"sudo zpool export data",
    			"sudo zpool status"
    		]
    	}
    	]
    }
    ```
    
    StepModifyInstance and StepStopInstance are now shared between EBS and
    EBS-Volume builders - move them into the AWS common directory and rename
    them to indicate that they only apply to EBS-backed builders.

---
+++ builder/amazon/ebsvolume/step_tag_ebs_volumes.go
@@ -0,0 +1,77 @@
+package ebsvolume
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/mitchellh/multistep"
+	"github.com/mitchellh/packer/packer"
+)
+
+type stepTagEBSVolumes struct {
+	VolumeMapping []BlockDevice
+	VolumeIDs     **[]string
+}
+
+func (s *stepTagEBSVolumes) Run(state multistep.StateBag) multistep.StepAction {
+	ec2conn := state.Get("ec2").(*ec2.EC2)
+	instance := state.Get("instance").(*ec2.Instance)
+	ui := state.Get("ui").(packer.Ui)
+
+	volumeIds := make([]string, 0, len(s.VolumeMapping))
+	for _, instanceBlockDevices := range instance.BlockDeviceMappings {
+		for _, configVolumeMapping := range s.VolumeMapping {
+			if configVolumeMapping.DeviceName == *instanceBlockDevices.DeviceName {
+				volumeIds = append(volumeIds, *instanceBlockDevices.Ebs.VolumeId)
+			}
+		}
+	}
+	*s.VolumeIDs = &volumeIds
+
+	if len(s.VolumeMapping) > 0 {
+		ui.Say("Tagging EBS volumes...")
+
+		toTag := map[string][]*ec2.Tag{}
+		for _, mapping := range s.VolumeMapping {
+			if len(mapping.Tags) == 0 {
+				ui.Say(fmt.Sprintf("No tags specified for volume on %s...", mapping.DeviceName))
+				continue
+			}
+
+			tags := make([]*ec2.Tag, 0, len(mapping.Tags))
+			for key, value := range mapping.Tags {
+				tags = append(tags, &ec2.Tag{
+					Key:   aws.String(fmt.Sprintf("%s", key)),
+					Value: aws.String(fmt.Sprintf("%s", value)),
+				})
+			}
+
+			for _, v := range instance.BlockDeviceMappings {
+				if *v.DeviceName == mapping.DeviceName {
+					toTag[*v.Ebs.VolumeId] = tags
+				}
+			}
+		}
+
+		for volumeId, tags := range toTag {
+			_, err := ec2conn.CreateTags(&ec2.CreateTagsInput{
+				Resources: []*string{&volumeId},
+				Tags:      tags,
+			})
+			if err != nil {
+				err := fmt.Errorf("Error tagging EBS Volume %s on %s: %s", volumeId, *instance.InstanceId, err)
+				state.Put("error", err)
+				ui.Error(err.Error())
+				return multistep.ActionHalt
+			}
+
+		}
+	}
+
+	return multistep.ActionContinue
+}
+
+func (s *stepTagEBSVolumes) Cleanup(state multistep.StateBag) {
+	// No cleanup...
+}
