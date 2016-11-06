501845cb4f71149bcba166cb98aff09beef4826e
Taliesin Sisson <taliesins@yahoo.com>
2016-11-06 13:06:10 +0000

    SSHKeyPath and SSHWaitTimeout have been deprecated in favor of communicator configuration

--- builder/hyperv/common/ssh_config.go
+++ builder/hyperv/common/ssh_config.go
@@ -9,21 +9,8 @@ import (
 
 type SSHConfig struct {
 	Comm communicator.Config `mapstructure:",squash"`
-
-	// These are deprecated, but we keep them around for BC
-	// TODO(@mitchellh): remove
-	SSHKeyPath     string        `mapstructure:"ssh_key_path"`
-	SSHWaitTimeout time.Duration `mapstructure:"ssh_wait_timeout"`
 }
 
 func (c *SSHConfig) Prepare(ctx *interpolate.Context) []error {
-	// TODO: backwards compatibility, write fixer instead
-	if c.SSHKeyPath != "" {
-		c.Comm.SSHPrivateKey = c.SSHKeyPath
-	}
-	if c.SSHWaitTimeout != 0 {
-		c.Comm.SSHTimeout = c.SSHWaitTimeout
-	}
-
 	return c.Comm.Prepare(ctx)
 }
