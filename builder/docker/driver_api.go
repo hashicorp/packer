90cb6cafc50dfd7380765ddfb0edd94e830289e2
Rickard von Essen <rickard.von.essen@gmail.com>
2016-11-12 13:10:08 +0100

    Switched to go-dockerclient

--- builder/docker/driver_api.go
+++ builder/docker/driver_api.go
@@ -6,29 +6,26 @@ import (
 	"os"
 	"sync"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/network"
-	"github.com/docker/docker/client"
+	godocker "github.com/fsouza/go-dockerclient"
 	"github.com/hashicorp/go-version"
 	"github.com/mitchellh/packer/packer"
 	"github.com/mitchellh/packer/template/interpolate"
-	"golang.org/x/net/context"
 )
 
 type DockerApiDriver struct {
 	Ui  packer.Ui
 	Ctx *interpolate.Context
 
-	l         sync.Mutex
-	client    *client.Client
-	repoToken string
+	l             sync.Mutex
+	client        *godocker.Client
+	auth          godocker.AuthConfiguration
+	identityToken string
 }
 
 func dockerApiDriverInit(ctx *interpolate.Context, ui packer.Ui) DockerApiDriver {
 
 	// TODO Allow specefying DOCKER_
-	client, _ := client.NewEnvClient()
+	client, _ := godocker.NewClientFromEnv()
 
 	return DockerApiDriver{
 		Ui:     ui,
@@ -38,35 +35,32 @@ func dockerApiDriverInit(ctx *interpolate.Context, ui packer.Ui) DockerApiDriver
 }
 
 func (d DockerApiDriver) DeleteImage(id string) error {
-
 	log.Printf("Deleting image: %s", id)
-	_, err := d.client.ImageRemove(context.Background(), id, types.ImageRemoveOptions{})
-
-	return err
+	return d.client.RemoveImage(id)
 }
 
 func (d DockerApiDriver) Commit(id string) (string, error) {
 
-	resp, err := d.client.ContainerCommit(context.Background(), id, types.ContainerCommitOptions{})
+	image, err := d.client.CommitContainer(godocker.CommitContainerOptions{
+		Container: id,
+		// Repository: "",
+		// Tag:        "",
+		// Message:    "",
+		// Author:     "",
+	})
 	if err != nil {
 		return "", err
 	}
-	return resp.ID, nil
+	return image.ID, nil
 }
 
 func (d DockerApiDriver) Export(id string, dst io.Writer) error {
-
 	log.Printf("Exporting container: %s", id)
-	reader, err := d.client.ContainerExport(context.Background(), id)
-	if err != nil {
-		return err
-	}
-	_, err = io.Copy(dst, reader)
-	if err != nil {
-		return err
-	}
 
-	return nil
+	return d.client.ExportContainer(godocker.ExportContainerOptions{
+		ID:           id,
+		OutputStream: dst,
+	})
 }
 
 func (d DockerApiDriver) Import(path string, repo string) (string, error) {
@@ -77,74 +71,89 @@ func (d DockerApiDriver) Import(path string, repo string) (string, error) {
 	}
 	defer file.Close()
 
-	source := types.ImageImportSource{
-		Source: file,
-	}
-	resp, err := d.client.ImageImport(context.Background(), source, repo, types.ImageImportOptions{})
+	err = d.client.ImportImage(godocker.ImportImageOptions{
+		Repository:  repo,
+		Source:      path,
+		InputStream: file,
+		// TODO Fix !?
+	})
+
 	if err != nil {
 		return "", err
 	}
-	log.Printf("Imported: %v\n", resp) // TODO parse the respones
-	return "", nil                     // TODO
+	//log.Printf("Imported: %v\n", resp) // TODO parse the respones
+	return "", nil // TODO
 }
 
 func (d DockerApiDriver) IPAddress(id string) (string, error) {
 
-	resp, err := d.client.ContainerInspect(context.Background(), id)
+	resp, err := d.client.InspectContainer(id)
 	return resp.NetworkSettings.IPAddress, err
 }
 
 func (d DockerApiDriver) Login(repo, email, user, pass string) error {
-	auth := types.AuthConfig{
+	auth := godocker.AuthConfiguration{
 		ServerAddress: repo,
 		Email:         email,
 		Username:      user,
 		Password:      pass,
 	}
-	resp, err := d.client.RegistryLogin(context.Background(), auth)
-	d.repoToken = resp.IdentityToken
+	status, err := d.client.AuthCheck(&auth)
+
+	if err == nil {
+		d.auth = auth
+		d.identityToken = status.IdentityToken
+	}
 
 	return err
 }
 
 func (d DockerApiDriver) Logout(repo string) error {
-	d.repoToken = ""
+	d.identityToken = ""
 	return nil
 }
 
 func (d DockerApiDriver) Pull(image string) error {
-	// TODO handle login
 
-	//tmp := func() (string, error) {
-	//	return "", nil
-	//}
-
-	opts := types.ImagePullOptions{
-		All: false,
-		//RegistryAuth:  d.repoToken,
-		//PrivilegeFunc: nil,
+	// TODO reader
+	opts := godocker.PullImageOptions{
+		Repository: image,
+		Tag:        "latest", // TODO
+		// OutputStream      io.Writer     `qs:"-"`
 	}
-	reader, err := d.client.ImagePull(context.Background(), image, opts)
-	defer reader.Close()
+	err := d.client.PullImage(opts, d.auth)
+	//defer reader.Close()
 	if err != nil {
 		return err
 	}
-	return readAndStream(reader, d.Ui)
+	//return readAndStream(reader, d.Ui)
+	return nil
 }
 
 func (d DockerApiDriver) Push(name string) error {
 
-	reader, err := d.client.ImagePush(context.Background(), name, types.ImagePushOptions{})
+	opts := godocker.PushImageOptions{
+		Name: name,
+		// Tag: "",
+		// Registry: "",
+		// OutputStream: reader,
+	}
+	err := d.client.PushImage(opts, d.auth)
 	if err != nil {
 		return err
 	}
-	return readAndStream(reader, d.Ui)
+	// return readAndStream(reader, d.Ui)
+	return nil
 }
 
 func (d DockerApiDriver) SaveImage(id string, dst io.Writer) error {
 
+	opts := godocker.ExportImageOptions{
+		Name:         id,
+		OutputStream: dst,
+	}
 	log.Printf("Exporting image: %s", id)
-	_, err := d.client.ImageSave(context.Background(), []string{id})
+	err := d.client.ExportImage(opts)
 	return err
 }
 
@@ -154,7 +163,7 @@ func (d DockerApiDriver) StartContainer(config *ContainerConfig) (string, error)
 	// 	args = append(args, "-v", fmt.Sprintf("%s:%s", host, guest))
 	// }
 
-	conf := container.Config{
+	conf := godocker.Config{
 		AttachStdout: false,
 		Tty:          true,
 		Env:          []string{},
@@ -162,52 +171,66 @@ func (d DockerApiDriver) StartContainer(config *ContainerConfig) (string, error)
 		Image:        config.Image,
 		//Volumes:      config.Volumes, // TODO
 	}
-	hostConf := container.HostConfig{
+	hostCfg := godocker.HostConfig{
 		Privileged: config.Privileged,
 	}
-	networkConf := network.NetworkingConfig{}
+	network := godocker.NetworkingConfig{}
+
+	opts := godocker.CreateContainerOptions{
+		Name:             config.Image,
+		Config:           &conf,
+		HostConfig:       &hostCfg,
+		NetworkingConfig: &network,
+	}
 
 	d.Ui.Message("Creating container")
 	//  -d -i -t {{.Image}} /bin/bash
-	resp, err := d.client.ContainerCreate(context.Background(), &conf, &hostConf,
-		&networkConf, "")
+	container, err := d.client.CreateContainer(opts)
 	if err != nil {
 		return "", err
 	}
-	for warning := range resp.Warnings {
-		log.Printf("Warning: %s\n", warning)
-	}
+	log.Printf("Created container: %s", container.ID)
+	// for warning := range resp.Warnings {
+	// 	log.Printf("Warning: %s\n", warning)
+	// }
 
 	d.Ui.Message("Starting container")
-	opts := types.ContainerStartOptions{}
-	err = d.client.ContainerStart(context.Background(), resp.ID, opts)
+	err = d.client.StartContainer(container.ID, nil)
 	if err != nil {
 		return "", err
 	}
 
-	return resp.ID, nil
+	return container.ID, nil
 }
 
 func (d DockerApiDriver) StopContainer(id string) error {
 
-	err := d.client.ContainerKill(context.Background(), id, "KILL")
+	err := d.client.KillContainer(godocker.KillContainerOptions{
+		ID:     id,
+		Signal: godocker.SIGKILL,
+	})
 	if err != nil {
 		return err
 	}
 
-	return d.client.ContainerRemove(context.Background(), id, types.ContainerRemoveOptions{})
+	return d.client.RemoveContainer(godocker.RemoveContainerOptions{ID: id})
 }
 
 func (d DockerApiDriver) TagImage(id string, repo string, force bool) error {
-	// TODO force
-	return d.client.ImageTag(context.Background(), id, repo)
+	return d.client.TagImage(id, godocker.TagImageOptions{
+		Repo:  repo,
+		Force: force,
+		// Tag: "",
+	})
 }
 
 func (d DockerApiDriver) Verify() error {
 	d.Ui.Say("Warning! You are running the EXPERMINATAL Docker API driver!")
-	var err error
-	d.client, err = client.NewEnvClient()
-	return err
+	// var err error
+	// d.client, err = client.NewEnvClient()
+	// return err
+	// TODO
+	return nil
 }
 
 func (d DockerApiDriver) Version() (*version.Version, error) {
