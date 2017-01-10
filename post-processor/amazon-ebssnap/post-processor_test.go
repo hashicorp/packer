508e8bb01939e8fa937401c1277c9ae18322dfcd
dnABic (Andreja Babic) <ababic@gmail.com>
2017-01-10 23:59:53 +0100

    Removing comments

--- post-processor/amazon-ebssnap/post-processor_test.go
+++ post-processor/amazon-ebssnap/post-processor_test.go
@@ -29,14 +29,3 @@ func testUi() *packer.BasicUi {
 func TestPostProcessor_ImplementsPostProcessor(t *testing.T) {
 	var _ packer.PostProcessor = new(PostProcessor)
 }
-
-// Test Helpers
-
-// See checksum and compress
-//func setup(t *testing.T) (packer.Ui, packer.Artifact, error) {
-//}
-//packer/post_processor_mock.go:// MockPostProcessor is an implementation of PostProcessor that can be
-//packer/post_processor_mock.go:type MockPostProcessor struct {
-//packer/post_processor_mock.go:func (t *MockPostProcessor) Configure(configs ...interface{}) error {
-//packer/post_processor_mock.go:func (t *MockPostProcessor) PostProcess(ui Ui, a Artifact) (Artifact, bool, error) {
-//packer/post_processor_mock.go:  return &MockArtifact{
