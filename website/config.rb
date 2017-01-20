1e23b22e7849e58ca058ef8188b3073faa168254
Matthew Hooker <mwhooker@gmail.com>
2017-01-20 12:19:24 -0800

    prep for 0.12.2 release

--- website/config.rb
+++ website/config.rb
@@ -2,6 +2,6 @@ set :base_url, "https://www.packer.io/"
 
 activate :hashicorp do |h|
   h.name        = "packer"
-  h.version     = "0.12.1"
+  h.version     = "0.12.2"
   h.github_slug = "mitchellh/packer"
 end
