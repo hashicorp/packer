4f040ed4a449c21f92ff84f272cd42ebb395639a
Alexander Laamanen <al@iki.fi>
2017-02-06 09:13:37 +0200

    Fix tests, move remote_ default values to driver_config.

--- builder/vmware/common/step_register_test.go
+++ builder/vmware/common/step_register_test.go
@@ -31,12 +31,9 @@ func TestStepRegister_regularDriver(t *testing.T) {
 
 func TestStepRegister_remoteDriver(t *testing.T) {
 	state := testState(t)
-	step := new(StepRegister)
+	step := &StepRegister{KeepRegistered: false}
 
 	driver := new(RemoteDriverMock)
-	var config Config
-	config.KeepRegistered = false
-	state.Put("config", &config)
 
 	state.Put("driver", driver)
 	state.Put("vmx_path", "foo")
@@ -71,12 +68,9 @@ func TestStepRegister_remoteDriver(t *testing.T) {
 }
 func TestStepRegister_WithoutUnregister_remoteDriver(t *testing.T) {
 	state := testState(t)
-	step := new(StepRegister)
+	step := &StepRegister{KeepRegistered: true}
 
 	driver := new(RemoteDriverMock)
-	var config Config
-	config.KeepRegistered = true
-	state.Put("config", &config)
 
 	state.Put("driver", driver)
 	state.Put("vmx_path", "foo")
