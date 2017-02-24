ce41055ac6b19c8040de9d8ec01e95eefeded5c2
Jimmy The Dog <github@kgibbs.co.uk>
2017-02-24 14:49:40 +0000

    builder/vmware-iso: do not append to output dir, as ovftool does that

--- builder/vmware/iso/step_export.go
+++ builder/vmware/iso/step_export.go
@@ -8,7 +8,6 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"runtime"
 	"strings"
 )
@@ -60,7 +59,7 @@ func (s *StepExport) Run(state multistep.StateBag) multistep.StepAction {
 
 	// Export the VM
 	localDir := state.Get("localDir").(localOutputDir)
-	outputPath := filepath.Join(localDir.dir, c.VMName+"."+s.Format)
+	outputPath := localDir.dir
 
 	if s.Format == "ova" {
 		os.MkdirAll(outputPath, 0755)
