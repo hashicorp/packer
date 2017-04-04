4835884721143186d2ecb2a497348e35714e32ac
Dan Fuchs <dfuchs@leapfrogonline.com>
2017-04-04 16:43:46 -0500

    inventory_directory option for ansible provisioner
    Add an `inventory_directory` setting to the Ansible provisioner that
    allows a user to specify a directory in which the Packer Ansible
    provisioner would write the generated inventory file.  If a value is
    specified for this setting, then have the Packer Ansible provisioner
    pass this directory as the -i arg when it calls ansible.
    
    This would allow an Ansible playbook used by the Packer Ansible
    provisioner to use variables specified in `host_vars` and `group_vars`
    in this inventory directory.

--- provisioner/ansible/provisioner_test.go
+++ provisioner/ansible/provisioner_test.go
@@ -76,6 +76,12 @@ func TestProvisionerPrepare_Defaults(t *testing.T) {
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
+
+	config["inventory_directory"] = "some_directory"
+	err = p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
 }
 
 func TestProvisionerPrepare_PlaybookFile(t *testing.T) {
@@ -243,6 +249,55 @@ func TestProvisionerPrepare_LocalPort(t *testing.T) {
 	err = p.Prepare(config)
 	if err != nil {
 		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvisioner_getInventoryArg(t *testing.T) {
+	var p Provisioner
+	config := testConfig(t)
+	defer os.Remove(config["command"].(string))
+
+	hostkey_file, err := ioutil.TempFile("", "hostkey")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer os.Remove(hostkey_file.Name())
+
+	publickey_file, err := ioutil.TempFile("", "publickey")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer os.Remove(publickey_file.Name())
+
+	playbook_file, err := ioutil.TempFile("", "playbook")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer os.Remove(playbook_file.Name())
+
+	config["ssh_host_key_file"] = hostkey_file.Name()
+	config["ssh_authorized_key_file"] = publickey_file.Name()
+	config["playbook_file"] = playbook_file.Name()
+
+	err = p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	p.config.inventoryFile = "somefile"
+	if p.getInventoryArg() != "somefile" {
+		t.Fatal("getInventoryArg should return inventoryFile if InventoryDirectory is not set.")
+	}
+
+	// Uses InventoryDirectory if set
+	config["inventory_directory"] = "somedirectory"
+	err = p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if p.getInventoryArg() != "somedirectory" {
+		t.Fatal("getInventoryArg should return InventoryDirectory if InventoryDirectory is set.")
 	}
 }
 
