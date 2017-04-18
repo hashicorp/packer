1e935bc2bd52007a43c49c7b8c1e7c8645878771
Weilu Jia <weilu.jia@gusto.com>
2017-04-17 18:15:53 -0700

    Add version selection for chef-solo provisioner in Windows too

--- provisioner/chef-solo/provisioner.go
+++ provisioner/chef-solo/provisioner.go
@@ -33,7 +33,7 @@ var guestOSTypeConfigs = map[string]guestOSTypeConfig{
 	},
 	provisioner.WindowsOSType: {
 		executeCommand: "c:/opscode/chef/bin/chef-solo.bat --no-color -c {{.ConfigPath}} -j {{.JsonPath}}",
-		installCommand: "powershell.exe -Command \". { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; install\"",
+		installCommand: "powershell.exe -Command \". { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; Install-Project{{if .Version}} -v {{.Version}}{{end}}\"",
 		stagingDir:     "C:/Windows/Temp/packer-chef-solo",
 	},
 }
