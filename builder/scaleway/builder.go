a8acbfb6a93cb40b31ffd42a17a5b349a6c7b20c
Edouard BONLIEU <ebonlieu@gmail.com>
2017-04-19 11:10:52 +0200

    Add existing SSH key support Update documentation

--- builder/scaleway/builder.go
+++ builder/scaleway/builder.go
@@ -43,8 +43,9 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 
 	steps := []multistep.Step{
 		&stepCreateSSHKey{
-			Debug:        b.config.PackerDebug,
-			DebugKeyPath: fmt.Sprintf("scw_%s.pem", b.config.PackerBuildName),
+			Debug:          b.config.PackerDebug,
+			DebugKeyPath:   fmt.Sprintf("scw_%s.pem", b.config.PackerBuildName),
+			PrivateKeyFile: b.config.Comm.SSHPrivateKey,
 		},
 		new(stepCreateServer),
 		new(stepServerInfo),
