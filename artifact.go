1d77b80f6bd0bd97eaee10e34d329b82490cc16b
LizaTretyakova <elizabet.tretyakova@gmail.com>
2017-04-22 11:36:34 +0300

    renamed BuilderId

--- artifact.go
+++ artifact.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-const BuilderId = "packer.post-processor.vsphere-device"
+const BuilderId = "LizaTretyakova.post-processor.vsphere-device"
 
 type ArtifactFile struct {
 	Name string `json:"name"`
