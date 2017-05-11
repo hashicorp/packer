94810a21e3a5dd5422b09f2f575d67ade740fb43
luis-silva <luissilva@Luiss-MacBook-Pro.local>
2017-05-11 16:12:27 +0100

    Fix issue 4875 - use retry logic when creating instance tags

--- builder/amazon/common/step_run_source_instance.go
+++ builder/amazon/common/step_run_source_instance.go
@@ -9,8 +9,10 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ec2"
 
+	retry "github.com/hashicorp/packer/common"
 	"github.com/hashicorp/packer/packer"
 	"github.com/hashicorp/packer/template/interpolate"
 	"github.com/mitchellh/multistep"
@@ -291,10 +293,23 @@ func (s *StepRunSourceInstance) Run(state multistep.StateBag) multistep.StepActi
 
 	ReportTags(ui, ec2Tags)
 
-	_, err = ec2conn.CreateTags(&ec2.CreateTagsInput{
-		Tags:      ec2Tags,
-		Resources: []*string{instance.InstanceId},
+	// Retry creating tags for about 2.5 minutes
+	err = retry.Retry(0.2, 30, 11, func() (bool, error) {
+		_, err := ec2conn.CreateTags(&ec2.CreateTagsInput{
+			Tags:      ec2Tags,
+			Resources: []*string{instance.InstanceId},
+		})
+		if err == nil {
+			return true, nil
+		}
+		if awsErr, ok := err.(awserr.Error); ok {
+			if awsErr.Code() == "InvalidInstanceID.NotFound" {
+				return false, nil
+			}
+		}
+		return true, err
 	})
+
 	if err != nil {
 		err := fmt.Errorf("Error tagging source instance: %s", err)
 		state.Put("error", err)
