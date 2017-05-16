9c3cfdeb78b81e7250e72ceee19a8bad87d1621a
Luke Farnell <lfarnell@outlook.com>
2017-05-16 17:05:40 -0400

    fixed logic error and fixed suggestion

--- builder/digitalocean/builder.go
+++ builder/digitalocean/builder.go
@@ -35,6 +35,16 @@ func (b *Builder) Prepare(raws ...interface{}) ([]string, error) {
 	return nil, nil
 }
 
+func contains(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
+}
+
 func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packer.Artifact, error) {
 	client := godo.NewClient(oauth2.NewClient(oauth2.NoContext, &apiTokenSource{
 		AccessToken: b.config.APIToken,
@@ -54,15 +64,34 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		}
 		regions, _, err := client.Regions.List(context.TODO(), opt)
 		if err != nil {
-			return nil, fmt.Errorf("DigitalOcean: Unable to get regions, %s.", err)
+			return nil, fmt.Errorf("DigitalOcean: Unable to get regions, %s", err)
 		}
-		for _, snapShotRegion := range b.config.SnapshotRegions {
-			for _, region := range regions {
-				if snapShotRegion == region.Slug {
-					continue
-				} else {
-					return nil, fmt.Errorf("DigitalOcean: Invalid region, %s.", snapShotRegion)
-				}
+
+		var dcs []string
+		for _, val := range regions {
+			dcs = append(dcs, val.Slug)
+		}
+
+		regionSet := make(map[string]struct{})
+		regionsMap := make([]string, 0, len(b.config.SnapshotRegions))
+		regionSet[b.config.Region] = struct{}{}
+		for _, region := range b.config.SnapshotRegions {
+			// If we already saw the region, then don't look again
+			if _, ok := regionSet[region]; ok {
+				continue
+			}
+
+			// Mark that we saw the region
+			regionSet[region] = struct{}{}
+
+			regionsMap = append(regionsMap, region)
+		}
+
+		for _, val := range regionsMap {
+			if contains(dcs, val) {
+				continue
+			} else {
+				return nil, fmt.Errorf("DigitalOcean: Invalid region, %s", val)
 			}
 		}
 	}
