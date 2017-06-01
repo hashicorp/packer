2372962892a56bd4b23a1837cc17bae867369db8
Megan Marsh <megan@hashicorp.com>
2017-06-01 11:25:53 -0700

    clean up formatting and add warning to packer push.

--- post-processor/atlas/post-processor.go
+++ post-processor/atlas/post-processor.go
@@ -145,9 +145,9 @@ func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (pac
 	// todo: remove/reword after the migration
 	if p.config.Type == "vagrant.box" {
 		ui.Message("\n-----------------------------------------------------------------------------------\n" +
-			"Warning: Vagrant-related functionality will be moved from Terraform Enterprise into " +
-			"its own product, Vagrant Cloud. This migration is currently planned for June 27th, " +
-			"2017 at 6PM EDT/3PM PDT/10PM UTC. For more information see " +
+			"Warning: Vagrant-related functionality will be moved from Terraform Enterprise into \n" +
+			"its own product, Vagrant Cloud. This migration is currently planned for June 27th, \n" +
+			"2017 at 6PM EDT/3PM PDT/10PM UTC. For more information see \n" +
 			"https://www.vagrantup.com/docs/vagrant-cloud/vagrant-cloud-migration.html\n" +
 			"-----------------------------------------------------------------------------------\n")
 	}
