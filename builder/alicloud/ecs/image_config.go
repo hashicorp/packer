2de00f2ff8091902ca015aebac5e700a5cb24a19
zhuzhih2017 <dongxiao.zzh@alibaba-inc.com>
2017-06-02 16:47:50 +0800

    add check rule for image_name

--- builder/alicloud/ecs/image_config.go
+++ builder/alicloud/ecs/image_config.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/denverdino/aliyungo/common"
 	"github.com/hashicorp/packer/template/interpolate"
+	"regexp"
+	"strings"
 )
 
 type AlicloudDiskDevice struct {
@@ -40,6 +42,15 @@ func (c *AlicloudImageConfig) Prepare(ctx *interpolate.Context) []error {
 	var errs []error
 	if c.AlicloudImageName == "" {
 		errs = append(errs, fmt.Errorf("image_name must be specified"))
+	} else if len(c.AlicloudImageName) < 2 || len(c.AlicloudImageName) > 128 {
+		errs = append(errs, fmt.Errorf("image_name must less than 128 letters and more than 1 letters"))
+	} else if strings.HasPrefix(c.AlicloudImageName, "http://") ||
+		strings.HasPrefix(c.AlicloudImageName, "https://") {
+		errs = append(errs, fmt.Errorf("image_name can't start with 'http://' or 'https://'"))
+	}
+	reg := regexp.MustCompile("\\s+")
+	if reg.FindString(c.AlicloudImageName) != "" {
+		errs = append(errs, fmt.Errorf("image_name can't include spaces"))
 	}
 
 	if len(c.AlicloudImageDestinationRegions) > 0 {
