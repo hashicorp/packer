dbd7aa6b95781501d69434f5f523d45d9eab04f6
Megan Marsh <megan@hashicorp.com>
2017-06-07 13:30:39 -0700

    capitalize all the things

--- command/push.go
+++ command/push.go
@@ -184,8 +184,8 @@ func (c *PushCommand) Run(args []string) int {
 				"its own product, Vagrant Cloud. This migration is currently planned for June 27th, \n" +
 				"2017 at 6PM EDT/3PM PDT/10PM UTC. For more information see \n" +
 				"https://www.vagrantup.com/docs/vagrant-cloud/vagrant-cloud-migration.html\n" +
-				"In the meantime, you should activate your vagrant cloud account and start replacing your \n" +
-				"Atlas post-processor with the vagrant cloud post-processor. See\n" +
+				"In the meantime, you should activate your Vagrant Cloud account and replace your \n" +
+				"Atlas post-processor with the Vagrant Cloud post-processor. See\n" +
 				"https://www.packer.io/docs/post-processors/vagrant-cloud.html for more details." +
 				"-----------------------------------------------------------------------------------\n")
 		}
