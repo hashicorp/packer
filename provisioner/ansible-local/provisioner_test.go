1060f252d0ff7372698aa01ff282a61c1805ad92
localghost <localghost@codeleet.com>
2017-07-03 23:44:10 +0200

    Test for ansible-local playbook_files with mocked Communicator.

--- provisioner/ansible-local/provisioner_test.go
+++ provisioner/ansible-local/provisioner_test.go
@@ -7,12 +7,44 @@ import (
 	"strings"
 	"testing"
 
+	"fmt"
 	"github.com/hashicorp/packer/packer"
 )
 
 func testConfig() map[string]interface{} {
 	m := make(map[string]interface{})
 	return m
+}
+
+func createTempFile() string {
+	file, err := ioutil.TempFile("", "")
+	if err != nil {
+		panic(fmt.Sprintf("err: %s", err))
+	}
+	return file.Name()
+}
+
+func createTempFiles(numFiles int) []string {
+	files := make([]string, 0, numFiles)
+	defer func() {
+		// Cleanup the files if not all were created.
+		if len(files) < numFiles {
+			for _, file := range files {
+				os.Remove(file)
+			}
+		}
+	}()
+
+	for i := 0; i < numFiles; i++ {
+		files = append(files, createTempFile())
+	}
+	return files
+}
+
+func removeFiles(files ...string) {
+	for _, file := range files {
+		os.Remove(file)
+	}
 }
 
 func TestProvisioner_Impl(t *testing.T) {
@@ -115,6 +147,58 @@ func TestProvisionerPrepare_PlaybookFiles(t *testing.T) {
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
+}
+
+func assertPlaybooksExecuted(comm *communicatorMock, playbooks []string) {
+	cmdIndex := 0
+	for _, playbook := range playbooks {
+		for ; cmdIndex < len(comm.startCommand); cmdIndex++ {
+			cmd := comm.startCommand[cmdIndex]
+			if strings.Contains(cmd, "ansible-playbook") && strings.Contains(cmd, playbook) {
+				break
+			}
+		}
+		if cmdIndex == len(comm.startCommand) {
+			panic(fmt.Sprintf("Playbook %s was not executed", playbook))
+		}
+	}
+}
+
+func assertPlaybooksUploaded(comm *communicatorMock, playbooks []string) {
+	uploadIndex := 0
+	for _, playbook := range playbooks {
+		for ; uploadIndex < len(comm.uploadDestination); uploadIndex++ {
+			dest := comm.uploadDestination[uploadIndex]
+			if strings.HasSuffix(dest, playbook) {
+				break
+			}
+		}
+		if uploadIndex == len(comm.uploadDestination) {
+			panic(fmt.Sprintf("Playbook %s was not uploaded", playbook))
+		}
+	}
+}
+
+func TestProvisionerProvision_PlaybookFiles(t *testing.T) {
+	var p Provisioner
+	config := testConfig()
+
+	playbooks := createTempFiles(3)
+	defer removeFiles(playbooks...)
+
+	config["playbook_files"] = playbooks
+	err := p.Prepare(config)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	comm := &communicatorMock{}
+	if err := p.Provision(&uiStub{}, comm); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	assertPlaybooksUploaded(comm, playbooks)
+	assertPlaybooksExecuted(comm, playbooks)
 }
 
 func TestProvisionerPrepare_InventoryFile(t *testing.T) {
