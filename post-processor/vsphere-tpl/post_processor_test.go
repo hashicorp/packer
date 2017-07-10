6ca5fb0f18cacd32da69c3069219eb0b1ebc177c
bugbuilder <nelson@bennu.cl>
2017-07-09 22:13:31 -0400

    format

--- post-processor/vsphere-tpl/post_processor_test.go
+++ post-processor/vsphere-tpl/post_processor_test.go
@@ -1,1 +1,19 @@
 package vsphere_tpl
+
+import (
+	"testing"
+)
+
+func TestConfigureURL(t *testing.T) {
+	var p PostProcessor
+	p.config.Username = "me"
+	p.config.Password = "notpassword"
+	p.config.Host = "myhost"
+	p.config.Datacenter = "mydc"
+	p.config.VMName = "my vm"
+	p.config.Insecure = true
+
+	if err := p.configureURL(); err == nil {
+		t.Errorf("Error: %s", err)
+	}
+}
