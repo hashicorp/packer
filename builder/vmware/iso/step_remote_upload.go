7dd9305781c377d8227277d67e26a3fd6777056a
bugbuilder <nelson@bennu.cl>
2017-07-24 00:11:30 -0400

    Making visible verify cache step

--- builder/vmware/iso/step_remote_upload.go
+++ builder/vmware/iso/step_remote_upload.go
@@ -3,23 +3,29 @@ package iso
 import (
 	"fmt"
 	"log"
-	"strings"
 
 	vmwcommon "github.com/hashicorp/packer/builder/vmware/common"
 	"github.com/hashicorp/packer/packer"
 	"github.com/mitchellh/multistep"
 )
 
-// stepRemoteUpload uploads some thing from the state bag to a remote driver
-// (if it can) and stores that new remote path into the state bag.
+// stepRemoteUpload uploads some thing from the state bag to a remoteUpload driver
+// (if it can) and stores that new remoteUpload path into the state bag.
 type stepRemoteUpload struct {
 	Key     string
 	Message string
+
+	// Set this to true for skip
+	Skip bool
 }
 
 func (s *stepRemoteUpload) Run(state multistep.StateBag) multistep.StepAction {
 	driver := state.Get("driver").(vmwcommon.Driver)
 	ui := state.Get("ui").(packer.Ui)
+
+	if s.Skip {
+		return multistep.ActionContinue
+	}
 
 	remote, ok := driver.(RemoteDriver)
 	if !ok {
@@ -35,20 +41,16 @@ func (s *stepRemoteUpload) Run(state multistep.StateBag) multistep.StepAction {
 	checksum := config.ISOChecksum
 	checksumType := config.ISOChecksumType
 
-	if !strings.HasPrefix(path, "skip_upload:") {
-		ui.Say(s.Message)
-		log.Printf("Remote uploading: %s", path)
-		newPath, err := remote.UploadISO(path, checksum, checksumType)
-		if err != nil {
-			err := fmt.Errorf("Error uploading file: %s", err)
-			state.Put("error", err)
-			ui.Error(err.Error())
-			return multistep.ActionHalt
-		}
-		state.Put(s.Key, newPath)
-	} else {
-		state.Put(s.Key, strings.Split(path, "skip_upload:")[1])
+	ui.Say(s.Message)
+	log.Printf("Remote uploading: %s", path)
+	newPath, err := remote.UploadISO(path, checksum, checksumType)
+	if err != nil {
+		err := fmt.Errorf("Error uploading file: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
 	}
+	state.Put(s.Key, newPath)
 
 	return multistep.ActionContinue
 }
