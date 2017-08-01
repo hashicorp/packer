d7f2044092ad27c257f8dfcf16908991536d4d73
Sander Saares <saares@axinom.com>
2017-08-01 12:23:19 +0300

    Enable use of separate temp path for Hyper-V VHD

--- builder/hyperv/common/driver_ps_4.go
+++ builder/hyperv/common/driver_ps_4.go
@@ -166,8 +166,8 @@ func (d *HypervPS4Driver) CreateVirtualSwitch(switchName string, switchType stri
 	return hyperv.CreateVirtualSwitch(switchName, switchType)
 }
 
-func (d *HypervPS4Driver) CreateVirtualMachine(vmName string, path string, ram int64, diskSize int64, switchName string, generation uint) error {
-	return hyperv.CreateVirtualMachine(vmName, path, ram, diskSize, switchName, generation)
+func (d *HypervPS4Driver) CreateVirtualMachine(vmName string, path string, vhdPath string, ram int64, diskSize int64, switchName string, generation uint) error {
+	return hyperv.CreateVirtualMachine(vmName, path, vhdPath, ram, diskSize, switchName, generation)
 }
 
 func (d *HypervPS4Driver) DeleteVirtualMachine(vmName string) error {
