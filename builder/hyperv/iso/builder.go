dd534767ec3fcc39f170e3088ad77adcea2db394
Tobias <tsmolka@gmail.com>
2017-08-08 13:21:53 +0000

    fix formatting

--- builder/hyperv/iso/builder.go
+++ builder/hyperv/iso/builder.go
@@ -92,7 +92,7 @@ type Config struct {
 // Prepare processes the build configuration parameters.
 func (b *Builder) Prepare(raws ...interface{}) ([]string, error) {
 	err := config.Decode(&b.config, &config.DecodeOpts{
-		Interpolate: true,
+		Interpolate:        true,
 		InterpolateContext: &b.config.ctx,
 		InterpolateFilter: &interpolate.RenderFilter{
 			Exclude: []string{
