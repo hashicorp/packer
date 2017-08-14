a09e1bde1f595065f38a661cabcffd5dbb45bba5
Megan Marsh <megan@hashicorp.com>
2017-08-14 09:20:08 -0700

    make force_deregister work in all regions, not just original region.

--- builder/amazon/ebs/builder.go
+++ builder/amazon/ebs/builder.go
@@ -182,9 +182,11 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 			EnableEnhancedNetworking: b.config.AMIEnhancedNetworking,
 		},
 		&awscommon.StepDeregisterAMI{
+			AccessConfig:        &b.config.AccessConfig,
 			ForceDeregister:     b.config.AMIForceDeregister,
 			ForceDeleteSnapshot: b.config.AMIForceDeleteSnapshot,
 			AMIName:             b.config.AMIName,
+			Regions:             b.config.AMIRegions,
 		},
 		&stepCreateAMI{},
 		&awscommon.StepCreateEncryptedAMICopy{
