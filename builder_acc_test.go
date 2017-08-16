9f9e0e9ae7a8fa0e1cdb48abf66338a69ee7d5b8
Michael Kuzmin <michael.kuzmin@jetbrains.com>
2017-08-16 20:38:59 +0300

    fix test

--- builder_acc_test.go
+++ builder_acc_test.go
@@ -17,14 +17,14 @@ const testBuilderAccBasic = `
 	"builders": [{
 		"type": "test",
 
-		"vcenter_server": "vcenter.vsphere5.test",
+		"vcenter_server": "vcenter.vsphere55.test",
 		"username": "root",
 		"password": "jetbrains",
 		"insecure_connection": true,
 
 		"template": "basic",
 		"vm_name": "test1",
-		"host": "esxi-1.vsphere5.test",
+		"host": "esxi-1.vsphere55.test",
 
 		"ssh_username": "jetbrains",
 		"ssh_password": "jetbrains"
