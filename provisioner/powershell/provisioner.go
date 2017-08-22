f932b78b7cb6f49661be57208a043ee477091f97
Megan Marsh <megan@hashicorp.com>
2017-08-22 14:03:28 -0700

    make every script name unique

--- provisioner/powershell/provisioner.go
+++ provisioner/powershell/provisioner.go
@@ -21,8 +21,6 @@ import (
 	"github.com/hashicorp/packer/template/interpolate"
 )
 
-const DefaultRemotePath = "c:/Windows/Temp/script.ps1"
-
 var retryableSleep = 2 * time.Second
 
 type Config struct {
@@ -137,7 +135,8 @@ func (p *Provisioner) Prepare(raws ...interface{}) error {
 	}
 
 	if p.config.RemotePath == "" {
-		p.config.RemotePath = DefaultRemotePath
+		uuid := uuid.TimeOrderedUUID()
+		p.config.RemotePath = fmt.Sprintf(`c:/Windows/Temp/script-%s.ps1`, uuid)
 	}
 
 	if p.config.Scripts == nil {
