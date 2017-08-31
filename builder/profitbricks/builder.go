e45223a86757573070ded161dca08a7e56ad5539
Vijaya Bhaskar Reddy Kondreddi <vijaya.reddy@ni.com>
2017-08-31 23:13:00 +0530

    Fix -on-error issue not working.
    When we supply -on-error flag for Packer with Hyper-v builder, it does not do anything.

--- builder/profitbricks/builder.go
+++ builder/profitbricks/builder.go
@@ -49,15 +49,7 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 
 	config := state.Get("config").(*Config)
 
-	if b.config.PackerDebug {
-		b.runner = &multistep.DebugRunner{
-			Steps:   steps,
-			PauseFn: common.MultistepDebugFn(ui),
-		}
-	} else {
-		b.runner = &multistep.BasicRunner{Steps: steps}
-	}
-
+	b.runner = common.NewRunner(steps, b.config.PackerConfig, ui)
 	b.runner.Run(state)
 
 	if rawErr, ok := state.GetOk("error"); ok {
