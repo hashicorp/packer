07bb47f793a073c0d0e7ad5ee12ebd4984c780d6
Matthew Hooker <mwhooker@gmail.com>
2017-09-02 16:09:29 -0700

    Replace pkcs12 code with upstream

--- builder/azure/pkcs12/rc2/rc2_test.go
+++ builder/azure/pkcs12/rc2/rc2_test.go
@@ -1,3 +1,7 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package rc2
 
 import (
@@ -8,6 +12,7 @@ import (
 
 func TestEncryptDecrypt(t *testing.T) {
 
+	// TODO(dgryski): add the rest of the test vectors from the RFC
 	var tests = []struct {
 		key    string
 		plain  string
@@ -84,22 +89,5 @@ func TestEncryptDecrypt(t *testing.T) {
 		if !bytes.Equal(dst[:], p) {
 			t.Errorf("decrypt failed: got % 2x wanted % 2x\n", dst, p)
 		}
-	}
-}
-
-func BenchmarkEncrypt(b *testing.B) {
-	r, _ := New([]byte{0, 0, 0, 0, 0, 0, 0, 0}, 64)
-	b.ResetTimer()
-	var src [8]byte
-	for i := 0; i < b.N; i++ {
-		r.Encrypt(src[:], src[:])
-	}
-}
-func BenchmarkDecrypt(b *testing.B) {
-	r, _ := New([]byte{0, 0, 0, 0, 0, 0, 0, 0}, 64)
-	b.ResetTimer()
-	var src [8]byte
-	for i := 0; i < b.N; i++ {
-		r.Decrypt(src[:], src[:])
 	}
 }
