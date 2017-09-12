0e7ed2cb9b5c654af32125e0485a676362a7a168
Andrew Pryde <andrew.pryde@oracle.com>
2017-09-12 16:30:39 +0100

    Rename Oracle BMCS builder to OCI
    Oracle Bare Metal Cloud Services (BMCS) has been rebranded as Oracle
    Cloud Infrastructure (OCI).

---
+++ builder/oracle/oci/step_image_test.go
@@ -0,0 +1,70 @@
+package oci
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/mitchellh/multistep"
+)
+
+func TestStepImage(t *testing.T) {
+	state := testState()
+	state.Put("instance_id", "ocid1...")
+
+	step := new(stepImage)
+	defer step.Cleanup(state)
+
+	if action := step.Run(state); action != multistep.ActionContinue {
+		t.Fatalf("bad action: %#v", action)
+	}
+
+	if _, ok := state.GetOk("image"); !ok {
+		t.Fatalf("should have image")
+	}
+}
+
+func TestStepImage_CreateImageErr(t *testing.T) {
+	state := testState()
+	state.Put("instance_id", "ocid1...")
+
+	step := new(stepImage)
+	defer step.Cleanup(state)
+
+	driver := state.Get("driver").(*driverMock)
+	driver.CreateImageErr = errors.New("error")
+
+	if action := step.Run(state); action != multistep.ActionHalt {
+		t.Fatalf("bad action: %#v", action)
+	}
+
+	if _, ok := state.GetOk("error"); !ok {
+		t.Fatalf("should have error")
+	}
+
+	if _, ok := state.GetOk("image"); ok {
+		t.Fatalf("should NOT have image")
+	}
+}
+
+func TestStepImage_WaitForImageCreationErr(t *testing.T) {
+	state := testState()
+	state.Put("instance_id", "ocid1...")
+
+	step := new(stepImage)
+	defer step.Cleanup(state)
+
+	driver := state.Get("driver").(*driverMock)
+	driver.WaitForImageCreationErr = errors.New("error")
+
+	if action := step.Run(state); action != multistep.ActionHalt {
+		t.Fatalf("bad action: %#v", action)
+	}
+
+	if _, ok := state.GetOk("error"); !ok {
+		t.Fatalf("should have error")
+	}
+
+	if _, ok := state.GetOk("image"); ok {
+		t.Fatalf("should not have image")
+	}
+}
