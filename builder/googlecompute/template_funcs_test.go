dfddbf0d6bd58a8ef5036a065ff7885575baaf33
Megan Marsh <megan@hashicorp.com>
2017-10-20 14:06:02 -0700

    add a couple of extra tests and reword documentation

--- builder/googlecompute/template_funcs_test.go
+++ builder/googlecompute/template_funcs_test.go
@@ -7,17 +7,32 @@ func Test_templateCleanImageName(t *testing.T) {
 		origName string
 		expected string
 	}{
+		// test that valid name is unchanged
 		{
 			origName: "abcde-012345xyz",
 			expected: "abcde-012345xyz",
 		},
+
+		//test that capital letters are converted to lowercase
 		{
 			origName: "ABCDE-012345xyz",
 			expected: "abcde-012345xyz",
 		},
+		// test that periods and colons are converted to hyphens
 		{
-			origName: "abcde-012345v1.0.0",
+			origName: "abcde-012345v1.0:0",
 			expected: "abcde-012345v1-0-0",
+		},
+		// Name starting with number is not valid, but not in scope of this
+		// function to correct
+		{
+			origName: "012345v1.0:0",
+			expected: "012345v1-0-0",
+		},
+		// Name over 64 chars is not valid, but not corrected by this function.
+		{
+			origName: "loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong",
+			expected: "loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong",
 		},
 	}
 
