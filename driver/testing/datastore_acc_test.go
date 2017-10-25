dc5a2e52cd37eab787a305afd8c9d5f227769458
Andrey Tonkikh <xosmig@gmail.com>
2017-10-26 00:38:33 +0300

    Simplify datastore test

--- driver/testing/datastore_acc_test.go
+++ driver/testing/datastore_acc_test.go
@@ -2,29 +2,17 @@ package testing
 
 import (
 	"testing"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/jetbrains-infra/packer-builder-vsphere/driver"
 )
 
-var infoParams = [][]string {
-	//{}, // FIXME: Doesn't work
-	//{"*"}, // FIXME: Doesn't work
-	{"host", "vm"}, // TODO: choose something more meaningful
-}
-
-func TestDatastore(t *testing.T) {
+func TestDatastoreAcc(t *testing.T) {
 	initDriverAcceptanceTest(t)
 
 	d := NewTestDriver(t)
 	ds, err := d.FindDatastore(DefaultDatastore)
 	if err != nil {
-		t.Fatalf("`FindDatatore` can't find default datastore '%v'. Error: %v",
-			DefaultDatastore, err)
+		t.Fatalf("Cannot find default datastore '%v': %v", DefaultDatastore, err)
 	}
-	for _, params := range infoParams {
-		_, err := ds.Info(params...)
-		if err != nil {
-			t.Errorf("Cannot read datastore properties with parameters %v: %v",
-				params, err)
-		}
-	}
-	// TODO: add more checks
+	CheckDatastoreName(t, ds, DefaultDatastore)
 }
