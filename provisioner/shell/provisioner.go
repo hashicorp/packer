bed52617138ae90c0824b2e7dfe347e993c58e01
Matthew Hooker <mwhooker@gmail.com>
2017-10-26 10:41:49 -0700

    rephrase log message.

--- provisioner/shell/provisioner.go
+++ provisioner/shell/provisioner.go
@@ -284,7 +284,9 @@ func (p *Provisioner) Provision(ui packer.Ui, comm packer.Communicator) error {
 		if cmd.ExitStatus == packer.CmdDisconnect {
 			if !p.config.ExpectDisconnect {
 				return fmt.Errorf("Script disconnected unexpectedly. " +
-					"Try adding \"expect_disconnect\": true in the shell provisioner parameters.")
+					"If you expected your script to disconnect, i.e. from a " +
+					"restart, you can try adding `\"expect_disconnect\": true` " +
+					"to the shell provisioner parameters.")
 			}
 		} else if cmd.ExitStatus != 0 {
 			return fmt.Errorf("Script exited with non-zero exit status: %d", cmd.ExitStatus)
