681585d5f282ac63b9d126b326f0e7ea45211360
Edward <freesky.edward@gmail.com>
2017-11-17 10:28:15 +0800

    Remove the deprecated extensions
    The Nova extension API was deprecated from OpenStack N release.
    this parts of code cannot work well with the newest OpenStack version.
    
    This patch is to remove the relative parts:
    1. Remove the step_load_extensions.go
    2. Remove the step of extension from builder.go
    3. Remove the parameter parsing from step_stop_server.go
    
    Resolves: #5581

--- builder/openstack/step_stop_server.go
+++ builder/openstack/step_stop_server.go
@@ -15,14 +15,7 @@ type StepStopServer struct{}
 func (s *StepStopServer) Run(_ context.Context, state multistep.StateBag) multistep.StepAction {
 	ui := state.Get("ui").(packer.Ui)
 	config := state.Get("config").(Config)
-	extensions := state.Get("extensions").(map[string]struct{})
 	server := state.Get("server").(*servers.Server)
-
-	// Verify we have the extension
-	if _, ok := extensions["os-server-start-stop"]; !ok {
-		ui.Say("OpenStack cluster doesn't support stop, skipping...")
-		return multistep.ActionContinue
-	}
 
 	// We need the v2 compute client
 	client, err := config.computeV2Client()
