70f7e251a30f15cc8f67f89d18e11a5b448ebc12
유성덕 <sungduk.yu@navercorp.com>
2018-01-11 18:57:53 +0900

    Support Naver Cloud Platform

---
+++ builder/ncloud/step_get_rootpassword.go
@@ -0,0 +1,57 @@
+package ncloud
+
+import (
+	"fmt"
+
+	ncloud "github.com/NaverCloudPlatform/ncloud-sdk-go/sdk"
+	"github.com/hashicorp/packer/packer"
+	"github.com/mitchellh/multistep"
+)
+
+type StepGetRootPassword struct {
+	Conn            *ncloud.Conn
+	GetRootPassword func(serverInstanceNo string, privateKey string) (string, error)
+	Say             func(message string)
+	Error           func(e error)
+}
+
+func NewStepGetRootPassword(conn *ncloud.Conn, ui packer.Ui) *StepGetRootPassword {
+	var step = &StepGetRootPassword{
+		Conn:  conn,
+		Say:   func(message string) { ui.Say(message) },
+		Error: func(e error) { ui.Error(e.Error()) },
+	}
+
+	step.GetRootPassword = step.getRootPassword
+
+	return step
+}
+
+func (s *StepGetRootPassword) getRootPassword(serverInstanceNo string, privateKey string) (string, error) {
+	reqParams := new(ncloud.RequestGetRootPassword)
+	reqParams.ServerInstanceNo = serverInstanceNo
+	reqParams.PrivateKey = privateKey
+
+	rootPassword, err := s.Conn.GetRootPassword(reqParams)
+	if err != nil {
+		return "", fmt.Errorf("error code: %d, error message: %s", rootPassword.ReturnCode, rootPassword.ReturnMessage)
+	}
+
+	return rootPassword.RootPassword, nil
+}
+
+func (s *StepGetRootPassword) Run(state multistep.StateBag) multistep.StepAction {
+	s.Say("Get Root Password")
+
+	serverInstanceNo := state.Get("InstanceNo").(string)
+	loginKey := state.Get("LoginKey").(*LoginKey)
+
+	rootPassword, err := s.GetRootPassword(serverInstanceNo, loginKey.PrivateKey)
+
+	state.Put("Password", rootPassword)
+
+	return processStepResult(err, s.Error, state)
+}
+
+func (*StepGetRootPassword) Cleanup(multistep.StateBag) {
+}
