d513f8212ed1f4ff99aed7fe7a53acb241d44513
Megan Marsh <megan@hashicorp.com>
2018-01-16 16:55:39 -0800

    stub out step_snapshot

--- builder/oracle/classic/step_create_instance.go
+++ builder/oracle/classic/step_create_instance.go
@@ -1,65 +1,37 @@
 package classic
 
 import (
-	"bytes"
 	"fmt"
-	"text/template"
 
+	"github.com/hashicorp/go-oracle-terraform/compute"
 	"github.com/hashicorp/packer/packer"
 	"github.com/mitchellh/multistep"
 )
 
-type instanceOptions struct {
-	Username       string
-	IdentityDomain string
-	SshKey         string
-	Shape          string
-	ImageList      string
-	InstanceIP     string
-}
-
-var instanceTemplate = template.Must(template.New("instanceRequestBody").Parse(`
-{
-  "instances": [{
-      "shape": "{{.Shape}}",
-      "sshkeys": ["/Compute-{{.IdentityDomain}}/{{Username}}/{{.SshKey}}"],
-      "name": "Compute-{{.IdentityDomain}}/{{Username}}/packer-instance",
-      "label": "packer-instance",
-      "imagelist": "/Compute-{{.IdentityDomain}}/{{Username}}/{{.ImageList}}",
-      "networking": {
-        "eth0": {
-          "nat": "ipreservation:/Compute-{{.IdentityDomain}}/{{Username}}/{{.InstanceIP}}"
-        }
-      }
-  }]
-}
-`))
-
 type stepCreateInstance struct{}
 
-func (s *stepCreateIPReservation) Run(state multistep.StateBag) multistep.StepAction {
-	ui := state.Get("ui").(packer.Ui)
-	config := state.Get("config").(Config)
-	const endpoint_path = "/launchplan/" // POST
-
+func (s *stepCreateInstance) Run(state multistep.StateBag) multistep.StepAction {
 	ui.Say("Creating Instance...")
 
-	// generate launch plan definition for this instance
-	var buffer bytes.Buffer
-	err = instanceTemplate.Execute(&buffer, instanceOptions{
-		Username:       config.Username,
-		IdentityDomain: config.IdentityDomain,
-		SshKey:         config.SshKey,
-		Shape:          config.Shape,
-		ImageList:      config.ImageList,
-	})
-	if err != nil {
-		fmt.Printf("Error creating launch plan definition: %s", err)
-		return "", err
+	// get variables from state
+	ui := state.Get("ui").(packer.Ui)
+	config := state.Get("config").(Config)
+	client := state.Get("client").(*compute.ComputeClient)
+	sshPublicKey := state.Get("publicKey").(string)
+
+	// get instances client
+	instanceClient := client.Instances()
+
+	// Instances Input
+	input := &compute.CreateInstanceInput{
+		Name:       config.ImageName,
+		Shape:      config.Shape,
+		ImageList:  config.ImageList,
+		SSHKeys:    []string{},
+		Attributes: map[string]interface{}{},
 	}
-	// for API docs see
-	// https://docs.oracle.com/en/cloud/iaas/compute-iaas-cloud/stcsa/op-launchplan--post.html
-	instanceID, err := client.CreateInstance(publicKey)
+
+	instanceInfo, err := instanceClient.CreateInstance(input)
 	if err != nil {
 		err = fmt.Errorf("Problem creating instance: %s", err)
 		ui.Error(err.Error())
@@ -67,7 +39,7 @@ func (s *stepCreateIPReservation) Run(state multistep.StateBag) multistep.StepAc
 		return multistep.ActionHalt
 	}
 
-	state.Put("instance_id", instanceID)
+	state.Put("instance_id", instanceInfo.ID)
 
 	ui.Say(fmt.Sprintf("Created instance (%s).", instanceID))
 }
