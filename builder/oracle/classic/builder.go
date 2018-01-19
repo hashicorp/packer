69ba710c2a1661eab9f26239d445b2666864dfb0
Megan Marsh <megan@hashicorp.com>
2018-01-19 15:37:06 -0800

    PROGRESS!  Now it only fails on the snapshot step

--- builder/oracle/classic/builder.go
+++ builder/oracle/classic/builder.go
@@ -3,7 +3,6 @@ package classic
 import (
 	"fmt"
 	"log"
-	"strings"
 
 	"github.com/hashicorp/go-cleanhttp"
 	"github.com/hashicorp/go-oracle-terraform/compute"
@@ -64,7 +63,7 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		&ocommon.StepKeyPair{
 			Debug:          b.config.PackerDebug,
 			DebugKeyPath:   fmt.Sprintf("oci_classic_%s.pem", b.config.PackerBuildName),
-			PrivateKeyFile: strings.TrimSpace(b.config.Comm.SSHPrivateKey),
+			PrivateKeyFile: b.config.Comm.SSHPrivateKey,
 		},
 		&stepCreateIPReservation{},
 		&stepCreateInstance{},
