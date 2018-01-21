e688888694a227acf57bab570b17424d1e8a564c
scott-pickthorn <scott.e.pickthorn@gmail.com>
2018-01-20 21:40:35 -0800

    VCenter is now an argument to VSphere post-processor

--- post-processor/vsphere/post-processor_test.go
+++ post-processor/vsphere/post-processor_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestArgs(t *testing.T) {
+func TestArgsWithHost(t *testing.T) {
 	var p PostProcessor
 
 	p.config.Username = "me"
@@ -26,6 +26,40 @@ func TestArgs(t *testing.T) {
 		url.QueryEscape(p.config.Username),
 		url.QueryEscape(p.config.Password),
 		p.config.Host,
+		p.config.Datacenter,
+		p.config.Cluster)
+
+	if p.config.ResourcePool != "" {
+		ovftool_uri += "/Resources/" + p.config.ResourcePool
+	}
+
+	args, err := p.BuildArgs(source, ovftool_uri)
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+
+	t.Logf("ovftool %s", strings.Join(args, " "))
+}
+
+func TestArgsWithVCenter(t *testing.T) {
+	var p PostProcessor
+
+	p.config.Username = "me"
+	p.config.Password = "notpassword"
+	p.config.Datacenter = "mydc"
+	p.config.Cluster = "mycluster"
+	p.config.VMName = "my vm"
+	p.config.Datastore = "my datastore"
+	p.config.Insecure = true
+	p.config.DiskMode = "thin"
+	p.config.VCenterIp = "192.000.0.000"
+	p.config.VMFolder = "my folder"
+
+	source := "something.vmx"
+	ovftool_uri := fmt.Sprintf("vi://%s:%s@%s/%s/host/%s",
+		url.QueryEscape(p.config.Username),
+		url.QueryEscape(p.config.Password),
+		p.config.vCenterIp,
 		p.config.Datacenter,
 		p.config.Cluster)
 
