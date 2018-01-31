9139a6029b22c510ee1dad6db127e696cb8e5d10
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2018-01-31 20:29:03 +0300

    Add support for multiple cdroms for iso builder

--- iso/step_add_cdrom.go
+++ iso/step_add_cdrom.go
@@ -4,10 +4,11 @@ import (
 	"github.com/hashicorp/packer/packer"
 	"github.com/jetbrains-infra/packer-builder-vsphere/driver"
 	"github.com/mitchellh/multistep"
+	"fmt"
 )
 
 type CDRomConfig struct {
-	ISOPath string `mapstructure:"iso_path"`
+	ISOPaths []string `mapstructure:"iso_paths"`
 }
 
 func (c *CDRomConfig) Prepare() []error {
@@ -21,13 +22,14 @@ type StepAddCDRom struct {
 func (s *StepAddCDRom) Run(state multistep.StateBag) multistep.StepAction {
 	ui := state.Get("ui").(packer.Ui)
 
-	ui.Say("Adding CDRom...")
+	ui.Say("Adding CDRoms...")
 
 	vm := state.Get("vm").(*driver.VirtualMachine)
-	err := vm.AddCdrom(s.config.ISOPath)
-	if err != nil {
-		state.Put("error", err)
-		return multistep.ActionHalt
+	for _, path := range s.config.ISOPaths {
+		if err := vm.AddCdrom(path); err != nil {
+			state.Put("error", fmt.Errorf("error adding a cdrom: %v", err))
+			return multistep.ActionHalt
+		}
 	}
 
 	return multistep.ActionContinue
