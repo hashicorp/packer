100deef512502050e3fd98940e20aa4d462a9425
Evan Machnic <emachnic@gmail.com>
2018-01-31 11:01:08 -0600

    Added policyGroup and policyName to Packer Communicator

--- provisioner/chef-client/provisioner.go
+++ provisioner/chef-client/provisioner.go
@@ -350,6 +350,8 @@ func (p *Provisioner) createConfig(
 	remoteKeyPath string,
 	validationClientName string,
 	chefEnvironment string,
+  policyGroup string,
+  policyName string,
 	sslVerifyMode string,
 	trustedCertsDir string) (string, error) {
 
