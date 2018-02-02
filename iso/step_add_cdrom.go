78d53c81ce0c88e08bb8942e2b1f115c4dff707a
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2018-02-02 21:04:32 +0300

    Remove cdroms and floppy in the cleanup stage of iso builder

--- iso/step_add_cdrom.go
+++ iso/step_add_cdrom.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jetbrains-infra/packer-builder-vsphere/driver"
 	"github.com/mitchellh/multistep"
 	"fmt"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
 type CDRomConfig struct {
@@ -21,10 +22,9 @@ type StepAddCDRom struct {
 
 func (s *StepAddCDRom) Run(state multistep.StateBag) multistep.StepAction {
 	ui := state.Get("ui").(packer.Ui)
+	vm := state.Get("vm").(*driver.VirtualMachine)
 
 	ui.Say("Adding CDRoms...")
-
-	vm := state.Get("vm").(*driver.VirtualMachine)
 	for _, path := range s.Config.ISOPaths {
 		if err := vm.AddCdrom(path); err != nil {
 			state.Put("error", fmt.Errorf("error adding a cdrom: %v", err))
@@ -36,5 +36,15 @@ func (s *StepAddCDRom) Run(state multistep.StateBag) multistep.StepAction {
 }
 
 func (s *StepAddCDRom) Cleanup(state multistep.StateBag) {
-	// nothing
+	ui := state.Get("ui").(packer.Ui)
+	vm := state.Get("vm").(*driver.VirtualMachine)
+
+	devices, err := vm.Devices()
+	if err != nil {
+		ui.Error(fmt.Sprintf("error removing cdroms: %v", err))
+	}
+	cdroms := devices.SelectByType((*types.VirtualCdrom)(nil))
+	if err = vm.RemoveDevice(false, cdroms...); err != nil {
+		ui.Error(fmt.Sprintf("error removing cdroms: %v", err))
+	}
 }
