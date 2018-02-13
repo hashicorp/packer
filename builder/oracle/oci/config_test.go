5847426463e007e1f24cad31a03729ec41e97902
Robert Neumayer <neumayer@protonmail.com>
2018-02-13 14:23:19 +0100

    Remove unrelated changes

--- builder/oracle/oci/config_test.go
+++ builder/oracle/oci/config_test.go
@@ -24,8 +24,7 @@ func testConfig(accessConfFile *os.File) map[string]interface{} {
 		"subnet_ocid": "ocd1...",
 
 		// Comm
-		"ssh_username":   "opc",
-		"use_private_ip": false,
+		"ssh_username": "opc",
 	}
 }
 
