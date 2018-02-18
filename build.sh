611d51310cf94fe569a2d30ec3ae768a10af090b
Michael Kuzmin <mkuzmin@gmail.com>
2018-02-18 05:30:42 +0300

    Rename incremental builder from 'vsphere' to 'vsphere-clone'

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ export GOARCH=amd64
 mkdir -p bin
 rm -f bin/*
 
-GOOS=darwin  go build -o bin/packer-builder-vsphere.macos ./cmd/clone
-GOOS=linux   go build -o bin/packer-builder-vsphere.linux ./cmd/clone
-GOOS=windows go build -o bin/packer-builder-vsphere.exe   ./cmd/clone
-
 GOOS=darwin  go build -o bin/packer-builder-vsphere-iso.macos ./cmd/iso
 GOOS=linux   go build -o bin/packer-builder-vsphere-iso.linux ./cmd/iso
 GOOS=windows go build -o bin/packer-builder-vsphere-iso.exe   ./cmd/iso
+
+GOOS=darwin  go build -o bin/packer-builder-vsphere-clone.macos ./cmd/clone
+GOOS=linux   go build -o bin/packer-builder-vsphere-clone.linux ./cmd/clone
+GOOS=windows go build -o bin/packer-builder-vsphere-clone.exe   ./cmd/clone
