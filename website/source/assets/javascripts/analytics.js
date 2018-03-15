2f420c38b2708585469e9b4b2ec3a0a9a7737e44
Jeff Escalante <gh.je@mailhero.io>
2018-03-15 13:05:42 -0400

    add optinmonster script, fix makefile, remove es6

--- website/source/assets/javascripts/analytics.js
+++ website/source/assets/javascripts/analytics.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-  track('.downloads .download a', el => {
+document.addEventListener('DOMContentLoaded', function() {
+  track('.downloads .download a', function(el) {
     return {
       event: 'Download',
       category: 'Button',
-      label: `Packer | v${el.href.match(/\/(\d+\.\d+\.\d+)\//)[1]}`
+      label: 'Packer | v' + el.href.match(/\/(\d+\.\d+\.\d+)\//)[1]
     }
   })
 })
