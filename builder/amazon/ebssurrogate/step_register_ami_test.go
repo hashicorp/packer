5bf1e3f5d0d97cb1ba5f20014057c4ca0003ea73
Joseph Wright <joseph@cloudboss.co>
2018-03-25 19:25:53 -0400

    Modify ebssurrogate builder to snapshot all launch devices
    Documentation for ebssurrogate states that all of the devices in
    `launch_block_device_mappings` are snapshotted and included in the
    image. In fact, only the device that was designated as the root
    device was snapshotted. This patch modifies the builder to create
    snapshots of all the devices and include them in the image. This
    allows creating images with separate filesystems preconfigured,
    rather than having to add volumes to `ami_block_device_mappings`
    and configure them after boot.

--- builder/amazon/ebssurrogate/step_register_ami_test.go
+++ builder/amazon/ebssurrogate/step_register_ami_test.go
@@ -1,37 +1,247 @@
 package ebssurrogate
 
 import (
+	"reflect"
+	"sort"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
-func GetStringPointer() *string {
-	tmp := "/dev/name"
-	return &tmp
+const sourceDeviceName = "/dev/xvdf"
+const rootDeviceName = "/dev/xvda"
+
+func newStepRegisterAMI(amiDevices, launchDevices []*ec2.BlockDeviceMapping) *StepRegisterAMI {
+	return &StepRegisterAMI{
+		RootDevice: RootBlockDevice{
+			SourceDeviceName:    sourceDeviceName,
+			DeviceName:          rootDeviceName,
+			DeleteOnTermination: true,
+			VolumeType:          "ebs",
+			VolumeSize:          10,
+		},
+		AMIDevices:    amiDevices,
+		LaunchDevices: launchDevices,
+	}
 }
 
-func GetTestDevice() *ec2.BlockDeviceMapping {
-	TestDev := ec2.BlockDeviceMapping{
-		DeviceName: GetStringPointer(),
-	}
-	return &TestDev
+func sorted(devices []*ec2.BlockDeviceMapping) []*ec2.BlockDeviceMapping {
+	sort.SliceStable(devices, func(i, j int) bool {
+		return *devices[i].DeviceName < *devices[j].DeviceName
+	})
+	return devices
 }
 
-func TestStepRegisterAmi_DeduplicateRootVolume(t *testing.T) {
-	TestRootDevice := RootBlockDevice{}
-	TestRootDevice.SourceDeviceName = "/dev/name"
-
-	blockDevices := []*ec2.BlockDeviceMapping{}
-	blockDevicesExcludingRoot := DeduplicateRootVolume(blockDevices, TestRootDevice, "12342351")
-	if len(blockDevicesExcludingRoot) != 1 {
-		t.Fatalf("Unexpected length of block devices list")
+func TestStepRegisterAmi_combineDevices(t *testing.T) {
+	cases := []struct {
+		snapshotIds   map[string]string
+		amiDevices    []*ec2.BlockDeviceMapping
+		launchDevices []*ec2.BlockDeviceMapping
+		allDevices    []*ec2.BlockDeviceMapping
+	}{
+		{
+			snapshotIds:   map[string]string{},
+			amiDevices:    []*ec2.BlockDeviceMapping{},
+			launchDevices: []*ec2.BlockDeviceMapping{},
+			allDevices:    []*ec2.BlockDeviceMapping{},
+		},
+		{
+			snapshotIds: map[string]string{},
+			amiDevices:  []*ec2.BlockDeviceMapping{},
+			launchDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs:        &ec2.EbsBlockDevice{},
+					DeviceName: aws.String(sourceDeviceName),
+				},
+			},
+			allDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs:        &ec2.EbsBlockDevice{},
+					DeviceName: aws.String(rootDeviceName),
+				},
+			},
+		},
+		{
+			// Minimal single device
+			snapshotIds: map[string]string{
+				sourceDeviceName: "snap-0123456789abcdef1",
+			},
+			amiDevices: []*ec2.BlockDeviceMapping{},
+			launchDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs:        &ec2.EbsBlockDevice{},
+					DeviceName: aws.String(sourceDeviceName),
+				},
+			},
+			allDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						SnapshotId: aws.String("snap-0123456789abcdef1"),
+					},
+					DeviceName: aws.String(rootDeviceName),
+				},
+			},
+		},
+		{
+			// Single launch device with AMI device
+			snapshotIds: map[string]string{
+				sourceDeviceName: "snap-0123456789abcdef1",
+			},
+			amiDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs:        &ec2.EbsBlockDevice{},
+					DeviceName: aws.String("/dev/xvdg"),
+				},
+			},
+			launchDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs:        &ec2.EbsBlockDevice{},
+					DeviceName: aws.String(sourceDeviceName),
+				},
+			},
+			allDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						SnapshotId: aws.String("snap-0123456789abcdef1"),
+					},
+					DeviceName: aws.String(rootDeviceName),
+				},
+				&ec2.BlockDeviceMapping{
+					Ebs:        &ec2.EbsBlockDevice{},
+					DeviceName: aws.String("/dev/xvdg"),
+				},
+			},
+		},
+		{
+			// Multiple launch devices
+			snapshotIds: map[string]string{
+				sourceDeviceName: "snap-0123456789abcdef1",
+				"/dev/xvdg":      "snap-0123456789abcdef2",
+			},
+			amiDevices: []*ec2.BlockDeviceMapping{},
+			launchDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs:        &ec2.EbsBlockDevice{},
+					DeviceName: aws.String(sourceDeviceName),
+				},
+				&ec2.BlockDeviceMapping{
+					Ebs:        &ec2.EbsBlockDevice{},
+					DeviceName: aws.String("/dev/xvdg"),
+				},
+			},
+			allDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						SnapshotId: aws.String("snap-0123456789abcdef1"),
+					},
+					DeviceName: aws.String(rootDeviceName),
+				},
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						SnapshotId: aws.String("snap-0123456789abcdef2"),
+					},
+					DeviceName: aws.String("/dev/xvdg"),
+				},
+			},
+		},
+		{
+			// Multiple launch devices with encryption
+			snapshotIds: map[string]string{
+				sourceDeviceName: "snap-0123456789abcdef1",
+				"/dev/xvdg":      "snap-0123456789abcdef2",
+			},
+			amiDevices: []*ec2.BlockDeviceMapping{},
+			launchDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						Encrypted: aws.Bool(true),
+					},
+					DeviceName: aws.String(sourceDeviceName),
+				},
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						Encrypted: aws.Bool(true),
+					},
+					DeviceName: aws.String("/dev/xvdg"),
+				},
+			},
+			allDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						SnapshotId: aws.String("snap-0123456789abcdef1"),
+						// Encrypted: true stripped from snapshotted devices
+					},
+					DeviceName: aws.String(rootDeviceName),
+				},
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						SnapshotId: aws.String("snap-0123456789abcdef2"),
+					},
+					DeviceName: aws.String("/dev/xvdg"),
+				},
+			},
+		},
+		{
+			// Multiple launch devices and AMI devices with encryption
+			snapshotIds: map[string]string{
+				sourceDeviceName: "snap-0123456789abcdef1",
+				"/dev/xvdg":      "snap-0123456789abcdef2",
+			},
+			amiDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						Encrypted: aws.Bool(true),
+						KmsKeyId:  aws.String("keyId"),
+					},
+					// Source device name can be used in AMI devices
+					// since launch device of same name gets renamed
+					// to root device name
+					DeviceName: aws.String(sourceDeviceName),
+				},
+			},
+			launchDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						Encrypted: aws.Bool(true),
+					},
+					DeviceName: aws.String(sourceDeviceName),
+				},
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						Encrypted: aws.Bool(true),
+					},
+					DeviceName: aws.String("/dev/xvdg"),
+				},
+			},
+			allDevices: []*ec2.BlockDeviceMapping{
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						Encrypted: aws.Bool(true),
+						KmsKeyId:  aws.String("keyId"),
+					},
+					DeviceName: aws.String(sourceDeviceName),
+				},
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						SnapshotId: aws.String("snap-0123456789abcdef1"),
+					},
+					DeviceName: aws.String(rootDeviceName),
+				},
+				&ec2.BlockDeviceMapping{
+					Ebs: &ec2.EbsBlockDevice{
+						SnapshotId: aws.String("snap-0123456789abcdef2"),
+					},
+					DeviceName: aws.String("/dev/xvdg"),
+				},
+			},
+		},
 	}
-
-	TestBlockDevice := GetTestDevice()
-	blockDevices = append(blockDevices, TestBlockDevice)
-	blockDevicesExcludingRoot = DeduplicateRootVolume(blockDevices, TestRootDevice, "12342351")
-	if len(blockDevicesExcludingRoot) != 1 {
-		t.Fatalf("Unexpected length of block devices list")
+	for _, tc := range cases {
+		stepRegisterAmi := newStepRegisterAMI(tc.amiDevices, tc.launchDevices)
+		allDevices := stepRegisterAmi.combineDevices(tc.snapshotIds)
+		if !reflect.DeepEqual(sorted(allDevices), sorted(tc.allDevices)) {
+			t.Fatalf("Unexpected output from combineDevices")
+		}
 	}
 }
