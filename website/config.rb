6816d835a5d5415828ef1646c90f6d436b15a1e5
Megan Marsh <megan@hashicorp.com>
2018-03-26 13:57:48 -0700

    cut v1.2.2

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.packer.io/"
 
 activate :hashicorp do |h|
   h.name         = "packer"
-  h.version      = "1.2.1"
+  h.version      = "1.2.2"
   h.github_slug  = "hashicorp/packer"
   h.website_root = "website"
 end
