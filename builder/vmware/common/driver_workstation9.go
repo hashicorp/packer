371bb9cbc7aba6bbbbde110adb38f97405f9342f
Ali Rizvi-Santiago <arizvisa@gmail.com>
2018-03-29 13:04:25 -0500

    Replaced the call to os.Open and ReadNetworkMap to just a single call to ReadNetmapConfig in both the Workstation9 and Player5 VMware drivers.

--- builder/vmware/common/driver_workstation9.go
+++ builder/vmware/common/driver_workstation9.go
@@ -162,14 +162,9 @@ func (d *Workstation9Driver) Verify() error {
 		if _, err := os.Stat(pathNetmap); err != nil {
 			return nil, fmt.Errorf("Could not find netmap conf file: %s", pathNetmap)
 		}
+		log.Printf("Located networkmapper configuration file using Workstation: %s", pathNetmap)
 
-		fd, err := os.Open(pathNetmap)
-		if err != nil {
-			return nil, err
-		}
-		defer fd.Close()
-
-		return ReadNetworkMap(fd)
+		return ReadNetmapConfig(pathNetmap)
 	}
 	return nil
 }
