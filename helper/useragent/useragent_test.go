6a1f109797ded23a3c89731badd5995957c4c823
Seth Vargo <seth@sethvargo.com>
2018-04-05 14:25:11 -0400

    Include arch and os

--- helper/useragent/useragent_test.go
+++ helper/useragent/useragent_test.go
@@ -7,11 +7,13 @@ import (
 func TestUserAgent(t *testing.T) {
 	projectURL = "https://packer-test.com"
 	rt = "go5.0"
+	goos = "linux"
+	goarch = "amd64"
 	versionFunc = func() string { return "1.2.3" }
 
 	act := String()
 
-	exp := "Packer/1.2.3 (+https://packer-test.com; go5.0)"
+	exp := "Packer/1.2.3 (+https://packer-test.com; go5.0; linux/amd64)"
 	if exp != act {
 		t.Errorf("expected %q to be %q", act, exp)
 	}
