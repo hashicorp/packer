3bc9b007894021ebaf9e438638b8976c31b5fd8b
Matthew Hooker <mwhooker@gmail.com>
2018-04-12 23:05:05 -0700

    Add key press interval to virtualbox.

--- builder/virtualbox/common/step_type_boot_command.go
+++ builder/virtualbox/common/step_type_boot_command.go
@@ -64,7 +64,7 @@ func (s *StepTypeBootCommand) Run(ctx context.Context, state multistep.StateBag)
 
 		return driver.VBoxManage(args...)
 	}
-	d := bootcommand.NewPCATDriver(sendCodes, 20)
+	d := bootcommand.NewPCATDriver(sendCodes, 25)
 
 	ui.Say("Typing the boot command...")
 	for i, command := range s.BootCommand {
