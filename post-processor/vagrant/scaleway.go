c6299972b9d21af337f615358d5327e5754c14e7
Matthew Hooker <mwhooker@gmail.com>
2018-04-18 14:53:59 -0700

    s/boot_command/bootcommand/

--- post-processor/vagrant/scaleway.go
+++ post-processor/vagrant/scaleway.go
@@ -3,9 +3,10 @@ package vagrant
 import (
 	"bytes"
 	"fmt"
-	"github.com/hashicorp/packer/packer"
 	"strings"
 	"text/template"
+
+	"github.com/hashicorp/packer/packer"
 )
 
 type scalewayVagrantfileTemplate struct {
