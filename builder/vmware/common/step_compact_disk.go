569588b310137dd4dd530e7bcc998c02faf698b9
DanHam <DanHam@users.noreply.github.com>
2018-04-24 23:23:48 +0100

    Simplify handling of disks by collating requirements and unifying ops

--- builder/vmware/common/step_compact_disk.go
+++ builder/vmware/common/step_compact_disk.go
@@ -14,7 +14,7 @@ import (
 //
 // Uses:
 //   driver Driver
-//   full_disk_path string
+//   disk_full_paths ([]string) - The full paths to all created disks
 //   ui     packer.Ui
 //
 // Produces:
@@ -26,28 +26,19 @@ type StepCompactDisk struct {
 func (s StepCompactDisk) Run(_ context.Context, state multistep.StateBag) multistep.StepAction {
 	driver := state.Get("driver").(Driver)
 	ui := state.Get("ui").(packer.Ui)
-	full_disk_path := state.Get("full_disk_path").(string)
+	diskPaths := state.Get("disk_full_paths").([]string)
 
 	if s.Skip {
 		log.Println("Skipping disk compaction step...")
 		return multistep.ActionContinue
 	}
 
-	ui.Say("Compacting the disk image")
-	if err := driver.CompactDisk(full_disk_path); err != nil {
-		state.Put("error", fmt.Errorf("Error compacting disk: %s", err))
-		return multistep.ActionHalt
-	}
-
-	if state.Get("additional_disk_paths") != nil {
-		if moreDisks := state.Get("additional_disk_paths").([]string); len(moreDisks) > 0 {
-			for i, path := range moreDisks {
-				ui.Say(fmt.Sprintf("Compacting additional disk image %d", i+1))
-				if err := driver.CompactDisk(path); err != nil {
-					state.Put("error", fmt.Errorf("Error compacting additional disk %d: %s", i+1, err))
-					return multistep.ActionHalt
-				}
-			}
+	ui.Say("Compacting all attached disk images")
+	for i, diskPath := range diskPaths {
+		ui.Message(fmt.Sprintf("Compacting disk image %d", i+1))
+		if err := driver.CompactDisk(diskPath); err != nil {
+			state.Put("error", fmt.Errorf("Error compacting disk: %s", err))
+			return multistep.ActionHalt
 		}
 	}
 
