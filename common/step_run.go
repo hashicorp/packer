ce51275d52d6e541bd119cbd681e8812254dea04
Michael Kuzmin <mkuzmin@gmail.com>
2018-05-06 17:46:40 +0300

    move `boot_wait` into `boot_command`

--- common/step_run.go
+++ common/step_run.go
@@ -6,28 +6,15 @@ import (
 	"fmt"
 	"github.com/jetbrains-infra/packer-builder-vsphere/driver"
 	"strings"
-	"time"
 	"context"
 )
 
 type RunConfig struct {
 	BootOrder   string        `mapstructure:"boot_order"` // example: "floppy,cdrom,ethernet,disk"
-	RawBootWait string        `mapstructure:"boot_wait"`  // example: "1m30s"; default: "10s"
-	bootWait    time.Duration ``
 }
 
 func (c *RunConfig) Prepare() []error {
 	var errs []error
-
-	if c.RawBootWait == "" {
-		c.RawBootWait = "10s"
-	}
-
-	var err error
-	c.bootWait, err = time.ParseDuration(c.RawBootWait)
-	if err != nil {
-		errs = append(errs, fmt.Errorf("failed parsing boot_wait: %s", err))
-	}
 
 	return errs
 }
@@ -40,8 +27,7 @@ func (s *StepRun) Run(_ context.Context, state multistep.StateBag) multistep.Ste
 	ui := state.Get("ui").(packer.Ui)
 	vm := state.Get("vm").(*driver.VirtualMachine)
 
-	ui.Say("Power on VM...")
-
+	ui.Say("Set boot order...")
 	if s.Config.BootOrder != "" {
 		if err := vm.SetBootOrder(strings.Split(s.Config.BootOrder, ",")); err != nil {
 			state.Put("error", fmt.Errorf("error selecting boot order: %v", err))
@@ -49,26 +35,11 @@ func (s *StepRun) Run(_ context.Context, state multistep.StateBag) multistep.Ste
 		}
 	}
 
+	ui.Say("Power on VM...")
 	err := vm.PowerOn()
 	if err != nil {
 		state.Put("error", fmt.Errorf("error powering on VM: %v", err))
 		return multistep.ActionHalt
-	}
-
-	if int64(s.Config.bootWait) > 0 {
-		ui.Say(fmt.Sprintf("Waiting %s for boot...", s.Config.bootWait))
-		wait := time.After(s.Config.bootWait)
-	WAITLOOP:
-		for {
-			select {
-			case <-wait:
-				break WAITLOOP
-			case <-time.After(1 * time.Second):
-				if _, ok := state.GetOk(multistep.StateCancelled); ok {
-					return multistep.ActionHalt
-				}
-			}
-		}
 	}
 
 	return multistep.ActionContinue
