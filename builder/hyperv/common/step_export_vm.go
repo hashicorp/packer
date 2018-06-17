a65c5729b0afb4272dbc0b8add479b1d04af449d
DanHam <DanHam@users.noreply.github.com>
2018-06-17 13:27:52 +0100

    Remove disk compaction from the export step

--- builder/hyperv/common/step_export_vm.go
+++ builder/hyperv/common/step_export_vm.go
@@ -9,9 +9,8 @@ import (
 )
 
 type StepExportVm struct {
-	OutputDir      string
-	SkipCompaction bool
-	SkipExport     bool
+	OutputDir  string
+	SkipExport bool
 }
 
 func (s *StepExportVm) Run(_ context.Context, state multistep.StateBag) multistep.StepAction {
@@ -23,29 +22,11 @@ func (s *StepExportVm) Run(_ context.Context, state multistep.StateBag) multiste
 		return multistep.ActionContinue
 	}
 
-	// Get the VM name; Get the temp directory used to store the VMs files
-	// during the build process
-	vmName := state.Get("vmName").(string)
-	tmpPath := state.Get("packerTempDir").(string)
-
-	// Compact disks first so the export process has less to do
-	if s.SkipCompaction {
-		ui.Say("Skipping disk compaction...")
-	} else {
-		ui.Say("Compacting disks...")
-		err := driver.CompactDisks(tmpPath)
-		if err != nil {
-			err := fmt.Errorf("Error compacting disks: %s", err)
-			state.Put("error", err)
-			ui.Error(err.Error())
-			return multistep.ActionHalt
-		}
-	}
-
 	ui.Say("Exporting virtual machine...")
 	// The export process exports the VM to a folder named 'vmName' under
 	// the output directory. This contains the usual 'Snapshots', 'Virtual
 	// Hard Disks' and 'Virtual Machines' directories.
+	vmName := state.Get("vmName").(string)
 	err := driver.ExportVirtualMachine(vmName, s.OutputDir)
 	if err != nil {
 		err = fmt.Errorf("Error exporting vm: %s", err)
--- builder/hyperv/vmcx/builder.go
+++ builder/hyperv/vmcx/builder.go
@@ -476,9 +476,8 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 			Generation: b.config.Generation,
 		},
 		&hypervcommon.StepExportVm{
-			OutputDir:      b.config.OutputDir,
-			SkipCompaction: b.config.SkipCompaction,
-			SkipExport:     b.config.SkipExport,
+			OutputDir:  b.config.OutputDir,
+			SkipExport: b.config.SkipExport,
 		},
 
 		// the clean up actions for each step will be executed reverse order
