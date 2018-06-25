78ea5cdffc8b8cd6f6c7b64db8eac499cd625ebc
Bob Brumfield <bob.brumfield@auth0.com>
2018-06-25 08:28:32 -0700

    Notify that we are skipping file, but retain error message

--- builder/vmware/common/driver.go
+++ builder/vmware/common/driver.go
@@ -358,7 +358,7 @@ func (d *VmwareDriver) GuestIP(state multistep.StateBag) (string, error) {
 		// open up the lease and read its contents
 		fh, err := os.Open(dhcpLeasesPath)
 		if err != nil {
-			log.Printf("Unable to open lease path, skipping: %s", dhcpLeasesPath)
+			log.Printf("Error while reading DHCP lease path file %s: %s", dhcpLeasesPath, err.Error())
 			continue
 		}
 		defer fh.Close()
