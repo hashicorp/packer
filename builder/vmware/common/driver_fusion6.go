3c1877f6ad77519fa17eb36921bc594174a97a8b
Conrad Jones <cjones@turboit.co.uk>
2018-06-30 16:31:42 +0100

    Add cloneType to fusion driver

--- builder/vmware/common/driver_fusion6.go
+++ builder/vmware/common/driver_fusion6.go
@@ -19,10 +19,18 @@ type Fusion6Driver struct {
 }
 
 func (d *Fusion6Driver) Clone(dst, src string, linked bool) error {
+
+	var cloneType string
+	if linked {
+		cloneType = "linked"
+	} else {
+		cloneType = "full"
+	}
+
 	cmd := exec.Command(d.vmrunPath(),
 		"-T", "fusion",
 		"clone", src, dst,
-		"full")
+		cloneType)
 	if _, _, err := runAndLog(cmd); err != nil {
 		if strings.Contains(err.Error(), "parameters was invalid") {
 			return fmt.Errorf(
