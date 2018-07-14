33caed3531ecc79769781e26771e290ad3df735e
Tom Carrio <tom@carrio.me>
2018-07-14 00:25:26 -0400

    Completed filters and most_recent processing using OpenStack imageservice API

---
+++ builder/openstack/step_source_image_info.go
@@ -0,0 +1,87 @@
+package openstack
+
+import (
+	"log"
+	"fmt"
+	"context"
+
+
+	"github.com/hashicorp/packer/packer"
+	"github.com/hashicorp/packer/helper/multistep"
+	"github.com/gophercloud/gophercloud/openstack/imageservice/v2/images"
+	"github.com/gophercloud/gophercloud/pagination"
+)
+
+type StepSourceImageInfo struct {
+	SourceImage        string
+	SourceImageName    string
+	ImageFilters       ImageFilterOptions
+}
+
+type ImageFilterOptions struct {
+	Filters      map[string]string
+	MostRecent   bool                    `mapstructure:"most_recent"`
+}
+
+func (s *StepSourceImageInfo) Run(_ context.Context, state multistep.StateBag) multistep.StepAction {
+	config := state.Get("config").(Config)
+	ui := state.Get("ui").(packer.Ui)
+
+	client, err := config.computeV2Client()
+
+	// if an ID is provided we skip the filter since that will return a single image
+	if s.SourceImage != "" {
+		state.Put("source_image", s.SourceImage)
+		return multistep.ActionContinue
+	}
+
+	params := &images.ListOpts{}
+
+	// build ListOpts from filters
+	if len(s.ImageFilters.Filters) > 0 {
+		err = buildImageFilters(s.ImageFilters.Filters, params)
+		if err != nil {
+			err := fmt.Errorf("Errors encountered in filter parsing.\n%s" + err.Error())
+			state.Put("error", err)
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
+	}
+
+	if s.ImageFilters.MostRecent {
+		applyMostRecent(params)
+	}
+
+	log.Printf("Using Image Filters %v", params)
+	image := &images.Image{}
+	err = images.List(client, params).EachPage(func (page pagination.Page) (bool, error) {
+		i, err := images.ExtractImages(page)
+		if err != nil {
+			return false, err
+		}
+
+		switch len(i) {
+		case 0:
+			return false, fmt.Errorf("No image was found matching filters: %v", )
+		case 1:
+			*image = i[0]
+			return true, nil
+		default:
+			return false, fmt.Errorf("Your query returned more than one result. Please try a more specific search, or set most_recent to true.")
+		}
+
+		return true, nil
+	})
+
+	if err != nil {
+		err := fmt.Errorf("Error querying image: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+
+	ui.Message(fmt.Sprintf("Found Image ID: %s", image.ID))
+
+	state.Put("source_image", image)
+	return multistep.ActionContinue
+}
