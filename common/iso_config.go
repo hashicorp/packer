8c2ac7ae72943aa975463c264054f9f1f2b5eb2f
Megan Marsh <megan@hashicorp.com>
2018-07-23 09:54:08 -0700

    don't fail if you can't find abs or relative path.

--- common/iso_config.go
+++ common/iso_config.go
@@ -147,12 +147,14 @@ func (c *ISOConfig) parseCheckSumFile(rd *bufio.Reader) error {
 
 	absPath, err := filepath.Abs(u.Path)
 	if err != nil {
-		return fmt.Errorf("Unable to generate absolute path from provided iso_url: %s", err)
+		log.Printf("Unable to generate absolute path from provided iso_url: %s", err)
+		absPath = ""
 	}
 
 	relpath, err := filepath.Rel(filepath.Dir(checksumurl.Path), absPath)
 	if err != nil {
-		return err
+		log.Printf("Unable to determine relative pathing; continuing with abspath.")
+		relpath = ""
 	}
 
 	filename := filepath.Base(u.Path)
