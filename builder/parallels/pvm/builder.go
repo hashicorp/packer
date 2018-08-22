e5ffa9e5b3b1fbe37db70a9e7c68d367f97539b6
Adrien Delorme <adrien.delorme@icloud.com>
2018-08-22 18:23:23 +0200

    use SSHConfigFunc on right member in builder/parallels/pvm/builder.go

--- builder/parallels/pvm/builder.go
+++ builder/parallels/pvm/builder.go
@@ -85,7 +85,7 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		&communicator.StepConnect{
 			Config:    &b.config.SSHConfig.Comm,
 			Host:      parallelscommon.CommHost,
-			SSHConfig: b.config.SSHConfig.SSHConfigFunc(),
+			SSHConfig: b.config.SSHConfig.Comm.SSHConfigFunc(),
 		},
 		&parallelscommon.StepUploadVersion{
 			Path: b.config.PrlctlVersionFile,
