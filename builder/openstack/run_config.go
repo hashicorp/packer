11d0b082fda86cc4a048dab0d5a4c9230f0e7e78
Rickard von Essen <rickard.von.essen@gmail.com>
2018-08-22 13:37:43 +0200

    OpenStack: refactored how source_image_filter is handled to remove reflection

--- builder/openstack/run_config.go
+++ builder/openstack/run_config.go
@@ -19,23 +19,22 @@ type RunConfig struct {
 	SSHInterface         string              `mapstructure:"ssh_interface"`
 	SSHIPVersion         string              `mapstructure:"ssh_ip_version"`
 
-	SourceImage        string             `mapstructure:"source_image"`
-	SourceImageName    string             `mapstructure:"source_image_name"`
-	SourceImageFilters ImageFilterOptions `mapstructure:"source_image_filter"`
-	SourceImageOpts    images.ListOpts    `mapstructure:""`
-	Flavor             string             `mapstructure:"flavor"`
-	AvailabilityZone   string             `mapstructure:"availability_zone"`
-	RackconnectWait    bool               `mapstructure:"rackconnect_wait"`
-	FloatingIPNetwork  string             `mapstructure:"floating_ip_network"`
-	FloatingIP         string             `mapstructure:"floating_ip"`
-	ReuseIPs           bool               `mapstructure:"reuse_ips"`
-	SecurityGroups     []string           `mapstructure:"security_groups"`
-	Networks           []string           `mapstructure:"networks"`
-	Ports              []string           `mapstructure:"ports"`
-	UserData           string             `mapstructure:"user_data"`
-	UserDataFile       string             `mapstructure:"user_data_file"`
-	InstanceName       string             `mapstructure:"instance_name"`
-	InstanceMetadata   map[string]string  `mapstructure:"instance_metadata"`
+	SourceImage        string            `mapstructure:"source_image"`
+	SourceImageName    string            `mapstructure:"source_image_name"`
+	SourceImageFilters ImageFilter       `mapstructure:"source_image_filter"`
+	Flavor             string            `mapstructure:"flavor"`
+	AvailabilityZone   string            `mapstructure:"availability_zone"`
+	RackconnectWait    bool              `mapstructure:"rackconnect_wait"`
+	FloatingIPNetwork  string            `mapstructure:"floating_ip_network"`
+	FloatingIP         string            `mapstructure:"floating_ip"`
+	ReuseIPs           bool              `mapstructure:"reuse_ips"`
+	SecurityGroups     []string          `mapstructure:"security_groups"`
+	Networks           []string          `mapstructure:"networks"`
+	Ports              []string          `mapstructure:"ports"`
+	UserData           string            `mapstructure:"user_data"`
+	UserDataFile       string            `mapstructure:"user_data_file"`
+	InstanceName       string            `mapstructure:"instance_name"`
+	InstanceMetadata   map[string]string `mapstructure:"instance_metadata"`
 
 	ConfigDrive bool `mapstructure:"config_drive"`
 
@@ -50,6 +49,52 @@ type RunConfig struct {
 	// Not really used, but here for BC
 	OpenstackProvider string `mapstructure:"openstack_provider"`
 	UseFloatingIp     bool   `mapstructure:"use_floating_ip"`
+
+	sourceImageOpts images.ListOpts
+}
+
+type ImageFilter struct {
+	Filters    ImageFilterOptions `mapstructure:"filters"`
+	MostRecent bool               `mapstructure:"most_recent"`
+}
+
+type ImageFilterOptions struct {
+	Name       string   `mapstructure:"name"`
+	Owner      string   `mapstructure:"owner"`
+	Tags       []string `mapstructure:"tags"`
+	Visibility string   `mapstructure:"visibility"`
+}
+
+func (f *ImageFilterOptions) Empty() bool {
+	return f.Name == "" && f.Owner == "" && len(f.Tags) == 0 && f.Visibility == ""
+}
+
+func (f *ImageFilterOptions) Build() (*images.ListOpts, error) {
+	opts := images.ListOpts{}
+	// Set defaults for status, member_status, and sort
+	opts.Status = images.ImageStatusActive
+	opts.MemberStatus = images.ImageMemberStatusAccepted
+	opts.Sort = "created_at:desc"
+
+	var err error
+
+	if f.Name != "" {
+		opts.Name = f.Name
+	}
+	if f.Owner != "" {
+		opts.Owner = f.Owner
+	}
+	if len(f.Tags) > 0 {
+		opts.Tags = f.Tags
+	}
+	if f.Visibility != "" {
+		v, err := getImageVisibility(f.Visibility)
+		if err == nil {
+			opts.Visibility = *v
+		}
+	}
+
+	return &opts, err
 }
 
 func (c *RunConfig) Prepare(ctx *interpolate.Context) []error {
@@ -78,7 +123,7 @@ func (c *RunConfig) Prepare(ctx *interpolate.Context) []error {
 		}
 	}
 
-	if c.SourceImage == "" && c.SourceImageName == "" && c.SourceImageFilters.Filters == nil {
+	if c.SourceImage == "" && c.SourceImageName == "" && c.SourceImageFilters.Filters.Empty() {
 		errs = append(errs, errors.New("Either a source_image, a source_image_name, or source_image_filter must be specified"))
 	} else if len(c.SourceImage) > 0 && len(c.SourceImageName) > 0 {
 		errs = append(errs, errors.New("Only a source_image or a source_image_name can be specified, not both."))
@@ -116,21 +161,32 @@ func (c *RunConfig) Prepare(ctx *interpolate.Context) []error {
 
 	// if neither ID or image name is provided outside the filter, build the filter
 	if len(c.SourceImage) == 0 && len(c.SourceImageName) == 0 {
-		params := &images.ListOpts{}
 
-		if len(c.SourceImageFilters.Filters) > 0 {
-			filterErrs := buildImageFilters(c.SourceImageFilters.Filters, params)
-			if len(filterErrs.Errors) > 0 {
-				errs = append(errs, filterErrs.Errors...)
-			}
+		listOpts, filterErr := c.SourceImageFilters.Filters.Build()
+
+		if filterErr != nil {
+			errs = append(errs, filterErr)
 		}
-
-		if c.SourceImageFilters.MostRecent {
-			applyMostRecent(params)
-		}
-
-		c.SourceImageOpts = *params
+		c.sourceImageOpts = *listOpts
 	}
 
 	return errs
+}
+
+// Retrieve the specific ImageVisibility using the exported const from images
+func getImageVisibility(visibility string) (*images.ImageVisibility, error) {
+	visibilities := [...]images.ImageVisibility{
+		images.ImageVisibilityPublic,
+		images.ImageVisibilityPrivate,
+		images.ImageVisibilityCommunity,
+		images.ImageVisibilityShared,
+	}
+
+	for _, v := range visibilities {
+		if string(v) == visibility {
+			return &v, nil
+		}
+	}
+
+	return nil, fmt.Errorf("Not a valid visibility: %s", visibility)
 }
