41f6e0334dd47cf1826c07e69b978e5c5e113c1d
Adrien Delorme <adrien.delorme@icloud.com>
2018-08-22 17:02:23 +0200

    refactor all copy pasted sshConfig with into communicator.Config.SSHConfigFunc
    * still need to append the auth methods into an array for gracefullness

--- builder/digitalocean/ssh.go
+++ builder/digitalocean/ssh.go
@@ -1,32 +1,10 @@
 package digitalocean
 
 import (
-	"fmt"
-
-	"golang.org/x/crypto/ssh"
-
 	"github.com/hashicorp/packer/helper/multistep"
 )
 
 func commHost(state multistep.StateBag) (string, error) {
 	ipAddress := state.Get("droplet_ip").(string)
 	return ipAddress, nil
-}
-
-func sshConfig(state multistep.StateBag) (*ssh.ClientConfig, error) {
-	config := state.Get("config").(Config)
-	privateKey := state.Get("privateKey").(string)
-
-	signer, err := ssh.ParsePrivateKey([]byte(privateKey))
-	if err != nil {
-		return nil, fmt.Errorf("Error setting up SSH config: %s", err)
-	}
-
-	return &ssh.ClientConfig{
-		User: config.Comm.SSHUsername,
-		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
-		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	}, nil
 }
--- builder/googlecompute/ssh.go
+++ builder/googlecompute/ssh.go
@@ -1,32 +1,10 @@
 package googlecompute
 
 import (
-	"fmt"
-
 	"github.com/hashicorp/packer/helper/multistep"
-	"golang.org/x/crypto/ssh"
 )
 
 func commHost(state multistep.StateBag) (string, error) {
 	ipAddress := state.Get("instance_ip").(string)
 	return ipAddress, nil
-}
-
-// sshConfig returns the ssh configuration.
-func sshConfig(state multistep.StateBag) (*ssh.ClientConfig, error) {
-	config := state.Get("config").(*Config)
-	privateKey := state.Get("ssh_private_key").(string)
-
-	signer, err := ssh.ParsePrivateKey([]byte(privateKey))
-	if err != nil {
-		return nil, fmt.Errorf("Error setting up SSH config: %s", err)
-	}
-
-	return &ssh.ClientConfig{
-		User: config.Comm.SSHUsername,
-		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(signer),
-		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	}, nil
 }
