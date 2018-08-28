ef08c441c968fcaa3c9aff8a9d00bac8d853a612
Adrien Delorme <adrien.delorme@icloud.com>
2018-08-28 15:30:44 +0200

    builder.aws: use Comm field for StepKeyPair & StepGetPassword

--- builder/amazon/instance/builder.go
+++ builder/amazon/instance/builder.go
@@ -258,10 +258,9 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		},
 		&awscommon.StepKeyPair{
 			Debug:                b.config.PackerDebug,
+			Comm:                 &b.config.RunConfig.Comm,
 			SSHAgentAuth:         b.config.Comm.SSHAgentAuth,
 			DebugKeyPath:         fmt.Sprintf("ec2_%s.pem", b.config.PackerBuildName),
-			KeyPairName:          b.config.SSHKeyPairName,
-			PrivateKeyFile:       b.config.RunConfig.Comm.SSHPrivateKeyFile,
 			TemporaryKeyPairName: b.config.TemporaryKeyPairName,
 		},
 		&awscommon.StepSecurityGroup{
