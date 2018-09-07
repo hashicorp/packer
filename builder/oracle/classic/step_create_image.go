7c26ffef9cc680ae35a62e570d7e232da77d0648
Matthew Hooker <mwhooker@gmail.com>
2018-09-07 16:18:47 -0700

    builder instance now runs and is connected to

--- builder/oracle/classic/step_create_image.go
+++ builder/oracle/classic/step_create_image.go
@@ -2,6 +2,8 @@ package classic
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/hashicorp/packer/helper/multistep"
 	"github.com/hashicorp/packer/packer"
@@ -13,31 +15,27 @@ func (s *stepCreateImage) Run(_ context.Context, state multistep.StateBag) multi
 	//hook := state.Get("hook").(packer.Hook)
 	ui := state.Get("ui").(packer.Ui)
 	comm := state.Get("communicator").(packer.Communicator)
-	commands := []string{
-		"mkdir ./builder",
-		"sudo mkfs -t ext3 /dev/xvdb",
-		"sudo mount /dev/xvdb ./builder",
-		"sudo chown opc:opc ./builder",
-		"cd ./builder",
-		"sudo dd if=/dev/xvdc bs=8M status=progress | cp --sparse=always /dev/stdin diskimage.raw",
-		"tar czSf ./diskimage.tar.gz ./diskimage.raw",
-	}
-	for _, c := range commands {
-		cmd := packer.RemoteCmd{
-			Command: c,
-		}
-		cmd.StartWithUi(comm, ui)
-	}
-	//	comm.Start("
+	command := `#!/bin/sh
+	set -e
+	mkdir /builder
+	mkfs -t ext3 /dev/xvdb
+	mount /dev/xvdb /builder
+	chown opc:opc /builder
+	cd /builder
+	dd if=/dev/xvdc bs=8M status=progress | cp --sparse=always /dev/stdin diskimage.raw
+	tar czSf ./diskimage.tar.gz ./diskimage.raw`
 
-	/*
-		// Provision
-		log.Println("Running the provision hook")
-		if err := hook.Run(packer.HookProvision, ui, comm, nil); err != nil {
-			state.Put("error", err)
-			return multistep.ActionHalt
-		}
-	*/
+	dest := "/tmp/create-packer-diskimage.sh"
+	comm.Upload(dest, strings.NewReader(command), nil)
+	cmd := &packer.RemoteCmd{
+		Command: fmt.Sprintf("sudo /bin/sh %s", dest),
+	}
+	if err := cmd.StartWithUi(comm, ui); err != nil {
+		err = fmt.Errorf("Problem creating volume: %s", err)
+		ui.Error(err.Error())
+		state.Put("error", err)
+		return multistep.ActionHalt
+	}
 
 	return multistep.ActionContinue
 }
