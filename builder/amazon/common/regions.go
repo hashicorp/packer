c3509e19819f910518706bdeb56271b608b8a569
Megan Marsh <megan@hashicorp.com>
2018-09-10 15:43:01 -0700

    remove test-related special casing

--- builder/amazon/common/regions.go
+++ builder/amazon/common/regions.go
@@ -1,8 +1,6 @@
 package common
 
 import (
-	"flag"
-
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
@@ -25,21 +23,6 @@ func listEC2Regions() []string {
 // ValidateRegion returns true if the supplied region is a valid AWS
 // region and false if it's not.
 func ValidateRegion(region string) bool {
-
-	// To pass tests
-	if v := flag.Lookup("test.v"); v != nil || v.Value.String() == "true" {
-		regions := []string{
-			"us-east-1",
-			"us-east-2",
-			"us-west-1",
-		}
-		for _, valid := range regions {
-			if region == valid {
-				return true
-			}
-		}
-	}
-
 	// Normal run
 	for _, valid := range listEC2Regions() {
 		if region == valid {
