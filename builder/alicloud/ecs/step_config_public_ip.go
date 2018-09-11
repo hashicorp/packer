743be8a808b7cfaa50f381e9cc3bbe9fc5ff42c3
bozhi.ch <bozhi.ch@alibaba-inc.com>
2018-09-11 16:56:57 +0800

    support ssh with private ip address

--- builder/alicloud/ecs/step_config_public_ip.go
+++ builder/alicloud/ecs/step_config_public_ip.go
@@ -12,12 +12,23 @@ import (
 type stepConfigAlicloudPublicIP struct {
 	publicIPAddress string
 	RegionId        string
+	SSHPrivateIp    bool
 }
 
 func (s *stepConfigAlicloudPublicIP) Run(_ context.Context, state multistep.StateBag) multistep.StepAction {
 	client := state.Get("client").(*ecs.Client)
 	ui := state.Get("ui").(packer.Ui)
 	instance := state.Get("instance").(*ecs.InstanceAttributesType)
+
+	if s.SSHPrivateIp {
+		ipaddress := instance.InnerIpAddress.IpAddress
+		if len(ipaddress) == 0 {
+			ui.Say("Failed to get private ip of instance")
+			return multistep.ActionHalt
+		}
+		state.Put("ipaddress", ipaddress[0])
+		return multistep.ActionContinue
+	}
 
 	ipaddress, err := client.AllocatePublicIpAddress(instance.InstanceId)
 	if err != nil {
