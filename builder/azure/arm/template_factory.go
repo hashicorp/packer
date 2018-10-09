c983ea91c18486d265cf5d71e38166697c0fd2a5
mbearup <mbearup@microsoft.com>
2018-10-09 13:56:49 -0700

    Convert shared_image_gallery options to a struct Updates tests and documentation for shared_image_gallery

--- builder/azure/arm/template_factory.go
+++ builder/azure/arm/template_factory.go
@@ -72,15 +72,15 @@ func GetVirtualMachineDeployment(config *Config) (*resources.Deployment, error) 
 			config.ImageVersion)
 
 		builder.SetManagedMarketplaceImage(config.Location, config.ImagePublisher, config.ImageOffer, config.ImageSku, config.ImageVersion, imageID, config.managedImageStorageAccountType)
-	} else if config.SharedGallerySubscription != "" && config.SharedGalleryResourceGroup != "" && config.SharedGalleryName != "" && config.SharedGalleryImageName != "" {
+	} else if config.SharedGallery.Subscription != "" {
 		imageID := fmt.Sprintf("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Compute/galleries/%s/images/%s",
-			config.SharedGallerySubscription,
-			config.SharedGalleryResourceGroup,
-			config.SharedGalleryName,
-			config.SharedGalleryImageName)
-		if config.SharedGalleryImageVersion != "" {
+			config.SharedGallery.Subscription,
+			config.SharedGallery.ResourceGroup,
+			config.SharedGallery.GalleryName,
+			config.SharedGallery.ImageName)
+		if config.SharedGallery.ImageVersion != "" {
 			imageID += fmt.Sprintf("/versions/%s",
-				config.SharedGalleryImageVersion)
+				config.SharedGallery.ImageVersion)
 		}
 
 		builder.SetSharedGalleryImage(config.Location, imageID)
