86f6cf478af8e218b2f84bbe5d9b0cb63bc3ed63
Matthew Hooker <mwhooker@gmail.com>
2018-10-24 14:49:09 -0700

    cleanup

--- builder/oracle/classic/step_create_image.go
+++ builder/oracle/classic/step_create_image.go
@@ -27,43 +27,20 @@ func (s *stepCreateImage) Run(_ context.Context, state multistep.StateBag) multi
 		Description: "Packer generated TODO",
 		// The three-part name of the object
 		Name: config.ImageName,
-		// image_file.tar.gz, where image_file is the .tar.gz name of the machine image file that you have uploaded to Oracle Cloud Infrastructure Object Storage Classic.
+		// image_file.tar.gz, where image_file is the .tar.gz name of the
+		// machine image file that you have uploaded to Oracle Cloud
+		// Infrastructure Object Storage Classic.
 		File: imageFile,
 	}
-	log.Printf("CreateMachineImageInput: %+v", createMI)
 	mi, err := machineImageClient.CreateMachineImage(createMI)
 	if err != nil {
-		err = fmt.Errorf("Error creating machine image: %s, %+v", err, mi)
+		err = fmt.Errorf("Error creating machine image: %s", err)
 		ui.Error(err.Error())
 		state.Put("error", err)
 		return multistep.ActionHalt
 	}
-	log.Printf("Registered machine image: %+v", mi)
+	log.Println("Registered machine image.")
 	state.Put("machine_image", mi.Name)
-	/*
-		Registered machine image: &{
-			Account:/Compute-ptstest/cloud_storage
-			Attributes:map[]
-			Audited:
-			Description:Packer generated TODO
-			ErrorReason: Hypervisor:map[mode:hvm]
-			ImageFormat:raw
-			File:mwhooker_test_1539898463.tar.gz
-			Name:mwhooker_test_1539898463
-			NoUpload:true
-			Platform:linux
-			Sizes:map[uploaded:5.79793509e+08 total:5.79793509e+08 decompressed:1.610612736e+10]
-			State:available
-			URI:https://api-z61.compute.us6.oraclecloud.com/machineimage/Compute-ptstest/mhooker%40hashicorp.com/mwhooker_test_1539898463
-		}
-	*/
-	/* TODO:
-	* Documentation
-	* Configuration (master/builder images & entry, destination stuff, etc)
-		* Image entry for both master/builder
-		https://github.com/hashicorp/packer/issues/6833
-	* split master/builder image/connection config. i.e. build anything, master only linux
-	*/
 
 	return multistep.ActionContinue
 }
