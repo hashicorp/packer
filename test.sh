4dcd6c1f9dc84142e88bb5038fac5b0328f8ea80
Rémi Jouannet <remijouannet@gmail.com>
2018-10-30 21:46:36 +0100

    add gofmt to test.sh (#167)
    add gofmt to test.sh, the script exit 1 if one go files doesn't respect gofmt rules

--- test.sh
+++ test.sh
@@ -4,4 +4,17 @@ set -eux
 
 export PACKER_ACC=1
 
+RETVAL=0
+
+for file in $(find ./ -type f ! -path "./vendor/*" -name '*.go' -print)
+do
+    if [[ -n "$(gofmt -l "$file")" ]]
+    then
+        echo -e "$file does not conform to gofmt rules. Run: gofmt -s -w $file"
+        RETVAL=1
+    fi
+done
+
+exit $RETVAL
+
 go test -v -count 1 -timeout 20m ./driver ./iso ./clone
