b2d1675d399d2a1f0c177b12bd240019ee64895a
Amrita Dutta <amdut@microsoft.com>
2018-11-07 03:23:17 +0000

    Added tests

---
+++ builder/azure/arm/step_snapshot_data_disks_test.go
@@ -0,0 +1,69 @@
+package arm
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/packer/builder/azure/common/constants"
+	"github.com/hashicorp/packer/helper/multistep"
+	"testing"
+)
+
+func TestStepSnapshotDataDisksShouldFailIfSnapshotFails(t *testing.T) {
+	var testSubject = &StepSnapshotDataDisks{
+		create: func(context.Context, string, string, string, map[string]*string, string) error {
+			return fmt.Errorf("!! Unit Test FAIL !!")
+		},
+		say:   func(message string) {},
+		error: func(e error) {},
+	}
+
+	stateBag := createTestStateBagStepSnapshotDataDisks()
+
+	var result = testSubject.Run(context.Background(), stateBag)
+	if result != multistep.ActionHalt {
+		t.Fatalf("Expected the step to return 'ActionHalt', but got '%d'.", result)
+	}
+
+	if _, ok := stateBag.GetOk(constants.Error); ok == false {
+		t.Fatalf("Expected the step to set stateBag['%s'], but it was not.", constants.Error)
+	}
+}
+
+func TestStepSnapshotDataDisksShouldPassIfSnapshotPasses(t *testing.T) {
+	var testSubject = &StepSnapshotDataDisks{
+		create: func(context.Context, string, string, string, map[string]*string, string) error {
+			return nil
+		},
+		say:   func(message string) {},
+		error: func(e error) {},
+	}
+
+	stateBag := createTestStateBagStepSnapshotDataDisks()
+
+	var result = testSubject.Run(context.Background(), stateBag)
+	if result != multistep.ActionContinue {
+		t.Fatalf("Expected the step to return 'ActionContinue', but got '%d'.", result)
+	}
+
+	if _, ok := stateBag.GetOk(constants.Error); ok == true {
+		t.Fatalf("Expected the step to not set stateBag['%s'], but it was.", constants.Error)
+	}
+}
+
+func createTestStateBagStepSnapshotDataDisks() multistep.StateBag {
+	stateBag := new(multistep.BasicStateBag)
+
+	stateBag.Put(constants.ArmResourceGroupName, "Unit Test: ResourceGroupName")
+	stateBag.Put(constants.ArmLocation, "Unit Test: Location")
+
+	value := "Unit Test: Tags"
+	tags := map[string]*string{
+		"tag01": &value,
+	}
+	stateBag.Put(constants.ArmTags, tags)
+
+	stateBag.Put(constants.ArmAdditionalDiskVhds, []string{"subscriptions/123-456-789/resourceGroups/existingresourcegroup/providers/Microsoft.Compute/disks/osdisk"})
+	stateBag.Put(constants.ArmManagedImageDataDiskSnapshotPrefix, "Unit Test: ManagedImageDataDiskSnapshotPrefix")
+
+	return stateBag
+}
