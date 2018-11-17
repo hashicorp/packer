6778dae2429983ec20818eaf534005234b06705f
Ali Rizvi-Santiago <arizvisa@gmail.com>
2018-11-17 04:24:32 -0600

    Added options for the cpu count, memory, audio, and usb to the virtualbox builder.

---
+++ builder/virtualbox/common/hw_config.go
@@ -0,0 +1,46 @@
+package common
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/packer/template/interpolate"
+)
+
+type HWConfig struct {
+
+	// cpu information
+	CpuCount   int `mapstructure:"cpu_count"`
+	MemorySize int `mapstructure:"memory_size"`
+
+	// device presence
+	Sound string `mapstructure:"sound"`
+	USB   bool   `mapstructure:"usb"`
+}
+
+func (c *HWConfig) Prepare(ctx *interpolate.Context) []error {
+	var errs []error
+
+	// Hardware and cpu options
+	if c.CpuCount < 0 {
+		errs = append(errs, fmt.Errorf("An invalid cpu_count was specified (cpu_count < 0): %d", c.CpuCount))
+		c.CpuCount = 0
+	}
+	if c.CpuCount == 0 {
+		c.CpuCount = 1
+	}
+
+	if c.MemorySize < 0 {
+		errs = append(errs, fmt.Errorf("An invalid memory_size was specified (memory_size < 0): %d", c.MemorySize))
+		c.MemorySize = 0
+	}
+	if c.MemorySize == 0 {
+		c.MemorySize = 512
+	}
+
+	// devices
+	if c.Sound == "" {
+		c.Sound = "none"
+	}
+
+	return nil
+}
