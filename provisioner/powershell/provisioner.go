e69391f28bb79e00cbdd3854a08b6061d0808c5b
Megan Marsh <megan@hashicorp.com>
2018-12-05 16:39:46 -0800

    save users some bash scripting by exposing IP and port separately as well as together

--- provisioner/powershell/provisioner.go
+++ provisioner/powershell/provisioner.go
@@ -380,9 +380,18 @@ func (p *Provisioner) createFlattenedEnvVars(elevated bool) (flattened string) {
 	envVars["PACKER_BUILD_NAME"] = p.config.PackerBuildName
 	envVars["PACKER_BUILDER_TYPE"] = p.config.PackerBuilderType
 
+	// expose ip address variables
 	httpAddr := common.GetHTTPAddr()
 	if httpAddr != "" {
 		envVars["PACKER_HTTP_ADDR"] = httpAddr
+	}
+	httpIP := common.GetHTTPIP()
+	if httpIP != "" {
+		envVars["PACKER_HTTP_IP"] = httpIP
+	}
+	httpPort := common.GetHTTPPort()
+	if httpPort != "" {
+		envVars["PACKER_HTTP_PORT"] = httpPort
 	}
 
 	// interpolate environment variables
