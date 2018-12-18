0f8f58371e9c26cc2d057a0e2907a0297ad8db2f
Ladar Levison <ladar@lavabit.com>
2018-12-18 02:13:43 -0600

    Reverting accidental changes to boot command/plugin go files.

--- common/bootcommand/boot_command.go
+++ common/bootcommand/boot_command.go
@@ -1190,6 +1190,7 @@ func newParser(filename string, b []byte, opts ...Option) *parser {
 		Stats:           &stats,
 		// start rule is rule [0] unless an alternate entrypoint is specified
 		entrypoint: g.rules[0].name,
+		emptyState: make(storeDict),
 	}
 	p.setOptions(opts)
 
@@ -1278,6 +1279,9 @@ type parser struct {
 	choiceNoMatch string
 	// recovery expression stack, keeps track of the currently available recovery expression, these are traversed in reverse
 	recoveryStack []map[string]interface{}
+
+	// emptyState contains an empty storeDict, which is used to optimize cloneState if global "state" store is not used.
+	emptyState storeDict
 }
 
 // push a variable set on the vstack.
@@ -1449,6 +1453,13 @@ type Cloner interface {
 func (p *parser) cloneState() storeDict {
 	if p.debug {
 		defer p.out(p.in("cloneState"))
+	}
+
+	if len(p.cur.state) == 0 {
+		if len(p.emptyState) > 0 {
+			p.emptyState = make(storeDict)
+		}
+		return p.emptyState
 	}
 
 	state := make(storeDict, len(p.cur.state))
