bbe6dafc145ddf75e78c2511656ad3b745af0f04
Ladar Levison <ladar@lavabit.com>
2018-12-17 22:04:43 -0600

    Add changes var to docker driver import func.

--- builder/docker/driver_docker.go
+++ builder/docker/driver_docker.go
@@ -97,15 +97,25 @@ func (d *DockerDriver) Export(id string, dst io.Writer) error {
 	return nil
 }
 
-func (d *DockerDriver) Import(path string, repo string) (string, error) {
+func (d *DockerDriver) Import(path string, changes []string, repo string) (string, error) {
 	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("docker", "import", "-", repo)
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 	stdin, err := cmd.StdinPipe()
-	if err != nil {
+
+  if err != nil {
 		return "", err
 	}
+
+	args := []string{"import"}
+
+  for _, change := range changes {
+    args = append(args, "--change", change)
+  }
+
+  args = append(args, "-")
+	args = append(args, repo)
+  cmd := exec.Command("docker", args...)
 
 	// There should be only one artifact of the Docker builder
 	file, err := os.Open(path)
@@ -114,6 +124,8 @@ func (d *DockerDriver) Import(path string, repo string) (string, error) {
 	}
 	defer file.Close()
 
+  log.Printf("Importing container with args: %v", args)
+  
 	if err := cmd.Start(); err != nil {
 		return "", err
 	}
