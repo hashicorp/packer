fe77b596839e92cd464ed983f863ec19a8305887
Megan Marsh <megan@hashicorp.com>
2019-01-09 14:29:43 -0800

    allow virtualbox-ovf builder to start from a vagrant box as its source file

--- builder/virtualbox/ovf/step_import.go
+++ builder/virtualbox/ovf/step_import.go
@@ -3,8 +3,13 @@ package ovf
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	vboxcommon "github.com/hashicorp/packer/builder/virtualbox/common"
+	commonhelper "github.com/hashicorp/packer/helper/common"
 	"github.com/hashicorp/packer/helper/multistep"
 	"github.com/hashicorp/packer/packer"
 )
@@ -14,13 +19,41 @@ type StepImport struct {
 	Name        string
 	ImportFlags []string
 
-	vmName string
+	vmName     string
+	extractDir string
 }
 
 func (s *StepImport) Run(_ context.Context, state multistep.StateBag) multistep.StepAction {
 	driver := state.Get("driver").(vboxcommon.Driver)
 	ui := state.Get("ui").(packer.Ui)
 	vmPath := state.Get("vm_path").(string)
+
+	// if it's a vagrant box, extract the box and read the ovf inside.
+	if strings.HasSuffix(vmPath, ".box") {
+		// Use Dir of the vm_path to extract box to
+		vmDir := filepath.Dir(vmPath)
+		s.extractDir = filepath.Join(vmDir, s.Name)
+		err := os.Mkdir(s.extractDir, 0777)
+		if err != nil {
+			err = fmt.Errorf("Trouble extracting vagrant box for use: %s", err.Error())
+			state.Put("error", err)
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
+
+		commonhelper.UntarBox(s.extractDir, vmPath)
+		vmPath = filepath.Join(s.extractDir, "box.ovf")
+
+		log.Printf("vmPath is %s", vmPath)
+		if _, err := os.Stat(vmPath); os.IsNotExist(err) {
+			err = fmt.Errorf("Could not find a box.ovf inside of the given " +
+				"vagrant box; please make sure that this box has virtualbox " +
+				"as its provider")
+			state.Put("error", err)
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
+	}
 
 	ui.Say(fmt.Sprintf("Importing VM: %s", vmPath))
 	if err := driver.Import(s.Name, vmPath, s.ImportFlags); err != nil {
@@ -54,5 +87,10 @@ func (s *StepImport) Cleanup(state multistep.StateBag) {
 	ui.Say("Deregistering and deleting imported VM...")
 	if err := driver.Delete(s.vmName); err != nil {
 		ui.Error(fmt.Sprintf("Error deleting VM: %s", err))
+	}
+
+	// clean up extracted version of vagrant box
+	if s.extractDir != "" {
+		os.RemoveAll(s.extractDir)
 	}
 }
