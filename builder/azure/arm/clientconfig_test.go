97e3ef8d20d5add80d0e7fe866cc9c92e15fa633
Paul Meyer <paulmey@microsoft.com>
2019-01-11 20:54:15 +0000

    Allow certificate bearer JWT client authentication
    This allows certificate based authentication, both by just referencing
    the certificate file as well as by providing a bearer JWT. This last
    option allows authentication without exposing the private key to packer
    using an expiring JWT containting the thumbprint (and sometimes the
    whole certificate for subject/issuer based auth), signed using the
    certificate private key.

--- builder/azure/arm/clientconfig_test.go
+++ builder/azure/arm/clientconfig_test.go
@@ -1,11 +1,19 @@
 package arm
 
 import (
+	crand "crypto/rand"
+	"crypto/rsa"
+	"encoding/base64"
+	"encoding/binary"
 	"fmt"
+	"io"
+	mrand "math/rand"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/Azure/go-autorest/autorest/azure"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/hashicorp/packer/packer"
 )
 
@@ -29,7 +37,7 @@ func Test_ClientConfig_RequiredParametersSet(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "client_id without client_secret should error",
+			name: "client_id without client_secret, client_cert_path or client_jwt should error",
 			config: ClientConfig{
 				ClientID: "error",
 			},
@@ -43,10 +51,60 @@ func Test_ClientConfig_RequiredParametersSet(t *testing.T) {
 			wantErr: true,
 		},
 		{
+			name: "client_cert_path without client_id should error",
+			config: ClientConfig{
+				ClientCertPath: "/dev/null",
+			},
+			wantErr: true,
+		},
+		{
+			name: "client_jwt without client_id should error",
+			config: ClientConfig{
+				ClientJWT: "error",
+			},
+			wantErr: true,
+		},
+		{
 			name: "missing subscription_id when using secret",
 			config: ClientConfig{
 				ClientID:     "ok",
 				ClientSecret: "ok",
+			},
+			wantErr: true,
+		},
+		{
+			name: "missing subscription_id when using certificate",
+			config: ClientConfig{
+				ClientID:       "ok",
+				ClientCertPath: "ok",
+			},
+			wantErr: true,
+		},
+		{
+			name: "missing subscription_id when using JWT",
+			config: ClientConfig{
+				ClientID:  "ok",
+				ClientJWT: "ok",
+			},
+			wantErr: true,
+		},
+		{
+			name: "too many client_* values",
+			config: ClientConfig{
+				SubscriptionID: "ok",
+				ClientID:       "ok",
+				ClientSecret:   "ok",
+				ClientCertPath: "error",
+			},
+			wantErr: true,
+		},
+		{
+			name: "too many client_* values (2)",
+			config: ClientConfig{
+				SubscriptionID: "ok",
+				ClientID:       "ok",
+				ClientSecret:   "ok",
+				ClientJWT:      "error",
 			},
 			wantErr: true,
 		},
@@ -105,6 +163,66 @@ func Test_ClientConfig_ClientPassword(t *testing.T) {
 		SubscriptionID:   getEnvOrSkip(t, "AZURE_SUBSCRIPTION"),
 		ClientID:         getEnvOrSkip(t, "AZURE_CLIENTID"),
 		ClientSecret:     getEnvOrSkip(t, "AZURE_CLIENTSECRET"),
+		TenantID:         getEnvOrSkip(t, "AZURE_TENANTID"),
+		cloudEnvironment: getCloud(),
+	}
+	assertValid(t, cfg)
+
+	spt, sptkv, err := cfg.getServicePrincipalTokens(func(s string) { fmt.Printf("SAY: %s\n", s) })
+	if err != nil {
+		t.Fatalf("Expected nil err, but got: %v", err)
+	}
+	token := spt.Token()
+	if token.AccessToken == "" {
+		t.Fatal("Expected management token to have non-nil access token")
+	}
+	if token.RefreshToken != "" {
+		t.Fatal("Expected management token to have no refresh token")
+	}
+	kvtoken := sptkv.Token()
+	if kvtoken.AccessToken == "" {
+		t.Fatal("Expected keyvault token to have non-nil access token")
+	}
+	if kvtoken.RefreshToken != "" {
+		t.Fatal("Expected keyvault token to have no refresh token")
+	}
+}
+
+func Test_ClientConfig_ClientCert(t *testing.T) {
+	cfg := ClientConfig{
+		SubscriptionID:   getEnvOrSkip(t, "AZURE_SUBSCRIPTION"),
+		ClientID:         getEnvOrSkip(t, "AZURE_CLIENTID"),
+		ClientCertPath:   getEnvOrSkip(t, "AZURE_CLIENTCERT"),
+		TenantID:         getEnvOrSkip(t, "AZURE_TENANTID"),
+		cloudEnvironment: getCloud(),
+	}
+	assertValid(t, cfg)
+
+	spt, sptkv, err := cfg.getServicePrincipalTokens(func(s string) { fmt.Printf("SAY: %s\n", s) })
+	if err != nil {
+		t.Fatalf("Expected nil err, but got: %v", err)
+	}
+	token := spt.Token()
+	if token.AccessToken == "" {
+		t.Fatal("Expected management token to have non-nil access token")
+	}
+	if token.RefreshToken != "" {
+		t.Fatal("Expected management token to have no refresh token")
+	}
+	kvtoken := sptkv.Token()
+	if kvtoken.AccessToken == "" {
+		t.Fatal("Expected keyvault token to have non-nil access token")
+	}
+	if kvtoken.RefreshToken != "" {
+		t.Fatal("Expected keyvault token to have no refresh token")
+	}
+}
+
+func Test_ClientConfig_ClientJWT(t *testing.T) {
+	cfg := ClientConfig{
+		SubscriptionID:   getEnvOrSkip(t, "AZURE_SUBSCRIPTION"),
+		ClientID:         getEnvOrSkip(t, "AZURE_CLIENTID"),
+		ClientJWT:        getEnvOrSkip(t, "AZURE_CLIENTJWT"),
 		TenantID:         getEnvOrSkip(t, "AZURE_TENANTID"),
 		cloudEnvironment: getCloud(),
 	}
@@ -192,8 +310,88 @@ func Test_ClientConfig_CanUseClientSecretWithTenantID(t *testing.T) {
 	assertValid(t, cfg)
 }
 
+func Test_ClientConfig_CanUseClientJWT(t *testing.T) {
+	cfg := emptyClientConfig()
+	cfg.SubscriptionID = "12345"
+	cfg.ClientID = "12345"
+	cfg.ClientJWT = getJWT(10*time.Minute, true)
+
+	assertValid(t, cfg)
+}
+
+func Test_ClientConfig_CanUseClientJWTWithTenantID(t *testing.T) {
+	cfg := emptyClientConfig()
+	cfg.SubscriptionID = "12345"
+	cfg.ClientID = "12345"
+	cfg.ClientJWT = getJWT(10*time.Minute, true)
+	cfg.TenantID = "12345"
+
+	assertValid(t, cfg)
+}
+
+func Test_ClientConfig_CannotUseBothClientJWTAndSecret(t *testing.T) {
+	cfg := emptyClientConfig()
+	cfg.SubscriptionID = "12345"
+	cfg.ClientID = "12345"
+	cfg.ClientSecret = "12345"
+	cfg.ClientJWT = getJWT(10*time.Minute, true)
+
+	assertInvalid(t, cfg)
+}
+
+func Test_ClientConfig_ClientJWTShouldBeValidForAtLeast5Minutes(t *testing.T) {
+	cfg := emptyClientConfig()
+	cfg.SubscriptionID = "12345"
+	cfg.ClientID = "12345"
+	cfg.ClientJWT = getJWT(time.Minute, true)
+
+	assertInvalid(t, cfg)
+}
+
+func Test_ClientConfig_ClientJWTShouldHaveThumbprint(t *testing.T) {
+	cfg := emptyClientConfig()
+	cfg.SubscriptionID = "12345"
+	cfg.ClientID = "12345"
+	cfg.ClientJWT = getJWT(10*time.Minute, false)
+
+	assertInvalid(t, cfg)
+}
+
 func emptyClientConfig() ClientConfig {
 	cfg := ClientConfig{}
 	_ = cfg.setCloudEnvironment()
 	return cfg
+}
+
+func Test_getJWT(t *testing.T) {
+	if getJWT(time.Minute, true) == "" {
+		t.Fatalf("getJWT is broken")
+	}
+}
+
+func newRandReader() io.Reader {
+	var seed int64
+	binary.Read(crand.Reader, binary.LittleEndian, &seed)
+
+	return mrand.New(mrand.NewSource(seed))
+}
+
+func getJWT(validFor time.Duration, withX5tHeader bool) string {
+	token := jwt.New(jwt.SigningMethodRS256)
+	key, _ := rsa.GenerateKey(newRandReader(), 2048)
+
+	token.Claims = jwt.MapClaims{
+		"aud": "https://login.microsoftonline.com/tenant.onmicrosoft.com/oauth2/token?api-version=1.0",
+		"iss": "355dff10-cd78-11e8-89fe-000d3afd16e3",
+		"sub": "355dff10-cd78-11e8-89fe-000d3afd16e3",
+		"jti": base64.URLEncoding.EncodeToString([]byte{0}),
+		"nbf": time.Now().Unix(),
+		"exp": time.Now().Add(validFor).Unix(),
+	}
+	if withX5tHeader {
+		token.Header["x5t"] = base64.URLEncoding.EncodeToString([]byte("thumbprint"))
+	}
+
+	jwt, _ := token.SignedString(key)
+	return jwt
 }
