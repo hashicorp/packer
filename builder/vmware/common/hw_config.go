e17d98af274e828ccc78127c81a5680c020b4b30
Ali Rizvi-Santiago <arizvisa@gmail.com>
2019-01-17 10:38:19 -0600

    Renamed the terse option, `cores`, to to the less-terse version `cores_per_socket`.

--- builder/vmware/common/hw_config.go
+++ builder/vmware/common/hw_config.go
@@ -14,7 +14,7 @@ type HWConfig struct {
 	// cpu information
 	CpuCount   int `mapstructure:"cpus"`
 	MemorySize int `mapstructure:"memory"`
-	CoreCount  int `mapstructure:"cores"`
+	CoreCount  int `mapstructure:"cores_per_socket"`
 
 	// network type and adapter
 	Network            string `mapstructure:"network"`
