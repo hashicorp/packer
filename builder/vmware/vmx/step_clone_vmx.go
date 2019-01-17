442230f206674d2c8bedbdafcdca65b913e49f78
Ali Rizvi-Santiago <arizvisa@gmail.com>
2019-01-17 17:31:00 -0600

    Removed the ability to download the .vmx file from vmware-vmx/step_clone_vmx.go since this only needs to be done if the machine is remote.

--- builder/vmware/vmx/step_clone_vmx.go
+++ builder/vmware/vmx/step_clone_vmx.go
@@ -42,25 +42,7 @@ func (s *StepCloneVMX) Run(ctx context.Context, state multistep.StateBag) multis
 		return halt(err)
 	}
 
-	// Read in the machine configuration from the cloned VMX file
-	//
-	// * The main driver needs the path to the vmx (set above) and the
-	// network type so that it can work out things like IP's and MAC
-	// addresses
-	// * The disk compaction step needs the paths to all attached disks
-	if remoteDriver, ok := driver.(vmwcommon.RemoteDriver); ok {
-		remoteVmxPath := vmxPath
-		tempDir, err := tmp.Dir("packer-vmx")
-		if err != nil {
-			return halt(err)
-		}
-		s.tempDir = tempDir
-		vmxPath = filepath.Join(tempDir, s.VMName+".vmx")
-		if err = remoteDriver.Download(remoteVmxPath, vmxPath); err != nil {
-			return halt(err)
-		}
-	}
-
+	// Now we can read the data from the vmx file
 	vmxData, err := vmwcommon.ReadVMX(vmxPath)
 	if err != nil {
 		return halt(err)
