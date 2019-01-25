0864b4d07bb842aa41847596f6c6f317ecd4e37a
Adrien Delorme <adrien.delorme@icloud.com>
2019-01-25 12:59:43 +0100

    name unclear import

--- builder/amazon/common/access_config.go
+++ builder/amazon/common/access_config.go
@@ -15,7 +15,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
-	"github.com/hashicorp/go-cleanhttp"
+	cleanhttp "github.com/hashicorp/go-cleanhttp"
 	"github.com/hashicorp/packer/template/interpolate"
 )
 
