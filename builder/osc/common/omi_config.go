22d08817216aba259211b4a697ea9c6aba47a2c0
Marin Salinas <marinssalinas@gmail.com>
2019-02-05 13:18:44 -0600

    feature: bsusurrogate, add StepUpdateOMIAttributes step

--- builder/osc/common/omi_config.go
+++ builder/osc/common/omi_config.go
@@ -10,25 +10,22 @@ import (
 
 // OMIConfig is for common configuration related to creating OMIs.
 type OMIConfig struct {
-	OMIName                 string            `mapstructure:"omi_name"`
-	OMIDescription          string            `mapstructure:"omi_description"`
-	OMIVirtType             string            `mapstructure:"omi_virtualization_type"`
-	OMIUsers                []string          `mapstructure:"omi_users"`
-	OMIGroups               []string          `mapstructure:"omi_groups"`
-	OMIProductCodes         []string          `mapstructure:"omi_product_codes"`
-	OMIRegions              []string          `mapstructure:"omi_regions"`
-	OMISkipRegionValidation bool              `mapstructure:"skip_region_validation"`
-	OMITags                 TagMap            `mapstructure:"tags"`
-	OMIENASupport           *bool             `mapstructure:"ena_support"`
-	OMISriovNetSupport      bool              `mapstructure:"sriov_support"`
-	OMIForceDeregister      bool              `mapstructure:"force_deregister"`
-	OMIForceDeleteSnapshot  bool              `mapstructure:"force_delete_snapshot"`
-	OMIEncryptBootVolume    bool              `mapstructure:"encrypt_boot"`
-	OMIKmsKeyId             string            `mapstructure:"kms_key_id"`
-	OMIRegionKMSKeyIDs      map[string]string `mapstructure:"region_kms_key_ids"`
-	SnapshotTags            TagMap            `mapstructure:"snapshot_tags"`
-	SnapshotUsers           []string          `mapstructure:"snapshot_users"`
-	SnapshotGroups          []string          `mapstructure:"snapshot_groups"`
+	OMIName                 string   `mapstructure:"omi_name"`
+	OMIDescription          string   `mapstructure:"omi_description"`
+	OMIVirtType             string   `mapstructure:"omi_virtualization_type"`
+	OMIAccountIDs           []string `mapstructure:"omi_account_ids"`
+	OMIGroups               []string `mapstructure:"omi_groups"`
+	OMIProductCodes         []string `mapstructure:"omi_product_codes"`
+	OMIRegions              []string `mapstructure:"omi_regions"`
+	OMISkipRegionValidation bool     `mapstructure:"skip_region_validation"`
+	OMITags                 TagMap   `mapstructure:"tags"`
+	OMIENASupport           *bool    `mapstructure:"ena_support"`
+	OMISriovNetSupport      bool     `mapstructure:"sriov_support"`
+	OMIForceDeregister      bool     `mapstructure:"force_deregister"`
+	OMIForceDeleteSnapshot  bool     `mapstructure:"force_delete_snapshot"`
+	SnapshotTags            TagMap   `mapstructure:"snapshot_tags"`
+	SnapshotAccountIDs      []string `mapstructure:"snapshot_account_ids"`
+	SnapshotGroups          []string `mapstructure:"snapshot_groups"`
 }
 
 func stringInSlice(s []string, searchstr string) bool {
@@ -47,51 +44,7 @@ func (c *OMIConfig) Prepare(accessConfig *AccessConfig, ctx *interpolate.Context
 		errs = append(errs, fmt.Errorf("omi_name must be specified"))
 	}
 
-	// Make sure that if we have region_kms_key_ids defined,
-	//  the regions in region_kms_key_ids are also in omi_regions
-	if len(c.OMIRegionKMSKeyIDs) > 0 {
-		for kmsKeyRegion := range c.OMIRegionKMSKeyIDs {
-			if !stringInSlice(c.OMIRegions, kmsKeyRegion) {
-				errs = append(errs, fmt.Errorf("Region %s is in region_kms_key_ids but not in omi_regions", kmsKeyRegion))
-			}
-		}
-	}
-
 	errs = append(errs, c.prepareRegions(accessConfig)...)
-
-	if len(c.OMIUsers) > 0 && c.OMIEncryptBootVolume {
-		errs = append(errs, fmt.Errorf("Cannot share OMI with encrypted boot volume"))
-	}
-
-	var kmsKeys []string
-	if len(c.OMIKmsKeyId) > 0 {
-		kmsKeys = append(kmsKeys, c.OMIKmsKeyId)
-	}
-	if len(c.OMIRegionKMSKeyIDs) > 0 {
-		for _, kmsKey := range c.OMIRegionKMSKeyIDs {
-			if len(kmsKey) == 0 {
-				kmsKeys = append(kmsKeys, c.OMIKmsKeyId)
-			}
-		}
-	}
-	for _, kmsKey := range kmsKeys {
-		if !validateKmsKey(kmsKey) {
-			errs = append(errs, fmt.Errorf("%s is not a valid KMS Key Id.", kmsKey))
-		}
-	}
-
-	if len(c.SnapshotUsers) > 0 {
-		if len(c.OMIKmsKeyId) == 0 && c.OMIEncryptBootVolume {
-			errs = append(errs, fmt.Errorf("Cannot share snapshot encrypted with default KMS key"))
-		}
-		if len(c.OMIRegionKMSKeyIDs) > 0 {
-			for _, kmsKey := range c.OMIRegionKMSKeyIDs {
-				if len(kmsKey) == 0 {
-					errs = append(errs, fmt.Errorf("Cannot share snapshot encrypted with default KMS key"))
-				}
-			}
-		}
-	}
 
 	if len(c.OMIName) < 3 || len(c.OMIName) > 128 {
 		errs = append(errs, fmt.Errorf("omi_name must be between 3 and 128 characters long"))
@@ -126,13 +79,6 @@ func (c *OMIConfig) prepareRegions(accessConfig *AccessConfig) (errs []error) {
 			// Mark that we saw the region
 			regionSet[region] = struct{}{}
 
-			// Make sure that if we have region_kms_key_ids defined,
-			// the regions in omi_regions are also in region_kms_key_ids
-			if len(c.OMIRegionKMSKeyIDs) > 0 {
-				if _, ok := c.OMIRegionKMSKeyIDs[region]; !ok {
-					errs = append(errs, fmt.Errorf("Region %s is in omi_regions but not in region_kms_key_ids", region))
-				}
-			}
 			if (accessConfig != nil) && (region == accessConfig.RawRegion) {
 				// make sure we don't try to copy to the region we originally
 				// create the OMI in.
