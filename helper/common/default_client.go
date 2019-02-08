3b3eccf592ea244b8efeefc576e7f4ee9c9f566d
Nicolai Willems <172633+nwillems@users.noreply.github.com>
2019-02-08 17:03:08 +0100

    Formatting

--- helper/common/default_client.go
+++ helper/common/default_client.go
@@ -3,19 +3,18 @@ package common
 import (
 	"net/http"
 
-    "github.com/hashicorp/go-rootcerts"
+	"github.com/hashicorp/go-rootcerts"
 )
 
 func HttpClientWithEnvironmentProxy() *http.Client {
-    httpTransport := &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-    }
-    rootcerts.ConfigureTLS(httpTransport.TLSClientConfig, nil)
+	httpTransport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+	}
+	rootcerts.ConfigureTLS(httpTransport.TLSClientConfig, nil)
 
 	httpClient := &http.Client{
 		Transport: httpTransport,
 	}
-    
 
 	return httpClient
 }
