e1b46d8b6f0a86fd8f1ca2adf263fe78a6920173
Marin Salinas <marinssalinas@gmail.com>
2019-02-11 16:43:16 -0600

    wip: add bsu plugin acc testing

---
+++ builder/osc/bsu/builder_acc_test.go
@@ -0,0 +1,55 @@
+/*
+Deregister the test image with
+aws oapi deregister-image --image-id $(aws oapi describe-images --output text --filters "Name=name,Values=packer-test-packer-test-dereg" --query 'Images[*].{ID:ImageId}')
+*/
+package bsu
+
+import (
+	"crypto/tls"
+	"net/http"
+	"testing"
+
+	"github.com/hashicorp/packer/builder/osc/common"
+	builderT "github.com/hashicorp/packer/helper/builder/testing"
+	"github.com/outscale/osc-go/oapi"
+)
+
+func TestBuilderAcc_basic(t *testing.T) {
+	builderT.Test(t, builderT.TestCase{
+		PreCheck: func() { testAccPreCheck(t) },
+		Builder:  &Builder{},
+		Template: testBuilderAccBasic,
+	})
+}
+
+func testAccPreCheck(t *testing.T) {
+}
+
+func testOAPIConn() (*oapi.Client, error) {
+	access := &common.AccessConfig{RawRegion: "us-east-1"}
+	clientConfig, err := access.Config()
+	if err != nil {
+		return nil, err
+	}
+
+	skipClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+
+	return oapi.NewClient(clientConfig, skipClient), nil
+}
+
+const testBuilderAccBasic = `
+{
+	"builders": [{
+		"type": "test",
+		"region": "eu-west-2",
+		"vm_type": "m3.medium",
+		"source_omi": "omi-76b2a71e",
+		"ssh_username": "ubuntu",
+		"omi_name": "packer-test {{timestamp}}"
+	}]
+}
+`
