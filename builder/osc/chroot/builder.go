00ab0097de0db22521bed127708dc620a59784a5
Marin Salinas <marinssalinas@gmail.com>
2019-02-25 16:31:08 -0600

    feature: add pre mount commands step for chroot builder

--- builder/osc/chroot/builder.go
+++ builder/osc/chroot/builder.go
@@ -249,6 +249,9 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		},
 		&StepLinkVolume{},
 		&StepEarlyUnflock{},
+		&StepPreMountCommands{
+			Commands: b.config.PreMountCommands,
+		},
 	)
 
 	// Run!
