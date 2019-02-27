6e6bdc4f42b144a5e47494c8c52176bfccf5b97a
Marin Salinas <marinssalinas@gmail.com>
2019-02-27 15:26:51 -0600

    feature: add mount device step in chroot builder

--- builder/osc/chroot/builder_acc_test.go
+++ builder/osc/chroot/builder_acc_test.go
@@ -8,9 +8,10 @@ import (
 
 func TestBuilderAcc_basic(t *testing.T) {
 	builderT.Test(t, builderT.TestCase{
-		PreCheck: func() { testAccPreCheck(t) },
-		Builder:  &Builder{},
-		Template: testBuilderAccBasic,
+		PreCheck:             func() { testAccPreCheck(t) },
+		Builder:              &Builder{},
+		Template:             testBuilderAccBasic,
+		SkipArtifactTeardown: true,
 	})
 }
 
@@ -22,9 +23,11 @@ const testBuilderAccBasic = `
 	"builders": [{
 		"type": "test",
 		"region": "eu-west-2",
-		"source_omi": "ami-46260446",
+		"source_omi": "ami-99466096",
 		"omi_name": "packer-test {{timestamp}}",
-		"omi_virtualization_type": "hvm"
+		"omi_virtualization_type": "hvm",
+		"device_path": "/dev/xvdf",
+		"mount_partition": "0"
 	}]
 }
 `
