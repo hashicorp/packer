d2eb1bf261533e259d70e065c8d32de0bc00353c
Marin Salinas <marinssalinas@gmail.com>
2019-02-28 11:19:34 -0600

    feature: add chroot provision step in chroot builder

---
+++ builder/osc/chroot/step_chroot_provision.go
@@ -0,0 +1,37 @@
+package chroot
+
+import (
+	"context"
+	"log"
+
+	"github.com/hashicorp/packer/helper/multistep"
+	"github.com/hashicorp/packer/packer"
+)
+
+// StepChrootProvision provisions the instance within a chroot.
+type StepChrootProvision struct {
+}
+
+func (s *StepChrootProvision) Run(_ context.Context, state multistep.StateBag) multistep.StepAction {
+	hook := state.Get("hook").(packer.Hook)
+	mountPath := state.Get("mount_path").(string)
+	ui := state.Get("ui").(packer.Ui)
+	wrappedCommand := state.Get("wrappedCommand").(CommandWrapper)
+
+	// Create our communicator
+	comm := &Communicator{
+		Chroot:     mountPath,
+		CmdWrapper: wrappedCommand,
+	}
+
+	// Provision
+	log.Println("Running the provision hook")
+	if err := hook.Run(packer.HookProvision, ui, comm, nil); err != nil {
+		state.Put("error", err)
+		return multistep.ActionHalt
+	}
+
+	return multistep.ActionContinue
+}
+
+func (s *StepChrootProvision) Cleanup(state multistep.StateBag) {}
