9603bdd24ca3f47fc469d4cdf841bccfdfc03d51
Marin Salinas <marinssalinas@gmail.com>
2019-02-28 10:18:57 -0600

    feature: add mount extra step in chroot builder

--- builder/osc/chroot/builder.go
+++ builder/osc/chroot/builder.go
@@ -259,6 +259,7 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 		&StepPostMountCommands{
 			Commands: b.config.PostMountCommands,
 		},
+		&StepMountExtra{},
 	)
 
 	// Run!
