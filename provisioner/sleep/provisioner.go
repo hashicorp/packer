9b1b017dff6faa4fb2906d7456ff29a5914a8e03
Adrien Delorme <adrien.delorme@icloud.com>
2019-04-08 13:38:55 +0200

    add a sleep provisioner
    mainly for testing purposes

---
+++ provisioner/sleep/provisioner.go
@@ -0,0 +1,30 @@
+package sleep
+
+import (
+	"context"
+	"time"
+
+	"github.com/hashicorp/packer/helper/config"
+	"github.com/hashicorp/packer/packer"
+)
+
+type Provisioner struct {
+	Duration time.Duration
+}
+
+var _ packer.Provisioner = new(Provisioner)
+
+func (p *Provisioner) Prepare(raws ...interface{}) error {
+	return config.Decode(&p, &config.DecodeOpts{}, raws...)
+}
+
+func (p *Provisioner) Provision(ctx context.Context, _ packer.Ui, _ packer.Communicator) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-time.After(p.Duration):
+		return nil
+	}
+}
+
+func (p *Provisioner) Cancel() {}
