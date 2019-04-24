f9c748726f73fda0b7b4eaca4451871edebde6c5
Adrien Delorme <adrien.delorme@icloud.com>
2019-04-24 14:03:36 +0200

    retry: encapsulate & return the last seen error in a RetryExhaustedError

--- common/retry/retry_test.go
+++ common/retry/retry_test.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"testing"
 	"time"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func success(context.Context) error { return nil }
@@ -18,12 +20,23 @@ var failErr = errors.New("woops !")
 
 func fail(context.Context) error { return failErr }
 
+type failOnce bool
+
+func (ran *failOnce) Run(context.Context) error {
+	if !*ran {
+		*ran = true
+		return failErr
+	}
+	return nil
+}
+
 func TestConfig_Run(t *testing.T) {
 	cancelledCtx, cancel := context.WithCancel(context.Background())
 	cancel()
 	type fields struct {
 		StartTimeout time.Duration
 		RetryDelay   func() time.Duration
+		Tries        int
 	}
 	type args struct {
 		ctx context.Context
@@ -36,26 +49,37 @@ func TestConfig_Run(t *testing.T) {
 		wantErr error
 	}{
 		{"success",
-			fields{StartTimeout: time.Second, RetryDelay: nil},
+			fields{StartTimeout: time.Second},
 			args{context.Background(), success},
 			nil},
 		{"context cancelled",
-			fields{StartTimeout: time.Second, RetryDelay: nil},
+			fields{StartTimeout: time.Second},
 			args{cancelledCtx, wait},
 			context.Canceled},
 		{"timeout",
 			fields{StartTimeout: 20 * time.Millisecond, RetryDelay: func() time.Duration { return 10 * time.Millisecond }},
 			args{cancelledCtx, fail},
 			failErr},
+		{"success after one failure",
+			fields{Tries: 2, RetryDelay: func() time.Duration { return 0 }},
+			args{context.Background(), new(failOnce).Run},
+			nil},
+		{"fail after one failure",
+			fields{Tries: 1, RetryDelay: func() time.Duration { return 0 }},
+			args{context.Background(), new(failOnce).Run},
+			&RetryExhaustedError{failErr},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			cfg := Config{
 				StartTimeout: tt.fields.StartTimeout,
 				RetryDelay:   tt.fields.RetryDelay,
+				Tries:        tt.fields.Tries,
 			}
-			if err := cfg.Run(tt.args.ctx, tt.args.fn); err != tt.wantErr {
-				t.Fatalf("Config.Run() error = %v, wantErr %v", err, tt.wantErr)
+			err := cfg.Run(tt.args.ctx, tt.args.fn)
+			if diff := cmp.Diff(err, tt.wantErr, DeepAllowUnexported(RetryExhaustedError{}, errors.New(""))); diff != "" {
+				t.Fatalf("Config.Run() unexpected error: %s", diff)
 			}
 		})
 	}
