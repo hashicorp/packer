f4413077ca8562b9d6a882309df665e3d83bf399
Adrien Delorme <adrien.delorme@icloud.com>
2019-04-30 15:35:22 +0200

    ListenRangeConfig.Listen: move trying port log after fslock trylock for less logs

--- common/net/configure_port.go
+++ common/net/configure_port.go
@@ -65,8 +65,6 @@ func (lc ListenRangeConfig) Listen(ctx context.Context) (*Listener, error) {
 			port += rand.Intn(portRange)
 		}
 
-		log.Printf("Trying port: %d", port)
-
 		lockFilePath, err := packer.CachePath("port", strconv.Itoa(port))
 		if err != nil {
 			return nil, err
@@ -80,6 +78,8 @@ func (lc ListenRangeConfig) Listen(ctx context.Context) (*Listener, error) {
 		if !locked {
 			continue // this port seems to be locked by another packer goroutine
 		}
+
+		log.Printf("Trying port: %d", port)
 
 		l, err := lc.ListenConfig.Listen(ctx, lc.Network, fmt.Sprintf("%s:%d", lc.Addr, port))
 		if err != nil {
