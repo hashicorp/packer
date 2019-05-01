c9cc28c17a714e874f24e8f0cfb1ca7f020767db
Marin Salinas <marinssalinas@gmail.com>
2019-05-01 14:10:15 -0500

    fix: change osc builder to be passed a context for cancellation and remove conflicts

--- builder/osc/bsuvolume/builder.go
+++ builder/osc/bsuvolume/builder.go
@@ -3,6 +3,7 @@
 package bsuvolume
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 	"log"
@@ -82,7 +83,7 @@ func (b *Builder) Prepare(raws ...interface{}) ([]string, error) {
 	return nil, nil
 }
 
-func (b *Builder) Run(ui packer.Ui, hook packer.Hook) (packer.Artifact, error) {
+func (b *Builder) Run(ctx context.Context, ui packer.Ui, hook packer.Hook) (packer.Artifact, error) {
 	clientConfig, err := b.config.Config()
 	if err != nil {
 		return nil, err
@@ -179,7 +180,7 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook) (packer.Artifact, error) {
 
 	// Run!
 	b.runner = common.NewRunner(steps, b.config.PackerConfig, ui)
-	b.runner.Run(state)
+	b.runner.Run(ctx, state)
 
 	// If there was an error, return that
 	if rawErr, ok := state.GetOk("error"); ok {
@@ -194,11 +195,4 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook) (packer.Artifact, error) {
 	}
 	ui.Say(fmt.Sprintf("Created Volumes: %s", artifact))
 	return artifact, nil
-}
-
-func (b *Builder) Cancel() {
-	if b.runner != nil {
-		log.Println("Cancelling the step runner...")
-		b.runner.Cancel()
-	}
 }
