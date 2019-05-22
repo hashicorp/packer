708165eaa801b10d5dd3d3a2525d66d086c73ac0
Megan Marsh <megan@hashicorp.com>
2019-05-22 10:16:42 -0700

    refactoring step_run_spot_instance and swapping out the calls to create a spot instance with a call to create a spot fleet of one, so that we can be more flexible with the instance type when desired

--- builder/amazon/common/step_run_spot_instance.go
+++ builder/amazon/common/step_run_spot_instance.go
@@ -13,6 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ec2"
 
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 	"github.com/hashicorp/packer/common/retry"
 	"github.com/hashicorp/packer/helper/communicator"
 	"github.com/hashicorp/packer/helper/multistep"
@@ -35,6 +36,7 @@ type StepRunSpotInstance struct {
 	SpotPrice                         string
 	SpotPriceProduct                  string
 	SpotTags                          TagMap
+	SpotInstanceTypes                 []string
 	Tags                              TagMap
 	VolumeTags                        TagMap
 	UserData                          string
@@ -45,17 +47,130 @@ type StepRunSpotInstance struct {
 	spotRequest *ec2.SpotInstanceRequest
 }
 
-func (s *StepRunSpotInstance) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {
-	ec2conn := state.Get("ec2").(*ec2.EC2)
-	securityGroupIds := aws.StringSlice(state.Get("securityGroupIds").([]string))
-	ui := state.Get("ui").(packer.Ui)
+func (s *StepRunSpotInstance) CalculateSpotPrice(az string, ec2conn ec2iface.EC2API) (string, error) {
+	// Calculate the spot price for a given availability zone
+	spotPrice := s.SpotPrice
 
+	if spotPrice == "auto" {
+		// Detect the spot price
+		startTime := time.Now().Add(-1 * time.Hour)
+		resp, err := ec2conn.DescribeSpotPriceHistory(&ec2.DescribeSpotPriceHistoryInput{
+			InstanceTypes:       []*string{&s.InstanceType},
+			ProductDescriptions: []*string{&s.SpotPriceProduct},
+			AvailabilityZone:    &az,
+			StartTime:           &startTime,
+		})
+		if err != nil {
+			return "", fmt.Errorf("Error finding spot price: %s", err)
+		}
+
+		var price float64
+		for _, history := range resp.SpotPriceHistory {
+			log.Printf("[INFO] Candidate spot price: %s", *history.SpotPrice)
+			current, err := strconv.ParseFloat(*history.SpotPrice, 64)
+			if err != nil {
+				log.Printf("[ERR] Error parsing spot price: %s", err)
+				continue
+			}
+			if price == 0 || current < price {
+				price = current
+				if az == "" {
+					az = *history.AvailabilityZone
+				}
+			}
+		}
+		if price == 0 {
+			return "", fmt.Errorf("No candidate spot prices found!")
+		} else {
+			// Add 0.5 cents to minimum spot bid to ensure capacity will be available
+			// Avoids price-too-low error in active markets which can fluctuate
+			price = price + 0.005
+		}
+
+		spotPrice = strconv.FormatFloat(price, 'f', -1, 64)
+	}
+
+	s.SpotPrice = spotPrice
+
+	return spotPrice, nil
+
+}
+
+func (s *StepRunSpotInstance) CreateTemplateData(userData *string, az string,
+	state multistep.StateBag, marketOptions *ec2.LaunchTemplateInstanceMarketOptionsRequest) *ec2.RequestLaunchTemplateData {
+	// Convert the BlockDeviceMapping into a
+	// LaunchTemplateBlockDeviceMappingRequest. These structs are identical,
+	// except for the EBS field -- on one, that field contains a
+	// LaunchTemplateEbsBlockDeviceRequest, and on the other, it contains an
+	// EbsBlockDevice. The EbsBlockDevice and
+	// LaunchTemplateEbsBlockDeviceRequest structs are themselves
+	// identical except for the struct's name, so you can cast one directly
+	// into the other.
+	blockDeviceMappings := s.BlockDevices.BuildLaunchDevices()
+	var launchMappingRequests []*ec2.LaunchTemplateBlockDeviceMappingRequest
+	for _, mapping := range blockDeviceMappings {
+		launchRequest := &ec2.LaunchTemplateBlockDeviceMappingRequest{
+			DeviceName:  mapping.DeviceName,
+			Ebs:         (*ec2.LaunchTemplateEbsBlockDeviceRequest)(mapping.Ebs),
+			NoDevice:    mapping.NoDevice,
+			VirtualName: mapping.VirtualName,
+		}
+		launchMappingRequests = append(launchMappingRequests, launchRequest)
+	}
+
+	// Create a launch template.
+	templateData := ec2.RequestLaunchTemplateData{
+		BlockDeviceMappings:   launchMappingRequests,
+		DisableApiTermination: aws.Bool(false),
+		EbsOptimized:          &s.EbsOptimized,
+		IamInstanceProfile:    &ec2.LaunchTemplateIamInstanceProfileSpecificationRequest{Name: &s.IamInstanceProfile},
+		ImageId:               &s.SourceAMI,
+		InstanceMarketOptions: marketOptions,
+		Placement: &ec2.LaunchTemplatePlacementRequest{
+			AvailabilityZone: &az,
+		},
+		UserData: userData,
+	}
+	// Create a network interface
+	securityGroupIds := aws.StringSlice(state.Get("securityGroupIds").([]string))
+	subnetId := state.Get("subnet_id").(string)
+
+	if subnetId != "" {
+		// Set up a full network interface
+		networkInterface := ec2.LaunchTemplateInstanceNetworkInterfaceSpecificationRequest{
+			Groups:              securityGroupIds,
+			DeleteOnTermination: aws.Bool(true),
+			DeviceIndex:         aws.Int64(0),
+			SubnetId:            aws.String(subnetId),
+		}
+		if s.AssociatePublicIpAddress {
+			networkInterface.SetAssociatePublicIpAddress(s.AssociatePublicIpAddress)
+		}
+		templateData.SetNetworkInterfaces([]*ec2.LaunchTemplateInstanceNetworkInterfaceSpecificationRequest{&networkInterface})
+	} else {
+		templateData.SetSecurityGroupIds(securityGroupIds)
+
+	}
+
+	// If instance type is not set, we'll just pick the lowest priced instance
+	// available.
+	if s.InstanceType != "" {
+		templateData.SetInstanceType(s.InstanceType)
+	}
+
+	if s.Comm.SSHKeyPairName != "" {
+		templateData.SetKeyName(s.Comm.SSHKeyPairName)
+	}
+
+	return &templateData
+}
+
+func (s *StepRunSpotInstance) LoadUserData() (string, error) {
 	userData := s.UserData
 	if s.UserDataFile != "" {
 		contents, err := ioutil.ReadFile(s.UserDataFile)
 		if err != nil {
-			state.Put("error", fmt.Errorf("Problem reading user data file: %s", err))
-			return multistep.ActionHalt
+			return "", fmt.Errorf("Problem reading user data file: %s", err)
 		}
 
 		userData = string(contents)
@@ -66,8 +181,16 @@ func (s *StepRunSpotInstance) Run(ctx context.Context, state multistep.StateBag)
 		log.Printf("[DEBUG] base64 encoding user data...")
 		userData = base64.StdEncoding.EncodeToString([]byte(userData))
 	}
+	return userData, nil
+}
 
-	ui.Say("Launching a source AWS instance...")
+func (s *StepRunSpotInstance) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {
+	ec2conn := state.Get("ec2").(*ec2.EC2)
+	ui := state.Get("ui").(packer.Ui)
+
+	ui.Say("Launching a spot AWS instance...")
+
+	// Get and validate the source AMI
 	image, ok := state.Get("source_image").(*ec2.Image)
 	if !ok {
 		state.Put("error", fmt.Errorf("source_image type assertion failed"))
@@ -83,157 +206,116 @@ func (s *StepRunSpotInstance) Run(ctx context.Context, state multistep.StateBag)
 		return multistep.ActionHalt
 	}
 
-	spotPrice := s.SpotPrice
 	azConfig := ""
 	if azRaw, ok := state.GetOk("availability_zone"); ok {
 		azConfig = azRaw.(string)
 	}
 	az := azConfig
 
-	if spotPrice == "auto" {
-		ui.Message(fmt.Sprintf(
-			"Finding spot price for %s %s...",
-			s.SpotPriceProduct, s.InstanceType))
-
-		// Detect the spot price
-		startTime := time.Now().Add(-1 * time.Hour)
-		resp, err := ec2conn.DescribeSpotPriceHistory(&ec2.DescribeSpotPriceHistoryInput{
-			InstanceTypes:       []*string{&s.InstanceType},
-			ProductDescriptions: []*string{&s.SpotPriceProduct},
-			AvailabilityZone:    &az,
-			StartTime:           &startTime,
-		})
-		if err != nil {
-			err := fmt.Errorf("Error finding spot price: %s", err)
-			state.Put("error", err)
-			ui.Error(err.Error())
-			return multistep.ActionHalt
-		}
-
-		var price float64
-		for _, history := range resp.SpotPriceHistory {
-			log.Printf("[INFO] Candidate spot price: %s", *history.SpotPrice)
-			current, err := strconv.ParseFloat(*history.SpotPrice, 64)
-			if err != nil {
-				log.Printf("[ERR] Error parsing spot price: %s", err)
-				continue
-			}
-			if price == 0 || current < price {
-				price = current
-				if azConfig == "" {
-					az = *history.AvailabilityZone
-				}
-			}
-		}
-		if price == 0 {
-			err := fmt.Errorf("No candidate spot prices found!")
-			state.Put("error", err)
-			ui.Error(err.Error())
-			return multistep.ActionHalt
-		} else {
-			// Add 0.5 cents to minimum spot bid to ensure capacity will be available
-			// Avoids price-too-low error in active markets which can fluctuate
-			price = price + 0.005
-		}
-
-		spotPrice = strconv.FormatFloat(price, 'f', -1, 64)
+	ui.Message(fmt.Sprintf("Finding spot price for %s %s...",
+		s.SpotPriceProduct, s.InstanceType))
+	spotPrice, err := s.CalculateSpotPrice(az, ec2conn)
+	if err != nil {
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
 	}
+
+	ui.Message(fmt.Sprintf("Determined spot instance price of: %s.", spotPrice))
 
 	var instanceId string
 
-	ui.Say("Adding tags to source instance")
+	ui.Say("Interpolating tags for spot instance...")
+	// s.Tags will tag the eventually launched instance
+	// s.SpotTags apply to the spot request itself, and do not automatically
+	// get applied to the spot instance that is launched once the request is
+	// fulfilled
 	if _, exists := s.Tags["Name"]; !exists {
 		s.Tags["Name"] = "Packer Builder"
 	}
 
+	// Convert tags from the tag map provided by the user into *ec2.Tag s
 	ec2Tags, err := s.Tags.EC2Tags(s.Ctx, *ec2conn.Config.Region, state)
 	if err != nil {
-		err := fmt.Errorf("Error tagging source instance: %s", err)
+		err := fmt.Errorf("Error generating tags for source instance: %s", err)
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return multistep.ActionHalt
 	}
+	// This prints the tags to the ui; it doesn't actually add them to the
+	// instance yet
 	ec2Tags.Report(ui)
 
-	ui.Message(fmt.Sprintf(
-		"Requesting spot instance '%s' for: %s",
-		s.InstanceType, spotPrice))
-
-	runOpts := &ec2.RequestSpotLaunchSpecification{
-		ImageId:            &s.SourceAMI,
-		InstanceType:       &s.InstanceType,
-		UserData:           &userData,
-		IamInstanceProfile: &ec2.IamInstanceProfileSpecification{Name: &s.IamInstanceProfile},
-		Placement: &ec2.SpotPlacement{
-			AvailabilityZone: &az,
-		},
-		BlockDeviceMappings: s.BlockDevices.BuildLaunchDevices(),
-		EbsOptimized:        &s.EbsOptimized,
-	}
-
-	subnetId := state.Get("subnet_id").(string)
-
-	if subnetId != "" && s.AssociatePublicIpAddress {
-		runOpts.NetworkInterfaces = []*ec2.InstanceNetworkInterfaceSpecification{
-			{
-				DeviceIndex:              aws.Int64(0),
-				AssociatePublicIpAddress: &s.AssociatePublicIpAddress,
-				SubnetId:                 &subnetId,
-				Groups:                   securityGroupIds,
-				DeleteOnTermination:      aws.Bool(true),
-			},
-		}
-	} else {
-		runOpts.SubnetId = &subnetId
-		runOpts.SecurityGroupIds = securityGroupIds
-	}
-
-	if s.Comm.SSHKeyPairName != "" {
-		runOpts.KeyName = &s.Comm.SSHKeyPairName
-	}
-	spotInstanceInput := &ec2.RequestSpotInstancesInput{
-		LaunchSpecification: runOpts,
-		SpotPrice:           &spotPrice,
+	spotOptions := ec2.LaunchTemplateSpotMarketOptionsRequest{
+		MaxPrice: &s.SpotPrice,
 	}
 	if s.BlockDurationMinutes != 0 {
-		spotInstanceInput.BlockDurationMinutes = &s.BlockDurationMinutes
+		spotOptions.BlockDurationMinutes = &s.BlockDurationMinutes
+	}
+	marketOptions := &ec2.LaunchTemplateInstanceMarketOptionsRequest{
+		SpotOptions: &spotOptions,
+	}
+	marketOptions.SetMarketType(ec2.MarketTypeSpot)
+
+	// Create a launch template for the instance
+	ui.Message("Loading User Data File...")
+	userData, err := s.LoadUserData()
+	if err != nil {
+		state.Put("error", err)
+		return multistep.ActionHalt
+	}
+	ui.Message("Creating Spot Fleet launch template...")
+	templateData := s.CreateTemplateData(&userData, az, state, marketOptions)
+	launchTemplate := &ec2.CreateLaunchTemplateInput{
+		LaunchTemplateData: templateData,
+		LaunchTemplateName: aws.String("packer-fleet-launch-template"),
+		VersionDescription: aws.String("template generated by packer for launching spot instances"),
 	}
 
-	runSpotResp, err := ec2conn.RequestSpotInstances(spotInstanceInput)
+	// Tell EC2 to create the template
+	_, err = ec2conn.CreateLaunchTemplate(launchTemplate)
 	if err != nil {
-		err := fmt.Errorf("Error launching source spot instance: %s", err)
+		err := fmt.Errorf("Error creating launch template for spot instance: %s", err)
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return multistep.ActionHalt
 	}
 
-	s.spotRequest = runSpotResp.SpotInstanceRequests[0]
-
-	spotRequestId := s.spotRequest.SpotInstanceRequestId
-	ui.Message(fmt.Sprintf("Waiting for spot request (%s) to become active...", *spotRequestId))
-	err = WaitUntilSpotRequestFulfilled(ctx, ec2conn, *spotRequestId)
-	if err != nil {
-		err := fmt.Errorf("Error waiting for spot request (%s) to become ready: %s", *spotRequestId, err)
-		state.Put("error", err)
-		ui.Error(err.Error())
-		return multistep.ActionHalt
+	// Add overrides for each user-provided instance type
+	var overrides []*ec2.FleetLaunchTemplateOverridesRequest
+	for _, instanceType := range s.SpotInstanceTypes {
+		override := ec2.FleetLaunchTemplateOverridesRequest{
+			InstanceType: aws.String(instanceType),
+		}
+		overrides = append(overrides, &override)
 	}
 
-	spotResp, err := ec2conn.DescribeSpotInstanceRequests(&ec2.DescribeSpotInstanceRequestsInput{
-		SpotInstanceRequestIds: []*string{spotRequestId},
-	})
-	if err != nil {
-		err := fmt.Errorf("Error finding spot request (%s): %s", *spotRequestId, err)
-		state.Put("error", err)
-		ui.Error(err.Error())
-		return multistep.ActionHalt
+	createFleetInput := &ec2.CreateFleetInput{
+		LaunchTemplateConfigs: []*ec2.FleetLaunchTemplateConfigRequest{
+			{
+				LaunchTemplateSpecification: &ec2.FleetLaunchTemplateSpecificationRequest{
+					LaunchTemplateName: aws.String("packer-fleet-launch-template"),
+					Version:            aws.String("1"),
+				},
+				Overrides: overrides,
+			},
+		},
+		ReplaceUnhealthyInstances: aws.Bool(false),
+		TargetCapacitySpecification: &ec2.TargetCapacitySpecificationRequest{
+			TotalTargetCapacity:       aws.Int64(1),
+			DefaultTargetCapacityType: aws.String("spot"),
+		},
+		Type: aws.String("instant"),
 	}
-	instanceId = *spotResp.SpotInstanceRequests[0].InstanceId
 
-	// Tag spot instance request
+	// Create the request for the spot instance.
+	req, createOutput := ec2conn.CreateFleetRequest(createFleetInput)
+	ui.Message(fmt.Sprintf("Sending spot request (%s)...", req.RequestID))
+
+	// Tag the spot instance request (not the eventual spot instance)
 	spotTags, err := s.SpotTags.EC2Tags(s.Ctx, *ec2conn.Config.Region, state)
 	if err != nil {
-		err := fmt.Errorf("Error tagging spot request: %s", err)
+		err := fmt.Errorf("Error generating tags for spot request: %s", err)
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return multistep.ActionHalt
@@ -248,7 +330,7 @@ func (s *StepRunSpotInstance) Run(ctx context.Context, state multistep.StateBag)
 		}.Run(ctx, func(ctx context.Context) error {
 			_, err := ec2conn.CreateTags(&ec2.CreateTagsInput{
 				Tags:      spotTags,
-				Resources: []*string{spotRequestId},
+				Resources: []*string{aws.String(req.RequestID)},
 			})
 			return err
 		})
@@ -260,20 +342,28 @@ func (s *StepRunSpotInstance) Run(ctx context.Context, state multistep.StateBag)
 		}
 	}
 
-	// Set the instance ID so that the cleanup works properly
-	s.instanceId = instanceId
-
-	ui.Message(fmt.Sprintf("Instance ID: %s", instanceId))
-	ui.Say(fmt.Sprintf("Waiting for instance (%v) to become ready...", instanceId))
-	describeInstance := &ec2.DescribeInstancesInput{
-		InstanceIds: []*string{aws.String(instanceId)},
-	}
-	if err := ec2conn.WaitUntilInstanceRunningWithContext(ctx, describeInstance); err != nil {
-		err := fmt.Errorf("Error waiting for instance (%s) to become ready: %s", instanceId, err)
+	// Actually send the spot connection request.
+	err = req.Send()
+	if err != nil {
+		err := fmt.Errorf("Error waiting for spot request (%s) to become ready: %s", req.RequestID, err)
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return multistep.ActionHalt
 	}
+
+	if len(createOutput.Errors) > 0 {
+		err := fmt.Errorf("error sending spot request: %s", *createOutput.Errors[0])
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+
+	instanceId = *createOutput.Instances[0].InstanceIds[0]
+
+	// Set the instance ID so that the cleanup works properly
+	s.instanceId = instanceId
+
+	ui.Message(fmt.Sprintf("Instance ID: %s", instanceId))
 
 	r, err := ec2conn.DescribeInstances(&ec2.DescribeInstancesInput{
 		InstanceIds: []*string{aws.String(instanceId)},
@@ -400,5 +490,13 @@ func (s *StepRunSpotInstance) Cleanup(state multistep.StateBag) {
 		if err := WaitUntilInstanceTerminated(aws.BackgroundContext(), ec2conn, s.instanceId); err != nil {
 			ui.Error(err.Error())
 		}
+	}
+
+	// Delete the launch template used to create the spot fleet
+	deleteInput := &ec2.DeleteLaunchTemplateInput{
+		LaunchTemplateName: aws.String("packer-fleet-launch-template"),
+	}
+	if _, err := ec2conn.DeleteLaunchTemplate(deleteInput); err != nil {
+		ui.Error(err.Error())
 	}
 }
