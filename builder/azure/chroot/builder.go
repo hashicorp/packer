45d3f28c67c5aeff32316e1652beeaa3ab548238
Paul Meyer <paulmey@microsoft.com>
2019-05-27 06:20:11 +0000

    Add StepCreateNewDisk

--- builder/azure/chroot/builder.go
+++ builder/azure/chroot/builder.go
@@ -3,9 +3,13 @@ package chroot
 import (
 	"context"
 	"errors"
+	"fmt"
+	"log"
 	"runtime"
 
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/compute/mgmt/compute"
 	azcommon "github.com/hashicorp/packer/builder/azure/common"
+	"github.com/hashicorp/packer/builder/azure/common/client"
 	"github.com/hashicorp/packer/common"
 	"github.com/hashicorp/packer/helper/config"
 	"github.com/hashicorp/packer/helper/multistep"
@@ -15,6 +19,11 @@ import (
 
 type Config struct {
 	common.PackerConfig `mapstructure:",squash"`
+
+	FromScratch bool `mapstructure:"from_scratch"`
+
+	OSDiskSizeGB             int32  `mapstructure:"osdisk_size_gb"`
+	OSDiskStorageAccountType string `mapstructure:"osdisk_storageaccounttype"`
 
 	ctx interpolate.Context
 }
@@ -36,9 +45,21 @@ func (b *Builder) Prepare(raws ...interface{}) ([]string, error) {
 		},
 	}, raws...)
 
+	// defaults
+	if b.config.OSDiskStorageAccountType == "" {
+		b.config.OSDiskStorageAccountType = string(compute.PremiumLRS)
+	}
+
 	// checks, accumulate any errors or warnings
 	var errs *packer.MultiError
 	var warns []string
+
+	if b.config.FromScratch {
+		if b.config.OSDiskSizeGB == 0 {
+			errs = packer.MultiErrorAppend(
+				errs, errors.New("osdisk_size_gb is required with from_scratch."))
+		}
+	}
 
 	if err != nil {
 		return nil, err
@@ -51,14 +72,48 @@ func (b *Builder) Run(ctx context.Context, ui packer.Ui, hook packer.Hook) (pack
 		return nil, errors.New("The azure-chroot builder only works on Linux environments.")
 	}
 
+	var azcli client.AzureClientSet
+
 	// Setup the state bag and initial state for the steps
 	state := new(multistep.BasicStateBag)
 	state.Put("config", &b.config)
 	state.Put("hook", hook)
 	state.Put("ui", ui)
 
+	info, err := azcli.MetadataClient().GetComputeInfo()
+	if err != nil {
+		log.Printf("MetadataClient().GetComputeInfo(): error: %+v", err)
+		err := fmt.Errorf(
+			"Error retrieving information ARM resource ID and location" +
+				"of the VM that Packer is running on.\n" +
+				"Please verify that Packer is running on a proper Azure VM.")
+		ui.Error(err.Error())
+		return nil, err
+	}
+
+	osDiskName := "PackerBuiltOsDisk"
+
+	state.Put("instance", info)
+	if err != nil {
+		return nil, err
+	}
+
 	// Build the steps
 	var steps []multistep.Step
+
+	if !b.config.FromScratch {
+		panic("Only from_scratch is currently implemented")
+		// create disk from PIR / managed image (warn on non-linux images)
+	} else {
+		steps = append(steps,
+			&StepCreateNewDisk{
+				SubscriptionID:         info.SubscriptionID,
+				ResourceGroup:          info.ResourceGroupName,
+				DiskName:               osDiskName,
+				DiskSizeGB:             b.config.OSDiskSizeGB,
+				DiskStorageAccountType: b.config.OSDiskStorageAccountType,
+			})
+	}
 
 	// Run!
 	b.runner = common.NewRunner(steps, b.config.PackerConfig, ui)
