abef9820c73c6842409ac99e1deff6d1746d1eed
Jason McIntosh <Jasonmcintosh@carfax.com>
2019-05-30 17:25:36 -0500

    Fixed spacing on comment

--- builder/amazon/common/ami_config.go
+++ builder/amazon/common/ami_config.go
@@ -59,7 +59,7 @@ func (c *AMIConfig) Prepare(accessConfig *AccessConfig, ctx *interpolate.Context
 
 	errs = append(errs, c.prepareRegions(accessConfig)...)
 
-    // Prevent sharing of default KMS key encrypted volumes with other aws users
+	// Prevent sharing of default KMS key encrypted volumes with other aws users
 	if len(c.AMIUsers) > 0 {
 		if len(c.AMIKmsKeyId) == 0 && c.AMIEncryptBootVolume != nil && *c.AMIEncryptBootVolume {
 			errs = append(errs, fmt.Errorf("Cannot share AMI encrypted with default KMS key"))
