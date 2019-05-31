4a94a3565615c5d4849beec79c14b30538623d19
Paul Meyer <paulmey@microsoft.com>
2019-05-31 19:01:47 +0000

    Add StepEarlyCleanup

--- builder/azure/chroot/builder.go
+++ builder/azure/chroot/builder.go
@@ -165,6 +165,7 @@ func (b *Builder) Run(ctx context.Context, ui packer.Ui, hook packer.Hook) (pack
 			Files: b.config.CopyFiles,
 		},
 		&chroot.StepChrootProvision{},
+		&chroot.StepEarlyCleanup{},
 	)
 
 	// Run!
