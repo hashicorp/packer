3845186c4c074d4206c6817628253912d447f189
Pratyush singhal <psinghal20@gmail.com>
2019-06-06 11:13:17 +0530

    feat: add feature to import user-data from a file
    Signed-off-by: Pratyush singhal <psinghal20@gmail.com>

--- builder/googlecompute/config.go
+++ builder/googlecompute/config.go
@@ -62,6 +62,7 @@ type Config struct {
 	Subnetwork                   string                         `mapstructure:"subnetwork"`
 	Tags                         []string                       `mapstructure:"tags"`
 	UseInternalIP                bool                           `mapstructure:"use_internal_ip"`
+	UserdataFile                 string                         `mapstructure:"userdata_file"`
 	Zone                         string                         `mapstructure:"zone"`
 
 	Account            AccountFile
