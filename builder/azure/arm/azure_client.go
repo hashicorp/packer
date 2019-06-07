19e502dda8de8bf3bc9dd6ff22be12d1dc6864bf
Ubuntu <amdut@amdut-p-3.otboe1aqjwuubfabqkoav5tsof.dx.internal.cloudapp.net>
2019-06-07 20:47:37 +0000

    Imports for publishing to SIG

--- builder/azure/arm/azure_client.go
+++ builder/azure/arm/azure_client.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-04-01/compute"
+	newCompute "github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-03-01/compute"
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-01-01/network"
 	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-02-01/resources"
 	armStorage "github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2017-10-01/storage"
@@ -41,6 +42,7 @@ type AzureClient struct {
 	armStorage.AccountsClient
 	compute.DisksClient
 	compute.SnapshotsClient
+	newCompute.Gallery
 
 	InspectorMaxLength int
 	Template           *CaptureTemplate
