c979cad579093d5fdb8fa858db05aa43012245c8
Megan Marsh <megan@hashicorp.com>
2019-06-28 14:20:02 -0700

    add a fixer and update docs for spot_price_auto_product

---
+++ fix/fixer_amazon_spot_price_product.go
@@ -0,0 +1,60 @@
+package fix
+
+import (
+	"github.com/mitchellh/mapstructure"
+)
+
+// FixerAmazonSpotPriceProductDeprecation removes the deprecated "vhd_temp_path" setting
+// from Amazon builder templates
+type FixerAmazonSpotPriceProductDeprecation struct{}
+
+func (FixerAmazonSpotPriceProductDeprecation) Fix(input map[string]interface{}) (map[string]interface{}, error) {
+	// The type we'll decode into; we only care about builders
+	type template struct {
+		Builders []map[string]interface{}
+	}
+
+	// Decode the input into our structure, if we can
+	var tpl template
+	if err := mapstructure.Decode(input, &tpl); err != nil {
+		return nil, err
+	}
+
+	for _, builder := range tpl.Builders {
+		builderTypeRaw, ok := builder["type"]
+		if !ok {
+			continue
+		}
+
+		builderType, ok := builderTypeRaw.(string)
+		if !ok {
+			continue
+		}
+
+		buildersToFix := []string{"amazon-ebs", "amazon-ebssurrogate",
+			"amazon-ebsvolume", "amazon-instance"}
+
+		matched := false
+		for _, b := range buildersToFix {
+			if builderType == b {
+				matched = true
+				break
+			}
+		}
+		if !matched {
+			continue
+		}
+
+		_, ok = builder["spot_price_auto_product"]
+		if ok {
+			delete(builder, "spot_price_auto_product")
+		}
+	}
+
+	input["builders"] = tpl.Builders
+	return input, nil
+}
+
+func (FixerAmazonSpotPriceProductDeprecation) Synopsis() string {
+	return `Removes the deprecated "spot_price_auto_product" setting from Amazon builder templates`
+}
