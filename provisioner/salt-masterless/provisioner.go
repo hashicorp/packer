c6a471d149796bc2860de4f7aa72801b45ffc3f4
David Hao <davidhao3300@yahoo.com>
2019-07-02 18:06:04 -0400

    [Salt] Replace scp with scp + mv to handle non-root case

--- provisioner/salt-masterless/provisioner.go
+++ provisioner/salt-masterless/provisioner.go
@@ -399,9 +399,14 @@ func (p *Provisioner) uploadFile(ui packer.Ui, comm packer.Communicator, dst, sr
 	}
 	defer f.Close()
 
-	if err = comm.Upload(dst, f, nil); err != nil {
+	_, temp_dst := filepath.Split(dst)
+
+	if err = comm.Upload(temp_dst, f, nil); err != nil {
 		return fmt.Errorf("Error uploading %s: %s", src, err)
 	}
+
+	p.moveFile(ui, comm, dst, temp_dst)
+
 	return nil
 }
 
@@ -468,14 +473,9 @@ func (p *Provisioner) removeDir(ui packer.Ui, comm packer.Communicator, dir stri
 }
 
 func (p *Provisioner) uploadDir(ui packer.Ui, comm packer.Communicator, dst, src string, ignore []string) error {
-	if err := p.createDir(ui, comm, dst); err != nil {
+	_, temp_dst := filepath.Split(dst)
+	if err := comm.UploadDir(temp_dst, src, ignore); err != nil {
 		return err
 	}
-
-	// Make sure there is a trailing "/" so that the directory isn't
-	// created on the other side.
-	if src[len(src)-1] != '/' {
-		src = src + "/"
-	}
-	return comm.UploadDir(dst, src, ignore)
+	return p.moveFile(ui, comm, dst, temp_dst)
 }
