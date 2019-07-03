360f00848d948a63f38f0c764089f2fc764f0992
Megan Marsh <megan@hashicorp.com>
2019-07-03 15:58:08 -0700

    supply complete dummy data for googlecompute tests

--- builder/googlecompute/config_test.go
+++ builder/googlecompute/config_test.go
@@ -500,6 +500,16 @@ func testMetadataFile(t *testing.T) string {
 	return tf.Name()
 }
 
-// This is just some dummy data that doesn't actually work (it was revoked
-// a long time ago).
-const testAccountContent = `{}`
+// This is just some dummy data that doesn't actually work
+const testAccountContent = `{
+  "type": "service_account",
+  "project_id": "test-project-123456789",
+  "private_key_id": "bananaphone",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nring_ring_ring_ring_ring_ring_ring_BANANAPHONE\n-----END PRIVATE KEY-----\n",
+  "client_email": "raffi-compute@developer.gserviceaccount.com",
+  "client_id": "1234567890",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://accounts.google.com/o/oauth2/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/12345-compute%40developer.gserviceaccount.com"
+}`
