4016c5dfb41bd04d65b17822f4aa6857c7a4f211
Megan Marsh <megan@hashicorp.com>
2019-07-03 13:30:29 -0700

    replace duplicate commHost functions with a single communicator helper that checks for ssh_host and does a statebag lookup for a given key

--- builder/cloudstack/ssh.go
+++ builder/cloudstack/ssh.go
@@ -7,21 +7,6 @@ import (
 	"github.com/hashicorp/packer/helper/multistep"
 )
 
-func commHost(host string) func(multistep.StateBag) (string, error) {
-	return func(state multistep.StateBag) (string, error) {
-		if host != "" {
-			log.Printf("Using ssh_host value: %s", host)
-			return host, nil
-		}
-		ip, hasIP := state.Get("ipaddress").(string)
-		if !hasIP {
-			return "", fmt.Errorf("Failed to retrieve IP address")
-		}
-
-		return ip, nil
-	}
-}
-
 func commPort(state multistep.StateBag) (int, error) {
 	commPort, hasPort := state.Get("commPort").(int)
 	if !hasPort {
--- builder/profitbricks/builder.go
+++ builder/profitbricks/builder.go
@@ -41,7 +41,7 @@ func (b *Builder) Run(ctx context.Context, ui packer.Ui, hook packer.Hook) (pack
 		new(stepCreateServer),
 		&communicator.StepConnect{
 			Config:    &b.config.Comm,
-			Host:      commHost(b.config.Comm.SSHHost),
+			Host:      communicator.CommHost(b.config.Comm.SSHHost, "server_ip"),
 			SSHConfig: b.config.Comm.SSHConfigFunc(),
 		},
 		&common.StepProvision{},
