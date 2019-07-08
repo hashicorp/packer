a72ec1ae8dc520af63e2e8e168908cc97e34f9ee
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2019-07-08 18:40:01 +0300

    Reuse library function for SSH provisioner config

--- common/config_ssh.go
+++ common/config_ssh.go
@@ -1,12 +1,7 @@
 package common
 
 import (
-	"fmt"
-	packerssh "github.com/hashicorp/packer/communicator/ssh"
-	"github.com/hashicorp/packer/helper/communicator"
 	"github.com/hashicorp/packer/helper/multistep"
-	"golang.org/x/crypto/ssh"
-	"io/ioutil"
 )
 
 func CommHost(host string) func(multistep.StateBag) (string, error) {
@@ -17,38 +12,4 @@ func CommHost(host string) func(multistep.StateBag) (string, error) {
 			return state.Get("ip").(string), nil
 		}
 	}
-}
-
-func SshConfig(state multistep.StateBag) (*ssh.ClientConfig, error) {
-	comm := state.Get("comm").(*communicator.Config)
-
-	var auth []ssh.AuthMethod
-
-	if comm.SSHPrivateKeyFile != "" {
-		privateKey, err := ioutil.ReadFile(comm.SSHPrivateKeyFile)
-		if err != nil {
-			return nil, fmt.Errorf("Error loading configured private key file: %s", err)
-		}
-
-		signer, err := ssh.ParsePrivateKey(privateKey)
-		if err != nil {
-			return nil, fmt.Errorf("Error setting up SSH config: %s", err)
-		}
-
-		auth = []ssh.AuthMethod{ssh.PublicKeys(signer)}
-	} else {
-		auth = []ssh.AuthMethod{
-			ssh.Password(comm.SSHPassword),
-			ssh.KeyboardInteractive(
-				packerssh.PasswordKeyboardInteractive(comm.SSHPassword)),
-		}
-	}
-
-	clientConfig := &ssh.ClientConfig{
-		User:            comm.SSHUsername,
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	}
-	clientConfig.Auth = auth
-
-	return clientConfig, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,5 @@ module github.com/jetbrains-infra/packer-builder-vsphere
 require (
 	github.com/hashicorp/packer v1.4.2
 	github.com/vmware/govmomi v0.20.0
-	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
 	golang.org/x/mobile v0.0.0-20190607214518-6fa95d984e88
 )
