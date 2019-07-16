7f5fd4851e14a5a1f3f3995d9a0609e59ed67d22
Jayson Cofell <70k10@users.noreply.github.com>
2019-07-16 08:55:19 -0600

    QEMU: Remove QMPEnable and depend upon VNC password usage for QMP enablement.

--- builder/qemu/step_configure_qmp.go
+++ builder/qemu/step_configure_qmp.go
@@ -26,7 +26,7 @@ func (s *stepConfigureQMP) Run(ctx context.Context, state multistep.StateBag) mu
 	config := state.Get("config").(*Config)
 	ui := state.Get("ui").(packer.Ui)
 
-	if !config.QMPEnable {
+	if !config.VNCUsePassword {
 		return multistep.ActionContinue
 	}
 
@@ -35,49 +35,46 @@ func (s *stepConfigureQMP) Run(ctx context.Context, state multistep.StateBag) mu
 	log.Print(msg)
 
 	// Only initialize and open QMP when we have a use for it.
-	// Handles cases where user may want the socket, but we don't
-	if config.VNCUsePassword {
-		// Open QMP socket
-		var err error
-		var cmd []byte
-		var result []byte
-		s.monitor, err = qmp.NewSocketMonitor("unix", config.QMPSocketPath, 2*time.Second)
-		if err != nil {
-			err := fmt.Errorf("Error opening QMP socket: %s", err)
-			state.Put("error", err)
-			ui.Error(err.Error())
-			return multistep.ActionHalt
-		}
-		QMPMonitor := s.monitor
-		vncPassword := state.Get("vnc_password")
-
-		// Connect to QMP
-		// function automatically calls capabilities so is immediately ready for commands
-		err = QMPMonitor.Connect()
-		if err != nil {
-			err := fmt.Errorf("Error connecting to QMP socket: %s", err)
-			state.Put("error", err)
-			ui.Error(err.Error())
-			return multistep.ActionHalt
-		}
-		log.Printf("QMP socket open SUCCESS")
-
-		cmd = []byte(fmt.Sprintf("{ \"execute\": \"change-vnc-password\", \"arguments\": { \"password\": \"%s\" } }",
-			vncPassword))
-		result, err = QMPMonitor.Run(cmd)
-		if err != nil {
-			err := fmt.Errorf("Error connecting to QMP socket: %s", err)
-			state.Put("error", err)
-			ui.Error(err.Error())
-			return multistep.ActionHalt
-		}
-		msg = fmt.Sprintf("QMP Command: %s\nResult: %s", cmd, result)
-		log.Printf(msg)
-
-		// Put QMP monitor in statebag in case there is a use in a following step
-		// Uncomment for future case as it is unused for now
-		//state.Put("qmp_monitor", QMPMonitor)
+	// Open QMP socket
+	var err error
+	var cmd []byte
+	var result []byte
+	s.monitor, err = qmp.NewSocketMonitor("unix", config.QMPSocketPath, 2*time.Second)
+	if err != nil {
+		err := fmt.Errorf("Error opening QMP socket: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
 	}
+	QMPMonitor := s.monitor
+	vncPassword := state.Get("vnc_password")
+
+	// Connect to QMP
+	// function automatically calls capabilities so is immediately ready for commands
+	err = QMPMonitor.Connect()
+	if err != nil {
+		err := fmt.Errorf("Error connecting to QMP socket: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+	log.Printf("QMP socket open SUCCESS")
+
+	cmd = []byte(fmt.Sprintf("{ \"execute\": \"change-vnc-password\", \"arguments\": { \"password\": \"%s\" } }",
+		vncPassword))
+	result, err = QMPMonitor.Run(cmd)
+	if err != nil {
+		err := fmt.Errorf("Error connecting to QMP socket: %s", err)
+		state.Put("error", err)
+		ui.Error(err.Error())
+		return multistep.ActionHalt
+	}
+	msg = fmt.Sprintf("QMP Command: %s\nResult: %s", cmd, result)
+	log.Printf(msg)
+
+	// Put QMP monitor in statebag in case there is a use in a following step
+	// Uncomment for future case as it is unused for now
+	//state.Put("qmp_monitor", QMPMonitor)
 
 	return multistep.ActionContinue
 }
