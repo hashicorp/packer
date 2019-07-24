1610280095d9f477d9809b7152a837f862274539
Brendan Devenney <brendan.devenney@form3.tech>
2019-07-24 16:52:07 +0100

    Add test to ensure all fixers are enabled
    Signed-off-by: Brendan Devenney <brendan.devenney@form3.tech>

--- command/fix_test.go
+++ command/fix_test.go
@@ -70,4 +70,19 @@ func TestFix_allFixersEnabled(t *testing.T) {
 	if len(f) != len(o) {
 		t.Fatalf("Fixers length (%d) does not match FixerOrder length (%d)", len(f), len(o))
 	}
+
+	for fixer, _ := range(f) {
+		found := false
+
+		for _, orderedFixer := range(o) {
+			if orderedFixer == fixer {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			t.Fatalf("Did not find Fixer %s in FixerOrder", fixer)
+		}
+	}
 }
