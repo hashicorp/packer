45f81893b9143752841f48d86eb16bdd529d0124
Simon Jones <sijones@pivotal.io>
2019-09-06 14:06:17 -0400

    RunWithUI already calls Wait on the command
    - Remove redundant Wait

--- provisioner/windows-restart/provisioner.go
+++ provisioner/windows-restart/provisioner.go
@@ -268,7 +268,6 @@ var waitForCommunicator = func(ctx context.Context, p *Provisioner) error {
 					log.Printf("Communication connection err: %s", err)
 					shouldContinue = true
 				}
-				cmdKeyCheck.Wait()
 
 				stdoutToRead := buf2.String()
 				if strings.Contains(stdoutToRead, "True") {
