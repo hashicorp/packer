d75e8d13f6d6e027269b08a36d4c3956c874e4a2
David Dymko <dymkod@gmail.com>
2019-09-13 08:48:32 -0400

    updating shutdown to not use API

--- builder/vultr/step_shutdown.go
+++ builder/vultr/step_shutdown.go
@@ -19,27 +19,28 @@ type stepShutdown struct {
 }
 
 func (s *stepShutdown) Run(ctx context.Context, state multistep.StateBag) multistep.StepAction {
-	c := state.Get("config").(*Config)
 	ui := state.Get("ui").(packer.Ui)
-
 	ui.Say("Performing graceful shutdown...")
 	time.Sleep(ShutdownDelaySec * time.Second)
-	id := state.Get("server_id").(string)
-	ui.Say("Performing graceful shutdown...")
 
-	err := s.client.Server.Halt(context.Background(), id)
+	comm := state.Get("communicator").(packer.Communicator)
 
-	if err != nil {
+	cmd := &packer.RemoteCmd{
+		Command: "shutdown -P now",
+	}
+
+	if err := comm.Start(ctx, cmd); err != nil {
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return multistep.ActionHalt
 	}
+	cmd.Wait()
 
-	err = waitForServerState("active", "stopped", id, s.client, c.stateTimeout)
-	if err != nil {
-		state.Put("error", err)
-		ui.Error(err.Error())
-		return multistep.ActionHalt
+	if cmd.ExitStatus() == packer.CmdDisconnect {
+		ui.Say("Server successfully shutdown")
+		time.Sleep(ShutdownDelaySec * time.Second)
+	} else {
+		ui.Say("Sleeping to ensure that server is shut down...")
 	}
 
 	return multistep.ActionContinue
