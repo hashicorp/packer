1edab5b6264d51a0f2d6624596f7988728326d00
Megan Marsh <megan@hashicorp.com>
2019-09-18 10:54:35 -0700

    copypasta

--- background_check_openbsd.go
+++ background_check_openbsd.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func openTTY() (packer.TTY, error) {
+func checkProcess(currentPID int) (bool, error) {
 	return false, fmt.Errorf("cannot determine if process is backgrounded in " +
 		"openbsd")
 }
