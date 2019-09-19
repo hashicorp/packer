4f9c00fe56c71b24ff15cd8f63cf13efe8e9209e
Megan Marsh <megan@hashicorp.com>
2019-09-19 08:57:50 -0700

    surface notimplemented errs too

--- background_check.go
+++ background_check.go
@@ -11,9 +11,12 @@ import (
 func checkProcess(currentPID int) (bool, error) {
 	myProc, err := process.NewProcess(int32(currentPID))
 	if err != nil {
-		return false, fmt.Errorf("Error figuring out Packer process info")
+		return false, fmt.Errorf("Process check error: %s", err)
 	}
-	bg, _ := myProc.Background()
+	bg, err := myProc.Background()
+	if err != nil {
+		return bg, fmt.Errorf("Process background check error: %s", err)
+	}
 
 	return bg, nil
 }
