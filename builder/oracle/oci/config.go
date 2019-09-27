70b2d300e6fae83cda07ce845f9c43f3542c9edb
Robert Neumayer <neumayer@protonmail.com>
2019-09-27 14:49:37 +0200

    Support defined tags for oci builder

--- builder/oracle/oci/config.go
+++ builder/oracle/oci/config.go
@@ -63,7 +63,8 @@ type Config struct {
 	SubnetID string `mapstructure:"subnet_ocid"`
 
 	// Tagging
-	Tags map[string]string `mapstructure:"tags"`
+	Tags        map[string]string                 `mapstructure:"tags"`
+	DefinedTags map[string]map[string]interface{} `mapstructure:"defined_tags"`
 
 	ctx interpolate.Context
 }
@@ -206,6 +207,9 @@ func NewConfig(raws ...interface{}) (*Config, error) {
 			}
 		}
 	}
+
+	//if c.DefinedTags != nil {
+	//	}
 
 	if c.ImageName == "" {
 		name, err := interpolate.Render("packer-{{timestamp}}", nil)
