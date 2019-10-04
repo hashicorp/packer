b5c0742951a8b3dc5912860188517011d5f43873
Paul Meyer <paulmey@microsoft.com>
2019-10-04 17:30:34 +0000

    Add test that verifies disksize bug (2/2)

--- builder/azure/chroot/builder_test.go
+++ builder/azure/chroot/builder_test.go
@@ -1,6 +1,7 @@
 package chroot
 
 import (
+	"reflect"
 	"regexp"
 	"testing"
 
@@ -24,5 +25,49 @@ func TestBuilder_Prepare_DiskAsInput(t *testing.T) {
 				t.Errorf("Did not expect an error about the 'source' field, but found %q", err)
 			}
 		}
+	}
+}
+
+func TestBuilder_Prepare(t *testing.T) {
+	type config map[string]interface{}
+	
+	tests := []struct {
+		name     string
+		config   config
+		want     []string
+		validate func(Config)
+		wantErr  bool
+	}{
+		{
+			name: "HappyPath",
+			config: config{
+				"client_id":         "123",
+				"client_secret":     "456",
+				"subscription_id":   "789",
+				"resource_group":    "rgname",
+				"image_resource_id": "/subscriptions/789/resourceGroups/otherrgname/providers/Microsoft.Compute/images/MyDebianOSImage-{{timestamp}}",
+				"source":            "credativ:Debian:9:latest",
+			},
+			wantErr: false,
+			validate: func(c Config){
+				if(c.OSDiskSizeGB!=0){
+					t.Fatalf("Expected OSDiskSizeGB to be 0, was %+v", c.OSDiskSizeGB)
+				}
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := &Builder{}
+
+			got, err := b.Prepare(tt.config)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Builder.Prepare() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Builder.Prepare() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
