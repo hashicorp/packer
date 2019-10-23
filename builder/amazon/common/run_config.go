524ae7c1d94ed6820bab516d6831ac798ec525b2
Megan Marsh <megan@hashicorp.com>
2019-10-23 09:48:52 -0700

    regerate code generate flat FlatPolicyDocument add PolicyDocument to generate command

--- builder/amazon/common/run_config.go
+++ builder/amazon/common/run_config.go
@@ -1,5 +1,5 @@
 //go:generate struct-markdown
-//go:generate mapstructure-to-hcl2 -type AmiFilterOptions,SecurityGroupFilterOptions,SubnetFilterOptions,VpcFilterOptions
+//go:generate mapstructure-to-hcl2 -type AmiFilterOptions,SecurityGroupFilterOptions,SubnetFilterOptions,VpcFilterOptions,PolicyDocument
 
 package common
 
