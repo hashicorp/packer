50a9d8b86c716084a0983403a322dd682f1a266a
Paul Meyer <paulmey@microsoft.com>
2019-10-24 04:21:08 +0000

    Move common steps together

--- builder/amazon/chroot/step_flock_test.go
+++ builder/amazon/chroot/step_flock_test.go
@@ -1,11 +1,15 @@
 package chroot
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/hashicorp/packer/common/chroot"
+)
 
 func TestFlockCleanupFunc_ImplementsCleanupFunc(t *testing.T) {
 	var raw interface{}
 	raw = new(StepFlock)
-	if _, ok := raw.(Cleanup); !ok {
+	if _, ok := raw.(chroot.Cleanup); !ok {
 		t.Fatalf("cleanup func should be a CleanupFunc")
 	}
 }
