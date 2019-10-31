0a13b620d6b48683a571f294ed6e9f607868edf3
Lars Lehtonen <lars.lehtonen@gmail.com>
2019-10-31 12:19:28 -0700

    builder/amazon/common: remove dead test function StepModifyEBSBackedInstance_EnableAMIENASupport()

--- builder/amazon/common/step_modify_ebs_instance_test.go
+++ builder/amazon/common/step_modify_ebs_instance_test.go
@@ -2,14 +2,11 @@ package common
 
 import (
 	"bytes"
-	"context"
 	"fmt"
-	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
-	helperconfig "github.com/hashicorp/packer/helper/config"
 	"github.com/hashicorp/packer/helper/multistep"
 	"github.com/hashicorp/packer/packer"
 )
@@ -43,63 +40,4 @@ func fakeModifyEBSBackedInstanceState() multistep.StateBag {
 	})
 	state.Put("instance", "i-12345")
 	return state
-}
-
-func StepModifyEBSBackedInstance_EnableAMIENASupport(t *testing.T) {
-	// Value is unset, so we shouldn't modify
-	stepModifyEBSBackedInstance := StepModifyEBSBackedInstance{
-		EnableAMIENASupport:      helperconfig.TriUnset,
-		EnableAMISriovNetSupport: false,
-	}
-
-	// mock out the region connection code
-	mockConn := &mockEC2Conn_ModifyEBS{
-		Config: aws.NewConfig(),
-	}
-
-	state := fakeModifyEBSBackedInstanceState()
-	state.Put("ec2", mockConn)
-	stepModifyEBSBackedInstance.Run(context.Background(), state)
-
-	if mockConn.modifyImageAttrCount > 0 {
-		t.Fatalf("Should not have modified image since EnableAMIENASupport is unset")
-	}
-
-	// Value is true, so we should modify
-	stepModifyEBSBackedInstance = StepModifyEBSBackedInstance{
-		EnableAMIENASupport:      helperconfig.TriTrue,
-		EnableAMISriovNetSupport: false,
-	}
-
-	// mock out the region connection code
-	mockConn = &mockEC2Conn_ModifyEBS{
-		Config: aws.NewConfig(),
-	}
-
-	state = fakeModifyEBSBackedInstanceState()
-	state.Put("ec2", mockConn)
-	stepModifyEBSBackedInstance.Run(context.Background(), state)
-
-	if mockConn.modifyImageAttrCount != 1 {
-		t.Fatalf("Should have modified image, since EnableAMIENASupport is true")
-	}
-
-	// Value is false, so we should modify
-	stepModifyEBSBackedInstance = StepModifyEBSBackedInstance{
-		EnableAMIENASupport:      helperconfig.TriFalse,
-		EnableAMISriovNetSupport: false,
-	}
-
-	// mock out the region connection code
-	mockConn = &mockEC2Conn_ModifyEBS{
-		Config: aws.NewConfig(),
-	}
-
-	state = fakeModifyEBSBackedInstanceState()
-	state.Put("ec2", mockConn)
-	stepModifyEBSBackedInstance.Run(context.Background(), state)
-
-	if mockConn.modifyImageAttrCount != 1 {
-		t.Fatalf("Should have modified image, since EnableAMIENASupport is true")
-	}
 }
