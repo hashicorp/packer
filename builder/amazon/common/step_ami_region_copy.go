d5bddfc63cff075850eccb8ace2089c41e9fcf24
Megan Marsh <megan@hashicorp.com>
2019-11-01 14:17:10 -0700

    delete amazon import post-processor intermediary snapshots

--- builder/amazon/common/step_ami_region_copy.go
+++ builder/amazon/common/step_ami_region_copy.go
@@ -159,48 +159,11 @@ func (s *StepAMIRegionCopy) Cleanup(state multistep.StateBag) {
 	// Delete the unencrypted amis and snapshots
 	ui.Say("Deregistering the AMI and deleting unencrypted temporary " +
 		"AMIs and snapshots")
-
-	resp, err := ec2conn.DescribeImages(&ec2.DescribeImagesInput{
-		ImageIds: []*string{&s.toDelete},
-	})
-
+	err := DestroyAMIs([]*string{&s.toDelete}, ec2conn)
 	if err != nil {
-		err := fmt.Errorf("Error describing AMI: %s", err)
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return
-	}
-
-	// Deregister image by name.
-	for _, i := range resp.Images {
-		_, err := ec2conn.DeregisterImage(&ec2.DeregisterImageInput{
-			ImageId: i.ImageId,
-		})
-
-		if err != nil {
-			err := fmt.Errorf("Error deregistering existing AMI: %s", err)
-			state.Put("error", err)
-			ui.Error(err.Error())
-			return
-		}
-		ui.Say(fmt.Sprintf("Deregistered AMI id: %s", *i.ImageId))
-
-		// Delete snapshot(s) by image
-		for _, b := range i.BlockDeviceMappings {
-			if b.Ebs != nil && aws.StringValue(b.Ebs.SnapshotId) != "" {
-				_, err := ec2conn.DeleteSnapshot(&ec2.DeleteSnapshotInput{
-					SnapshotId: b.Ebs.SnapshotId,
-				})
-
-				if err != nil {
-					err := fmt.Errorf("Error deleting existing snapshot: %s", err)
-					state.Put("error", err)
-					ui.Error(err.Error())
-					return
-				}
-				ui.Say(fmt.Sprintf("Deleted snapshot: %s", *b.Ebs.SnapshotId))
-			}
-		}
 	}
 }
 
