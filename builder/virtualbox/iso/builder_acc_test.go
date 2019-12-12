c6b9efb522fc3f97ded8c1426820bcb5241248b2
Sylvia Moss <moss@hashicorp.com>
2019-12-12 10:43:51 +0100

    Fix virtualbox-iso TestBuilderAcc_basic (#8481)
    make sure the test works for later usage

--- builder/virtualbox/iso/builder_acc_test.go
+++ builder/virtualbox/iso/builder_acc_test.go
@@ -1,30 +1,22 @@
 package iso
 
 import (
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	builderT "github.com/hashicorp/packer/helper/builder/testing"
 )
 
 func TestBuilderAcc_basic(t *testing.T) {
+	templatePath := filepath.Join("testdata", "minimal.json")
+	bytes, err := ioutil.ReadFile(templatePath)
+	if err != nil {
+		t.Fatalf("failed to load template file %s", templatePath)
+	}
+
 	builderT.Test(t, builderT.TestCase{
 		Builder:  &Builder{},
-		Template: testBuilderAccBasic,
+		Template: string(bytes),
 	})
 }
-
-const testBuilderAccBasic = `
-{
-	"builders": [{
-		"type": "test",
-		"guest_os_type": "Ubuntu_64",
-		"iso_url": "http://releases.ubuntu.com/12.04/ubuntu-12.04.5-server-amd64.iso",
-		"iso_checksum": "769474248a3897f4865817446f9a4a53",
-		"iso_checksum_type": "md5",
-		"ssh_username": "packer",
-		"ssh_password": "packer",
-		"ssh_wait_timeout": "30s",
-		"shutdown_command": "echo 'packer' | sudo -S shutdown -P now"
-	}]
-}
-`
