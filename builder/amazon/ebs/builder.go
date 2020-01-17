9eb996657bb17b963d347b81775280ea78612fed
Rich Cavanaugh <rcavanaugh@gmail.com>
2020-01-17 11:56:07 -0500

    allow validation to pass when subnet filters are present

--- builder/amazon/ebs/builder.go
+++ builder/amazon/ebs/builder.go
@@ -209,6 +209,7 @@ func (b *Builder) Run(ctx context.Context, ui packer.Ui, hook packer.Hook) (pack
 			AMISkipBuildRegion: b.config.AMISkipBuildRegion,
 			VpcId:              b.config.VpcId,
 			SubnetId:           b.config.SubnetId,
+			HasSubnetFilter:    len(b.config.SubnetFilter.Filters) > 0,
 		},
 		&awscommon.StepSourceAMIInfo{
 			SourceAmi:                b.config.SourceAmi,
