61bee60ecfc5bdd7a76a46523da7c40965e505bd
Moss <moss@hashicorp.com>
2020-01-28 10:22:10 +0100

    Add StepHttpIpDiscover tests

---
+++ builder/qemu/step_http_ip_discover_test.go
@@ -0,0 +1,29 @@
+package qemu
+
+import (
+	"context"
+	"github.com/hashicorp/packer/common"
+	"github.com/hashicorp/packer/helper/multistep"
+	"testing"
+)
+
+func TestStepHTTPIPDiscover_Run(t *testing.T) {
+	state := new(multistep.BasicStateBag)
+	step := new(stepHTTPIPDiscover)
+	hostIp := "10.0.2.2"
+	previousHttpIp := common.GetHTTPIP()
+
+	// Test the run
+	if action := step.Run(context.Background(), state); action != multistep.ActionContinue {
+		t.Fatalf("bad action: %#v", action)
+	}
+	if _, ok := state.GetOk("error"); ok {
+		t.Fatal("should NOT have error")
+	}
+	httpIp := common.GetHTTPIP()
+	if httpIp != hostIp {
+		t.Fatalf("bad: Http ip is %s but was supposed to be %s", httpIp, hostIp)
+	}
+
+	common.SetHTTPIP(previousHttpIp)
+}
