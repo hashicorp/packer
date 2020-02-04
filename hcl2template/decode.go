aa166f7e38cfd059018ca03fc7a7ed7a2b1627d8
Adrien Delorme <adrien.delorme@icloud.com>
2020-02-04 12:01:57 +0100

    document Decodable and HCL2Ref

--- hcl2template/decode.go
+++ hcl2template/decode.go
@@ -6,6 +6,9 @@ import (
 	"github.com/zclconf/go-cty/cty"
 )
 
+// Decodable structs are structs that can tell their hcl2 ObjectSpec; this
+// config spec will be passed to hcldec.Decode and the result will be a
+// cty.Value. This Value can then be applied on the said struct.
 type Decodable interface {
 	ConfigSpec() hcldec.ObjectSpec
 }
