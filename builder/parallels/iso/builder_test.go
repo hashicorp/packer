5967c76389f8820a417eace5ad0fc25549283d85
Adrien Delorme <adrien.delorme@icloud.com>
2020-02-10 14:32:52 +0100

    Update builder_test.go

--- builder/parallels/iso/builder_test.go
+++ builder/parallels/iso/builder_test.go
@@ -10,7 +10,7 @@ import (
 
 func testConfig() map[string]interface{} {
 	return map[string]interface{}{
-		"iso_checksum":           "foo",
+		"iso_checksum":           "md5:foo",
 		"iso_url":                "http://www.google.com/",
 		"shutdown_command":       "yes",
 		"ssh_username":           "foo",
