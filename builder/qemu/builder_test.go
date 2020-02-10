61f751d80048a0d87add67370ac65791713b5d78
Adrien Delorme <adrien.delorme@icloud.com>
2020-02-10 14:35:17 +0100

    Update builder_test.go

--- builder/qemu/builder_test.go
+++ builder/qemu/builder_test.go
@@ -44,7 +44,7 @@ eHLc1nkCgYEApm/motCTPN32nINZ+Vvywbv64ZD+gtpeMNP3CLrbe1X9O+H52AXa
 
 func testConfig() map[string]interface{} {
 	return map[string]interface{}{
-		"iso_checksum":            "foo",
+		"iso_checksum":            "md5:foo",
 		"iso_url":                 "http://www.google.com/",
 		"ssh_username":            "foo",
 		packer.BuildNameConfigKey: "foo",
