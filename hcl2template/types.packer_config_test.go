3a750756382d030e0adbda0bf8b9de1479e5e1c6
Moss <moss@hashicorp.com>
2020-02-20 16:36:34 +0100

    Rebase master

--- hcl2template/types.packer_config_test.go
+++ hcl2template/types.packer_config_test.go
@@ -33,9 +33,9 @@ func TestParser_complete(t *testing.T) {
 					},
 					"availability_zone_names": &Variable{
 						DefaultValue: cty.ListVal([]cty.Value{
-							cty.StringVal("a"),
-							cty.StringVal("b"),
-							cty.StringVal("c"),
+							cty.StringVal("A"),
+							cty.StringVal("B"),
+							cty.StringVal("C"),
 						}),
 					},
 				},
@@ -43,8 +43,25 @@ func TestParser_complete(t *testing.T) {
 					"feefoo": &Variable{
 						DefaultValue: cty.StringVal("value_image-id-default"),
 					},
-					"standard_tags": &Variable{},
-					"abc_map":       &Variable{},
+					"standard_tags": &Variable{
+						DefaultValue: cty.ObjectVal(map[string]cty.Value{
+							"Component":   cty.StringVal("user-service"),
+							"Environment": cty.StringVal("production"),
+						}),
+					},
+					"abc_map": &Variable{
+						DefaultValue: cty.TupleVal([]cty.Value{
+							cty.ObjectVal(map[string]cty.Value{
+								"id": cty.StringVal("a"),
+							}),
+							cty.ObjectVal(map[string]cty.Value{
+								"id": cty.StringVal("b"),
+							}),
+							cty.ObjectVal(map[string]cty.Value{
+								"id": cty.StringVal("c"),
+							}),
+						}),
+					},
 				},
 				Sources: map[SourceRef]*SourceBlock{
 					refVBIsoUbuntu1204: {Type: "virtualbox-iso", Name: "ubuntu-1204"},
