7ec39ebb4ea30c2c6a203118cad5d9225e0d48da
Adrien Delorme <adrien.delorme@icloud.com>
2020-03-12 18:01:20 +0100

    TestStringSlice_Set: check error even if none is returned

--- helper/flag-kv/flag_strings_test.go
+++ helper/flag-kv/flag_strings_test.go
@@ -22,7 +22,10 @@ func TestStringSlice_Set(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			for _, value := range tt.args.values {
-				tt.s.Set(value)
+				err := tt.s.Set(value)
+				if err != nil {
+					t.Fatal(err)
+				}
 			}
 			if diff := cmp.Diff(tt.s, tt.wantStringSlice); diff != "" {
 				t.Fatal(diff)
