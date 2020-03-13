cba0ef490c8fe074514181b2e254ad06e7a8a60e
Moss <moss@hashicorp.com>
2020-03-13 17:50:05 +0100

    Add acc test validation to avoid running with unit tests

--- helper/tests/core.go
+++ helper/tests/core.go
@@ -2,6 +2,7 @@ package testshelper
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"testing"
 
@@ -64,4 +65,23 @@ func FatalCommand(t *testing.T, m command.Meta) {
 		"Bad exit code.\n\nStdout:\n\n%s\n\nStderr:\n\n%s",
 		out.String(),
 		err.String())
+}
+
+const TestEnvVar = "PACKER_ACC"
+
+func AccTestPreValidate(t *testing.T) {
+	// We only run acceptance tests if an env var is set because they're
+	// slow and generally require some outside configuration.
+	if os.Getenv(TestEnvVar) == "" {
+		t.Skip(fmt.Sprintf(
+			"Acceptance tests skipped unless env '%s' set",
+			TestEnvVar))
+		return
+	}
+
+	// We require verbose mode so that the user knows what is going on.
+	if !testing.Verbose() {
+		t.Fatal("Acceptance tests must be run with the -v flag on tests")
+		return
+	}
 }
