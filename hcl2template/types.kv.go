df31162121f2aea2e487b300257485eb44ca4dae
Adrien Delorme <adrien.delorme@icloud.com>
2020-03-13 17:17:00 +0100

    add kvfilter type that regroups very frequently used filters

--- hcl2template/types.kv.go
+++ hcl2template/types.kv.go
@@ -6,3 +6,19 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+type KVFilter struct {
+	Filters map[string]string
+	Filter  []KeyValue
+}
+
+func (kvf *KVFilter) Prepare() error {
+	for _, filter := range kvf.Filter {
+		kvf.Filters[filter.Key] = filter.Value
+	}
+	return nil
+}
+
+func (kvf *KVFilter) Empty() bool {
+	return len(kvf.Filters) == 0
+}
