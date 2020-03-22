35398bacbc00c61b47d5eddd41ac7e5fd1bd8610
Jorge Marey <jorge.marey.oton.next@bbva.com>
2020-03-22 11:44:33 +0100

    Format code

--- builder/openstack/step_add_image_members.go
+++ builder/openstack/step_add_image_members.go
@@ -38,16 +38,16 @@ func (s *stepAddImageMembers) Run(ctx context.Context, state multistep.StateBag)
 	}
 
 	if config.ImageAutoAcceptMembers {
-        for _, member := range config.ImageMembers {
-            ui.Say(fmt.Sprintf("Accepting image %s for member '%s'", imageId, member))
-            r := members.Update(imageClient, imageId, member, members.UpdateOpts{Status: "accepted"})
-            if _, err = r.Extract(); err != nil {
-                err = fmt.Errorf("Error accepting image for member: %s", err)
-                state.Put("error", err)
-                return multistep.ActionHalt
-            }
-        }
-    }
+		for _, member := range config.ImageMembers {
+			ui.Say(fmt.Sprintf("Accepting image %s for member '%s'", imageId, member))
+			r := members.Update(imageClient, imageId, member, members.UpdateOpts{Status: "accepted"})
+			if _, err = r.Extract(); err != nil {
+				err = fmt.Errorf("Error accepting image for member: %s", err)
+				state.Put("error", err)
+				return multistep.ActionHalt
+			}
+		}
+	}
 
 	return multistep.ActionContinue
 }
