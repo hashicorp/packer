5bebcb097cc03833a41949bff30cc18931dc5749
Jeff Escalante <jescalan@users.noreply.github.com>
2020-03-24 20:03:49 -0400

    update deps

--- website/next.config.js
+++ website/next.config.js
@@ -4,21 +4,20 @@ const path = require('path')
 module.exports = withHashicorp({
   defaultLayout: true,
   transpileModules: ['is-absolute-url', '@hashicorp/react-mega-nav'],
-  mdx: { resolveIncludes: path.join(__dirname, 'pages/partials') }
+  mdx: { resolveIncludes: path.join(__dirname, 'pages/partials') },
 })({
   experimental: {
     css: true,
     modern: true,
-    polyfillsOptimization: true,
     rewrites: () => [
       {
         source: '/api/:path*',
-        destination: '/api-docs/:path*'
-      }
-    ]
+        destination: '/api-docs/:path*',
+      },
+    ],
   },
   exportTrailingSlash: true,
   env: {
-    HASHI_ENV: process.env.HASHI_ENV
-  }
+    HASHI_ENV: process.env.HASHI_ENV,
+  },
 })
