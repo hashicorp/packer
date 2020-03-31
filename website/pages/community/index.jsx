8ca9f2a58c98d670c8a834317a15d753405cbf91
Jeff Escalante <jescalan@users.noreply.github.com>
2020-03-31 16:11:11 -0400

    add community page css

--- website/pages/community/index.jsx
+++ website/pages/community/index.jsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 
 export default function CommunityPage() {
   return (
-    <div id="community">
+    <div id="p-community">
       <Head>
         <title key="title">Community | Packer by HashiCorp</title>
       </Head>
@@ -17,33 +17,33 @@ export default function CommunityPage() {
         data={[
           {
             header: 'IRC',
-            body: '`#packer-tool` on Freenode'
+            body: '`#packer-tool` on Freenode',
           },
           {
             header: 'Announcement List',
             body:
-              '[HashiCorp Announcement Google Group](https://groups.google.com/group/hashicorp-announce)'
+              '[HashiCorp Announcement Google Group](https://groups.google.com/group/hashicorp-announce)',
           },
           {
             header: 'Discussion List',
             body:
-              '[Packer Google Group](https://groups.google.com/group/packer-tool)'
+              '[Packer Google Group](https://groups.google.com/group/packer-tool)',
           },
           {
             header: 'Community Forum',
             body:
-              '[Packer Community Forum](https://discuss.hashicorp.com/c/packer)'
+              '[Packer Community Forum](https://discuss.hashicorp.com/c/packer)',
           },
           {
             header: 'Bug Tracker',
             body:
-              '[Issue tracker on GitHub](https://github.com/hashicorp/packer/issues). Please only use this for reporting bugs. Do not ask for general help here. Use IRC or the mailing list for that.'
+              '[Issue tracker on GitHub](https://github.com/hashicorp/packer/issues). Please only use this for reporting bugs. Do not ask for general help here. Use IRC or the mailing list for that.',
           },
           {
             header: 'Training',
             body:
-              'Paid [HashiCorp training courses](https://www.hashicorp.com/training) are also available in a city near you. Private training courses are also available.'
-          }
+              'Paid [HashiCorp training courses](https://www.hashicorp.com/training) are also available in a city near you. Private training courses are also available.',
+          },
         ]}
       />
     </div>
