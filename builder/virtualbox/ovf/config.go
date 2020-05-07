b86efe7604e11c7bd4fcb54712e0953d199b0b3d
Ryan Hartje <ryan.hartje@microsoft.com>
2020-05-07 18:08:52 -0500

    Vbox Checksum Bugfix (#9101)
    * attempting to repro github issue 9049
    
    * update vbox ovf configtest to table test for mixedcase bug

--- builder/virtualbox/ovf/config.go
+++ builder/virtualbox/ovf/config.go
@@ -155,8 +155,6 @@ func (c *Config) Prepare(raws ...interface{}) ([]string, error) {
 	errs = packer.MultiErrorAppend(errs, c.GuestAdditionsConfig.Prepare(&c.ctx)...)
 
 	c.ChecksumType = strings.ToLower(c.ChecksumType)
-	c.Checksum = strings.ToLower(c.Checksum)
-
 	if c.SourcePath == "" {
 		errs = packer.MultiErrorAppend(errs, fmt.Errorf("source_path is required"))
 	}
