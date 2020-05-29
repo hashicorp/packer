20172e185d29cb2b9aec691d80a250f33cd58a8a
Jeff Escalante <jescalan@users.noreply.github.com>
2020-05-29 17:13:27 -0400

    formatting fixes

--- website/layouts/intro.jsx
+++ website/layouts/intro.jsx
@@ -1,5 +1,5 @@
 import DocsPage from '@hashicorp/react-docs-page'
-import order from '../data/intro-navigation.js'
+import order from 'data/intro-navigation.js'
 import { frontMatter as data } from '../pages/intro/**/*.mdx'
 import Head from 'next/head'
 import Link from 'next/link'
@@ -14,14 +14,14 @@ function IntroLayoutWrapper(pageMeta) {
           is: Head,
           title: `${pageMeta.page_title} | Packer by HashiCorp`,
           description: pageMeta.description,
-          siteName: 'Packer by HashiCorp'
+          siteName: 'Packer by HashiCorp',
         }}
         sidenav={{
           Link,
           category: 'intro',
           currentPage: props.path,
           data,
-          order
+          order,
         }}
         resourceURL={`https://github.com/hashicorp/packer/blob/master/website/pages/${pageMeta.__resourcePath}`}
       />
