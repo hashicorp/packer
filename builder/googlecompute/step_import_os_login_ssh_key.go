fc83240508a67ca12143db591ca89aedb8764a69
Wilken Rivera <dev@wilkenrivera.com>
2020-06-30 15:59:44 -0400

    Add oauth2 API call to get account email when no account file is available

--- builder/googlecompute/step_import_os_login_ssh_key.go
+++ builder/googlecompute/step_import_os_login_ssh_key.go
@@ -8,11 +8,13 @@ import (
 
 	"github.com/hashicorp/packer/helper/multistep"
 	"github.com/hashicorp/packer/packer"
+	"google.golang.org/api/oauth2/v2"
 )
 
 // StepCreateSSHKey represents a Packer build step that generates SSH key pairs.
 type StepImportOSLoginSSHKey struct {
-	Debug bool
+	Debug        bool
+	accountEmail string
 }
 
 // Run executes the Packer build step that generates SSH key pairs.
@@ -26,22 +28,43 @@ func (s *StepImportOSLoginSSHKey) Run(ctx context.Context, state multistep.State
 		return multistep.ActionContinue
 	}
 
-	ui.Say("Importing SSH public key for OSLogin...")
+	// If no public key information is available chances are that a private key was provided
+	//  or that the user is using a SSH agent for authentication.
+	if config.Comm.SSHPublicKey == nil {
+		ui.Say("No public SSH key found; skipping SSH public key import for OSLogin...")
+		return multistep.ActionContinue
+	}
 
+	ui.Say("Importing SSH public key for OSLogin...")
 	// Generate SHA256 fingerprint of SSH public key
 	// Put it into state to clean up later
 	sha256sum := sha256.Sum256(config.Comm.SSHPublicKey)
 	state.Put("ssh_key_public_sha256", hex.EncodeToString(sha256sum[:]))
 
-	// TODO: @cpwc need to obtain user's email
-	if config.account == nil {
-		err := fmt.Errorf("Error importing SSH public key for OSLogin: need user's ID/email")
-		state.Put("error", err)
-		ui.Error(err.Error())
-		return multistep.ActionHalt
+	if config.account != nil {
+		s.accountEmail = config.account.Email
 	}
 
-	loginProfile, err := driver.ImportOSLoginSSHKey(config.account.Email, string(config.Comm.SSHPublicKey))
+	if s.accountEmail == "" {
+		svc, err := oauth2.NewService(ctx)
+		if err != nil {
+			err := fmt.Errorf("Error initializing oauth service needed for OsLogin: %s", err)
+			state.Put("error", err)
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
+
+		info, err := svc.Tokeninfo().Context(ctx).Do()
+		if err != nil {
+			err := fmt.Errorf("Error obtaining token information needed for OsLogin: %s", err)
+			state.Put("error", err)
+			ui.Error(err.Error())
+			return multistep.ActionHalt
+		}
+		s.accountEmail = info.Email
+	}
+
+	loginProfile, err := driver.ImportOSLoginSSHKey(s.accountEmail, string(config.Comm.SSHPublicKey))
 	if err != nil {
 		err := fmt.Errorf("Error importing SSH public key for OSLogin: %s", err)
 		state.Put("error", err)
@@ -81,18 +104,17 @@ func (s *StepImportOSLoginSSHKey) Cleanup(state multistep.StateBag) {
 	driver := state.Get("driver").(Driver)
 	ui := state.Get("ui").(packer.Ui)
 
-	ui.Say("Deleting SSH public key for OSLogin...")
-
-	// TODO: @cpwc need to obtain user's email
-	if config.account == nil {
-		err := fmt.Errorf("Error deleting SSH public key for OSLogin: need user's ID/email")
-		state.Put("error", err)
-		ui.Error(err.Error())
+	if !config.UseOSLogin {
 		return
 	}
 
 	fingerprint := state.Get("ssh_key_public_sha256").(string)
-	err := driver.DeleteOSLoginSSHKey(config.account.Email, fingerprint)
+	if fingerprint == "" {
+		return
+	}
+
+	ui.Say("Deleting SSH public key for OSLogin...")
+	err := driver.DeleteOSLoginSSHKey(s.accountEmail, fingerprint)
 	if err != nil {
 		ui.Error(fmt.Sprintf("Error deleting SSH public key for OSLogin. Please delete it manually.\n\nError: %s", err))
 		return
