6e9eaa1edd13dce5f951f17602cf6c50df00dc3e
Megan Marsh <megan@hashicorp.com>
2020-07-07 16:01:41 -0700

    generate code

--- builder/googlecompute/step_start_tunnel.hcl2spec.go
+++ builder/googlecompute/step_start_tunnel.hcl2spec.go
@@ -9,10 +9,11 @@ import (
 // FlatIAPConfig is an auto-generated flat version of IAPConfig.
 // Where the contents of a field with a `mapstructure:,squash` tag are bubbled up.
 type FlatIAPConfig struct {
-	IAP              *bool   `mapstructure:"use_iap" required:"false" cty:"use_iap" hcl:"use_iap"`
-	IAPLocalhostPort *int    `mapstructure:"iap_localhost_port" cty:"iap_localhost_port" hcl:"iap_localhost_port"`
-	IAPHashBang      *string `mapstructure:"iap_hashbang" required:"false" cty:"iap_hashbang" hcl:"iap_hashbang"`
-	IAPExt           *string `mapstructure:"iap_ext" required:"false" cty:"iap_ext" hcl:"iap_ext"`
+	IAP                    *bool   `mapstructure:"use_iap" required:"false" cty:"use_iap" hcl:"use_iap"`
+	IAPLocalhostPort       *int    `mapstructure:"iap_localhost_port" cty:"iap_localhost_port" hcl:"iap_localhost_port"`
+	IAPHashBang            *string `mapstructure:"iap_hashbang" required:"false" cty:"iap_hashbang" hcl:"iap_hashbang"`
+	IAPExt                 *string `mapstructure:"iap_ext" required:"false" cty:"iap_ext" hcl:"iap_ext"`
+	IAPTunnelLaunchTimeout *int    `mapstructure:"iap_tunnel_launch_timeout" required:"false" cty:"iap_tunnel_launch_timeout" hcl:"iap_tunnel_launch_timeout"`
 }
 
 // FlatMapstructure returns a new FlatIAPConfig.
@@ -27,10 +28,11 @@ func (*IAPConfig) FlatMapstructure() interface{ HCL2Spec() map[string]hcldec.Spe
 // The decoded values from this spec will then be applied to a FlatIAPConfig.
 func (*FlatIAPConfig) HCL2Spec() map[string]hcldec.Spec {
 	s := map[string]hcldec.Spec{
-		"use_iap":            &hcldec.AttrSpec{Name: "use_iap", Type: cty.Bool, Required: false},
-		"iap_localhost_port": &hcldec.AttrSpec{Name: "iap_localhost_port", Type: cty.Number, Required: false},
-		"iap_hashbang":       &hcldec.AttrSpec{Name: "iap_hashbang", Type: cty.String, Required: false},
-		"iap_ext":            &hcldec.AttrSpec{Name: "iap_ext", Type: cty.String, Required: false},
+		"use_iap":                   &hcldec.AttrSpec{Name: "use_iap", Type: cty.Bool, Required: false},
+		"iap_localhost_port":        &hcldec.AttrSpec{Name: "iap_localhost_port", Type: cty.Number, Required: false},
+		"iap_hashbang":              &hcldec.AttrSpec{Name: "iap_hashbang", Type: cty.String, Required: false},
+		"iap_ext":                   &hcldec.AttrSpec{Name: "iap_ext", Type: cty.String, Required: false},
+		"iap_tunnel_launch_timeout": &hcldec.AttrSpec{Name: "iap_tunnel_launch_timeout", Type: cty.Number, Required: false},
 	}
 	return s
 }
