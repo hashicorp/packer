b0238e6f4a3f4b688a2d0010d6633e86a31c03cb
Adrien Delorme <adrien.delorme@icloud.com>
2020-07-07 12:00:36 +0200

    Update build_windows_test.go

--- command/build_windows_test.go
+++ command/build_windows_test.go
@@ -5,6 +5,7 @@ import "strings"
 func init() {
 	spaghettiCarbonara = fixWindowsLineEndings(spaghettiCarbonara)
 	lasagna = fixWindowsLineEndings(lasagna)
+	tiramisu = fixWindowsLineEndings(tiramisu)
 }
 
 func fixWindowsLineEndings(s string) string {
