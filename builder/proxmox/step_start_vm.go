d3d7cc3e59bea677addb2ced5267de382ba30844
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2020-07-16 17:56:58 +0100

    configure packet queues on nics

--- builder/proxmox/step_start_vm.go
+++ builder/proxmox/step_start_vm.go
@@ -106,6 +106,10 @@ func generateProxmoxNetworkAdapters(nics []nicConfig) proxmox.QemuDevices {
 		setDeviceParamIfDefined(devs[idx], "bridge", nics[idx].Bridge)
 		setDeviceParamIfDefined(devs[idx], "tag", nics[idx].VLANTag)
 		setDeviceParamIfDefined(devs[idx], "firewall", strconv.FormatBool(nics[idx].Firewall))
+
+		if nics[idx].PacketQueues > 0 {
+			devs[idx]["queues"] = nics[idx].PacketQueues
+		}
 	}
 	return devs
 }
