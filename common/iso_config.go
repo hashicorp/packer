8533c448dd108f8fc7c4a17afb8ec5f3a9233ad1
sylviamoss <moss@hashicorp.com>
2020-07-31 11:08:17 +0200

    add hcl2 examples to boot command and iso config partials

--- common/iso_config.go
+++ common/iso_config.go
@@ -32,11 +32,49 @@ import (
 // go-getter can guess the checksum type based on `iso_checksum` length, and it is
 // also possible to specify the checksum type.
 //
-// For example, some possible values for `iso_checksum` are:
-//   * `946a6077af6f5f95a51f82fdc44051c7aa19f9cfc5f737954845a6050543d7c2`
-//   * `file:ubuntu.org/..../ubuntu-14.04.1-server-amd64.iso.sum`
-//   * `file://./shasums.txt`
-//   * `file:./shasums.txt`
+// In JSON:
+//
+// ```json
+//   "iso_checksum": "946a6077af6f5f95a51f82fdc44051c7aa19f9cfc5f737954845a6050543d7c2",
+//   "iso_url": "ubuntu.org/.../ubuntu-14.04.1-server-amd64.iso"
+// ```
+//
+// ```json
+//   "iso_checksum": "file:ubuntu.org/..../ubuntu-14.04.1-server-amd64.iso.sum",
+//   "iso_url": "ubuntu.org/.../ubuntu-14.04.1-server-amd64.iso"
+// ```
+//
+// ```json
+//   "iso_checksum": "file://./shasums.txt",
+//   "iso_url": "ubuntu.org/.../ubuntu-14.04.1-server-amd64.iso"
+// ```
+//
+// ```json
+//   "iso_checksum": "file:./shasums.txt",
+//   "iso_url": "ubuntu.org/.../ubuntu-14.04.1-server-amd64.iso"
+// ```
+//
+// In HCL2:
+//
+// ```hcl
+//   iso_checksum = "946a6077af6f5f95a51f82fdc44051c7aa19f9cfc5f737954845a6050543d7c2"
+//   iso_url = "ubuntu.org/.../ubuntu-14.04.1-server-amd64.iso"
+// ```
+//
+// ```hcl
+//   iso_checksum = "file:ubuntu.org/..../ubuntu-14.04.1-server-amd64.iso.sum"
+//   iso_url = "ubuntu.org/.../ubuntu-14.04.1-server-amd64.iso"
+// ```
+//
+// ```hcl
+//   iso_checksum = "file://./shasums.txt"
+//   iso_url = "ubuntu.org/.../ubuntu-14.04.1-server-amd64.iso"
+// ```
+//
+// ```hcl
+//   iso_checksum = "file:./shasums.txt",
+//   iso_url = "ubuntu.org/.../ubuntu-14.04.1-server-amd64.iso"
+// ```
 //
 type ISOConfig struct {
 	// The checksum for the ISO file or virtual hard drive file. The type of
