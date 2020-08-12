78c280b1b7245af27acf1a0b5d918398b2a12b25
jsmcnair <john@jsmcnair.com>
2020-08-12 22:34:13 +0100

    Use common funcs / fix panic / clarify docs

--- common/template/funcs.go
+++ common/template/funcs.go
@@ -1,8 +1,14 @@
 package template
 
 import (
+	"errors"
+	"fmt"
 	"log"
+	"os"
+	"strings"
 	"sync"
+
+	vaultapi "github.com/hashicorp/vault/api"
 )
 
 // DeprecatedTemplateFunc wraps a template func to warn users that it's
@@ -16,4 +22,42 @@ func DeprecatedTemplateFunc(funcName, useInstead string, deprecated func(string)
 		})
 		return deprecated(in)
 	}
+}
+
+// Vault retrieves a secret from an HC vault KV store
+func Vault(path string, key string) (string, error) {
+
+	if token := os.Getenv("VAULT_TOKEN"); token == "" {
+		return "", errors.New("Must set VAULT_TOKEN env var in order to use vault template function")
+	}
+
+	vaultConfig := vaultapi.DefaultConfig()
+	cli, err := vaultapi.NewClient(vaultConfig)
+	if err != nil {
+		return "", fmt.Errorf("Error getting Vault client: %s", err)
+	}
+	secret, err := cli.Logical().Read(path)
+	if err != nil {
+		return "", fmt.Errorf("Error reading vault secret: %s", err)
+	}
+	if secret == nil {
+		return "", errors.New("Vault Secret does not exist at the given path")
+	}
+
+	data, ok := secret.Data["data"]
+	if !ok {
+		// maybe ths is v1, not v2 kv store
+		value, ok := secret.Data[key]
+		if ok {
+			return value.(string), nil
+		}
+
+		// neither v1 nor v2 proudced a valid value
+		return "", fmt.Errorf("Vault data was empty at the given path. Warnings: %s", strings.Join(secret.Warnings, "; "))
+	}
+
+	if val, ok := data.(map[string]interface{})[key]; ok {
+		return val.(string), nil
+	}
+	return "", errors.New("Vault path does not contain the requested key")
 }
--- hcl2template/function/vault.go
+++ hcl2template/function/vault.go
@@ -1,15 +1,10 @@
 package function
 
 import (
-	"errors"
-	"fmt"
-	"os"
-	"strings"
-
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/function"
 
-	vaultapi "github.com/hashicorp/vault/api"
+	commontpl "github.com/hashicorp/packer/common/template"
 )
 
 // VaultFunc constructs a function that retrieves KV secrets from HC vault
@@ -26,47 +21,11 @@ var VaultFunc = function.New(&function.Spec{
 	},
 	Type: function.StaticReturnType(cty.String),
 	Impl: func(args []cty.Value, retType cty.Type) (cty.Value, error) {
-
 		path := args[0].AsString()
 		key := args[1].AsString()
 
-		if token := os.Getenv("VAULT_TOKEN"); token == "" {
-			return cty.StringVal(""), errors.New("Must set VAULT_TOKEN env var in order to " +
-				"use vault template function")
-		}
+		val, err := commontpl.Vault(path, key)
 
-		vaultConfig := vaultapi.DefaultConfig()
-		cli, err := vaultapi.NewClient(vaultConfig)
-		if err != nil {
-			return cty.StringVal(""), fmt.Errorf("Error getting Vault client: %s", err)
-		}
-		secret, err := cli.Logical().Read(path)
-		if err != nil {
-			return cty.StringVal(""), fmt.Errorf("Error reading vault secret: %s", err)
-		}
-		if secret == nil {
-			return cty.StringVal(""), errors.New("Vault Secret does not exist at the given path")
-		}
-
-		data, ok := secret.Data["data"]
-		if !ok {
-			// maybe ths is v1, not v2 kv store
-			value, ok := secret.Data[key]
-			if ok {
-				return cty.StringVal(value.(string)), nil
-			}
-
-			// neither v1 nor v2 proudced a valid value
-			return cty.StringVal(""), fmt.Errorf("Vault data was empty at the "+
-				"given path. Warnings: %s", strings.Join(secret.Warnings, "; "))
-		}
-
-		value := data.(map[string]interface{})[key].(string)
-		return cty.StringVal(value), nil
+		return cty.StringVal(val), err
 	},
 })
-
-// Vault returns a secret from a KV store in HC vault
-func Vault() (cty.Value, error) {
-	return VaultFunc.Call([]cty.Value{})
-}
