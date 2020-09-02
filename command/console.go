efd69aea2a4ae83e344f886a8478e2f30de71cae
Adrien Delorme <azr@users.noreply.github.com>
2020-09-02 17:19:56 +0200

    console: allow to use console when there are untyped vars (#9864)
    * the var will simply be "unknown"
    * add a test

--- command/console.go
+++ command/console.go
@@ -60,11 +60,7 @@ func (c *ConsoleCommand) RunContext(ctx context.Context, cla *ConsoleArgs) int {
 		return ret
 	}
 
-	diags := packerStarter.Initialize()
-	ret = writeDiags(c.Ui, nil, diags)
-	if ret != 0 {
-		return ret
-	}
+	_ = packerStarter.Initialize()
 
 	// Determine if stdin is a pipe. If so, we evaluate directly.
 	if c.StdinPiped() {
