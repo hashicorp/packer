536421efafa333fca99cb2f8c717b850a3ab7804
Adrien Delorme <adrien.delorme@icloud.com>
2020-10-20 12:00:49 +0200

    debug mode: don't dump the ssh key if its from a file

--- builder/googlecompute/builder.go
+++ builder/googlecompute/builder.go
@@ -63,7 +63,7 @@ func (b *Builder) Run(ctx context.Context, ui packer.Ui, hook packer.Hook) (pack
 			CommConf:            &b.config.Comm,
 			SSHTemporaryKeyPair: b.config.Comm.SSH.SSHTemporaryKeyPair,
 		},
-		multistep.If(b.config.PackerDebug,
+		multistep.If(b.config.PackerDebug && b.config.Comm.SSHPrivateKeyFile == "",
 			&communicator.StepDumpSSHKey{
 				Path: fmt.Sprintf("gce_%s.pem", b.config.PackerBuildName),
 			},
