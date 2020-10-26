b77eb0d60f36434bb497f7a1a2fafb84f2d40af0
Adrien Delorme <adrien.delorme@icloud.com>
2020-10-26 14:34:15 +0100

    remove module references

--- hcl2template/version_required.go
+++ hcl2template/version_required.go
@@ -7,9 +7,9 @@ import (
 	pkrversion "github.com/hashicorp/packer/version"
 )
 
-// CheckCoreVersionRequirements visits each of the modules in the given
-// configuration tree and verifies that any given Core version constraints
-// match with the version of Packer Core that is being used.
+// CheckCoreVersionRequirements visits each of the block in the given
+// configuration and verifies that any given Core version constraints match
+// with the version of Packer Core that is being used.
 //
 // The returned diagnostics will contain errors if any constraints do not match.
 // The returned diagnostics might also return warnings, which should be
