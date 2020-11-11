fa233a6a56505f7cb5b589771b7a7a70bb839f1c
Megan Marsh <megan@hashicorp.com>
2020-11-11 15:07:20 -0800

    remove unused code leftover from original progress bar implementation

--- common/terminal_test.go
+++
@@ -1,9 +0,0 @@
-package common
-
-import "testing"
-
-func TestGetTerminalDimensions(t *testing.T) {
-	if _, _, err := GetTerminalDimensions(); err != nil {
-		t.Fatalf("Unable to get terminal dimensions: %s", err)
-	}
-}
