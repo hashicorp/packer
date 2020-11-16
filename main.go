e2addf6ca22e66c84ca9031dbd2c9d71b1359e2e
Megan Marsh <megan@hashicorp.com>
2020-11-16 12:05:57 -0800

    fix website

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 // This is the main package for the `packer` application.
 
 //go:generate go run ./scripts/generate-plugins.go
-//go:generate go generate ./common/bootcommand/...
+//go:generate go generate ./packer-plugin-sdk/bootcommand/...
 package main
 
 import (
