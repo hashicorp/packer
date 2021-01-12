261abe0caefba9bd62235b52f60170759dd6a05d
teddylear <teddylear@gmail.com>
2021-01-11 21:53:27 -0500

    Setting recursive fmt to false, updatting recursive fmt test to validate formatted files

--- command/fmt_test.go
+++ command/fmt_test.go
@@ -1,10 +1,14 @@
 package command
 
 import (
+	"io/ioutil"
+	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	packersdk "github.com/hashicorp/packer-plugin-sdk/packer"
 	"github.com/stretchr/testify/assert"
 )
@@ -56,9 +60,76 @@ func TestFmt_Recursive(t *testing.T) {
 		Meta: testMeta(t),
 	}
 
-	args := []string{"-check=true", "-recursive=true", filepath.Join(testFixture("fmt"), "")}
+	unformattedData, err := ioutil.ReadFile("test-fixtures/fmt/unformatted.pkrvars.hcl")
+	if err != nil {
+		t.Fatalf("failed to open the unformatted fixture %s", err)
+	}
 
-	if code := c.Run(args); code != 3 {
+	var subDir string
+	subDir, err = ioutil.TempDir("test-fixtures/fmt", "sub_dir")
+	if err != nil {
+		t.Fatalf("failed to create sub level recurisve directory for test %s", err)
+	}
+	defer os.Remove(subDir)
+
+	var superSubDir string
+	superSubDir, err = ioutil.TempDir(subDir, "super_sub_dir")
+	if err != nil {
+		t.Fatalf("failed to create sub level recurisve directory for test %s", err)
+	}
+	defer os.Remove(superSubDir)
+
+	tf, err := ioutil.TempFile(subDir, "*.pkrvars.hcl")
+	if err != nil {
+		t.Fatalf("failed to create top level tempfile for test %s", err)
+	}
+	defer os.Remove(tf.Name())
+
+	_, _ = tf.Write(unformattedData)
+	tf.Close()
+
+	subTf, err := ioutil.TempFile(superSubDir, "*.pkrvars.hcl")
+	if err != nil {
+		t.Fatalf("failed to create sub level tempfile for test %s", err)
+	}
+	defer os.Remove(subTf.Name())
+
+	_, _ = subTf.Write(unformattedData)
+	subTf.Close()
+
+	var directoryDelimiter string
+	if runtime.GOOS == "windows" {
+		directoryDelimiter = "\\"
+	} else {
+		directoryDelimiter = "/"
+	}
+
+	dirSplit := strings.Split(subDir, directoryDelimiter)
+	// Need just last bit to of top level temp directory to call command
+	subDirIsolated := dirSplit[len(dirSplit)-1]
+	args := []string{"-recursive=true", filepath.Join(testFixture("fmt"), subDirIsolated)}
+
+	if code := c.Run(args); code != 0 {
 		fatalCommand(t, c.Meta)
+	}
+
+	formattedData, err := ioutil.ReadFile("test-fixtures/fmt/formatted.pkrvars.hcl")
+	if err != nil {
+		t.Fatalf("failed to open the formatted fixture %s", err)
+	}
+
+	validateFileIsFormatted(t, formattedData, tf)
+	validateFileIsFormatted(t, formattedData, subTf)
+}
+
+func validateFileIsFormatted(t *testing.T, formattedData []byte, testFile *os.File) {
+	//lets re-read the tempfile which should now be formatted
+	data, err := ioutil.ReadFile(testFile.Name())
+	if err != nil {
+		t.Fatalf("failed to open the newly formatted fixture %s", err)
+	}
+
+	if diff := cmp.Diff(string(data), string(formattedData)); diff != "" {
+		t.Errorf("Unexpected format tfData output %s", diff)
 	}
 }
--- hcl2template/testdata/format/sub_directory/unformatted.pkrvars.hcl
+++
@@ -1,3 +0,0 @@
-ami_filter_name ="amzn2-ami-hvm-*-x86_64-gp2"
-ami_filter_owners =[ "137112412989" ]
-
