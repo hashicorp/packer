a34265443a6df00e1fe244e2b8f0eca02d1379f9
Adrien Delorme <adrien.delorme@icloud.com>
2021-01-22 17:40:11 +0100

    remove plugin_ext files

--- packer/plugin_ext_unix.go
+++
@@ -1,9 +0,0 @@
-// +build aix darwin dragonfly freebsd js,wasm linux netbsd openbsd solaris
-
-package packer
-
-import pluginsdk "github.com/hashicorp/packer-plugin-sdk/plugin"
-
-var (
-	PluginFileExtension = "_x" + pluginsdk.APIVersion // OS-Specific plugin file extention
-)
--- packer/plugin_ext_windows.go
+++
@@ -1,7 +0,0 @@
-package packer
-
-import pluginsdk "github.com/hashicorp/packer-plugin-sdk/plugin"
-
-var (
-	PluginFileExtension = "_x" + pluginsdk.APIVersion + ".exe" // OS-Specific plugin file extention
-)
