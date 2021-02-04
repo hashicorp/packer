a6559efa1999702d481af5a8157f39cd9019ac00
Adrien Delorme <adrien.delorme@icloud.com>
2021-02-04 11:45:22 +0100

    remove os specific folder prefix.
    So that everything is next to one another and
    we can see why the lister/getter ignores those.

--- command/init_test.go
+++ command/init_test.go
@@ -43,14 +43,14 @@ func TestInitCommand_Run(t *testing.T) {
 			// that's a no-op
 			"already-installed-no-op",
 			map[string]string{
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64":                 "1",
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64_SHA256SUM":       "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b",
-				"plugin.d/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe":           "1.exe",
-				"plugin.d/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe_SHA256SUM": "07d8453027192ee0c4120242e6e84e2ca2328b8e0f506e2f818a1a5b82790a0b",
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64":                  "1.out",
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64_SHA256SUM":        "59031c50e0dfeedfde2b4e9445754804dce3f29e4efa737eead0ca9b4f5b85a5",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64":                "1",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64_SHA256SUM":      "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe":           "1.exe",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe_SHA256SUM": "07d8453027192ee0c4120242e6e84e2ca2328b8e0f506e2f818a1a5b82790a0b",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64":                 "1.out",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64_SHA256SUM":       "59031c50e0dfeedfde2b4e9445754804dce3f29e4efa737eead0ca9b4f5b85a5",
 			},
-			"h1:K4LnrskKho2cVCfhwQ56wS9ZoTVZceWflhc3kmZkCTQ=",
+			"h1:Q5qyAOdD43hL3CquQdVfaHpOYGf0UsZ/+wVA9Ry6cbA=",
 			`# cfg.pkr.hcl
 			packer {
 				required_plugins {
@@ -63,7 +63,7 @@ func TestInitCommand_Run(t *testing.T) {
 			cfg.dir("1"),
 			0,
 			nil,
-			"h1:K4LnrskKho2cVCfhwQ56wS9ZoTVZceWflhc3kmZkCTQ=",
+			"h1:Q5qyAOdD43hL3CquQdVfaHpOYGf0UsZ/+wVA9Ry6cbA=",
 		},
 		{
 			// here we pre-write plugins with valid checksums, Packer will
@@ -71,14 +71,14 @@ func TestInitCommand_Run(t *testing.T) {
 			// But because we require version 0.2.19, we will upgrade.
 			"already-installed-upgrade",
 			map[string]string{
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64":                 "1",
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64_SHA256SUM":       "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b",
-				"plugin.d/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe":           "1.exe",
-				"plugin.d/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe_SHA256SUM": "07d8453027192ee0c4120242e6e84e2ca2328b8e0f506e2f818a1a5b82790a0b",
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64":                  "1.out",
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64_SHA256SUM":        "59031c50e0dfeedfde2b4e9445754804dce3f29e4efa737eead0ca9b4f5b85a5",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64":                "1",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64_SHA256SUM":      "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe":           "1.exe",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe_SHA256SUM": "07d8453027192ee0c4120242e6e84e2ca2328b8e0f506e2f818a1a5b82790a0b",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64":                 "1.out",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64_SHA256SUM":       "59031c50e0dfeedfde2b4e9445754804dce3f29e4efa737eead0ca9b4f5b85a5",
 			},
-			"h1:K4LnrskKho2cVCfhwQ56wS9ZoTVZceWflhc3kmZkCTQ=",
+			"h1:Q5qyAOdD43hL3CquQdVfaHpOYGf0UsZ/+wVA9Ry6cbA=",
 			`# cfg.pkr.hcl
 			packer {
 				required_plugins {
@@ -91,27 +91,27 @@ func TestInitCommand_Run(t *testing.T) {
 			cfg.dir("1"),
 			0,
 			[]string{
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64",
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64_SHA256SUM",
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64",
-				".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64_SHA256SUM",
-				"plugin.d/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe",
-				"plugin.d/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe_SHA256SUM",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_darwin_amd64_SHA256SUM",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_linux_amd64_SHA256SUM",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe",
+				"github.com/sylviamoss/comment/packer-plugin-comment_v0.2.18_x5.0_windows_amd64.exe_SHA256SUM",
 				map[string]string{
-					"darwin":  ".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_darwin_amd64_SHA256SUM",
-					"linux":   ".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_linux_amd64_SHA256SUM",
-					"windows": "plugin.d/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_windows_amd64.exe_SHA256SUM",
+					"darwin":  "github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_darwin_amd64_SHA256SUM",
+					"linux":   "github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_linux_amd64_SHA256SUM",
+					"windows": "github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_windows_amd64.exe_SHA256SUM",
 				}[runtime.GOOS],
 				map[string]string{
-					"darwin":  ".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_darwin_amd64",
-					"linux":   ".plugin/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_linux_amd64",
-					"windows": "plugin.d/github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_windows_amd64.exe",
+					"darwin":  "github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_darwin_amd64",
+					"linux":   "github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_linux_amd64",
+					"windows": "github.com/sylviamoss/comment/packer-plugin-comment_v0.2.19_x5.0_windows_amd64.exe",
 				}[runtime.GOOS],
 			},
 			map[string]string{
-				"darwin":  "h1:O50ZsZD5Um4/BKSpPWHT0pF6fSYBQgCBxelct9XEBJE=",
-				"linux":   "h1:a5F11lgF22JY1MmHRhrzNLnbZYxAZKzCuC9z2trXVMk=",
-				"windows": "h1:OmkFZQfC17rHaZFuci+Ib7qChb9Cukzk1VZr3l2dJnw=",
+				"darwin":  "h1:ORwcCYUx8z/5n/QvuTJo2vrgKpfJA4AxlNg1G9/BCDI=",
+				"linux":   "h1:CGym0+Nd0LEANgzqL0wx/LDjRL8bYwlpZ0HajPJo/hs=",
+				"windows": "h1:ag0/C1YjP7KoEDYOiJHE0K+lhFgs0tVgjriWCXVT1fg=",
 			}[runtime.GOOS],
 		},
 	}
@@ -142,11 +142,7 @@ func TestInitCommand_Run(t *testing.T) {
 				Meta: testMetaFile(t),
 			}
 
-			pluginDir := ".plugin"
-			if runtime.GOOS == "windows" {
-				pluginDir = "plugin.d"
-			}
-			c.CoreConfig.Components.PluginConfig.KnownPluginFolders = []string{filepath.Join(tt.packerConfigDir, pluginDir)}
+			c.CoreConfig.Components.PluginConfig.KnownPluginFolders = []string{tt.packerConfigDir}
 			if got := c.Run(args); got != tt.want {
 				t.Errorf("InitCommand.Run() = %v, want %v", got, tt.want)
 			}
