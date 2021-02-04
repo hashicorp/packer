5b6182754fb8b4028d4c4e432cd986e0d0c38495
Sylvia Moss <moss@hashicorp.com>
2021-02-04 11:14:19 +0100

    Update hcl2template/types.packer_config.go
    Co-authored-by: Adrien Delorme <azr@users.noreply.github.com>

--- hcl2template/types.packer_config.go
+++ hcl2template/types.packer_config.go
@@ -108,7 +108,17 @@ func (cfg *PackerConfig) EvalContext(ctx BlockContext, variables map[string]cty.
 			}),
 		},
 	}
-
+	
+	// Currently the places where you can make references to other blocks
+	// from one is very 'procedural', and in this specific case, we could make
+	// the data sources available to other datasources, but this would be
+	// order dependant, meaning that if you define two datasources in two
+	// different blocks, the second one can use the first one, but not the 
+	// other way around; which would be totally confusing; so - for now -
+	// datasources can't use other datasources.
+	// In the future we'd like to load and execute HCL blocks using a graph
+	// dependency tree, so that any block can use any block whatever the
+	// order.
 	switch ctx {
 	case LocalContext, BuildContext:
 		datasourceVariables, _ := cfg.Datasources.Values()
