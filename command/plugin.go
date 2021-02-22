daaed63f6315bec36682c84ea93b7734aebf28ee
Calvin Ho <calvinho@microsoft.com>
2021-02-22 11:39:20 -0800

    Clone hyperv-iso into hyperv-wim

--- command/plugin.go
+++ command/plugin.go
@@ -31,6 +31,7 @@ import (
 	hyperonebuilder "github.com/hashicorp/packer/builder/hyperone"
 	hypervisobuilder "github.com/hashicorp/packer/builder/hyperv/iso"
 	hypervvmcxbuilder "github.com/hashicorp/packer/builder/hyperv/vmcx"
+	hypervwimbuilder "github.com/hashicorp/packer/builder/hyperv/wim"
 	jdcloudbuilder "github.com/hashicorp/packer/builder/jdcloud"
 	linodebuilder "github.com/hashicorp/packer/builder/linode"
 	lxcbuilder "github.com/hashicorp/packer/builder/lxc"
@@ -132,6 +133,7 @@ var Builders = map[string]packersdk.Builder{
 	"hyperone":            new(hyperonebuilder.Builder),
 	"hyperv-iso":          new(hypervisobuilder.Builder),
 	"hyperv-vmcx":         new(hypervvmcxbuilder.Builder),
+	"hyperv-wim":          new(hypervwimbuilder.Builder),
 	"jdcloud":             new(jdcloudbuilder.Builder),
 	"linode":              new(linodebuilder.Builder),
 	"lxc":                 new(lxcbuilder.Builder),
