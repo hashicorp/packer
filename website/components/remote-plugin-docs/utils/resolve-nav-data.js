c8df1997c64f423c273fe7e04a54697e70c454e6
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-03-09 15:48:56 -0500

    Add comment on resolve-nav-data.js

--- website/components/remote-plugin-docs/utils/resolve-nav-data.js
+++ website/components/remote-plugin-docs/utils/resolve-nav-data.js
@@ -4,6 +4,17 @@ const mergeRemotePlugins = require('./merge-remote-plugins')
 const validateFilePaths = require('@hashicorp/react-docs-sidenav/utils/validate-file-paths')
 const validateRouteStructure = require('@hashicorp/react-docs-sidenav/utils/validate-route-structure')
 
+/**
+ * Resolves nav-data from file, including optional
+ * resolution of remote plugin docs entries
+ *
+ * @param {string} navDataFile path to the nav-data.json file, relative to the cwd. Example: "data/docs-nav-data.json".
+ * @param {string} localContentDir path to the content root, relative to the cwd. Example: "content/docs".
+ * @param {object} options optional configuration object
+ * @param {string} options.isDev if true, then will NOT throw errors if remote fetches fail
+ * @param {string} options.remotePluginsFile path to a remote-plugins.json file, relative to the cwd. Example: "data/docs-remote-plugins.json".
+ * @returns {object} the resolved navData. This includes NavBranch nodes pulled from remote plugin repositories, as well as filePath properties on all local NavLeaf nodes, and remoteFile properties on all remote NavLeaf nodes.
+ */
 async function resolveNavData(navDataFile, localContentDir, options = {}) {
   const { remotePluginsFile, isDev } = options
   // Read in files
