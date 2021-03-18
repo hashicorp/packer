8b3e7e6f2ff0b94c2d58f86cbbaafd19ad6043db
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-03-18 12:24:36 -0400

    website: use revised remote-plugin-docs server implementation
    - also bumps to stable docs-page, and makes related api changes for intro and guides routes

--- website/components/remote-plugin-docs/server.js
+++ website/components/remote-plugin-docs/server.js
@@ -3,13 +3,9 @@ import path from 'path'
 import {
   getNodeFromPath,
   getPathsFromNavData,
-  validateNavData,
 } from '@hashicorp/react-docs-page/server'
 import renderPageMdx from '@hashicorp/react-docs-page/render-page-mdx'
-import fetchGithubFile from './utils/fetch-github-file'
-import mergeRemotePlugins from './utils/merge-remote-plugins'
-
-const IS_DEV = process.env.VERCEL_ENV !== 'production'
+import resolveNavData from './utils/resolve-nav-data'
 
 async function generateStaticPaths(navDataFile, contentDir, options = {}) {
   const navData = await resolveNavData(navDataFile, contentDir, options)
@@ -21,7 +17,8 @@ async function generateStaticProps(
   navDataFile,
   localContentDir,
   params,
-  { productName, remotePluginsFile, additionalComponents } = {}
+  product,
+  { remotePluginsFile, additionalComponents, mainBranch = 'main' } = {}
 ) {
   const navData = await resolveNavData(navDataFile, localContentDir, {
     remotePluginsFile,
@@ -30,12 +27,15 @@ async function generateStaticProps(
   const navNode = getNodeFromPath(pathToMatch, navData, localContentDir)
   const { filePath, remoteFile, pluginTier } = navNode
   //  Fetch the MDX file content
-  const [err, mdxString] = filePath
-    ? //  Read local content from the filesystem
-      [null, fs.readFileSync(path.join(process.cwd(), filePath), 'utf8')]
-    : // Fetch remote content using GitHub's API
-      await fetchGithubFile(remoteFile)
-  if (err) throw new Error(err)
+  const mdxString = remoteFile
+    ? remoteFile.fileString
+    : fs.readFileSync(path.join(process.cwd(), filePath), 'utf8')
+  // Construct the githubFileUrl, used for "Edit this page" link
+  // Note: for custom ".docs-artifacts" directories, the "Edit this page"
+  // link will lead to the artifact file rather than the "docs" source file
+  const githubFileUrl = remoteFile
+    ? remoteFile.sourceUrl
+    : `https://github.com/hashicorp/${product.slug}/blob/${mainBranch}/website/${filePath}`
   // For plugin pages, prefix the MDX content with a
   // label that reflects the plugin tier
   // (current options are "Official" or "Community")
@@ -48,40 +48,21 @@ async function generateStaticProps(
   }
   const { mdxSource, frontMatter } = await renderPageMdx(mdxString, {
     additionalComponents,
-    productName,
+    productName: product.name,
     mdxContentHook,
   })
   // Build the currentPath from page parameters
   const currentPath = !params.page ? '' : params.page.join('/')
-  // In development, set a flag if there is no GITHUB_API_TOKEN,
-  // as this means dev is seeing only local content, and we want to flag that
-  const isDevMissingRemotePlugins = IS_DEV && !process.env.GITHUB_API_TOKEN
+
   return {
     currentPath,
     frontMatter,
-    isDevMissingRemotePlugins,
     mdxSource,
     mdxString,
+    githubFileUrl,
     navData,
     navNode,
   }
-}
-
-async function resolveNavData(navDataFile, localContentDir, options = {}) {
-  const { remotePluginsFile } = options
-  // Read in files
-  const navDataPath = path.join(process.cwd(), navDataFile)
-  const navData = JSON.parse(fs.readFileSync(navDataPath, 'utf8'))
-  const remotePluginsPath = path.join(process.cwd(), remotePluginsFile)
-  const remotePlugins = JSON.parse(fs.readFileSync(remotePluginsPath, 'utf-8'))
-  // Resolve plugins, this yields branches with NavLeafRemote nodes
-  const withPlugins = await mergeRemotePlugins(remotePlugins, navData, IS_DEV)
-  // Resolve local filePaths for NavLeaf nodes
-  const withFilePaths = await validateNavData(withPlugins, localContentDir)
-  // Return the nav data with:
-  // 1. Plugins merged, transformed into navData structures with NavLeafRemote nodes
-  // 2. filePaths added to all local NavLeaf nodes
-  return withFilePaths
 }
 
 export default { generateStaticPaths, generateStaticProps }
