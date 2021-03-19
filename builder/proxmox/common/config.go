4ee7676083294820496136f980a2568731ae2475
Andreas Botzner <paginabianca66@gmail.com>
2021-03-19 09:35:01 +0100

    added go:gen

--- builder/proxmox/common/config.go
+++ builder/proxmox/common/config.go
@@ -1,4 +1,4 @@
-//go:generate mapstructure-to-hcl2 -type Config,nicConfig,diskConfig,vgaConfig,additionalISOsConfig
+//go:generate mapstructure-to-hcl2 -type Config,nicConfig,diskConfig,vgaConfig,additionalISOsConfig,cdFilesConfig
 
 package proxmox
 
