3a11352dfad6dad7d86f64de96e55bd3e0ac2380
sangkyu.kim <sangkyu.kim@navercorp.com>
2021-03-30 13:59:59 +0900

    Fix ncloud builder unhandled buildvar type

--- builder/ncloud/step_create_public_ip_instance.go
+++ builder/ncloud/step_create_public_ip_instance.go
@@ -307,9 +307,6 @@ func (s *StepCreatePublicIP) Run(ctx context.Context, state multistep.StateBag) 
 	if err == nil {
 		state.Put("public_ip", *publicIPInstance.PublicIp)
 		state.Put("public_ip_instance", publicIPInstance)
-		// instance_id is the generic term used so that users can have access to the
-		// instance id inside of the provisioners, used in step_provision.
-		state.Put("instance_id", *publicIPInstance)
 	}
 
 	return processStepResult(err, s.Error, state)
