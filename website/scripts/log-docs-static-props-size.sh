92e911bcfb335faf95568028a4a741b06261a55e
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-04-01 21:39:15 -0400

    website: remove script to check static props size

--- website/scripts/log-docs-static-props-size.sh
+++
@@ -1,6 +0,0 @@
-cd .next/server/pages/docs
-find . -name "*.json" -print0 | while read -d $'\0' file; do
-    size_kb=$(du -k "$file" | cut -f1)
-    basename=${file##*/}
-    printf "%4s kB | $basename\n" "$size_kb"
-done
