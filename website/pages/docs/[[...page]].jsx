5701abda660cf8d9e7a73df0fb3cbe93e37253eb
Jeff Escalante <jescalan@users.noreply.github.com>
2021-05-24 17:25:19 -0400

    fix styling on centered image and checklist

--- website/pages/docs/[[...page]].jsx
+++ website/pages/docs/[[...page]].jsx
@@ -2,6 +2,7 @@ import { productName, productSlug } from 'data/metadata'
 import DocsPage from '@hashicorp/react-docs-page'
 import PluginTierLabel from 'components/plugin-tier-label'
 import DevAlert from 'components/dev-alert'
+import Checklist from 'components/checklist'
 // Imports below are only used server-side
 import {
   generateStaticPaths,
@@ -9,7 +10,7 @@ import {
 } from 'components/remote-plugin-docs/server'
 
 //  Configure the docs path and remote plugin docs loading
-const additionalComponents = { PluginTierLabel }
+const additionalComponents = { PluginTierLabel, Checklist }
 const baseRoute = 'docs'
 const localContentDir = 'content/docs'
 const mainBranch = 'master'
