870cf5102eccf2079f6d474470f06bc7edbd860a
Brandon Romano <brandon@hashicorp.com>
2021-06-08 21:26:12 -0700

    Homepage Update

--- website/components/section-break-cta/index.tsx
+++ website/components/section-break-cta/index.tsx
@@ -1,11 +1,12 @@
 import Button from '@hashicorp/react-button'
 import s from './style.module.css'
 
-export default function SectionBreakCta({ heading, link }) {
+export default function SectionBreakCta({ heading, description, link }) {
   return (
     <div className={s.sectionBreakCta}>
       <hr />
       <h4 className={s.heading}>{heading}</h4>
+      {description && <p className={s.description}>{description}</p>}
       <Button
         title={link.text}
         url={link.url}
