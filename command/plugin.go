e22346f0faffd61acc1f570d4481045cabf00398
Wilken Rivera <wilken@hashicorp.com>
2021-06-14 17:30:58 -0400

    Extract ProfitBricks Plugin (#11084)
    * Extract ProfitBricks Plugin
    
    * Update CHANGELOG

--- command/plugin.go
+++ command/plugin.go
@@ -15,7 +15,6 @@ import (
 
 	filebuilder "github.com/hashicorp/packer/builder/file"
 	nullbuilder "github.com/hashicorp/packer/builder/null"
-	profitbricksbuilder "github.com/hashicorp/packer/builder/profitbricks"
 	artificepostprocessor "github.com/hashicorp/packer/post-processor/artifice"
 	checksumpostprocessor "github.com/hashicorp/packer/post-processor/checksum"
 	compresspostprocessor "github.com/hashicorp/packer/post-processor/compress"
@@ -38,9 +37,8 @@ type PluginCommand struct {
 }
 
 var Builders = map[string]packersdk.Builder{
-	"file":         new(filebuilder.Builder),
-	"null":         new(nullbuilder.Builder),
-	"profitbricks": new(profitbricksbuilder.Builder),
+	"file": new(filebuilder.Builder),
+	"null": new(nullbuilder.Builder),
 }
 
 var Provisioners = map[string]packersdk.Provisioner{
