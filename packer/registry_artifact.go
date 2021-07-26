5e6b264c56c60c3f8de8c5f6db3b2481f23b4547
Wilken Rivera <dev@wilkenrivera.com>
2021-07-26 16:29:39 -0400

    Add PAR URL to post-processor display

---
+++ packer/registry_artifact.go
@@ -0,0 +1,37 @@
+package packer
+
+import (
+	"fmt"
+)
+
+const BuilderId = "packer.post-processor.packer-registry"
+
+type RegistryArtifact struct {
+	BucketSlug  string
+	IterationID string
+	BuildName   string
+}
+
+func (a *RegistryArtifact) BuilderId() string {
+	return BuilderId
+}
+
+func (*RegistryArtifact) Id() string {
+	return ""
+}
+
+func (a *RegistryArtifact) Files() []string {
+	return []string{}
+}
+
+func (a *RegistryArtifact) String() string {
+	return fmt.Sprintf("Published metadata to HCP Packer registry packer/%s/iterations/%s", a.BucketSlug, a.IterationID)
+}
+
+func (*RegistryArtifact) State(name string) interface{} {
+	return nil
+}
+
+func (a *RegistryArtifact) Destroy() error {
+	return nil
+}
