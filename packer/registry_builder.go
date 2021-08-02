66fe9a42622a74802cf775471c810198b7ee7422
Wilken Rivera <wilken@hashicorp.com>
2021-08-02 14:03:57 -0400

    Remove unneeded artiface slice

--- packer/registry_builder.go
+++ packer/registry_builder.go
@@ -2,6 +2,7 @@ package packer
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"strings"
 
@@ -28,9 +29,9 @@ func (b *RegistryBuilder) Run(ctx context.Context, ui packersdk.Ui, hook packers
 		for {
 			select {
 			case <-ctx.Done():
-				log.Printf("[TRACE] marking build %q as cancelled in Packer registry", b.Name)
+				log.Printf("[TRACE] marking build %q as cancelled in HCP Packer registry", b.Name)
 				if err := b.ArtifactMetadataPublisher.PublishBuildStatus(context.TODO(), b.Name, models.HashicorpCloudPackerBuildStatusCANCELLED); err != nil {
-					log.Printf("[TRACE] failed to update Packer registry status for %q: %s", b.Name, err)
+					log.Printf("[TRACE] failed to update HCP Packer registry status for %q: %s", b.Name, err)
 				}
 				return
 			case <-runCompleted:
@@ -40,59 +41,55 @@ func (b *RegistryBuilder) Run(ctx context.Context, ui packersdk.Ui, hook packers
 	}()
 
 	if err := b.ArtifactMetadataPublisher.PublishBuildStatus(ctx, b.Name, models.HashicorpCloudPackerBuildStatusRUNNING); err != nil {
-		log.Printf("[TRACE] failed to update Packer registry status for %q: %s", b.Name, err)
+		log.Printf("[TRACE] failed to update HCP Packer registry status for %q: %s", b.Name, err)
 	}
 
+	ui.Say(fmt.Sprintf("Publishing build details for %s to the HCP Packer registry", b.Name))
 	artifact, err := b.Builder.Run(ctx, ui, hook)
 	if err != nil {
 		if parErr := b.ArtifactMetadataPublisher.PublishBuildStatus(ctx, b.Name, models.HashicorpCloudPackerBuildStatusFAILED); parErr != nil {
-			log.Printf("[TRACE] failed to update Packer registry status for %q: %s", b.Name, parErr)
+			log.Printf("[TRACE] failed to update HCP Packer registry status for %q: %s", b.Name, parErr)
 		}
 	}
 
 	// close chan to mark completion
 	close(runCompleted)
 
-	var artifacts []packersdk.Artifact
-	artifacts = append(artifacts, artifact)
+	// Lets post state
+	if artifact != nil {
+		metadata := make(map[string]string)
+		metadata[artifact.BuilderId()] = artifact.String()
+		metadata[artifact.BuilderId()+".files"] = strings.Join(artifact.Files(), ", ")
+		err := b.ArtifactMetadataPublisher.AddBuildMetadata(b.Name, metadata)
+		if err != nil {
+			log.Printf("[TRACE] failed to add build labels for %q: %s", b.Name, err)
+		}
 
-	for _, artifact := range artifacts {
-		// Lets post state
-		if artifact != nil {
-			metadata := make(map[string]string)
-			metadata[artifact.BuilderId()] = artifact.String()
-			metadata[artifact.BuilderId()+".files"] = strings.Join(artifact.Files(), ", ")
-			err := b.ArtifactMetadataPublisher.AddBuildMetadata(b.Name, metadata)
-			if err != nil {
-				log.Printf("[TRACE] failed to add build labels for %q: %s", b.Name, err)
+		switch state := artifact.State("par.artifact.metadata").(type) {
+		case map[interface{}]interface{}:
+			m := make(map[string]string)
+			for k, v := range state {
+				m[k.(string)] = v.(string)
 			}
-
-			switch state := artifact.State("par.artifact.metadata").(type) {
-			case map[interface{}]interface{}:
-				m := make(map[string]string)
-				for k, v := range state {
-					m[k.(string)] = v.(string)
-				}
-				// TODO handle these error better
+			// TODO handle these error better
+			err := b.ArtifactMetadataPublisher.AddImageToBuild(b.Name, packerregistry.Image{
+				ProviderName:   m["ProviderName"],
+				ProviderRegion: m["ProviderRegion"],
+				ID:             m["ImageID"],
+			})
+			if err != nil {
+				log.Printf("[TRACE] failed to add image artifact for %q: %s", b.Name, err)
+			}
+		case []interface{}:
+			for _, d := range state {
+				d := d.(map[interface{}]interface{})
 				err := b.ArtifactMetadataPublisher.AddImageToBuild(b.Name, packerregistry.Image{
-					ProviderName:   m["ProviderName"],
-					ProviderRegion: m["ProviderRegion"],
-					ID:             m["ImageID"],
+					ProviderName:   d["ProviderName"].(string),
+					ProviderRegion: d["ProviderRegion"].(string),
+					ID:             d["ImageID"].(string),
 				})
 				if err != nil {
 					log.Printf("[TRACE] failed to add image artifact for %q: %s", b.Name, err)
-				}
-			case []interface{}:
-				for _, d := range state {
-					d := d.(map[interface{}]interface{})
-					err := b.ArtifactMetadataPublisher.AddImageToBuild(b.Name, packerregistry.Image{
-						ProviderName:   d["ProviderName"].(string),
-						ProviderRegion: d["ProviderRegion"].(string),
-						ID:             d["ImageID"].(string),
-					})
-					if err != nil {
-						log.Printf("[TRACE] failed to add image artifact for %q: %s", b.Name, err)
-					}
 				}
 			}
 		}
