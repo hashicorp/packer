6981c9a3977cbe992f6dbdd7643377b84a679018
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-10-14 10:11:38 -0400

    Update banner text and link (#11331)

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -3,9 +3,8 @@ export const ALERT_BANNER_ACTIVE = true
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
   tag: 'Oct 19-21',
-  url:
-    'https://hashiconf.com/global/?utm_campaign=22Q3_WW_HASHICONFGLOBAL_EVENT-USER&utm_source=CorpBanner&utm_medium=EVT&utm_offer=EVENT-USER',
-  text: 'The countdown to HashiConf Global is on. View the full schedule now.',
+  url: 'https://hashiconf.com/global/schedule/the-future-of-hcp-packer/',
+  text: 'Learn about the future of HCP Packer at HashiConf.',
   linkText: 'View Schedule',
   // Set the expirationDate prop with a datetime string (e.g. '2020-01-31T12:00:00-07:00')
   // if you'd like the component to stop showing at or after a certain date
