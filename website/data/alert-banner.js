29b227fd60873b58f375d67317b14e4f150c7b98
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2021-10-19 10:00:18 -0400

    website: Update alert banner for HashiConf (#11340)
    * Update alert banner for HashiConf
    
    * Closer expiration date
    
    * Final cleanup

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -2,11 +2,11 @@ export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'Oct 19-21',
-  url: 'https://hashiconf.com/global/schedule/the-future-of-hcp-packer/',
-  text: 'Learn about the future of HCP Packer at HashiConf.',
-  linkText: 'View Schedule',
-  // Set the expirationDate prop with a datetime string (e.g. '2020-01-31T12:00:00-07:00')
-  // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '2021-10-19T12:00:00-07:00',
+  tag: 'Oct 19-21',
+  url: 'https://hashiconf.com/global/?utm_source=DocsBanner',
+  text: 'HashiConf Global is live.',
+  linkText: 'Join Now',
+  // Set the expirationDate prop with a datetime string (e.g. '2020-01-31T12:00:00-07:00')
+  // if you'd like the component to stop showing at or after a certain date
+  expirationDate: '2021-10-21T12:00:00-07:00',
 }
