0f786e79b79c77b48d3b30353369d27e30bed783
Kevin Wang <kwangsan@gmail.com>
2021-12-21 16:26:30 -0500

    chore: remove unused code

--- website/pages/docs/[[...page]].jsx
+++ website/pages/docs/[[...page]].jsx
@@ -3,13 +3,12 @@ import DocsPage from '@hashicorp/react-docs-page'
 import Badge from 'components/badge'
 import BadgesHeader from 'components/badges-header'
 import PluginBadge from 'components/plugin-badge'
-import DevAlert from 'components/dev-alert'
 import Checklist from 'components/checklist'
 // Imports below are only used server-side
 import {
   generateStaticPaths,
   generateStaticProps,
-} from '@hashicorp/react-docs-page'
+} from '@hashicorp/react-docs-page/server'
 
 //  Configure the docs path and remote plugin docs loading
 const additionalComponents = { Badge, BadgesHeader, PluginBadge, Checklist }
@@ -21,37 +20,12 @@ const product = { name: productName, slug: productSlug }
 
 function DocsLayout({ isDevMissingRemotePlugins, ...props }) {
   return (
-    <>
-      {isDevMissingRemotePlugins ? (
-        <DevAlert>
-          <strong className="g-type-label-strong">
-            Note for local development
-          </strong>
-          <p>
-            <span role="img" aria-label="Alert: ">
-              🚨
-            </span>{' '}
-            <strong>This preview is missing plugin docs</strong> pulled from
-            remote repos.
-          </p>
-
-          <p>
-            <span role="img" aria-label="Fix: ">
-              🛠
-            </span>{' '}
-            <strong>To preview docs pulled from plugin repos</strong>, please
-            include a <code>GITHUB_API_TOKEN</code> in{' '}
-            <code>website/.env.local</code>.
-          </p>
-        </DevAlert>
-      ) : null}
-      <DocsPage
-        additionalComponents={additionalComponents}
-        baseRoute={baseRoute}
-        product={product}
-        staticProps={props}
-      />
-    </>
+    <DocsPage
+      additionalComponents={additionalComponents}
+      baseRoute={baseRoute}
+      product={product}
+      staticProps={props}
+    />
   )
 }
 
