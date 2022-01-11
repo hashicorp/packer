b3341164cf40c085c8556b4c7a13dfbe2f8b01af
Kevin Wang <kwangsan@gmail.com>
2022-01-11 10:26:05 -0500

    feat: extract `/plugins` (#11464)
    * feat: extract `/plugins`
    
    Co-authored-by: Zachary Shilton <4624598+zchsh@users.noreply.github.com>
    Co-authored-by: Adrien Delorme <azr@users.noreply.github.com>

--- website/pages/docs/[[...page]].jsx
+++ website/pages/docs/[[...page]].jsx
@@ -3,13 +3,12 @@ import DocsPage from '@hashicorp/react-docs-page'
 import Badge from 'components/badge'
 import BadgesHeader from 'components/badges-header'
 import PluginBadge from 'components/plugin-badge'
-import DevAlert from 'components/dev-alert'
 import Checklist from 'components/checklist'
 // Imports below are only used server-side
 import {
   generateStaticPaths,
   generateStaticProps,
-} from 'components/remote-plugin-docs/server'
+} from '@hashicorp/react-docs-page/server'
 
 //  Configure the docs path and remote plugin docs loading
 const additionalComponents = { Badge, BadgesHeader, PluginBadge, Checklist }
@@ -18,41 +17,15 @@ const localContentDir = 'content/docs'
 const mainBranch = 'master'
 const navDataFile = 'data/docs-nav-data.json'
 const product = { name: productName, slug: productSlug }
-const remotePluginsFile = 'data/docs-remote-plugins.json'
 
 function DocsLayout({ isDevMissingRemotePlugins, ...props }) {
   return (
-    <>
-      {isDevMissingRemotePlugins ? (
-        <DevAlert>
-          <strong className="g-type-label-strong">
-            Note for local development
-          </strong>
-          <p>
-            <span role="img" aria-label="Alert: ">
-              🚨
-            </span>{' '}
-            <strong>This preview is missing plugin docs</strong> pulled from
-            remote repos.
-          </p>
-
-          <p>
-            <span role="img" aria-label="Fix: ">
-              🛠
-            </span>{' '}
-            <strong>To preview docs pulled from plugin repos</strong>, please
-            include a <code>GITHUB_API_TOKEN</code> in{' '}
-            <code>website/.env.local</code>.
-          </p>
-        </DevAlert>
-      ) : null}
-      <DocsPage
-        additionalComponents={additionalComponents}
-        baseRoute={baseRoute}
-        product={product}
-        staticProps={props}
-      />
-    </>
+    <DocsPage
+      additionalComponents={additionalComponents}
+      baseRoute={baseRoute}
+      product={product}
+      staticProps={props}
+    />
   )
 }
 
@@ -60,7 +33,6 @@ export async function getStaticPaths() {
   const paths = await generateStaticPaths({
     localContentDir,
     navDataFile,
-    remotePluginsFile,
   })
   return { paths, fallback: false }
 }
@@ -73,7 +45,6 @@ export async function getStaticProps({ params }) {
     navDataFile,
     params,
     product,
-    remotePluginsFile,
   })
   return { props }
 }
