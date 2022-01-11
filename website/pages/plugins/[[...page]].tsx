f4b8e67eda0223b3ce8f0cde72c41e76e0b84291
Kevin Wang <kwangsan@gmail.com>
2022-01-11 16:09:14 -0500

    chore: showVersionSelect=false

--- website/pages/plugins/[[...page]].tsx
+++ website/pages/plugins/[[...page]].tsx
@@ -58,6 +58,7 @@ const DocsLayout: NextPage<Props> = ({
         product={product}
         // @ts-expect-error
         staticProps={props}
+        showVersionSelect={false}
       />
     </>
   )
