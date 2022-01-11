ef115b0b6d650767c14b96138451c94d2ba36d5e
Kevin Wang <kwangsan@gmail.com>
2022-01-11 13:30:55 -0500

    fix: handle multiple redirects

--- website/components/remote-plugin-docs/utils/fetch-latest-release-tag.js
+++ website/components/remote-plugin-docs/utils/fetch-latest-release-tag.js
@@ -1,24 +1,26 @@
+const fetch = require('node-fetch')
+
+/**
+ * @param {string} repo
+ * @returns {Promise<string|boolean>}
+ */
 async function fetchLatestReleaseTag(repo) {
   const latestReleaseUrl = `https://github.com/${repo}/releases/latest`
-  const redirectedUrl = await getRedirectedUrl(latestReleaseUrl)
-  if (!redirectedUrl) return false
-  const latestTag = redirectedUrl.match(/tag\/(.*)/)[1]
-  return latestTag
-}
+  let res = await fetch(latestReleaseUrl)
 
-async function getRedirectedUrl(url) {
-  return new Promise((resolve, reject) => {
-    const https = require('https')
-    const req = https.request(url, (res) => {
-      if (res.statusCode >= 300 && res.statusCode < 400) {
-        resolve(res.headers.location)
-      } else {
-        resolve(false)
-      }
-    })
-    req.on('error', reject)
-    req.end()
-  })
+  if (res.status !== 200) {
+    console.error(`failed to fetch: ${latestReleaseUrl}`)
+    return false
+  }
+
+  const matches = res.url.match(/tag\/(.*)/)
+
+  if (!matches) {
+    console.error(`failed to parse tag from: ${res.url}`)
+    return false
+  }
+
+  return matches[1]
 }
 
 module.exports = fetchLatestReleaseTag
