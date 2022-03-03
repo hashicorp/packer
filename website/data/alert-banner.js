bf1b550e95777a326a640b35869d9aef39aac629
Pamela Bortnick <pbortnick@gmail.com>
2022-03-03 12:43:25 -0500

    Add expiration date to banner

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -9,5 +9,5 @@ export default {
   linkText: 'Sign up for free',
   // Set the expirationDate prop with a datetime string (e.g. '2020-01-31T12:00:00-07:00')
   // if you'd like the component to stop showing at or after a certain date
-  // expirationDate: '2021-12-17T23:00:00-07:00',
+  expirationDate: '2022-04-07T23:00:00-07:00',
 }
