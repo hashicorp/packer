b2b6a78bcad775df185580a38b60146f8121512e
teddylear <teddylear@protonmail.com>
2022-05-10 17:30:25 -0400

    Update datasource/http/data.go
    Co-authored-by: Sylvia Moss <sylviamoss.m@gmail.com>

--- datasource/http/data.go
+++ datasource/http/data.go
@@ -23,7 +23,7 @@ type Config struct {
 	common.PackerConfig `mapstructure:",squash"`
 	// The URL to request data from. This URL must respond with a `200 OK` response and a `text/*` or `application/json` Content-Type
 	Url string `mapstructure:"url" required:"true"`
-	// Request headers for call
+	// A map of strings representing additional HTTP headers to include in the request.
 	Request_headers map[string]string `mapstructure:"request_headers" required:"false"`
 }
 
