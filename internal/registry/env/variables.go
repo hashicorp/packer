e829967a7a44de192e6fcb6b9f7e1d63e1acb58e
Wilken Rivera <dev@wilkenrivera.com>
2022-10-06 17:46:33 -0400

    Add env for HCP_PACKER_BUILD_FINGERPRINT

--- internal/registry/env/variables.go
+++ internal/registry/env/variables.go
@@ -1,8 +1,9 @@
 package env
 
 const (
-	HCPClientID       = "HCP_CLIENT_ID"
-	HCPClientSecret   = "HCP_CLIENT_SECRET"
-	HCPPackerRegistry = "HCP_PACKER_REGISTRY"
-	HCPPackerBucket   = "HCP_PACKER_BUCKET_NAME"
+	HCPClientID               = "HCP_CLIENT_ID"
+	HCPClientSecret           = "HCP_CLIENT_SECRET"
+	HCPPackerRegistry         = "HCP_PACKER_REGISTRY"
+	HCPPackerBucket           = "HCP_PACKER_BUCKET_NAME"
+	HCPPackerBuildFingerprint = "HCP_PACKER_BUILD_FINGERPRINT"
 )
