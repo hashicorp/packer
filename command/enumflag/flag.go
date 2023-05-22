a5a0f5f1c5862835a59ef2a3bee6348e7aa90f91
Seweryn Chlewicki <schlewicki@palantir.com>
2023-05-22 14:19:57 +0100

    Update comments

--- command/enumflag/flag.go
+++ command/enumflag/flag.go
@@ -10,11 +10,14 @@ type enumFlag struct {
 	options []string
 }
 
-// New returns a flag.Value implementation for parsing flags with a one-of-a-set value
+// New returns a flag.Value implementation for parsing flags with a one-of-a-set value.
+// The first argument is a pointer to the variable to be set by the flag.
+// The second argument is the default value.
+// The remaining arguments are the allowed values.
 func New(target *string, value string, options ...string) *enumFlag {
 	ret := &enumFlag{target: target, options: options}
 	if err := ret.Set(value); err != nil {
-		panic(err)
+		panic(err) // happens only if the default value is not in the options
 	}
 
 	return ret
