ec1e161f09a244396bb7856a95d0a8f6e480bf4e
guoguangwu <guoguangwu@magic-shield.com>
2023-09-21 05:14:19 +0000

    backport of commit 1dfc1baddf4356694ba4f75c1813c0fb870f97ac

--- provisioner/breakpoint/provisioner.go
+++ provisioner/breakpoint/provisioner.go
@@ -67,8 +67,7 @@ func (p *Provisioner) Provision(ctx context.Context, ui packersdk.Ui, comm packe
 		ui.Say("Pausing at breakpoint provisioner.")
 	}
 
-	message := fmt.Sprintf(
-		"Press enter to continue.")
+	message := "Press enter to continue."
 
 	var g errgroup.Group
 	result := make(chan string, 1)
