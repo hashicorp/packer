6d2dfb0679744f523c88ca36366f2cb4a4e9fb71
Lucas Bajolet <lucas.bajolet@hashicorp.com>
2023-09-28 11:21:36 -0400

    hcl2template: don't error if test wants diags
    The required_plugins parse tests define a `want_diags' flag, but don't
    actually use it in subtests, so if one was to add a test that is
    expected to produce errors on this step, this would always fail.

--- hcl2template/types.required_plugins_test.go
+++ hcl2template/types.required_plugins_test.go
@@ -138,7 +138,9 @@ func TestPackerConfig_required_plugin_parse(t *testing.T) {
 			if len(diags) > 0 {
 				t.Fatal(diags)
 			}
-			if diags := cfg.decodeRequiredPluginsBlock(file); len(diags) > 0 {
+
+			diags = cfg.decodeRequiredPluginsBlock(file)
+			if !tt.wantDiags && len(diags) > 0 {
 				t.Fatal(diags)
 			}
 
