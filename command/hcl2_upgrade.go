7751a601975925bc45343ddc26cc5a26c181b9bf
Wilken Rivera <dev@wilkenrivera.com>
2023-11-28 20:01:04 +0000

    backport of commit 518cbb62df6aca726767a054d75c8dddc9b50565

--- command/hcl2_upgrade.go
+++ command/hcl2_upgrade.go
@@ -104,6 +104,7 @@ var knownPlugins = map[string]string{
 	"googlecompute": "github.com/hashicorp/googlecompute",
 	"qemu":          "github.com/hashicorp/qemu",
 	"vagrant":       "github.com/hashicorp/vagrant",
+	"virtualbox":    "github.com/hashicorp/virtualbox",
 	"vmware":        "github.com/hashicorp/vmware",
 	"vsphere":       "github.com/hashicorp/vsphere",
 }
