9897dbb213e2794d8314e3df07549498c9ab9a44
Devashish <devashish.patel@hashicorp.com>
2024-07-17 17:17:57 +0530

    Fmt

--- internal/hcp/registry/json.go
+++ internal/hcp/registry/json.go
@@ -97,7 +97,7 @@ func (h *JSONRegistry) CompleteBuild(
 	buildErr error,
 ) ([]sdkpacker.Artifact, error) {
 	buildName := build.Name()
-	buildMetadata, envMetadata := build.(*packer.CoreBuild).GetMetadata(),  h.metadata
+	buildMetadata, envMetadata := build.(*packer.CoreBuild).GetMetadata(), h.metadata
 	err := h.bucket.Version.AddMetadataToBuild(ctx, buildName, buildMetadata, envMetadata)
 	if err != nil {
 		return nil, err
