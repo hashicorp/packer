77e35b9216a6550e79cdc7ccf62f9556cfc65a05
Wenfeng Pan <vinsonring@gmail.com>
2024-08-13 11:01:57 -0700

    Address by comment

--- hcl2template/function/encoding.go
+++ hcl2template/function/encoding.go
@@ -44,9 +44,9 @@ var Base64GzipFunc = function.New(&function.Spec{
 // Base64Gzip compresses a string with gzip and then encodes the result in
 // Base64 encoding.
 //
-// Terraform uses the "standard" Base64 alphabet as defined in RFC 4648 section 4.
+// Packer uses the "standard" Base64 alphabet as defined in RFC 4648 section 4.
 //
-// Strings in the Terraform language are sequences of unicode characters rather
+// Strings in the Packer language are sequences of unicode characters rather
 // than bytes, so this function will first encode the characters from the string
 // as UTF-8, then apply gzip compression, and then finally apply Base64 encoding.
 func Base64Gzip(str cty.Value) (cty.Value, error) {
